7e8bbc5e86dd671d9ac0137bb29d5a4e0cf84cc7
Brian Henning <henninb@msn.com>
2019-11-19 18:19:38 -0600

    nfl predictions

--- nfl_schedule.sh
+++ nfl_schedule.sh
@@ -4,5 +4,6 @@ echo http://www.espn.com/nfl/schedulegrid
 echo https://www.pro-football-reference.com/years/2019/games.htm
 
 wget https://www.pro-football-reference.com/years/2019/games.htm -O nfl_games.html
+wget https://projects.fivethirtyeight.com/2019-nfl-predictions/
 
 exit 0
