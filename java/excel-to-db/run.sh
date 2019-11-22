c95e7d3e4997a7f17e93c9c04223ccc4cbd0da39
Brian Henning <henninb@msn.com>
2019-11-22 15:03:19 -0600

    added excel-to-db

---
+++ java/excel-to-db/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+APP=excel-to-db
+HOST_BASEDIR=$(pwd)
+GUEST_BASEDIR=/opt/$APP
+echo dnf install java-1.8.0-openjdk-devel
+./gradlew clean build
+mkdir -p logs
+mkdir -p input
+touch env.secrets
+
+docker rm $APP -f
+docker build -t $APP .
+#docker run -it --name $APP --env-file env --env-file env.secrets --rm -d $APP
+docker run --name $APP --env-file env --env-file env.secrets -v $HOST_BASEDIR/logs:$GUEST_BASEDIR/logs -v $HOST_BASEDIR/input:$GUEST_BASEDIR/input --rm -t $APP
+
+echo docker container prune
+
+exit 0
---
+++ java/excel-to-db/src/main/java/excel-to-db/services/ExcelService.java
@@ -0,0 +1,109 @@
+package excel_to_db.services;
+
+import org.apache.poi.poifs.crypt.Decryptor;
+import org.apache.poi.poifs.crypt.EncryptionInfo;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.core.env.Environment;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+@Service
+public class ExcelService {
+    private Environment env;
+
+    public ExcelService(Environment env) {
+        this.env = env;
+    }
+
+    //private String FILE_NAME = env.getProperty("custom.project.excel.file-path") + "/" + "finance_db_master.xlsm";
+
+
+    //@Scheduled(fixedDelay = 1000 * 20)
+    public void excelReadExcel() throws IOException {
+        try {
+            String fname = env.getProperty("custom.project.excel.file-path") + "/" + "finance_db_master.xlsm";
+            FileInputStream excelFile = new FileInputStream(new File(fname));
+
+            Workbook workbook = new XSSFWorkbook(excelFile);
+            //Workbook workbook = new XSSFWorkbook(fs);
+            Sheet datatypeSheet = workbook.getSheetAt(0);
+            int numberOfSheets = workbook.getNumberOfSheets();
+            Iterator<Row> iterator = datatypeSheet.iterator();
+
+            String sheetName = workbook.getSheetName(0);
+            workbook.getAllNames();
+
+            System.out.println("numberOfSheets: " + numberOfSheets);
+            System.out.println("sheetName: " + sheetName);
+            while (iterator.hasNext()) {
+                Row currentRow = iterator.next();
+
+                for (Cell currentCell : currentRow) {
+                    //getCellTypeEnum shown as deprecated for version 3.15
+                    //getCellTypeEnum ill be renamed to getCellType starting from version 4.0
+                    if (currentCell.getCellType() == CellType.STRING) {
+                        System.out.print(currentCell.getStringCellValue() + "--");
+                    } else if (currentCell.getCellType() == CellType.NUMERIC) {
+                        System.out.print(currentCell.getNumericCellValue() + "--");
+                    }
+                }
+                System.out.println();
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Scheduled(fixedDelay = 1000 * 20)
+    public void readPasswordProtected() throws Exception {
+        String fname = env.getProperty("custom.project.excel.file-path") + "/" + "finance_db_master.xlsm";
+      try {
+        POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(fname));
+        EncryptionInfo info = new EncryptionInfo(fs);
+
+        Decryptor decryptor = Decryptor.getInstance(info);
+        decryptor.verifyPassword(env.getProperty("custom.project.excel.password"));
+
+        InputStream is = decryptor.getDataStream(fs);
+
+        Workbook workbook = new XSSFWorkbook(is);
+
+        //String sheetName = workbook.getSheetName(0);
+        //workbook.getAllNames();
+          Map<String,Integer> sheetMap = new HashMap<>();
+        for (int idx = 0; idx < workbook.getNumberOfSheets(); idx++) {
+            if( (workbook.getSheetName(idx).contains("_brian")) || (workbook.getSheetName(idx).contains("_kari")) ) {
+                System.out.println("Sheet name: " + workbook.getSheetName(idx));
+                sheetMap.put(workbook.getSheetName(idx), idx);
+                this.printSheet(workbook, idx);
+            }
+        }
+
+        } catch( FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void printSheet(Workbook workbook, int sheetNumber) {
+        Sheet datatypeSheet = workbook.getSheetAt(sheetNumber);
+        //int numberOfSheets = workbook.getNumberOfSheets();
+        for (Row currentRow : datatypeSheet) {
+
+            for (Cell currentCell : currentRow) {
+                if (currentCell.getCellType() == CellType.STRING) {
+                    System.out.print(currentCell.getStringCellValue() + "--");
+                } else if (currentCell.getCellType() == CellType.NUMERIC) {
+                    System.out.print(currentCell.getNumericCellValue() + "--");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
