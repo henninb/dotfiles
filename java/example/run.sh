5c1d532e0d4a15532231337fb3c996b45d4819fc
Brian Henning <henninb@msn.com>
2019-11-22 10:57:07 -0600

    java example updates

--- java/example/run.sh
+++ java/example/run.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 APP=example
+HOST_BASEDIR=$(pwd)
+GUEST_BASEDIR=/opt/$APP
 echo dnf install java-1.8.0-openjdk-devel
 ./gradlew clean build
+mkdir logs
 
 docker rm $APP -f
 docker build -t $APP .
 #docker run -it --name $APP --env-file env --env-file env.secrets --rm -d $APP
-docker run --name $APP --env-file env --env-file env.secrets --rm -t $APP
+docker run --name $APP --env-file env --env-file env.secrets -v $HOST_BASEDIR/logs:$GUEST_BASEDIR/logs --rm -t $APP
 
 echo docker container prune
 
