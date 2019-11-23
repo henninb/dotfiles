42f6ee9e29771bb67558107077d77d07410b5eaf
Brian Henning <henninb@msn.com>
2019-11-23 16:45:25 -0600

    java excel added json features

--- java/excel-to-db/src/main/java/excel_to_db/domain/Transaction.java
+++ java/excel-to-db/src/main/java/excel_to_db/domain/Transaction.java
@@ -1,17 +1,50 @@
-import lombok.Data;
+package excel_to_db.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import java.math.BigDecimal;
+//import java.sql.Date;
+import java.util.Date;
+import java.sql.Timestamp;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Transaction {
-  private Long transactionId;
+  @JsonProperty("guid")
   private String guid;
+
+  @JsonProperty("account_type")
   private String accountType;
+
+  @JsonProperty("account_name_owner")
   private String accountNameOwner;
-  private String transactionDate;
+
+  @JsonProperty("transaction_date")
+  private Date transactionDate;
+
+  @JsonProperty("description")
   private String description;
+
+  @JsonProperty("category")
   private String category;
-  private String amount;
-  private String isCleared;
+
+  @JsonProperty("amount")
+  private BigDecimal amount;
+
+  @JsonProperty("cleared")
+  private int cleared;
+
+  @JsonProperty("notes")
   private String notes;
-  private String dateUpdated;
-  private String dateAdded;
+
+  @JsonProperty("date_updated")
+  private Date dateUpdated;
+
+  @JsonProperty("date_added")
+  private Date dateAdded;
+
+  @JsonProperty("reoccurring")
+  Boolean reoccurring;
+
 }
--- java/excel-to-db/src/main/java/excel_to_db/services/ExcelService.java
+++ java/excel-to-db/src/main/java/excel_to_db/services/ExcelService.java
@@ -1,5 +1,7 @@
 package excel_to_db.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.poi.poifs.crypt.Decryptor;
 import org.apache.poi.poifs.crypt.EncryptionInfo;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
@@ -7,17 +9,29 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import excel_to_db.domain.Transaction;
 import org.springframework.core.env.Environment;
 import java.io.*;
+import java.util.Date;
 import java.util.stream.IntStream;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 @Service
 public class ExcelService {
     private Environment env;
+    private final ObjectMapper mapper = new ObjectMapper();
+
     private static final int COL_GUID = 1;
+    private static final int COL_TRANSACTION_DATE = 2;
     private static final int COL_DESCRIPTION = 3;
     private static final int COL_CATEGORY = 4;
+    private static final int COL_AMOUNT = 5;
+    private static final int COL_CLEARED = 6;
     private static final int COL_NOTES = 7;
+    private static final int COL_DATE_ADDED = 8;
+    private static final int COL_DATE_UPDATED = 9;
 
     public ExcelService(Environment env) {
         this.env = env;
@@ -38,17 +52,18 @@ public class ExcelService {
 
         Workbook workbook = new XSSFWorkbook(is);
 
-          //sheetMap.put(workbook.getSheetName(idx), idx);
           IntStream.range(0, workbook.getNumberOfSheets()).filter(idx -> ( (workbook.getSheetName(idx).contains("_brian") || workbook.getSheetName(idx).contains("_kari")) && ! workbook.isSheetHidden(idx)  )).forEach(idx -> {
 
 
-if( workbook.getSheetName(idx).contentEquals("giftcards_brian") || workbook.getSheetName(idx).contentEquals("scottrade_ira_brian") || workbook.getSheetName(idx).contentEquals("vacation_brian") || workbook.getSheetName(idx).contentEquals("vacation_kari") || workbook.getSheetName(idx).contentEquals("401k_brian") || workbook.getSheetName(idx).contentEquals("401k_kari") || workbook.getSheetName(idx).contentEquals("pension_brian") || workbook.getSheetName(idx).contentEquals("pension_kari") || workbook.getSheetName(idx).contentEquals("amazongift_brian")
-                ) {
-                } else {
-
+              if (!workbook.getSheetName(idx).contentEquals("giftcards_brian") && !workbook.getSheetName(idx).contentEquals("scottrade_ira_brian") && !workbook.getSheetName(idx).contentEquals("vacation_brian") && !workbook.getSheetName(idx).contentEquals("vacation_kari") && !workbook.getSheetName(idx).contentEquals("401k_brian") && !workbook.getSheetName(idx).contentEquals("401k_kari") && !workbook.getSheetName(idx).contentEquals("pension_brian") && !workbook.getSheetName(idx).contentEquals("pension_kari") && !workbook.getSheetName(idx).contentEquals("amazongift_brian")
+              ) {
                 System.out.println("Sheet name: " + workbook.getSheetName(idx).trim() );
-                this.printSheet(workbook, idx);
-                }
+                  try {
+                      this.printSheet(workbook, idx);
+                  } catch (JsonProcessingException e) {
+                      e.printStackTrace();
+                  }
+              }
           });
 
           is.close();
@@ -63,11 +78,13 @@ if( workbook.getSheetName(idx).contentEquals("giftcards_brian") || workbook.getS
         }
     }
 
-    private void printSheet(Workbook workbook, int sheetNumber) {
+    private void printSheet(Workbook workbook, int sheetNumber ) throws JsonProcessingException {
         Sheet datatypeSheet = workbook.getSheetAt(sheetNumber);
         boolean blank = false;
 
         for (Row currentRow : datatypeSheet) {
+                Transaction transaction = new Transaction();
+                transaction.setAccountNameOwner(workbook.getSheetName(sheetNumber).trim());
             for (Cell currentCell : currentRow) {
                 int col = currentCell.getColumnIndex();
                 blank = false;
@@ -77,13 +94,40 @@ if( workbook.getSheetName(idx).contentEquals("giftcards_brian") || workbook.getS
                 }
                 if (currentCell.getAddress().getRow() != 0 ) {
                   if (currentCell.getCellType() == CellType.STRING) {
-                      if ( col == COL_DESCRIPTION || col == COL_NOTES ) {
-                        System.out.print(  capitalizeWords( currentCell.getStringCellValue().trim() ) + "--");
+                      if ( col == COL_GUID ) {
+                        String val = currentCell.getStringCellValue().trim();
+                        transaction.setGuid(val);
+                      } else if ( col == COL_DESCRIPTION ) {
+                        String val = capitalizeWords(currentCell.getStringCellValue().trim());
+                        transaction.setDescription(val);
+                      } else if ( col == COL_CATEGORY ) {
+                        String val = currentCell.getStringCellValue().trim();
+                        transaction.setCategory(val);
+                      } else if ( col == COL_NOTES ) {
+                        String val = capitalizeWords(currentCell.getStringCellValue().trim());
+                        transaction.setNotes(val);
                       } else {
                         System.out.print(currentCell.getStringCellValue().trim() + "--");
                       }
                   } else if (currentCell.getCellType() == CellType.NUMERIC) {
-                      System.out.print(currentCell.getNumericCellValue() + "--");
+                      if ( col == COL_CLEARED ) {
+                        int val = (int)currentCell.getNumericCellValue();
+                        transaction.setCleared(val);
+                      } else if ( col == COL_DATE_UPDATED ) {
+                        Date date = DateUtil.getJavaDate(currentCell.getNumericCellValue());
+                        transaction.setDateUpdated(date);
+                      } else if ( col == COL_DATE_ADDED ) {
+                        Date date = DateUtil.getJavaDate(currentCell.getNumericCellValue());
+                        transaction.setDateAdded(date);
+                      } else if ( col == COL_TRANSACTION_DATE ) {
+                        Date date = DateUtil.getJavaDate(currentCell.getNumericCellValue());
+                        transaction.setTransactionDate(date);
+                      } else if ( col == COL_AMOUNT ) {
+                        BigDecimal val = new BigDecimal(currentCell.getNumericCellValue());
+                        transaction.setAmount(val);
+                      } else {
+                        System.out.print(currentCell.getNumericCellValue() + "--");
+                      }
                   } else if (currentCell.getCellType() == CellType.BLANK) {
                   } else if (currentCell.getCellType() == CellType.FORMULA) {
                   } else {
@@ -93,6 +137,7 @@ if( workbook.getSheetName(idx).contentEquals("giftcards_brian") || workbook.getS
                 }
             }
             if ( !blank ) {
+              System.out.println(mapper.writeValueAsString(transaction));
               System.out.println();
             }
         }
@@ -114,22 +159,5 @@ if( workbook.getSheetName(idx).contentEquals("giftcards_brian") || workbook.getS
             return capitalizeWord.toString().trim();
        }
        return str;
-    }
-
-    private String capitalizeString(String element) {
-        char[] chars = element.toLowerCase().toCharArray();
-        boolean found = false;
-        int i = 0;
-        while (i < chars.length) {
-            if (!found && Character.isLetter(chars[i])) {
-                chars[i] = Character.toUpperCase(chars[i]);
-                found = true;
-            } else if (Character.isWhitespace(chars[i]) ) {
-            //} else if (chars[i] == ' ' ) {
-                found = false;
-            }
-            i++;
-        }
-        return String.valueOf(chars);
     }
 }
