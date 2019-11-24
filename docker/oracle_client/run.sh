77d03463f6300606578d9bcacb79d9841bd86912
Brian Henning <henninb@msn.com>
2019-11-24 08:31:39 -0600

    oracle client updates

--- docker/oracle_client/run.sh
+++ docker/oracle_client/run.sh
@@ -16,7 +16,7 @@ if [ ! -f "oracle-instantclient19.3-precomp-19.3.0.0.0-1.x86_64.rpm" ]; then
 fi
 
 if [ ! -f "oracle-instantclient19.3-sqlplus-19.3.0.0.0-1.x86_64.rpm" ]; then
-  scp pi@$RASPI_IP:/home/pi/downoads/oracle-instantclient19.3-sqlplus-19.3.0.0.0-1.x86_64.rpm .
+  scp pi@$RASPI_IP:/home/pi/downloads/oracle-instantclient19.3-sqlplus-19.3.0.0.0-1.x86_64.rpm .
 fi
 
 sudo docker image build -t oracle_client .
@@ -25,7 +25,7 @@ docker rm oracle_client -f
 #sudo docker run --name=oracle_client -h oracle_client --restart unless-stopped -d oracle_client
 sudo docker run --name=oracle_client -h oracle_client -d oracle_client
 echo $?
-docker exec -it --user root oracle_client /bin/bash
+docker exec -it --user henninb oracle_client /bin/bash
 echo $?
 
 exit 0
