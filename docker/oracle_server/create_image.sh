2da45832f9711e29185ee13a5b3cf680891907b2
Brian Henning <henninb@msn.com>
2019-11-23 19:53:20 -0600

    docker fixes for oracle

--- docker/oracle_server/create_image.sh
+++ docker/oracle_server/create_image.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+RASPI_IP=$(nmap -sP --host-timeout 10 192.168.100.0/24 | grep raspb | grep -o '[0-9.]\+[0-9]')
+echo $RASPI_IP
+
 cd $HOME/projects
-git clone git@github.com:oracle/docker-images.git oracle_docker
-cd $HOME/projects/oracle_docker/OracleDatabase/SingleInstance/dockerfiles/
+git clone git@github.com:oracle/docker-images.git oracle-server-docker
+cd $HOME/projects/oracle-server-docker/OracleDatabase/SingleInstance/dockerfiles/
 if [ ! -f "11.2.0.2/oracle-xe-11.2.0-1.0.x86_64.rpm.zip" ]; then
-  scp pi@192.168.100.25:/home/pi/oracle-xe-11.2.0-1.0.x86_64.rpm.zip 11.2.0.2/
+  scp pi@$RASPI_IP:/home/pi/oracle-xe-11.2.0-1.0.x86_64.rpm.zip 11.2.0.2/
 fi
 ./buildDockerImage.sh -v 11.2.0.2 -x
 
