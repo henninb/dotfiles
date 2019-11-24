ad0dfded01b5c202ca5c7b72e34c048d0f8db98c
Brian Henning <henninb@msn.com>
2019-11-23 20:37:40 -0600

    docker oracle server updates

--- docker/oracle_server/create_image.sh
+++ docker/oracle_server/create_image.sh
@@ -7,7 +7,7 @@ cd $HOME/projects
 git clone git@github.com:oracle/docker-images.git oracle-server-docker
 cd $HOME/projects/oracle-server-docker/OracleDatabase/SingleInstance/dockerfiles/
 if [ ! -f "11.2.0.2/oracle-xe-11.2.0-1.0.x86_64.rpm.zip" ]; then
-  scp pi@$RASPI_IP:/home/pi/oracle-xe-11.2.0-1.0.x86_64.rpm.zip 11.2.0.2/
+  scp pi@$RASPI_IP:/home/pi/downloads/oracle-xe-11.2.0-1.0.x86_64.rpm.zip 11.2.0.2/
 fi
 ./buildDockerImage.sh -v 11.2.0.2 -x
 
--- docker/oracle_server/run.sh
+++ docker/oracle_server/run.sh
@@ -7,6 +7,7 @@ sudo docker rm oraclebox -f
 #sudo docker run --name=oraclebox -h oraclebox --restart unless-stopped -d oraclebox
 sudo docker run --name=oraclebox --shm-size=1g -p 1521:1521 -v ~/data/oradata:/u01/app/oracle/oradata -h oraclebox -d oraclebox
 echo sqlplus SYSTEM/monday1@localhost
+echo sqlplus SYSTEM/monday1@localhost:1521
 sudo docker exec -it --user root oraclebox /bin/sh
 
 exit 0
