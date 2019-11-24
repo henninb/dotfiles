df12e5ce6ab7c2ec75f2937c9b0d478241aea3f6
Brian Henning <henninb@msn.com>
2019-11-23 19:52:01 -0600

    updated oracle java to gen rand data

--- java/example_db/src/main/java/example_db/services/BatchExecuteService.java
+++ java/example_db/src/main/java/example_db/services/BatchExecuteService.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import java.util.Locale;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
 
 import example_db.entities.Person;
 
@@ -13,6 +16,8 @@ public class BatchExecuteService {
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     private PersonService personService;
+
+    private FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en-US"), new RandomService());
 
     @Autowired
     BatchExecuteService(PersonService personService) {
@@ -24,11 +29,11 @@ public class BatchExecuteService {
     public void execute() {
         System.out.println("test");
         Person person = new Person();
-        person.setFirstName("Brian");
-        person.setPersonId(1);
+        person.setFirstName(fakeValuesService.regexify("[a-z]{8}"));
+        person.setPersonId( Integer.parseInt(fakeValuesService.regexify("[0-9]{5}")) );
         personService.addPerson(person);
         System.out.println("test complete");
-        System.exit(2);
+//        System.exit(2);
         //List<Map<String, Object>> list = jdbcTemplate.queryForList(tlogQuery);
 
 //        list.forEach(row -> {
