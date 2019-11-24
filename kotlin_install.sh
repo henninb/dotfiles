4efd658f28e0119f3ebfc080d868b2d0a678d3b6
Brian Henning <henninb@msn.com>
2019-11-24 09:34:27 -0600

    kotlin install updates

--- kotlin_install.sh
+++ kotlin_install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-#KOTLIN_VER=1.3.50
-KOTLIN_VER=$(curl https://blog.jetbrains.com/kotlin/category/releases/ | grep -o "Kotlin 1.3.50 released" | head -1 | grep -o '[0-9.]\+[0-9]')
+KOTLIN_VER=$(curl https://blog.jetbrains.com/kotlin/category/releases/ | grep -io "Kotlin [0-9.]\+[0-9] released" | head -1 | grep -o '[0-9.]\+[0-9]')
+echo $KOTLIN_VER
 
 if [ ! -f "kotlin-compiler-${KOTLIN_VER}.zip" ]; then
   wget https://github.com/JetBrains/kotlin/releases/download/v${KOTLIN_VER}/kotlin-compiler-${KOTLIN_VER}.zip
 fi
 cd /opt
-sudo unzip $HOME/kotlin-compiler-${KOTLIN_VER}.zip
+sudo unzip -o $HOME/kotlin-compiler-${KOTLIN_VER}.zip
 
 exit 0
