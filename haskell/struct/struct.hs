d15fda3d86baa2251663b255b8b5cf1bdbe93a29
Brian Henning <henninb@msn.com>
2019-12-08 08:09:01 -0600

    struct example haskell

---
+++ haskell/struct/struct.hs
@@ -0,0 +1,21 @@
+
+data Transaction = Integer String Integer String Int String String Float Int Bool String Int Int String
+
+
+-- import Data.Dates
+-- import Data.BigDecimal
+-- , _transactionId: Long = 0L
+-- , _guid: String = "",
+-- , _accountId: Long = 0
+-- , _accountType: AccountType = AccountType.Credit,
+-- , _accountNameOwner: String = ""
+-- , _transactionDate: Date = Date(0),
+-- , _description: String = ""
+-- , _category: String = "",
+-- , _amount: BigDecimal = BigDecimal(0.00)
+-- , _cleared: Int = 0,
+-- , _reoccurring: Boolean = false
+-- , _notes: String = "",
+-- , _dateUpdated: Timestamp = Timestamp(0),
+-- , _dateAdded: Timestamp = Timestamp(0),
+-- , _sha256: String = ""
