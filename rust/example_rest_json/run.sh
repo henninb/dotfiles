b1ec980086f1a562db0f9ab6ee0e06ca8ccefacd
Brian Henning <henninb@msn.com>
2019-12-14 09:55:19 -0600

    rust docker updates

--- rust/example_rest_json/run.sh
+++ rust/example_rest_json/run.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
-docker rm example_rest_json -f
-docker build -t example_rest_json .
-#docker run -it --rm --name example_rest_json -d example_rest_json
-docker run -it --name example_rest_json -d example_rest_json
+APP=example_rest_json
+TIMEZONE='America/Chicago'
+USERNAME=henninb
+
+touch env
+touch env.secrets
+
+docker rm ${APP} -f
+docker build -t $APP --build-arg TIMEZONE=${TIMEZONE} --build-arg APP=${APP} --build-arg USERNAME=${USERNAME} .
+if [ $? -ne 0 ]; then
+  echo "docker build failed."
+  exit 1
+fi
+
+#docker run -it --rm --name ${APP} -d ${APP}
+docker run -it -h ${APP} -p 8081:8080 --env-file env.secrets --env-file env --rm --name ${APP} ${APP}
 
 exit 0
