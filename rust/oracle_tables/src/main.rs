11107e5cb42a04bba0710df17cec5aa83e0b3643
Brian Henning <henninb@msn.com>
2019-12-15 13:58:41 -0600

    updated rust oracle tool.

--- rust/oracle_tables/src/main.rs
+++ rust/oracle_tables/src/main.rs
@@ -1,12 +1,14 @@
 extern crate log;
 extern crate oracle;
 
-use log::{info, trace, warn};
+use log::{info, warn};
 use oracle::Connection;
 use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::io::{BufRead, BufReader};
+use std::fs;
+
 //use std::io::Error;
 //use std::env::VarError;
 //use std::option;
@@ -15,7 +17,9 @@ use std::io::{BufRead, BufReader};
 
 fn get_env_var(key: &str) -> String {
     match env::var(key) {
-        Ok(x) => return x,
+        Ok(x) => {
+            return x
+        },
         Err(_) => panic!("Failure to set the value for key {}", key),
     };
 }
@@ -27,13 +31,19 @@ fn open_file(filename: &str) -> File {
     }
 }
 
+// fn get_value( val: std::option::Option<&str> ) -> String {
+// }
+
+// fn take(vec: Vec<&str>, index: usize) -> &str {
+//     vec.into_iter().nth(index)
+// }
+
 fn main() {
-    const UPGRADES: &'static [&'static str] = &["list"];
-    let username = "";
-    let passwd_key = "ORACLE_PASS";
+    //let username = "SYSTEM";
+    let passwd_key = "ORACLE_PASSWORD";
+    let username_key = "ORACLE_USERNAME";
     let sites_filename = "site.txt";
-    let port = "1522";
-    let owner = "";
+    let owner = "SYSTEM";
     let table_list_sql = format!(
         "SELECT table_name FROM dba_tables WHERE owner = '{}' ORDER BY table_name",
         owner
@@ -47,17 +57,23 @@ fn main() {
 
         info!("{}", str_line);
         let site = &str_line;
+        //let site_args = site.split(",");
+//        let site_args = site.split(",");
+        let site_args: Vec<&str> = site.split(',').collect();
+        let servername = site_args.clone().into_iter().nth(0).unwrap();
+        let port = site_args.clone().into_iter().nth(1).unwrap();
+        let service_name = site_args.clone().into_iter().nth(2).unwrap();
         let passwd = get_env_var(passwd_key);
-        let mut servername = format!("rclp{}", site);
-        if UPGRADES.contains(&str_line.as_str()) {
-            servername = format!("racp{}", site);
-        }
-        let service_name = format!("DDDP{}", site);
+        let username = get_env_var(username_key);
+
         let connection_string = format!("(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={})(PORT={}))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME={})))", servername, port, service_name);
+        println!("{}", connection_string);
 
         let conn = match Connection::connect(username, passwd, connection_string) {
             Ok(cn) => cn,
-            Err(e) => panic!("{:?}", e),
+            Err(e) => {
+                println!("did the uername/password fail?");
+                panic!("{:?}", e)},
         };
         let table_list_rows = match conn.query(&table_list_sql, &[]) {
             Ok(rs) => rs,
@@ -75,10 +91,11 @@ fn main() {
             }
         }
 
+        fs::create_dir_all("tables").unwrap();
         for table_name in &table_list {
             let package_sql = format!("SELECT column_name, data_type, data_length FROM all_tab_cols WHERE owner = '{}' and table_name = '{}' AND column_id IS NOT NULL", owner, table_name);
-            let ofname = format!("t{}-{}_COLUMNS.sql", site, table_name);
-            let mut file = std::fs::File::create(ofname).expect("creating a file failed");
+            let ofname = format!("tables/{}-{}_COLUMNS.sql", site, table_name);
+            let mut file = std::fs::File::create(ofname).expect("creating a file failed.");
             let table_col_rows = conn.query(&package_sql, &[]).unwrap();
 
             for row_result in table_col_rows {
