e42ee1106926b7ee425dd10dd90ec9fa032973f6
Brian Henning <henninb@msn.com>
2019-12-19 20:29:49 -0600

    emacs install updates

--- emacs_doom_install.sh
+++ emacs_doom_install.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
+if [ "$OS" = "Arch Linux" ]; then
+  sudo pacman --noconfirm --needed -S emacs
+elif [ \( "$OS" = "Linux Mint" \) -o \(  "$OS" = "Ubuntu" \) -o \(  "$OS" = "Raspbian GNU/Linux" \) ]; then
+  wget https://ftp.gnu.org/gnu/emacs/emacs-26.3.tar.xz
+  tar xvf emacs-26.3.tar.xz
+  cd emacs-26.3
+  ./autocofig.sh
+  ./configure --with-xpm=no --with-jpeg=no --with-gif=no --with-tiff=no
+else
+  echo $OS is not yet implemented.
+  exit 1
+fi
+
 git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
 ~/.emacs.d/bin/doom refresh
-
 
 #cd projects
 #git clone git@github.com:emacs-mirror/emacs.git
@@ -12,10 +24,5 @@ git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 #sudo add-apt-repository ppa:ubuntu-elisp/ppa
 #sudo apt-get update
 #sudo apt-get install emacs-snapshot
-wget https://ftp.gnu.org/gnu/emacs/emacs-26.3.tar.xz
-tar xvf emacs-26.3.tar.xz
-cd emacs-26.3
-./autocofig.sh
-./configure --with-xpm=no --with-jpeg=no --with-gif=no --with-tiff=no
 
 exit 0
