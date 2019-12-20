a6bae3c9e11f5d21469ac117fff68babf62a5b49
Brian Henning <henninb@msn.com>
2019-12-19 18:38:57 -0600

    mint upgrade script

--- mint_upgrade.sh
+++ mint_upgrade.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 if [ "$OS" = "Linux Mint" ]; then
-  echo "upgrade from 19.1 to 19.2"
+  #echo "upgrade from 19.1 to 19.2"
+  echo "upgrade from 19.2 to 19.3"
+  echo Tricia
   echo $VERSION
   cat /etc/lsb-release
-  sudo sed -i 's/tessa/tina/g' /etc/apt/sources.list.d/official-package-repositories.list
+  #sudo sed -i 's/tessa/tina/g' /etc/apt/sources.list.d/official-package-repositories.list
+  echo switch to tricia 19.3
+  sudo sed -i 's/tina/tricia/g' /etc/apt/sources.list.d/official-package-repositories.list
   sudo apt update -y
   sudo apt dist-upgrade -y
   cat /etc/lsb-release
