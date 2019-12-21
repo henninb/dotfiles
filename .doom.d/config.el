19f419536b70349ddd3e1bff86f5df8f50083b69
Brian Henning <henninb@msn.com>
2019-12-21 08:27:57 -0600

    emacs config cleanup

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,10 +1,7 @@
-;;; .doom.d/config.el -*- lexical-binding: t; -*-
+;; disable x - did not work
+;(advice-add #'x-apply-session-resources :override #'ignore)
 
-;; Place your private configuration here
-
-(advice-add #'x-apply-session-resources :override #'ignore)
-
-;; disable backup files
+;; disable backup file from being created
 (setq make-backup-files nil)
 
 ;; disable xwindows
@@ -14,6 +11,15 @@
 ; (setq inhibit-startup-message t)
 ; (setq initial-scratch-message nil)
 
-(setq doom-font (font-spec :family "Mononoki Nerd Font Mono" :size 15))
-(setq doom-theme 'doom-dracula) (def-package! highlight-indent-guides   :commands highlight-indent-guides-mode   :hook (prog-mode . highlight-indent-guides-mode)   :config
-(setq highlight-indent-guides-method 'character         highlight-indent-guides-character ?\⇨         highlight-indent-guides-delay 0.01         highlight-indent-guides-responsive 'top         highlight-indent-guides-auto-enabled nil))
+;; not sure if this works
+(setq doom-font (font-spec :family "Mononoki Nerd Font Mono" :size 9))
+;(setq doom-font (font-spec :family "Inconsolata" :size 20))
+
+; (ignore-errors
+;   (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;         doom-variable-pitch-font (font-spec :family "Noto Sans" :size 13)))
+
+; pick a theme
+; (setq doom-theme 'doom-dracula)
+;   (def-package! highlight-indent-guides   :commands highlight-indent-guides-mode   :hook (prog-mode . highlight-indent-guides-mode)   :config
+; (setq highlight-indent-guides-method 'character highlight-indent-guides-character ?\⇨ highlight-indent-guides-delay 0.01 highlight-indent-guides-responsive 'top highlight-indent-guides-auto-enabled nil))
--- fonts_mono_install.sh
+++ fonts_mono_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PROJECTS="sauce-code-pro-nerd-fonts.zip monofur-fonts.zip mononoki-fonts.zip"
+PROJECTS="sauce-code-pro-nerd-fonts.zip monofur-fonts.zip mononoki-fonts.zip inconsolata-fonts.zip"
 for i in $(echo $PROJECTS); do
   cd ~/.fonts
   unzip -o ../$i
