272f6edb3e9a699312e348f1298fdf574f3f1b29
Brian Henning <henninb@msn.com>
2019-12-22 10:11:45 -0600

    updates to git clone

--- git_clone_projects.sh
+++ git_clone_projects.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PROJECTS="raspi-finance-excel raspi-finance-endpoint raspi-finance-transform raspi-finance-ui raspi-finance-database raspi-finance raspi-finance-convert nfl-database src-common"
+PROJECTS="raspi-finance-endpoint raspi-finance-ui raspi-finance-database raspi-finance raspi-finance-client raspi-finance-convert nfl-database src-common"
 for i in $(echo $PROJECTS); do
   cd ~/projects
   git clone git@github.com:BitExplorer/$i.git
