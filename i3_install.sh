5d883b160179e09fc5fd57ceb5613e90f3bde2dc
Brian Henning <henninb@msn.com>
2019-12-29 17:54:12 -0600

    i3 install updates

--- i3_install.sh
+++ i3_install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 sudo mkdir -p /usr/share/i3blocks/
+#sudo cp iface cpu_usage memory /usr/share/i3blocks
 
 if [ \( "$OS" = "Linux Mint" \) -o \(  "$OS" = "Ubuntu" \) -o \(  "$OS" = "Raspbian GNU/Linux" \) ]; then
   sudo apt remove -y lightdm
