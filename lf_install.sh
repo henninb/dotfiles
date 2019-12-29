d831ce8014ddb80d1ddd0825e88d763499eb5497
Brian Henning <henninb@msn.com>
2019-12-28 19:44:13 -0600

    lf added

---
+++ lf_install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#go get -u github.com/gokcehan/lf
+
+wget https://github.com/gokcehan/lf/releases/download/r13/lf-linux-amd64.tar.gz
+tar xvf lf-linux-amd64.tar.gz
+mv lf .local/bin
+
+exit 0
