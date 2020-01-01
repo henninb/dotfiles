4029d3cc65b6e399fe73c99f3cd4796e764d605b
Brian Henning <henninb@msn.com>
2020-01-01 07:48:35 -0600

    useradd updates

--- useradd.sh
+++ useradd.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
 if [ "$OS" = "Linux Mint" ]; then
-  sudo groupadd wheel
+  id -g wheel &>/dev/null || sudo groupadd wheel
   #sudo usermod -a -G wheel brian
   sudo useradd -m -G wheel -s /bin/bash brian
-
   # add group to existing list of secondary groups
   sudo usermod  -a -G tomcat henninb
+  sudo usermod  -a -G firefox henninb
+  sudo usermod  -a -G intellij henninb
+  sudo usermod  -a -G arduino henninb
 #sudo adduser -m -G wheel -s /bin/bash henninb
-elif [ $"OS" = "Fedora" ]; then
-  sudo useradd -m -G wheel -s /bin/bash brian
+elif [ "$OS" = "Fedora" ]; then
+  sudo groupadd wheel
+  id -u brian &>/dev/null || sudo useradd -m -G wheel -s /bin/bash brian
+  sudo usermod  -a -G tomcat henninb
+  sudo usermod  -a -G firefox henninb
+  sudo usermod  -a -G intellij henninb
+  sudo usermod  -a -G arduino henninb
 else
-  echo "$OS is not implemented."
+   echo $OS is not yet implemented.
 fi
 
 exit 0
