6418df10e4866e27f66a67b1007b3a828354f109
Brian Henning <henninb@msn.com>
2020-01-01 07:31:14 -0600

    useradd updates

--- useradd.sh
+++ useradd.sh
@@ -8,6 +8,10 @@ if [ "$OS" = "Linux Mint" ]; then
   # add group to existing list of secondary groups
   sudo usermod  -a -G tomcat henninb
 #sudo adduser -m -G wheel -s /bin/bash henninb
+elif [ $"OS" = "Fedora" ]; then
+  sudo useradd -m -G wheel -s /bin/bash brian
+else
+  echo "$OS is not implemented."
 fi
 
 exit 0
