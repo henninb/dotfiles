0c10e321d9e9df19b8ad285551f0ec0c5d4a6048
Brian Henning <henninb@msn.com>
2020-01-02 20:04:01 -0600

    influxdb status updates

--- influxdb-status.sh
+++ influxdb-status.sh
@@ -3,5 +3,6 @@
 sudo systemctl status influxdb
 netstat -na | grep LISTEN | grep tcp | grep 8086
 sudo fuser 8086/tcp
+curl -i -XPOST http://localhost:8086/query -u henninb:monday1 --data-urlencode "q=CREATE DATABASE metrics"
 
 exit 0
