cc1dd3f66b006477da2e53b9e34cd7c1624e0a53
Brian Henning <henninb@msn.com>
2020-01-02 18:38:51 -0600

    factorial update in fsharp

--- fsharp/factorial/factorial.fs
+++ fsharp/factorial/factorial.fs
@@ -2,5 +2,6 @@ namespace factorial
 
 module mymodule =
   let rec factorial x = if x < 1 then 1 else x * factorial (x - 1)
-  let x = factorial(4)
-  printfn "x=%d" x
+  printfn "factorial(6)=%d" (factorial(6))
+  printfn "factorial(5)=%d" (factorial(5))
+  printfn "factorial(4)=%d" (factorial(4))
