134bb142ca3aa2fcdda6dc7a5159c556252def35
Brian Henning <henninb@msn.com>
2020-01-03 18:05:41 -0600

    added i3-gaps

--- i3-install.sh
+++ i3-install.sh
@@ -16,6 +16,7 @@ if [ \( "$OS" = "Linux Mint" \) -o \(  "$OS" = "Ubuntu" \) -o \(  "$OS" = "Raspb
   sudo apt install -y feh
   sudo apt install -y ranger
   sudo apt install -y suckless-tools
+  sudo apt install -y libev-dev
 elif [ "$OS" = "Arch Linux" ]; then
   sudo pacman -Rsnc lightdm
   sudo pacman -Rsnc gdm
@@ -82,5 +83,19 @@ else
   echo $OS is not yet implemented.
   exit 1
 fi
+
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool
+sudo apt-get install libxcb-xrm-dev
+cd ~/projects
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+git checkout gaps && git pull
+autoreconf --force --install
+rm -rf build
+mkdir build
+cd build
+../configure --prefix=/usr --sysconfdir=/etc
+make
+sudo make install
 
 exit 0
