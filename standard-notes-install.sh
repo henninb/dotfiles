d2dd95882a2093d6d1f1b95f6aecf533f7e89e9c
Brian Henning <henninb@msn.com>
2020-01-05 16:23:13 -0600

    fuse install gentoo

--- standard-notes-install.sh
+++ standard-notes-install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-wget https://github.com/standardnotes/desktop/releases/download/v3.0.12/standard-notes-3.0.12-x86_64.AppImage
+VER=3.0.24
+wget https://github.com/standardnotes/desktop/releases/download/v${VER}/standard-notes-${VER}-x86_64.AppImage
+chmod 755 standard-notes-${VER}-x86_64.AppImage
+mv standard-notes-${VER}-x86_64.AppImage $HOME/.local/bin/standard-notes
 
 exit 0
