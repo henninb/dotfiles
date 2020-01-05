4c1c726b16e0f0953d13bd86b3f41937b673838b
Brian Henning <henninb@msn.com>
2020-01-05 13:35:47 -0600

    xrdp start updates

--- xrdp-start.sh
+++ xrdp-start.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
+
+# future
 #export DISPLAY=localhost:0
-export $(dbus-launch)
-exec dbus-launch --exit-with-x11 /etc/xrdp/startwm.sh
+#export $(dbus-launch)
+#exec dbus-launch --exit-with-x11 /etc/xrdp/startwm.sh
 #sudo dbus-launch --exit-with-x11 /etc/xrdp/startwm.sh
+
+sudo kill -9 $(pgrep -f xrdp)
+sudo xrdp
+
+sudo kill -9 $(pgrep -f xrdp-sesman)
+sudo xrdp-sesman -n
+pgrep -f xrdp-sesman
+
+exit 0
