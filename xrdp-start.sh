c3e17e89e4a19d50e055f671aafd4400f683ba7f
Brian Henning <henninb@msn.com>
2020-01-05 14:09:52 -0600

    xrdp start updates

--- xrdp-start.sh
+++ xrdp-start.sh
@@ -10,8 +10,17 @@ ps -ef| grep xrdp | grep -v grep
 if [ $? -ne 0 ]; then
   sudo pkill xrdp
   sleep 1
+  sudo rm -v rm /var/run/xrdp/xrdp.pid
+  sudo rm -v /run/xrdp.pid
+  sleep 1
+  sudo xrdp
+else
+  sudo rm -v rm /run/xrdp.pid
+  sudo rm -v /var/log/xrdp.log
   sudo xrdp
 fi
+
+exit 1
 
 sudo kill -9 $(pgrep -f xrdp-sesman)
 sudo xrdp-sesman -n
