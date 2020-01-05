dd224819e65d6e3d64b87a90c41e94b9dccd4f4a
Brian Henning <henninb@msn.com>
2020-01-05 17:27:09 -0600

    standard notes install updates

--- standard-notes-install.sh
+++ standard-notes-install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 VER=3.0.24
-wget https://github.com/standardnotes/desktop/releases/download/v${VER}/standard-notes-${VER}-x86_64.AppImage
+#wget https://github.com/standardnotes/desktop/releases/download/v3.0.24/Standard-Notes-3.0.24.AppImage
+wget https://github.com/standardnotes/desktop/releases/download/v${VER}/standard-notes-${VER}.AppImage
 chmod 755 standard-notes-${VER}-x86_64.AppImage
 mv standard-notes-${VER}-x86_64.AppImage $HOME/.local/bin/standard-notes
 
