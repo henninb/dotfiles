0f62b25be692131eb3ee53e463b1959e3021eff3
Brian Henning <henninb@msn.com>
2020-01-08 19:35:50 -0600

    xrdp openrc scripts and shell script for gentoo

--- xrdp-start.sh
+++ xrdp-start.sh
@@ -1,24 +1,15 @@
 #!/bin/sh
 
-# future
-#export DISPLAY=localhost:0
-#export $(dbus-launch)
-#exec dbus-launch --exit-with-x11 /etc/xrdp/startwm.sh
-#sudo dbus-launch --exit-with-x11 /etc/xrdp/startwm.sh
-
-#ps -ef| grep '^xrdp$' | grep -v grep
-
-#pgrep -x xrdp
-sudo rm -rfv rm /run/xrdp.pid
+sudo rm -rfv /run/xrdp.pid
 sudo rm -rfv /var/log/xrdp.log
 sudo pkill -x xrdp
 sudo nohup xrdp > /dev/null 2>&1
-
 pgrep -x xrdp
 
+sudo rm -rfv /run/xrdp-sesman.pid
+sudo rm -rfv /var/log/xrdp-sesman.log
 sudo pkill -x xrdp-sesman
-#sudo kill -9 $(pgrep -f xrdp-sesman)
-sudo nohup xrdp-sesman -n > /dev/null 2>&1 &
+sudo nohup xrdp-sesman > /dev/null 2>&1 &
 pgrep -x xrdp-sesman
 
 exit 0
