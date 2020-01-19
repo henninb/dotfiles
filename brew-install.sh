de58245b47ed606daa73b8334dc384f41cdfdf74
Brian Henning <henninb@msn.com>
2020-01-19 07:44:46 -0600

    brew added

---
+++ brew-install.sh
@@ -0,0 +1,133 @@
+#!/bin/sh
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
+
+brew install coreutils
+brew install ctags
+brew install activemq
+brew install gcc
+brew install gnutls
+brew install jpeg
+brew install libgcrypt
+brew install libtermkey
+brew install lua
+brew install mpfr
+brew install openssl
+brew install python
+brew install tomcat
+brew install xz
+brew install adns
+brew install gdbm
+brew install go
+brew install kafka
+brew install libgpg-error
+brew install libtool
+brew install lua@5.1
+brew install msgpack
+brew install p11-kit
+brew install python@2
+brew install unbound
+brew install yarn
+brew install ansible
+brew install gettext
+brew install gradle
+brew install kubernetes-cli
+brew install libidn2
+brew install libunistring
+brew install luajit
+brew install ncurses
+brew install pcre
+brew install qemu
+brew install unibilium
+brew install zookeeper
+brew install bfg
+brew install git-flow
+brew install htop
+brew install leiningen
+brew install libksba
+brew install libusb
+brew install luarocks
+brew install neovim
+brew install perl
+brew install readline
+#brew install vim
+brew install cscope
+brew install glib
+brew install icu4c
+brew install libassuan
+brew install libmpc
+brew install libuv
+brew install macvim
+brew install nettle
+brew install pinentry
+brew install ruby
+brew install websocketd
+brew install curl
+brew install gmp
+brew install isl
+brew install libevent
+brew install libpng
+brew install libvterm
+brew install maven
+brew install node
+brew install pixman
+brew install sqlite
+brew install wget
+brew install emacs
+brew install gnupg
+brew install jemalloc
+brew install libffi
+brew install libtasn1
+brew install libyaml
+brew install npth
+brew install pkg-config
+brew install telnet
+brew install xmlstarlet
+brew install tmux
+#brew install docker
+brew install jenv
+brew install wine
+brew install pwgen
+brew install fish
+brew install dos2unix
+brew install clang-format
+brew install astyle
+brew install cppcheck
+brew install azure-cli
+brew install cheat
+brew cask install vimr
+brew install jenkins
+brew install lastpass-cli
+brew install mplayer
+#brew install sshfs
+brew install pacvim
+brew cask install xquartz
+brew install postgresql
+brew install fzf
+brew install liquibase
+brew install kotlin
+brew install pyenv
+brew install figlet
+brew install ghc
+brew install travis
+brew install stack
+brew install lf
+brew install pssh
+brew install charmbracelet/homebrew-tap/glow
+brew tap tgt/brewhouse git@git.target.com:brew/house.git
+echo brew cask install iterm2
+brew tap mongodb/brew
+brew install mongodb-community-shell
+#brew unlink md5sha1sum
+brew install md5sha1sum
+
+brew tap homebrew/cask-versions
+git -C "$(brew --repo homebrew/core)" fetch --unshallow
+brew cask install java11
+
+ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
+
+echo postgresql
+echo https://gist.github.com/ibraheem4/ce5ccd3e4d7a65589ce84f2a3b7c23a3
+
+exit 0
