5539366906d70855f1678d6626c70fe5a4a69c90
Brian Henning <henninb@msn.com>
2020-01-25 08:56:35 -0600

    intellij setup

---
+++ intellij-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+find ~/.IntelliJIdea* -type d -exec touch -t $(date +"%Y%m%d%H%M") {} \;
+rm -rf $HOME/.IntelliJIdea*/config/eval
+rm -rf $HOME/.IntelliJIdea*/config/options/other.xml
+rm -rf ~/.java/.userPrefs/jetbrains
+
+exit 0
