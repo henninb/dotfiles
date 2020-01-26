e6b2b29a19dd630810326f1a31363849278878bd
Brian Henning <henninb@msn.com>
2020-01-26 16:11:01 -0600

    rust installer

--- rust-install.sh
+++ rust-install.sh
@@ -4,5 +4,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup --version &>/dev/null && rustup update
 rustup --version &>/dev/null || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 #echo source $HOME/.cargo/env
+echo git checkout .bash_profile  .profile .zprofile
 
 exit 0
