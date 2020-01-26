013ee7e6c09f6bc083116fd4233f438443811899
Brian Henning <henninb@msn.com>
2020-01-25 21:43:34 -0600

    updates to java code

--- java/example_json/run.sh
+++ java/example_json/run.sh
@@ -1,19 +1,69 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <prod or local>"
+  exit 1
+fi
+ENV=$1
+
 APP=example_json
+TIMEZONE='America/Chicago'
+USERNAME=henninb
 HOST_BASEDIR=$(pwd)
-GUEST_BASEDIR=/opt/$APP
-echo dnf install java-1.8.0-openjdk-devel
-./gradlew clean build
+GUEST_BASEDIR=/opt/${APP}
+
+if [ $ENV = "prod" ]; then
+  echo prod
+elif [ $ENV = "local" ]; then
+  echo local
+else
+  echo "Usage: $0 <prod or local>"
+  exit 2
+fi
+
+# "$OSTYPE" == "darwin"*
+if [ \( "$OS" = "Linux Mint" \) -o \(  "$OS" = "Ubuntu" \) ]; then
+  HOST_IP=$(hostname -I | awk '{print $1}')
+elif [ "$OS" = "Arch Linux" ]; then
+  HOST_IP=$(hostname -i | awk '{print $1}')
+elif [ "$OS" = "Fedora" ]; then
+  HOST_IP=192.168.100.130
+elif [ "$OS" = "Darwin" ]; then
+  HOST_IP=$(ipconfig getifaddr en0)
+elif [ "$OS" = "Gentoo" ]; then
+  HOST_IP=$(hostname -i | awk '{print $1}')
+else
+  echo $OS is not yet implemented.
+  exit 1
+fi
+
+mkdir -p .idea/runConfigurations/
 mkdir -p logs
+mkdir -p ssl
+mkdir -p src/main/resources/templates
+mkdir -p src/test/unit/groovy
+mkdir -p src/test/integration/groovy
+mkdir -p src/test/functional/groovy
+mkdir -p src/test/performance/groovy
+
 touch env.secrets
-git ls-files | ctags --links=no --languages=c,c++,javascript,java,python -L-
 
-docker rm $APP -f
-docker build -t $APP .
-#docker run -it --name $APP --env-file env --env-file env.secrets --rm -d $APP
-docker run --name $APP --env-file env --env-file env.secrets -v $HOST_BASEDIR/logs:$GUEST_BASEDIR/logs --rm -t $APP
+./gradlew clean build
+if [ $? -ne 0 ]; then
+  echo "gradle build failed."
+  exit 1
+fi
+docker build -t $APP --build-arg TIMEZONE=${TIMEZONE} --build-arg APP=${APP} --build-arg USERNAME=${USERNAME} .
+if [ $? -ne 0 ]; then
+  echo "docker build failed."
+  exit 1
+fi
 
-echo docker container prune
+echo docker run -it -h ${APP} --add-host hornsup:$HOST_IP --env-file env.secrets --env-file env -v $HOST_BASEDIR/logs:$GUEST_BASEDIR/logs -v $HOST_BASEDIR/ssl:$GUEST_BASEDIR/ssl -v $HOST_BASEDIR/json_in:$GUEST_BASEDIR/json_in -v $HOST_BASEDIR/config:$GUEST_BASEDIR/config -v $HOST_BASEDIR/excel_in:$GUEST_BASEDIR/excel_in --rm ${APP} bash
+docker run -it -h ${APP} --add-host hornsup:$HOST_IP -p 8082:8080 --env-file env.secrets --env-file env -v $HOST_BASEDIR/logs:$GUEST_BASEDIR/logs -v $HOST_BASEDIR/ssl:$GUEST_BASEDIR/ssl -v $HOST_BASEDIR/json_in:$GUEST_BASEDIR/json_in -v $HOST_BASEDIR/config:$GUEST_BASEDIR/config -v $HOST_BASEDIR/excel_in:$GUEST_BASEDIR/excel_in --rm --name ${APP} ${APP}
+if [ $? -ne 0 ]; then
+  echo "docker run failed."
+  exit 1
+fi
 
 exit 0
