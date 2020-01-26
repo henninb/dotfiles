0abc1e013b571d25b99e276a8b5026c7c9eaa742
Brian Henning <henninb@msn.com>
2020-01-26 13:43:19 -0600

    unmounted updates

--- unmounted-list.sh
+++ unmounted-list.sh
@@ -7,5 +7,6 @@ lsblk  --noheadings --raw | awk '$1~/s.*[[:digit:]]/ && $7==""'
 lsblk --noheadings --raw -o NAME,MOUNTPOINT | awk '$1~/[[:digit:]]/ && $2 == ""'
 
 echo udisksctl mount -b /dev/sdb1
+echo udisksctl mount -b /dev/sda1
 
 exit 0
