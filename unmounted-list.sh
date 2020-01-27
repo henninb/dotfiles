5199a2777400b701f3c70d06e4a9af675a9ce0e2
Brian Henning <henninb@msn.com>
2020-01-26 20:42:25 -0600

    unmounted

--- unmounted-list.sh
+++ unmounted-list.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sudo pacman --noconfirm --needed -S ntfs-3g
 sudo parted -l
 
 lsblk  --noheadings --raw | awk '$1~/s.*[[:digit:]]/ && $7==""'
@@ -8,5 +9,6 @@ lsblk --noheadings --raw -o NAME,MOUNTPOINT | awk '$1~/[[:digit:]]/ && $2 == ""'
 
 echo udisksctl mount -b /dev/sdb1
 echo udisksctl mount -b /dev/sda1
+echo sudo ntfsfix /dev/yourPartitionToMount
 
 exit 0
