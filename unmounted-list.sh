5c0ba2611a677d0f1c35fdfc9e865e4d8a2a3d0a
Brian Henning <henninb@msn.com>
2020-01-27 19:38:24 -0600

    unmounted updates

--- unmounted-list.sh
+++ unmounted-list.sh
@@ -10,5 +10,6 @@ lsblk --noheadings --raw -o NAME,MOUNTPOINT | awk '$1~/[[:digit:]]/ && $2 == ""'
 echo udisksctl mount -b /dev/sdb1
 echo udisksctl mount -b /dev/sda1
 echo sudo ntfsfix /dev/yourPartitionToMount
+echo sudo mkfs.ext4 /dev/sda1
 
 exit 0
