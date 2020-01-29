388b9f3dbbfa65782cd76ccb0f5bb1f3a42068c3
Brian Henning <henninb@msn.com>
2020-01-28 20:17:19 -0600

    xrdp status update

--- xrdp-status.sh
+++ xrdp-status.sh
@@ -7,9 +7,25 @@ else
 fi
 
 netstat -na | grep 3389 | grep LIST
+if [ $? -ne 0 ]; then
+  echo 3389 port is not running.
+fi
+
 netstat -na | grep 3350 | grep LIST
+if [ $? -ne 0 ]; then
+  echo 3350 port is not running - xrdp-sesman
+fi
+
 sudo fuser 3389/tcp
+if [ $? -ne 0 ]; then
+  echo 3389 port is not running.
+fi
+
 sudo fuser 3350/tcp
+if [ $? -ne 0 ]; then
+  echo 3350 port is not running - xrdp-sesman
+fi
+
 sudo lsof -Pi | grep LISTEN | grep xrdp
 
 exit 0
