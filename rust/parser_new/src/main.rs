b5f8913ec644e760f965a540246f5ea3ea185629
Brian Henning <henninb@msn.com>
2020-01-29 18:44:45 -0600

    updates to rust program

--- rust/parser_new/src/main.rs
+++ rust/parser_new/src/main.rs
@@ -1,6 +1,6 @@
-use std::io::BufReader;
-use std::io::BufRead;
 use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
 use std::io::Error;
 
 fn parse_line(line: &str) {
@@ -11,19 +11,18 @@ fn parse_line(line: &str) {
     println!("{}", line);
 }
 
-//playground
 //https://play.rust-lang.org/?gist=63908c351a8c1038b07a9edc3a30ff16&version=stable
-fn read_file(ifp: File ) -> Result<(), Error> {
+fn read_file(ifp: File) -> Result<(), Error> {
     let reader = BufReader::new(&ifp);
-    let lines : Vec<_> = reader.lines().collect();
-        for line in lines {
+    let lines: Vec<_> = reader.lines().collect();
+    for line in lines {
         //println!("{}", line?);
         parse_line(&line?.to_string());
     }
     Ok(())
 }
 
-fn read_by_line( filename: &str ) -> Result<(), Error> {
+fn read_by_line(filename: &str) -> Result<(), Error> {
     let file = File::open(filename).expect("file not found");
     read_file(file)
 }
