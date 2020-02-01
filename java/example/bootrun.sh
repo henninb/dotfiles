49c318d507556440bb040b95ea5778977d8c6bbc
Brian Henning <henninb@msn.com>
2020-02-01 12:17:48 -0600

    bootrun.sh

--- java/example/bootrun.sh
+++ java/example/bootrun.sh
@@ -9,10 +9,6 @@ fi
 
 export PATH=${JAVA_HOME}/bin:${PATH}
 
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home
-export PATH=${JAVA_HOME}/bin:${PATH}
-
 touch env.secrets
 touch env.console
 
