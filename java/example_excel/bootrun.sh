883d1979ec5189ff11b4b383f8f393fbb711b44b
Brian Henning <henninb@msn.com>
2020-02-01 12:20:24 -0600

    bootrun updates

---
+++ java/example_excel/bootrun.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ "$OSTYPE" = "linux-gnu" ]; then
+  export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
+else
+  export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home
+  export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home
+fi
+
+export PATH=${JAVA_HOME}/bin:${PATH}
+
+touch env.secrets
+touch env.console
+
+set -a
+. ./env.console
+. ./env.secrets
+set +a
+
+./gradlew clean build bootRun
+
+exit 0
