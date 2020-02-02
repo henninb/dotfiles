1a6d6c0a7a94e7e76b10a32045023abd8d15ebcf
Brian Henning <henninb@msn.com>
2020-02-02 09:30:13 -0600

    javascript install

--- javascript-install.sh
+++ javascript-install.sh
@@ -10,7 +10,7 @@ unset NVM_DIR
 if [ \( "$OS" = "Linux Mint" \) -o \(  "$OS" = "Ubuntu" \) -o \(  "$OS" = "Raspbian GNU/Linux" \) ]; then
   curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-  sudo apt update -y
+  #sudo apt update -y
 #  sudo apt install -y gcc yarn
 elif [ "$OS" = "Arch Linux" ]; then
   sudo pacman --noconfirm --needed -S yarn
@@ -40,6 +40,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 #https://github.com/yarnpkg/yarn/archive/v1.21.1.tar.gz
 
 nvm install 10.16.0
+sudo npm install -g yarn
 # echo yarn add react
 # yarn add coc-python
 # yarn add coc-html
