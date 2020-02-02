3ae157885fe1e44a7bfaaa6aac78ccde2f2c438a
Brian Henning <henninb@msn.com>
2020-02-02 07:58:32 -0600

    javascript install updates

--- javascript-install.sh
+++ javascript-install.sh
@@ -14,17 +14,19 @@ if [ \( "$OS" = "Linux Mint" \) -o \(  "$OS" = "Ubuntu" \) -o \(  "$OS" = "Raspb
 #  sudo apt install -y gcc yarn
 elif [ "$OS" = "Arch Linux" ]; then
   sudo pacman --noconfirm --needed -S yarn
+elif [ "$OS" = "void" ]; then
+  echo void
 elif [ "$OS" = "Gentoo" ]; then
   #echo "=sys-apps/yarn-1.15.2 ~amd64" | sudo tee -a /etc/portage/package.accept_keywords
   #echo "=dev-libs/openssl-1.1.1b-r2 ~amd64" | sudo tee -a /etc/portage/package.accept_keywords
   #echo "=dev-libs/openssl-1.1.1b-r2" | sudo tee -a /etc/portage/package.unmask
   cat /var/lib/portage/world | grep yarn
-  sudo emerge -pf sys-apps/yarn
-  sudo emerge -upDN sys-apps/yarn
-  sudo emerge sys-apps/yarn
+  #sudo emerge -pf sys-apps/yarn
+  #sudo emerge -upDN sys-apps/yarn
+  #sudo emerge sys-apps/yarn
 elif [ "$OS" = "CentOS Linux" ]; then
   echo centos
-  exit 1
+  #exit 1
 elif [ "$OS" = "FreeBSD" ]; then
   sudo pkg install -y yarn gcc python27 gmake
   sudo pkg install -y yarn-node10-1.16.0
@@ -37,34 +39,33 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 
 #https://github.com/yarnpkg/yarn/archive/v1.21.1.tar.gz
 
-echo nvm add 10.16.0
-echo yarn add react
-yarn add coc-python
-yarn add coc-html
-yarn add coc-java
-yarn add coc-powershell
-yarn add coc-rls
-yarn add coc-svg
-yarn add coc-git
-yarn add coc-fsharp
-echo yarn add coc-gocode
-echo yarn add coc-eslint
-yarn add coc-xml
-yarn add coc-clojure
-yarn add coc-vimlsp
-yarn add coc-go
-yarn add coc-docker
-yarn add coc-elixir
-yarn add coc-lua
-yarn add coc-sql
-yarn add coc-angular
-yarn add coc-json
-yarn add coc-css
-yarn add coc-yaml
-
-yarn add eslint
-yarn add prettier
-yarn add eslint-plugin-prettier
-yarn add eslint-config-prettier
+nvm add 10.16.0
+# echo yarn add react
+# yarn add coc-python
+# yarn add coc-html
+# yarn add coc-java
+# yarn add coc-powershell
+# yarn add coc-rls
+# yarn add coc-svg
+# yarn add coc-git
+# yarn add coc-fsharp
+# echo yarn add coc-gocode
+# echo yarn add coc-eslint
+# yarn add coc-xml
+# yarn add coc-clojure
+# yarn add coc-vimlsp
+# yarn add coc-go
+# yarn add coc-docker
+# yarn add coc-elixir
+# yarn add coc-lua
+# yarn add coc-sql
+# yarn add coc-angular
+# yarn add coc-json
+# yarn add coc-css
+# yarn add coc-yaml
+# yarn add eslint
+# yarn add prettier
+# yarn add eslint-plugin-prettier
+# yarn add eslint-config-prettier
 
 exit 0
