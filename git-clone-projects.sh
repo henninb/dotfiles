969076136a6d8ed21208bc17c9f147c181d47ee1
Brian Henning <henninb@msn.com>
2020-02-05 19:42:57 -0600

    git clone updates

--- git-clone-projects.sh
+++ git-clone-projects.sh
@@ -2,7 +2,7 @@
 
 PROJECTS="raspi-finance-endpoint raspi-finance-ui raspi-finance-database raspi-finance raspi-finance-client raspi-finance-convert nfl-database src-common"
 for i in $(echo $PROJECTS); do
-  cd ~/projects
+  cd $HOME/projects
   git clone git@github.com:BitExplorer/$i.git
   cd $i
   git branch --set-upstream-to=origin/master master
@@ -10,10 +10,26 @@ for i in $(echo $PROJECTS); do
   git pull
 done
 
-cd ~/projects
+cd $HOME/projects
 git clone git@gitlab.com:BitExplorer/howto.git
 
+PLUGINS="autojump zsh-autosuggestions zsh-syntax-highlighting"
+for i in $(echo $PLUGINS); do
+  cd $HOME/plugins
+  echo $i
+  cd $i
+  git pull
+done
+
+THEME="agnoster-zsh-theme  alien  dracula-zsh-theme  spaceship-prompt"
+for i in $(echo $THEME); do
+  cd $HOME/themes
+  echo $i
+  cd $i
+  git pull
+done
+
 echo distrotube
-git clone git@gitlab.com:dwt1/wallpapers.git
+echo git clone git@gitlab.com:dwt1/wallpapers.git
 
 exit 0
