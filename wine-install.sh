b4f4acab80bc65aaa3ba670ae060667b257f8f57
Brian Henning <henninb@msn.com>
2020-02-13 19:28:37 -0600

    wine updates

--- wine-install.sh
+++ wine-install.sh
@@ -9,13 +9,14 @@ Include = /etc/pacman.d/mirrorlist
 EOF
 
 if [ "$OS" = "Arch Linux" ]; then
-  grep "^[multilib]" /etc/pacman.conf
-  if [ $? -ne 0 ]; then
-    cat pacman.conf | sudo tee -a /etc/pacman.conf
-    rm -rf pacman.conf
-  fi
-  sudo pacman --noconfirm --needed -Syy
-  sudo pacman --noconfirm --needed -S wine
+  # grep "^[multilib]" /etc/pacman.conf
+  # if [ $? -ne 0 ]; then
+  #   cat pacman.conf | sudo tee -a /etc/pacman.conf
+  #   rm -rf pacman.conf
+  # fi
+  # sudo pacman --noconfirm --needed -Syy
+  # sudo pacman --noconfirm --needed -S wine
+  yay -S multilib/wine
 elif [ "$OS" = "CentOS Linux" ]; then
   sudo yum install -y epel-release
   #sudo subscription-manager repos
