e796b40e1bba23a99ec2d2be8f1ad70ddf3c9f7a
Brian Henning <henninb@msn.com>
2020-02-15 16:15:45 -0600

    compton updates

--- compton-install.sh
+++ compton-install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+sudo apt install libxcb-damage0-dev
+sudo apt install uthash-dev
+sudo pacman -S uthash
+sudo apt install libxdg-basedir-dev
+
 cd $HOME/projects || exit
 git clone https://github.com/tryone144/compton.git
 cd compton || exit
