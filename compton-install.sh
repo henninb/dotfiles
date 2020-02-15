adf367ecf8e5cf97a4569779e8d8c137dcd1f52b
Brian Henning <henninb@msn.com>
2020-02-15 08:41:31 -0600

    compton install

---
+++ compton-install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd $HOME/projects || exit
+git clone https://github.com/tryone144/compton.git
+cd compton || exit
+git checkout feature/dual_kawase
+git submodule update --init --recursive
+meson --buildtype=release . build
+ninja -C build
+ninja -C build install
+
+exit 0
