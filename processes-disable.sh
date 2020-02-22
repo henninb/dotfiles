be39d468afdafb653779a75c8bbc1e4f91e3858f
Brian Henning <henninb@msn.com>
2020-02-22 11:09:03 -0600

    added script

---
+++ processes-disable.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo systemctl disable jenkins
+sudo systemctl disable influxdb
+sudo systemctl disable kafka
+sudo systemctl disable grafana-server
+sudo systemctl disable activemq
+sudo systemctl disable zookeeper
+
+exit 0
