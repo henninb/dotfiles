726d9e81699779c4622744a0513196bddf65c3c6
Brian Henning <henninb@msn.com>
2020-02-22 11:16:53 -0600

    kotlin version updated

--- kotlin-install.sh
+++ kotlin-install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 KOTLIN_VER=$(curl -s https://blog.jetbrains.com/kotlin/category/releases/ | grep -io "Kotlin [0-9.]\+[0-9] released" | head -1 | grep -o '[0-9.]\+[0-9]')
-echo $KOTLIN_VER
+KOTLIN_VER=1.3.61
+echo "$KOTLIN_VER"
 
 if [ ! -f "kotlin-compiler-${KOTLIN_VER}.zip" ]; then
   wget https://github.com/JetBrains/kotlin/releases/download/v${KOTLIN_VER}/kotlin-compiler-${KOTLIN_VER}.zip
