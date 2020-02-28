77b5f217963e752ed2e5767a9a6928c6bb711026
Brian Henning <henninb@msn.com>
2020-02-27 19:19:47 -0600

    ktor example

---
+++ src/ktor/raspi-finance-endpoint/src/Application.kt
@@ -0,0 +1,35 @@
+package example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.client.*
+import io.ktor.client.features.json.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    val client = HttpClient() {
+        install(JsonFeature) {
+            serializer = GsonSerializer()
+        }
+    }
+    runBlocking {
+        // Sample for making a HTTP Client request
+        /*
+        val message = client.post<JsonSampleClass> {
+            url("http://127.0.0.1:8080/path/to/endpoint")
+            contentType(ContentType.Application.Json)
+            body = JsonSampleClass(hello = "world")
+        }
+        */
+    }
+
+}
+
+data class JsonSampleClass(val hello: String)
+
