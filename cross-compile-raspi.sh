8c40e9d435bb814a5bd80e2019e9879f2bcb4a8f
Brian Henning <henninb@msn.com>
2020-03-01 18:02:57 -0600

    raspi cross compile

--- cross-compile-raspi.sh
+++ cross-compile-raspi.sh
@@ -3,4 +3,18 @@
 sudo apt install -y cmake ia32-libs
 git clone git://github.com/raspberrypi/tools.git
 
+
+echo https://blog.kitware.com/cross-compiling-for-raspberry-pi/
+
+mkdir -p  ~/src/RaspberryPi/toolchain
+mkdir -p ~/local/crosstool-ng
+
+sudo apt install -y bison cvs flex gperf texinfo automake libtool help2man
+wget http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-1.24.0.tar.xz
+tar xvf crosstool-ng-1.24.0.tar.xz
+cd crosstool-ng-1.24.0
+./configure --prefix=$HOME/.local
+make
+make install
+
 exit 0
