7872964946d42a2fa576de6d7bb6197a8d124e52
Brian Henning <henninb@msn.com>
2020-03-07 07:24:43 -0600

    polybar and i3 updates

---
+++ .config/polybar/launch-master.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u "${UID}" -x polybar >/dev/null; do sleep 1; done
+
+polybar -c "$HOME/.config/polybar/config-rice.ini" master 2>> "$HOME/polybar.log" &
+echo $? >> "$HOME/polybar.log"
