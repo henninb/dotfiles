1993cccffca492593edfda390b8505e6f9a55258
Brian Henning <henninb@msn.com>
2020-03-08 16:36:09 -0500

    i3 updates

--- i3-install.sh
+++ i3-install.sh
@@ -45,7 +45,7 @@ if [ \( "$OS" = "Linux Mint" \) -o \(  "$OS" = "Ubuntu" \) -o \(  "$OS" = "Raspb
   make
   sudo make install
 elif [ "$OS" = "void" ]; then
-  sudo xbps-install -y i3
+  sudo xbps-install -y i3-gaps
   sudo xbps-install -y i3lock
   sudo xbps-install -y sxhkd
   sudo xbps-install -y i3blocks
