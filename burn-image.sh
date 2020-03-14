307f6ed10bba7962c0ef786180fead73c71e2137
Brian Henning <henninb@msn.com>
2020-03-14 18:22:13 -0500

    burn images and git updates

--- burn-image.sh
+++ burn-image.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <file>"
-    exit 1
+  echo "Usage: $0 <file>"
+  exit 1
 fi
 
 FILE=$1
 
-sudo mkdir -p /media/cdrom
+wodim  dev=/dev/sr0 -checkdrive -prcap
+echo does not write dvdr+ disks
 
+if [ -x "$(command -v wodim)" ]; then
+  wodim dev=/dev/sr0 --scanbus
+  #sudo wodim dev=/dev/sr0 -v -data "${FILE}"
+  sudo wodim -eject -tao speed=0 dev=/dev/sr0 -v -data  "${FILE}"
+  #driver=mmc_dvdplusr
+  #driveropts=burnfree
+else
+  echo wodim is not installed.
+fi
+
+echo sudo mount -t iso9660 /dev/cdrom /media/cdrom
+echo sudo mkdir -p /media/cdrom
 echo cdrecord -scanbus
 echo cdrecord -v -eject speed=48 dev=ATAPI:0,0,0 CentOS-8-x86_64-1905-boot.iso
-
-ls -l /dev/cdrom
-wodim dev=/dev/sr0 --scanbus
-sudo wodim dev=/dev/sr0 -v -data $FILE
-echo sudo mount -t iso9660 /dev/cdrom /media/cdrom
+echo ls -l /dev/cdrom
 
 exit 0
