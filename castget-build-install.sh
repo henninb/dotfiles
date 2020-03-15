4893f8825fbf9ec7af8db6ad52bf7f7d3fcaf621
Brian Henning <henninb@msn.com>
2020-03-14 19:41:57 -0500

    castget updates

--- castget-build-install.sh
+++ castget-build-install.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-
-if [ \( "$OS" = "Linux Mint" \) -o \(  "$OS" = "Ubuntu" \) -o \(  "$OS" = "Raspbian GNU/Linux" \) ]; then
+if [  "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Raspbian GNU/Linux" ]; then
   sudo apt install -y libxml2-dev  libcurl4-openssl-dev libid3-3.8.3-dev
-elif [ \( "$OS" = "Arch Linux" \) -o \( "$OS" = "Manjaro Linux" \) ]; then
+elif [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ]; then
   sudo pacman --noconfirm --needed -S castget
 else
-  echo $OS is not yet implemented.
+  echo "$OS is not yet implemented."
   exit 1
 fi
 wget http://savannah.nongnu.org/download/castget/castget-1.2.4.tar.bz2 -O castget-1.2.4.tar.bz2
