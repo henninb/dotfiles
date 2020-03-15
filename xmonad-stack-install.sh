2e2b02fabbc587a588d663bcd3c59f74ba3318e3
Brian Henning <henninb@msn.com>
2020-03-15 14:37:06 -0500

    xmonad updates

--- xmonad-stack-install.sh
+++ xmonad-stack-install.sh
@@ -80,7 +80,7 @@ elif [ "$OS" = "void" ]; then
   done
   echo "Failures: $FAILURE"
 elif [ "$OS" = "Solus" ]; then
-  SOLUS_PKGS="feh xdotool w3m neofetch xz make gcc gmp-devel dunst alsa-lib-devel alsa-utils pulseaudio libxscrnsaver-devel libxrandr-devel libxft-devel xscreensaver wmname xdo"
+  SOLUS_PKGS="feh xdotool w3m neofetch xz make gcc gmp-devel dunst alsa-lib-devel alsa-utils pulseaudio libxscrnsaver-devel libxrandr-devel libxft-devel xscreensaver wmname xdo polybar"
   FAILURES=""
   for i in $(echo $SOLUS_PKGS); do
     if ! sudo eopkg install -y "$i"; then
