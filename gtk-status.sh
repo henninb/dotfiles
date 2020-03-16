5ed068f2976ebd595c915de267b4ded1cf3cc305
Brian Henning <henninb@msn.com>
2020-03-16 08:08:46 -0500

    updates

--- gtk-status.sh
+++ gtk-status.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-pkg-config --exists gtk+-3.0 && echo "Installed" || echo "Not installed"
+pkg-config --exists gtk+-3.0 && echo "gtk+ 3.0 is installed" || echo "gtk+ 3.0 is not installed"
+pkg-config --exists gtk+-2.0 && echo "gtk+ 2.0 is installed" || echo "gtk+ 2.0 is not installed"
 
 exit 0
