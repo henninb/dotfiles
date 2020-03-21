f0ee54eede903b9ae9560ed71a584322cd70d261
Brian Henning <henninb@msn.com>
2020-03-20 19:39:49 -0500

    wifi updates

---
+++ wifi-autostart.sh
@@ -0,0 +1,5 @@
+# systemctl enable netctl-auto@ <interface>.service
+# systemctl enable netctl-ifplugd@ <interface>.service
+
+sudo systemctl enable netctl-auto@wlp0s20u9.service
+sudo systemctl enable netctl-ifplugd@wlp0s20u9.service
