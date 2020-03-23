1a119e61d5935a11b378189f2059fd39e06f651d
Brian Henning <henninb@msn.com>
2020-03-23 09:00:37 -0500

    grub updates

--- grub-win-boot.sh
+++ grub-win-boot.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 echo next boot will be into windows - onetime
-
 echo /etc/default/grub set DEFAULT = saved
 echo sudo grub-editenv - set saved_entry=2
 echo GRUB_DEFAULT="Advanced options for Gentoo GNU/Linux>Gentoo GNU/Linux, with Linux 5.2.1-aufs"
@@ -19,8 +18,7 @@ echo sudo grub-editenv list
 #  exit 0
 #fi
 
-ip addr show | grep 192.168.100.135
-if [ $? -eq 0 ]; then
+if ip addr show | grep 192.168.100.135; then
   sudo grub-editenv - set next_entry='Windows 10 (loader) on /dev/sdb1'
   sudo grub-editenv - set saved_entry=1
   sudo grub-mkconfig -o /boot/grub/grub.cfg
@@ -30,8 +28,8 @@ if [ $? -eq 0 ]; then
 fi
 
 # Mint Linux
-ip addr show | grep 192.168.100.217
-if [ $? -eq 0 ]; then
+# ip addr show | grep 192.168.100.217
+if [ "$(uname -n)" = "silverfox" ]; then
   sudo grub-editenv - set next_entry='Windows Boot Manager (on /dev/nvme0n1p2)'
   sudo grub-editenv - set saved_entry=1
   sudo grub-mkconfig -o /boot/grub/grub.cfg
@@ -41,8 +39,7 @@ if [ $? -eq 0 ]; then
 fi
 
 # Arch Linux
-ip addr show | grep 192.168.100.218
-if [ $? -eq 0 ]; then
+if ip addr show | grep 192.168.100.218; then
   sudo grub-editenv - set next_entry='Windows 10 gpt2'
   sudo grub-editenv - set saved_entry=1
   sudo grub-mkconfig -o /boot/grub/grub.cfg
@@ -50,17 +47,5 @@ if [ $? -eq 0 ]; then
   echo sudo reboot - to reboot into windows
   exit 0
 fi
-
-exit 0
-
-echo will boot into windows onetime
-#sudo grub2-editenv - set next_entry='Windows Boot Manager (on /dev/nvme0n1p2)'
-sudo grub2-editenv - set next_entry='Windows 10 (loader) on /dev/sdb1'
-#sudo grub2-editenv - set saved_entry='CentOS Linux (3.10.0-862.2.3.el7.x86_64) 7 (Core)'
-sudo grub2-editenv - set saved_entry=1
-
-sudo grub2-mkconfig -o /boot/grub2/grub.cfg
-#sudo grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg
-sudo grub2-editenv list
 
 exit 0
