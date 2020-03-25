859408d837055aefcb385be8753b81aadf0f1dfb
Brian Henning <henninb@msn.com>
2020-03-25 17:52:09 -0500

    updates

--- fonts-mono-install.sh
+++ fonts-mono-install.sh
@@ -2,7 +2,9 @@
 
 wget https://fontlibrary.org/assets/downloads/symbola/cf81aeb303c13ce765877d31571dc5c7/symbola.zip
 
-PROJECTS="sauce-code-pro-nerd-fonts.zip monofur-fonts.zip mononoki-fonts.zip inconsolata-fonts.zip dejavu-sans-mono-fonts.zip jetbrains-fonts.zip symbola.zip"
+https://github.com/tonsky/FiraCode/releases/download/2/FiraCode_2.zip
+
+PROJECTS="sauce-code-pro-nerd-fonts.zip monofur-fonts.zip mononoki-fonts.zip inconsolata-fonts.zip dejavu-sans-mono-fonts.zip jetbrains-fonts.zip symbola.zip FiraCode_2.zip"
 for i in $(echo $PROJECTS); do
   cd ~/.fonts || exit
   unzip -o "../$i"
