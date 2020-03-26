1319a540940f171e6c68268fe6bf8819d893ac95
Brian Henning <henninb@msn.com>
2020-03-26 10:05:48 -0500

    laptop settings

--- laptop-lip.sh
+++ laptop-lip.sh
@@ -3,5 +3,6 @@
 echo sudo vi /etc/systemd/logind.conf
 echo HandleLidSwitch=ignore
 sudo systemctl restart systemd-logind
+# setterm -term linux -blank 10 -powerdown 5 </dev/tty1 >/dev/tty1
 
 exit 0
