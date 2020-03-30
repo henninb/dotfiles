d6fe63d97b8c073983324d55d1b22bb354ed0979
Brian Henning <henninb@msn.com>
2020-03-30 07:27:23 -0500

    run updates for gentoo

--- gentoo/run.sh
+++ gentoo/run.sh
@@ -9,6 +9,9 @@ sudo cp -v package.license /etc/portage/package.license
 sudo cp -v /etc/portage/package.use/zz-autounmask /tmp/zz-autounmask.bak.$$
 sudo cp -v zz-autounmask /etc/portage/package.use/zz-autounmask
 
+sudo cp -v /etc/portage/make.conf /etc/portage/make.conf.bak.$$
+sudo cp -v make.conf /etc/portage/make.conf
+
 echo "ruby_targets_ruby25" | sudo tee -a /etc/portage/use.mask
 
 exit 0
