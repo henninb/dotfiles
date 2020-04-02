6596bd672e266399d44a6307ee4ae65bf60ce79c
Brian Henning <henninb@msn.com>
2020-04-02 09:59:23 -0500

    arduino  updates

--- arduino-install.sh
+++ arduino-install.sh
@@ -22,11 +22,28 @@ sudo usermod -a -G arduino "$(whoami)"
 sudo usermod -a -G tty "$(whoami)"
 sudo pacman -S minicom
 sudo pacman -S moserial
+echo stty -F /dev/ttyUSB0 hupcl
+echo stty -F /dev/ttyUSB0 -hupcl
+echo stty -a -F /dev/ttyUSB0
 
 # cd "$HOME/projects"
 # git clone git@github.com:arduino/arduino-cli.git
 # cd arduino-cli
 # cd "$HOME"
-curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+# curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+go get -u github.com/arduino/arduino-cli
+
+# arduino-cli core search arduino
+# arduino-cli core search leonardo
+# arduino-cli core search uno
+# arduino-cli core search samd
+
+# Arduino board support like Leonardo/Uno
+arduino-cli core install arduino:avr
+# Or if you need SAMD21/SAMD51 support:
+arduino-cli core install arduino:samd
+
+arduino-cli compile --fqbn arduino:avr:uno MyFirstSketch
+arduino-cli upload --port /dev/ttyUSB0 --fqbn arduino:avr:uno MyFirstSketch
 
 exit 0
