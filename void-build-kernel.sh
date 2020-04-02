0cb51dce07e13a28a28d78e70ebcf79e8cfdd07b
Brian Henning <henninb@msn.com>
2020-04-02 18:53:18 -0500

    void kernel build

--- void-build-kernel.sh
+++ void-build-kernel.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
-# xbps-install bc wget
 
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.6.2.tar.xz
 tar xf linux-5.6.2.tar.xz
 cd linux-5.6.2
 
-# cp /boot/config-3.17.1_1 .config
-
 make -j"$(nproc)"
 
-make modules_install firmware_install
+sudo make modules_install
 
 sudo cp System.map /boot/System.map-5.6.2
 sudo cp arch/x86/boot/bzImage /boot/vmlinuz-5.6.2
 sudo cp .config /boot/config-5.6.2
-sudo dracut -f initramfs-5.6.2.img 5.6.2
+sudo dracut -f initramfs-5.6.2.img 5.6.2_1
 sudo cp initramfs-5.6.2.img /boot/
 
-# sudo update-grub
+sudo update-grub
+echo sudo update-grub
 # uname -a
 
 exit 0
