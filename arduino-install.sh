f9bc6317c8f3e1aaa84132ef3611e385ccce895a
Brian Henning <henninb@msn.com>
2020-04-02 07:01:05 -0500

    arduino updates

--- arduino-install.sh
+++ arduino-install.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
-RASPI_IP=$(nmap -sP 192.168.100.0/24 | grep raspb | grep -o '[0-9.]\+[0-9]')
-VER=$(curl -f https://www.arduino.cc/en/main/software | grep -o 'arduino-[0-9.]\+[0-9]-windows.exe' | sed 's/arduino-//' | sed 's/-windows.exe//')
-echo $VER
+VER=$(curl -s -f https://www.arduino.cc/en/main/software | grep -o 'arduino-[0-9.]\+[0-9]-windows.exe' | sed 's/arduino-//' | sed 's/-windows.exe//')
+echo "$VER"
 
 if [ ! -f "arduino-${VER}-linux64.tar.xz" ]; then
   rm -rf arduino-*-linux64.tar.xz
-  curl https://downloads.arduino.cc/arduino-${VER}-linux64.tar.xz --output arduino-${VER}-linux64.tar.xz
+  curl https://downloads.arduino.cc/arduino-${VER}-linux64.tar.xz --output "arduino-${VER}-linux64.tar.xz"
 fi
 
 sudo rm -rf /opt/arduino
-sudo rm -rf /opt/arduino-${VER}
-sudo tar -xJvf arduino-${VER}-linux64.tar.xz -C /opt
-sudo ln -s /opt/arduino-${VER} /opt/arduino
+sudo rm -rf "/opt/arduino-${VER}"
+sudo tar -xJvf "arduino-${VER}-linux64.tar.xz" -C /opt
+sudo ln -s "/opt/arduino-${VER}" /opt/arduino
 
 id -u arduino &>/dev/null || sudo useradd arduino -m
 sudo chown -R arduino:arduino /opt/arduino/
-sudo chown -R arduino:arduino /opt/arduino-${VER}/
+sudo chown -R arduino:arduino "/opt/arduino-${VER}/"
+
+sudo usermod -a -G arduino "$(whoami)"
+
+# cd "$HOME/projects"
+# git clone git@github.com:arduino/arduino-cli.git
+# cd arduino-cli
+# cd "$HOME"
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 
 exit 0
