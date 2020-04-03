a84e0a1e448f364f493d09dabc5c5011c2baef60
Brian Henning <henninb@msn.com>
2020-04-02 20:50:08 -0500

    void updates

--- void-build-kernel.sh
+++ void-build-kernel.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.6.2.tar.xz
+if [ ! -f linux-5.6.2.tar.xz ]; then
+  wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.6.2.tar.xz
+fi
 tar xf linux-5.6.2.tar.xz
 cd linux-5.6.2
+
+if [ ! -f .config ]; then
+  make menuconfig
+fi
 
 make -j"$(nproc)"
 
