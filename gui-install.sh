af0e57f2dc204d0ebac258f4bdceda307c9d3cfe
Brian Henning <henninb@msn.com>
2020-04-05 19:05:43 -0500

    gui updates

--- gui-install.sh
+++ gui-install.sh
@@ -42,8 +42,11 @@ elif [ "$OS" = "Linux Mint" ]; then
   echo sudo apt-add-repository ppa:dolphin-emu/ppa
   wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
   echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+  echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
   sudo apt update
   sudo apt install -y dbeaver-ce
+  sudo apt install -y balena-etcher-electron
   FAILURES=""
   for i in $(echo $MINT_PKGS); do
     sudo apt install -y $i
