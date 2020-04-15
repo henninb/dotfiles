d9916b408d7420812ce65e622601c74aa28c040d
Brian Henning <henninb@msn.com>
2020-04-15 18:24:56 -0500

    mint updates

--- mint-build-kernel.sh
+++ mint-build-kernel.sh
@@ -3,6 +3,7 @@
 # sudo zypper install -y flex
 # sudo zypper install -y libelf-devel
 sudo apt install libncurses-dev
+sudo apt-get install libncurses-dev flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf
 
 VER=5.6.4
 if [ ! -f "linux-5.6.4.tar.xz" ]; then
@@ -15,15 +16,23 @@ if [ ! -f .config ]; then
   make menuconfig
 fi
 
-make -j"$(nproc)"
-sudo make modules_install
+fakeroot make-kpkg --initrd --append-to-version=-customname kernel_image kernel_headers
 
-sudo cp System.map "/boot/System.map-$VER"
-sudo cp arch/x86/boot/bzImage "/boot/vmlinuz-$VER"
-sudo cp .config "/boot/config-$VER"
-# /lib/modules/5.6.2-1-default/
-sudo dracut -f "initramfs-$VER.img" "$VER-1-default"
-sudo cp "initramfs-$VER.img" /boot/
+cd ..
+sudo dpkg -i linux-image-*.deb linux-headers-*.deb
+sudo update-grub
+
+echo https://forums.linuxmint.com/viewtopic.php?t=131433
+
+# make -j"$(nproc)"
+# sudo make modules_install
+
+# sudo cp System.map "/boot/System.map-$VER"
+# sudo cp arch/x86/boot/bzImage "/boot/vmlinuz-$VER"
+# sudo cp .config "/boot/config-$VER"
+# # /lib/modules/5.6.2-1-default/
+# sudo dracut -f "initramfs-$VER.img" "$VER-1-default"
+# sudo cp "initramfs-$VER.img" /boot/
 # sudo grub2-mkconfig -o /boot/grub2/grub.cfg
 
 # sudo update-grub
