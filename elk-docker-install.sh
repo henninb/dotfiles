26e43777f7378a3a9913d89879f2b3ee3d60d79c
Brian Henning <henninb@msn.com>
2020-04-18 06:06:37 -0500

    elk updates

---
+++ elk-docker-install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+docker pull elasticsearch:7.6.2
+docker run -d --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.6.2
+
+# docker pull logstash:7.6.2
+sudo pacman -S logstash
+
+sudo /usr/share/logstash/bin/logstash-plugin install logstash-codec-sflow
+sudo /usr/share/logstash/bin/logstash-plugin install logstash-codec-netflow
+sudo /usr/share/logstash/bin/logstash-plugin update logstash-codec-netflow
+sudo /usr/share/logstash/bin/logstash-plugin install logstash-input-udp
+sudo /usr/share/logstash/bin/logstash-plugin update logstash-input-udp
+sudo /usr/share/logstash/bin/logstash-plugin install logstash-filter-dns
+sudo /usr/share/logstash/bin/logstash-plugin update logstash-filter-dns
+
+git clone git@github.com:deviantony/docker-elk.git
+
+exit 0
