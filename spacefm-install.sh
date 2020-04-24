78bb54d73455535d1120811130bf98a34295fefe
Brian Henning <henninb@msn.com>
2020-04-24 20:30:35 +0000

    spacefm updates

--- spacefm-install.sh
+++ spacefm-install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-sudo apt install -y spacefm udevil
+sudo apt install -y spacefm udevil libudev-dev libffmpegthumbnailer-dev
 
 cd "$HOME/projects"
 git clone git@github.com:IgnorantGuru/spacefm.git
 cd spacefm
+./autogen.sh
+make
+sudo make install
 
 exit 0
