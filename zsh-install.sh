f0141723ad0ef17ef0e928d3dc5fc20347222984
Brian Henning <henninb@msn.com>
2020-04-25 00:27:24 +0000

    zsh updates

--- zsh-install.sh
+++ zsh-install.sh
@@ -11,6 +11,8 @@ elif [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ]; then
   sudo pacman  --noconfirm --needed -S unzip
 elif [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Raspbian GNU/Linux" ]; then
   sudo apt install -y zsh
+  sudo apt install -y unzip
+  sudo apt install -y fontconfig
 elif [ "$OS" = "FreeBSD" ]; then
   sudo pkg install -y zsh
 elif [ "$OS" = "Gentoo" ]; then
