21e05843a45f3a67d128a289a913d2514a3eeeca
Brian Henning <henninb@msn.com>
2020-04-25 08:00:07 -0500

    lf install

--- lf-install.sh
+++ lf-install.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-sudo pacman -S go
-sudo apt install -y golang
-brew install golang
 
 if [ ! -x "$(command -v go)" ]; then
   echo go not installed.
   exit 1
+else
+  sudo pacman -S go
+  # sudo apt install -y golang
+  brew install golang
 fi
 
 go get github.com/gokcehan/lf
