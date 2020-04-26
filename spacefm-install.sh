daecfdc2f8b2089448d46c43bdd21bf0e6257ef6
Brian Henning <henninb@msn.com>
2020-04-25 19:32:31 -0500

    spacefm updates

--- spacefm-install.sh
+++ spacefm-install.sh
@@ -5,6 +5,9 @@ sudo apt install -y libgtk2.0-dev
 
 # sudo apt install -y autotools-dev bash build-essential intltool pkg-config fakeroot shared-mime-info desktop-file-utils libc6 libcairo2 libglib2.0-0 libglib2.0-dev libpango1.0-0 libx11-6 libx11-dev libudev1 libudev-dev libffmpegthumbnailer-dev
 
+sudo xbps-install -y intltool
+sudo xbps-install -y glib-devel
+
 cd "$HOME/projects"
 git clone git@github.com:IgnorantGuru/spacefm.git
 cd spacefm
