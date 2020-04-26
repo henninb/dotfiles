7cb1c821412c7cbbc4701216f07ffd076cf5bbea
Brian Henning <henninb@msn.com>
2020-04-26 18:01:07 -0500

    mongo updates

--- mongodb-install.sh
+++ mongodb-install.sh
@@ -5,5 +5,9 @@ git clone https://aur.archlinux.org/mongodb-bin mongodb-bin-aur
 cd mongodb-bin-aur
 makepkg -si
 
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+
 sudo systemctl enable mongodb
 sudo systemctl start mongodb
+
+exit 0
