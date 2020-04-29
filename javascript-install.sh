3d4556ea1af60b70158233f0b0240308690fba45
Brian Henning <henninb@msn.com>
2020-04-29 07:40:27 -0500

    javascript updates

--- javascript-install.sh
+++ javascript-install.sh
@@ -13,29 +13,21 @@ if [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Raspbian GNU/L
   #sudo apt update -y
 #  sudo apt install -y gcc yarn
 elif [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ]; then
-  sudo pacman --noconfirm --needed -S yarn
+  sudo pacman --noconfirm --needed -S nodejs
 elif [ "$OS" = "void" ]; then
   echo
 elif [ "$OS" = "Gentoo" ]; then
-  #echo "=sys-apps/yarn-1.15.2 ~amd64" | sudo tee -a /etc/portage/package.accept_keywords
-  #echo "=dev-libs/openssl-1.1.1b-r2 ~amd64" | sudo tee -a /etc/portage/package.accept_keywords
-  #echo "=dev-libs/openssl-1.1.1b-r2" | sudo tee -a /etc/portage/package.unmask
-  cat /var/lib/portage/world | grep yarn
-  #sudo emerge -pf sys-apps/yarn
-  #sudo emerge -upDN sys-apps/yarn
-  #sudo emerge sys-apps/yarn
+  echo
 elif [ "$OS" = "Solus" ]; then
-  echo Solus
+  sudo eopkg install nodejs
 elif [ "$OS" = "CentOS Linux" ]; then
   echo centos
 elif [ "$OS" = "FreeBSD" ]; then
   sudo pkg install -y yarn gcc python27 gmake
-  sudo pkg install -y yarn-node10-1.16.0
 else
   echo $OS is not yet implemented.
   exit 1
 fi
-#cd ~/.nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | zsh
 
 export NVM_DIR="$HOME/.nvm"
@@ -45,8 +37,11 @@ if [ $? -ne 0 ]; then
   source "$HOME/.nvm/nvm.sh"
 fi
 
-# nvm install 10.16.0
-# echo logout and login
-nvm install 13.8.0
+echo nvm install 13.8.0
+
+if [ ! -x "$(command -v n)" ]; then
+  sudo npm install -g n
+fi
+sudo n stable
 
 exit 0
