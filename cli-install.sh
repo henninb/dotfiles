03cce7f821b17b3bcbe478b61f56df0c2cbe6386
Brian Henning <henninb@msn.com>
2020-04-30 06:34:25 -0500

    shell scripts updates

--- cli-install.sh
+++ cli-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-DEBIAN_FRONTEND=noninteractive
+# DEBIAN_FRONTEND=noninteractive
 
 CENTOS_PKGS="epel-release mpg123 gcc ncurses-devel screen figlet neovim vim rtorrent wget ranger fx zsh gpg fossil subversion git mutt neomutt irssi htop nmon nmap python-pip libnova ffmpeg yarn alsa-utils cmus rsync tmux emacs stunnel wine mcrypt fetchmail pass fish lastpass-cli neofetch zip unzip"
 
@@ -39,117 +39,108 @@ echo "$MACOS_PKGS" > .cli/macos
 echo "$VOID_PKGS" > .cli/macos
 
 if [ "$OS" = "Linux Mint" ]; then
-  FAILURES=""
+  FAILURE=""
   curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
   sudo apt update
-  for i in $(echo $MINT_PKGS); do
-    sudo apt install --download-only -y "$i"
-    if [ 0 -ne $? ]; then
+  for i in $MINT_PKGS; do
+    if ! sudo apt install --download-only -y "$i"; then
       FAILURE="$i $FAILURE"
     fi
   done
-  for i in $(echo $MINT_PKGS); do
-    sudo apt install -y "$i"
-    if [ 0 -ne $? ]; then
+  for i in $MINT_PKGS; do
+    if ! sudo apt install -y "$i"; then
       FAILURE="$i $FAILURE"
     fi
   done
   sudo yarn global add prettier --prefix /usr/local
-  echo Failures: $FAILURE
+  echo "Failures: $FAILURE"
 elif [ "$OS" = "Ubuntu" ]; then
-  FAILURES=""
-  for i in $(echo $UBUNTU_PKGS); do
-    sudo apt install -y "$i"
-    if [ 0 -ne $? ]; then
+  FAILURE=""
+  for i in $UBUNTU_PKGS; do
+    if ! sudo apt install -y "$i"; then
       FAILURE="$i $FAILURE"
     fi
   done
   echo Failures: "$FAILURE"
 elif [ "$OS" = "openSUSE Tumbleweed" ]; then
-  FAILURES=""
-  for i in $(echo $SUSE_PKGS); do
+  FAILURE=""
+  for i in $SUSE_PKGS; do
     if ! sudo zypper install -y "$i"; then
       FAILURE="$i $FAILURE"
     fi
   done
   echo Failures: "$FAILURE"
 elif [ "$OS" = "void" ]; then
-  FAILURES=""
-  for i in $(echo $VOID_PKGS); do
+  FAILURE=""
+  for i in $VOID_PKGS; do
     if ! sudo xbps-install -y "$i"; then
       FAILURE="$i $FAILURE"
     fi
   done
   echo Failures: "$FAILURE"
 elif [ "$OS" = "Solus" ]; then
-  FAILURES=""
-  for i in $(echo $SOLUS_PKGS); do
+  FAILURE=""
+  for i in $SOLUS_PKGS; do
     if ! sudo eopkg install -y "$i"; then
       FAILURE="$i $FAILURE"
     fi
   done
   echo Failures: "$FAILURE"
 elif [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ]; then
-  FAILURES=""
-  for i in $(echo $ARCHLINUX_PKGS); do
-    sudo pacman --noconfirm --needed -S $i
-    if [ 0 -ne $? ]; then
+  FAILURE=""
+  for i in $ARCHLINUX_PKGS; do
+    if ! sudo pacman --noconfirm --needed -S "$i"; then
       FAILURE="$i $FAILURE"
     fi
   done
   echo Failures: "$FAILURE"
 elif [ "$OS" = "Gentoo" ]; then
-  FAILURES=""
-  for i in $(echo $GENTOO_PKGS); do
-    echo sudo emerge -uf $i
-    sudo emerge -uf $i
+  FAILURE=""
+  for i in $GENTOO_PKGS; do
+    echo "sudo emerge -uf $i"
+    sudo emerge -uf "$i"
   done
-  for i in $(echo $GENTOO_PKGS); do
-    sudo emerge --update --newuse $i
-    if [ 0 -ne $? ]; then
+  for i in $GENTOO_PKGS; do
+    if ! sudo emerge --update --newuse "$i"; then
       FAILURE="$i $FAILURE"
     fi
   done
-  echo Failures: $FAILURE
+  echo "Failures: $FAILURE"
 elif [ "$OS" = "FreeBSD" ]; then
-  for i in $(echo $FREEBSD_PKGS); do
-    sudo pkg install -y $i
-    if [ 0 -ne $? ]; then
+  for i in $FREEBSD_PKGS; do
+    if ! sudo pkg install -y "$i"; then
       FAILURE="$i $FAILURE"
     fi
   done
-  echo Failures: $FAILURE
+  echo "Failures: $FAILURE"
 elif [ "$OS" = "Raspbian GNU/Linux" ]; then
-  for i in $(echo $RASPI_PKGS); do
-    sudo apt install -y $i
-    if [ 0 -ne $? ]; then
+  for i in $RASPI_PKGS; do
+    if ! sudo apt install -y "$i"; then
       FAILURE="$i $FAILURE"
     fi
   done
-  echo Failures: $FAILURE
+  echo "Failures: $FAILURE"
 elif [ "$OS" = "Fedora" ]; then
   sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
   sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
   sudo dnf install dnf-plugins-core
   sudo dnf copr enable flatcap/neomutt
   sudo dnf update -y
-  FAILURES=""
-  for i in $(echo $FEDORA_PKGS); do
-    sudo dnf install -y $i
-    if [ 0 -ne $? ]; then
+  FAILURE=""
+  for i in $FEDORA_PKGS; do
+    if ! sudo dnf install -y "$i"; then
       FAILURE="$i $FAILURE"
     fi
   done
-  echo Failures: $FAILURE
+  echo "Failures: $FAILURE"
 elif [ "$OS" = "Darwin" ]; then
-  for i in $(echo $MACOS_PKGS); do
-    brew install $i
-    if [ 0 -ne $? ]; then
+  for i in $MACOS_PKGS; do
+    if ! brew install "$i"; then
       FAILURE="$i $FAILURE"
     fi
   done
-  echo Failures: $FAILURE
+  echo "Failures: $FAILURE"
   brew cask install alacritty
 elif [ "$OS" = "CentOS Linux" ]; then
   if [ "$OS_VER" = "8" ]; then
@@ -157,10 +148,9 @@ elif [ "$OS" = "CentOS Linux" ]; then
     sudo dnf update -y
     #sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
     sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-    FAILURES=""
-    for i in $(echo $CENTOS_PKGS); do
-      sudo dnf install -y $i
-      if [ 0 -ne $? ]; then
+    FAILURE=""
+    for i in $CENTOS_PKGS; do
+      if ! sudo dnf install -y "$i"; then
         FAILURE="$i $FAILURE"
       fi
     done
@@ -171,10 +161,9 @@ elif [ "$OS" = "CentOS Linux" ]; then
     sudo rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
     sudo cp -v neomutt-epel-7.repo /etc/yum.repos.d/
     sudo yum update -y
-    FAILURES=""
-    for i in $(echo $CENTOS_PKGS); do
-      sudo yum install -y $i
-      if [ 0 -ne $? ]; then
+    FAILURE=""
+    for i in $CENTOS_PKGS; do
+      if ! sudo yum install -y "$i"; then
         FAILURE="$i $FAILURE"
       fi
     done
