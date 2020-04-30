8930aa7633cbdf763b8bdcf1c3adbc74a83a628e
Brian Henning <henninb@msn.com>
2020-04-30 08:51:01 -0500

    dry install updates

--- dry-install.sh
+++ dry-install.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# curl -sSf https://moncho.github.io/dry/dryup.sh | sudo sh
-# sudo chmod 755 /usr/local/bin/dry
-
 if [ ! -x "$(command -v go)" ]; then
   echo go not installed.
   exit 1
@@ -10,7 +7,10 @@ else
   # sudo apt install -y golang
   brew install golang
 fi
+curl -sSf https://moncho.github.io/dry/dryup.sh | sudo sh
+sudo chmod 755 /usr/local/bin/dry
 
-go get github.com/moncho/dry
+
+# go get github.com/moncho/dry
 
 exit 0
