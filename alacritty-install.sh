17426998e4926fa3472fe72c6476bb4072e19bcd
Brian Henning <henninb@msn.com>
2020-05-09 06:20:12 -0500

    alacritty updates

--- alacritty-install.sh
+++ alacritty-install.sh
@@ -3,6 +3,10 @@
 sudo apt install -y libxcb-shape0-dev
 sudo apt install -y libxcb-xfixes0-dev
 
+sudo swupd bundle-add devpkg-freetype
+sudo swupd bundle-add devpkg-libxml2
+sudo swupd bundle-add devpkg-expat
+
 cd projects || exit
 rm -rf alacritty
 git clone git@github.com:alacritty/alacritty.git
