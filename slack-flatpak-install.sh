ef86aec52843d97db81f7d1666a8ea46f72dbe4e
Brian Henning <henninb@msn.com>
2020-05-11 06:02:32 -0500

    updates to flatpak

--- slack-flatpak-install.sh
+++ slack-flatpak-install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sudo flatpak remote-add flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.slack.Slack
 echo flatpak run com.slack.Slack
 
--- telegram-flatpak-install.sh
+++ telegram-flatpak-install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sudo  flatpak remote-add flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.telegram.desktop
 echo flatpak install --user flathub org.telegram.desktop
 echo flatpak run org.telegram.desktop
