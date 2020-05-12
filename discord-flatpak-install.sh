10ff03335d7be2bac12a84a7167ca9799de197cf
Brian Henning <henninb@msn.com>
2020-05-12 06:52:43 -0500

    polkit and discord updates

--- discord-flatpak-install.sh
+++ discord-flatpak-install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+sudo flatpak remote-add flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.discordapp.Discord
+#flatpak install -y --user flathub com.discordapp.Discord
 echo flatpak run com.discordapp.Discord
 
 exit 0
