ce24dc02af4cada2c59f6e19b045402b3f2fa2c7
Brian Henning <henninb@msn.com>
2020-05-15 06:01:27 -0500

    updates

---
+++ flatpak/stellarium-flatpak-install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub org.stellarium.Stellarium
+echo flatpak run org.stellarium.Stellarium
+
+exit 0
