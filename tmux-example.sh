f197e6a50a8a6d2239912a45c2219215654e8e8e
Brian Henning <henninb@msn.com>
2020-05-26 17:41:03 -0500

    updates

---
+++ tmux-example.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+tmux new -s foo
+tmux split-window -t foo.1
+tmux split-window -t foo.1
+tmux split-window -t foo.1
+tmux split-window -t foo.1
+tmux select-layout -t foo main-vertical
+tmux send -t foo.2 "ssh pi" ENTER
+tmux send -t foo.3 "ssh pi" ENTER
+tmux send -t foo.4 "ssh pi" ENTER
+tmux set-window synchronize-pane on
+
+exit 0
