36acd07b3df4bbea2f4d06c898c1842d89d44f28
Brian Henning <henninb@msn.com>
2020-06-12 07:40:38 -0500

    updates

--- unmounted-list.sh
+++ unmounted-list.sh
@@ -19,11 +19,14 @@ lsblk --noheadings --raw -o NAME,MOUNTPOINT | awk '$1~/[[:digit:]]/ && $2 == ""'
 echo udisksctl mount -b /dev/sda1
 echo udisksctl mount -b /dev/sdb1
 echo udisksctl mount -b /dev/sdc1
+echo udisksctl mount -b /dev/md126p1
 echo sshfs pi:/home/pi/downloads downloads
 
 echo sudo ntfsfix /dev/sda1
 echo sudo mkfs.ext4 /dev/sda1
 echo sudo mkfs.vfat -F 32 /dev/sdb1
 echo sudo gdisk -l /dev/sda
+echo sudo ntfslabel /dev/md126p1 Data
+echo sudo e2label /dev/sda1 Data
 
 exit 0
