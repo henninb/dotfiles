46007383e7e534a2d9902c64437174acedc5af69
Brian Henning <henninb@msn.com>
2020-06-20 10:00:20 -0500

    updates

--- bluetooth-connect.sh
+++ bluetooth-connect.sh
@@ -2,8 +2,11 @@
 
 address=""
 
-sudo bluetoothctl trust "$address"
-sudo bluetoothctl disconnect "$address"
-sudo bluetoothctl connect "$address"
+touch bluetooth.txt
+for address in $(cat bluetooth.txt);do
+  sudo bluetoothctl trust "$address"
+  sudo bluetoothctl disconnect "$address"
+  sudo bluetoothctl connect "$address"
+done
 
 exit 0
