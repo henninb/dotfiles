cf08bab0f287c5d7a0223ad7e29acb493a89f3fc
Brian Henning <henninb@msn.com>
2020-07-20 19:24:12 -0500

    mint upgrade

--- mint-upgrade.sh
+++ mint-upgrade.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
 if [ "$OS" = "Linux Mint" ]; then
-  #echo "upgrade from 19.1 to 19.2"
-  echo "upgrade from 19.2 to 19.3"
-  echo Tricia
-  echo $VERSION
+  echo "upgrade from 19.3 Tricia to 20.0 Ulyana"
+  echo "$VERSION"
   cat /etc/lsb-release
   #sudo sed -i 's/tessa/tina/g' /etc/apt/sources.list.d/official-package-repositories.list
-  echo switch to tricia 19.3
-  sudo sed -i 's/tina/tricia/g' /etc/apt/sources.list.d/official-package-repositories.list
+  sudo apt install mintupgrade -y
+  sudo sed -i 's/tricia/ulyana/g' /etc/apt/sources.list.d/official-package-repositories.list
   sudo apt update -y
   sudo apt dist-upgrade -y
   cat /etc/lsb-release
 else
-  echo $OS is not yet implemented.
+  echo "$OS is not yet implemented."
   exit 1
 fi
 
