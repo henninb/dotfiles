8d8587abdea9af65e79ae7c5f9704b3b47b2105a
Brian Henning <henninb@msn.com>
2020-07-23 06:14:09 -0500

    updates git

--- git-clone-projects.sh
+++ git-clone-projects.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PROJECTS="raspi-finance-endpoint raspi-finance-ui raspi-finance-database raspi-finance raspi-finance-client raspi-finance-convert nfl-database src-common example-java example-scala example-kotlin raspi-finance-react raspi-finance-endpoint-micronaut raspi-finance-haskell"
+PROJECTS="raspi-finance-endpoint raspi-finance-ui raspi-finance-database raspi-finance raspi-finance-client raspi-finance-convert nfl-database src-common example-java example-scala example-kotlin raspi-finance-react raspi-finance-endpoint-micronaut raspi-finance-haskell raspi-finance-cmdline"
 for i in $PROJECTS; do
   cd "$HOME/projects" || exit
   git clone "git@github.com:BitExplorer/$i.git"
