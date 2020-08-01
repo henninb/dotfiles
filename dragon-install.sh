1afe4086bfde4805d4c29bdcb6117bdbb33d2bdf
Brian Henning <henninb@msn.com>
2020-08-01 18:26:16 -0500

    dragon update

--- dragon-install.sh
+++ dragon-install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 sudo zypper install -y gtk3-devel
+sudo apt install -y libgtk2.0-dev
+sudo apt install -y libgtk-3-dev
 
 cd "$HOME/projects" || exit
 git clone git@github.com:mwh/dragon.git
