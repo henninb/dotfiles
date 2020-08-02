4e04db6ba881c7bb53a830905ce02499a84275e7
Brian Henning <henninb@msn.com>
2020-08-02 18:16:11 -0500

    activemq server updates

--- docker/activemq-server/run.sh
+++ docker/activemq-server/run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 mkdir -p $HOME/activemq_data
-sudo docker image build -t activemq .
-sudo docker stop activemq
-sudo docker rm activemq -f
+sudo docker image build -t activemq-server .
+sudo docker stop activemq-server
+sudo docker rm activemq-server -f
 #sudo sudo docker images
-sudo docker run --name=activemq -h activemq --network=bridge -h activemq --restart unless-stopped -p 8161:8161 -p 61616:61616 -p 61613:61613 -v $HOME/activemq_data:/opt/activemq/data -d activemq
-sudo docker exec -it --user root activemq /bin/sh
+sudo docker run --name=activemq-server -h activemq-server --network=bridge -h activemq-server --restart unless-stopped -p 8161:8161 -p 61616:61616 -p 61613:61613 -v $HOME/activemq_data:/opt/activemq/data -d activemq-server
+sudo docker exec -it --user root activemq-server /bin/sh
 
 exit 0
