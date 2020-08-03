7d38e42f2672e3ca3cd3c10f97384ed0986e3b1d
Brian Henning <henninb@msn.com>
2020-08-03 07:10:55 -0500

    docker kafka fix

--- docker/kafka-server/run.sh
+++ docker/kafka-server/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-sudo docker image build -t kafkabox .
-docker stop kafkabox
-docker rm kafkabox -f
-sudo docker run --name=kafkabox -h kafkabox --restart unless-stopped -d kafkabox
-docker exec -it --user root kafkabox /bin/sh
+sudo docker image build -t kafka-server .
+docker stop kafka-server
+docker rm kafka-server -f
+sudo docker run --name=kafka-server -h kafka-server --restart unless-stopped -d kafka-server
+docker exec -it --user root kafka-server /bin/bash
 
 exit 0
