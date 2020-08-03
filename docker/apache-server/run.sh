a13621cbd862d9759bdb2892fe763c95989a6ae3
Brian Henning <henninb@msn.com>
2020-08-03 06:55:18 -0500

    docker apache fix

--- docker/apache-server/run.sh
+++ docker/apache-server/run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-sudo docker build -t apache2box .
-sudo docker stop apache2box
-sudo docker rm apache2box -f
+sudo docker build -t apache-server .
+sudo docker stop apache-server
+sudo docker rm apache-server -f
 #sudo sudo docker images
-sudo docker run -dit --name apache2box -h apache2box -p 8080:80 apache2box
-sudo docker exec -it --user root apache2box /bin/bash
+#sudo docker run -dit --name apache-server -h apache-server -p 8080:80 apache-server
+sudo docker run --name=apache-server -h apache-server -h apache-server --restart unless-stopped -p 8080:80 -d apache-server
+echo running server on port 8080
+sudo docker exec -it --user root apache-server /bin/bash
 
 exit 0
