4c0d5fc2d12c001c688067bd60d00f318f7cc65a
Brian Henning <henninb@msn.com>
2020-09-05 14:07:54 -0500

    updates

--- alacritty-install.sh
+++ alacritty-install.sh
@@ -15,7 +15,7 @@ rm -rf alacritty
 git clone git@github.com:alacritty/alacritty.git
 cd alacritty || exit
 git pull origin master
-git checkout v0.4.2
+git checkout v0.5.0
 cargo build --release
 #mv -v target/release/alacritty "$HOME/.local/bin"
 sudo mv -v target/release/alacritty /usr/local/bin
