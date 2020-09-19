a571eccc70d69e08ab75936908e198b7bb1aad4d
Brian Henning <henninb@msn.com>
2020-09-19 11:44:52 -0500

    updates

--- locale-settings.sh
+++ locale-settings.sh
@@ -4,19 +4,31 @@ cat > locale.gen << EOF
 en_US.UTF-8 UTF-8
 EOF
 
-sudo mv -v locale.gen /etc/locale.gen¬
-sudo locale-gen
-echo sudo eselect locale set 4
-sudo eselect locale list
+if [ "$OS" = "Gentoo" ]; then
+  sudo mv -v locale.gen /etc/locale.gen¬
+  sudo locale-gen
+  echo sudo eselect locale set 4
+  sudo eselect locale list
+elif [ "$OS" = "void" ]; then
+  sudo xbps-reconfigure -f glibc-locales
+  sudo xbps-reconfigure -f glibc-locales
+  locale -a
+else
+  echo "OS is not configured."
+fi
 
-sudo xbps-reconfigure -f glibc-locales
-sudo xbps-reconfigure -f glibc-locales
-locale -a
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_NUMERIC=en_US.UTF-8
+export LC_TIME=en_US.UTF-8
+export LC_COLLATE=C
+export LC_MONETARY=en_US.UTF-8
+export LC_MESSAGES=en_US.UTF-8
+export LC_PAPER=en_US.UTF-8
+export LC_NAME=en_US.UTF-8
+export LC_ADDRESS=en_US.UTF-8
+export LC_TELEPHONE=en_US.UTF-8
+export LC_MEASUREMENT=en_US.UTF-8
+export LC_IDENTIFICATION=en_US.UTF-8
 
 exit 0
-
-
-!!! Error: Action 4 unknown
-exiting
-Setting LANG to en_US.utf8 ...
-Run ". /etc/profile" to update the variable in your shell.
