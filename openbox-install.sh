a84102297fc5b4a20c442cbaa95d262feefda44b
Brian Henning <henninb@msn.com>
2020-09-23 06:42:38 -0500

    updates

--- openbox-install.sh
+++ openbox-install.sh
@@ -3,5 +3,6 @@
 sudo pacman --noconfirm --needed -S openbox
 sudo pacman --noconfirm --needed -S tint2
 sudo pacman --noconfirm --needed -S obconf
+sudo pacman --noconfirm --needed -S menumaker
 
 exit 0
