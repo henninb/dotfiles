bb27aa01c653e4c73e1b974590758c6c377be3b2
Brian Henning <henninb@msn.com>
2020-09-24 14:06:23 -0500

    updates

--- spectrwm-install.sh
+++ spectrwm-install.sh
@@ -3,6 +3,9 @@
 sudo apt install libxcb-xinput-dev
 sudo apt install libxcb-xtest0-dev
 sudo apt install libx11-xcb-dev
+sudo apt install trayer
+sudo apt install copyq
+sudo apt install volumeicon-alsa
 
 cd "$HOME/projects" || exit
 git clone git@github.com:conformal/spectrwm.git
