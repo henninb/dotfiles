4db491098ddd00e4e17438c1b72e489ce39cad54
Brian Henning <henninb@msn.com>
2020-09-24 11:45:44 -0500

    spectrwm updates

--- spectrwm-install.sh
+++ spectrwm-install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
- sudo apt install libxcb-xinput-dev
- sudo apt install libxcb-xtest0-dev
+sudo apt install libxcb-xinput-dev
+sudo apt install libxcb-xtest0-dev
+sudo apt install lxlock
 
 cd "$HOME/projects" || exit
 git clone git@github.com:conformal/spectrwm.git
@@ -9,8 +10,5 @@ cd spectrwm || exit
 cd linux || exit
 make
 make install
-
-
-
 
 exit 0
