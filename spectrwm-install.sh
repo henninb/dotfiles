a36d0c31ef33c7ce3fdd6520807d7335f5ca20b5
Brian Henning <henninb@msn.com>
2020-09-27 13:30:57 -0500

    updates

--- spectrwm-install.sh
+++ spectrwm-install.sh
@@ -7,6 +7,10 @@ sudo apt install -y libxcb-util-dev
 sudo apt install -y libxcb-keysyms1-dev
 sudo apt install -y libxt-dev
 
+# sudo eopkg install -y xcb-util-keysyms
+sudo eopkg install -y xcb-util-keysyms-devel
+sudo eopkg install -y libxt-devel
+
 #sudo apt install -y trayer
 sudo apt install -y copyq
 sudo apt install -y volumeicon-alsa
