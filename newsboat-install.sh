87b8eec691ff6a4a918fa109214090dcb70e61c3
Brian Henning <henninb@msn.com>
2020-09-27 14:50:36 -0500

    updates

--- newsboat-install.sh
+++ newsboat-install.sh
@@ -6,4 +6,22 @@ sudo apt install -y mpv
 sudo apt install -y elinks
 sudo apt install -y task-spooler
 
+sudo eopkg install -y sqlite3-devel
+sudo eopkg install -y stfl-devel
+sudo eopkg install -y json-c-devel
+# sudo eopkg install -y asciidoc
+cd "$HOME/projects" || exit
+git clone git@github.com:xenogenesi/task-spooler.git
+cd task-spooler
+./configure
+autoreconf -f -i
+make
+
+
+cd "$HOME/projects" || exit
+git clone git@github.com:newsboat/newsboat.git
+cd newsboat || exit
+make
+sudo make install
+
 exit 0
