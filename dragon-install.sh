3ca8cd5e0d27a080a4b0b5ac93f2b56a697135f9
Brian Henning <henninb@msn.com>
2020-09-30 15:47:19 -0500

    updates

--- dragon-install.sh
+++ dragon-install.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-sudo zypper install -y gtk3-devel
-sudo apt install -y libgtk2.0-dev
-sudo apt install -y libgtk-3-dev
+if [ "$OS" = "Solus" ]; then
+  sudo eopkg install -y libgtk-2-devel
+  sudo eopkg install -y libgtk-3-devel
+elif [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Raspbian GNU/Linux" ]; then
+  sudo apt install -y libgtk2.0-dev
+  sudo apt install -y libgtk-3-dev
+elif [ "$OS" = "openSUSE Tumbleweed" ]; then
+  sudo zypper install -y gtk3-devel
+elif [ "$OS" = "Gentoo" ]; then
+  sudo emerge --update --newuse x11-libs/gtk+:2
+  sudo emerge --update --newuse x11-libs/gtk+:3
+else
+  echo "$OS is not implemented."
+fi
 
 cd "$HOME/projects" || exit
 git clone git@github.com:mwh/dragon.git
--- gtk-status.sh
+++ gtk-status.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
+if [ "$OS" = "Solus" ]; then
+  sudo eopkg install -y libgtk-2-devel
+  sudo eopkg install -y libgtk-3-devel
+elif [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Raspbian GNU/Linux" ]; then
+  sudo apt install -y libgtk2.0-dev
+  sudo apt install -y libgtk-3-dev
+elif [ "$OS" = "openSUSE Tumbleweed" ]; then
+  sudo zypper install -y gtk3-devel
+elif [ "$OS" = "Gentoo" ]; then
+  sudo emerge --update --newuse x11-libs/gtk+:2
+  sudo emerge --update --newuse x11-libs/gtk+:3
+else
+  echo "$OS is not implemented."
+fi
+
 pkg-config --exists gtk+-3.0 && echo "gtk+ 3.0 is installed" || echo "gtk+ 3.0 is not installed"
 pkg-config --exists gtk+-2.0 && echo "gtk+ 2.0 is installed" || echo "gtk+ 2.0 is not installed"
-
-# echo sudo apt install -y gtk2.0
-# echo sudo apt install -y gtk+3.0
-echo sudo apt install -y libgtk2.0-dev
-echo sudo apt install -y libgtk-3-dev
-echo sudo eopkg install -y libgtk-2-devel
-echo sudo eopkg install -y libgtk-3-devel
-
-echo sudo emerge --update --newuse x11-libs/gtk+:2
-echo sudo emerge --update --newuse x11-libs/gtk+:3
 
 exit 0
