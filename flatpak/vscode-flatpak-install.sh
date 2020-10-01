555897908c4655a124c7336dfe2cf8559f896131
Brian Henning <henninb@msn.com>
2020-10-01 12:59:58 -0500

    flatpak updates

---
+++ flatpak/vscode-flatpak-install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+cat > vscode-flatpak <<EOF
+#!/bin/sh
+
+flatpak run com.visualstudio.code
+
+exit 0
+EOF
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+if ! flatpak install -y flathub com.visualstudio.code; then
+  flatpak install flathub com.visualstudio.code
+fi
+chmod 755 vscode-flatpak
+mv -v vscode-flatpak "$HOME/.local/bin"
+
+exit 0
