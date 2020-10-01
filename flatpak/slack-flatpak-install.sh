cf5126fe5c69bfacad25a4496e4f3dee5bc316c5
Brian Henning <henninb@msn.com>
2020-09-30 20:16:16 -0500

    updates

--- flatpak/slack-flatpak-install.sh
+++ flatpak/slack-flatpak-install.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
+cat > slack-flatpak <<EOF
+#!/bin/sh
+
+flatpak run com.slack.Slack
+
+exit 0
+EOF
+
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+#sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak install flathub com.slack.Slack
-echo flatpak run com.slack.Slack
+chmod 755 slack-flatpak
+mv slack-flatpak "$HOME/.local/bin/"
 
 exit 0
