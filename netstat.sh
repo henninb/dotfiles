e01c0aaeb83f46aba8b11693e26b5af4df2ef651
Brian Henning <henninb@msn.com>
2020-10-01 18:10:36 -0500

    updates

--- netstat.sh
+++ netstat.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-sudo netstat -tulp
-netstat -na | grep tcp | grep LIST
-sockstat -4 -l
+if [ -x "$(command -v netstat)" ]; then
+  sudo netstat -tulp
+  netstat -na | grep tcp | grep LIST
+fi
+
+if [ -x "$(command -v socketstat)" ]; then
+  sockstat -4 -l
+fi
+ss -tulpn4
 
 exit 0
