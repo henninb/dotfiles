ad4c27cd6ca08a4fe7b948515821ad316faedf7e
Brian Henning <henninb@msn.com>
2020-10-03 07:52:46 -0500

    updates

--- mosquitto-install.sh
+++ mosquitto-install.sh
@@ -5,4 +5,7 @@ sudo pacman -S mosquitto
 sudo systemctl enable mosquitto
 sudo systemctl start mosquitto
 
+echo mosquitto_sub -h localhost -t test
+echo mosquitto_pub -h localhost -t test -m "hello world"
+
 exit 0
