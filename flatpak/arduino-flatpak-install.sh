4abe6e761fa149cc8b5f4ca67e578449d19a1ef4
Brian Henning <henninb@msn.com>
2020-10-03 08:11:44 -0500

    updates

---
+++ flatpak/arduino-flatpak-install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cat > arduino-flatpak <<EOF
+#!/bin/sh
+
+flatpak run cc.arduino.arduinoide
+
+exit 0
+EOF
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+if ! flatpak --user -y install flathub cc.arduino.arduinoide; then
+  flatpak -y install flathub cc.arduino.arduinoide
+fi
+
+chmod 755 arduino-flatpak
+mv arduino-flatpak "$HOME/.local/bin/"
+
+exit 0
