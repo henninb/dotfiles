0b5a5525dd11d97fe596fd91b23634155922e922
Brian Henning <henninb@msn.com>
2020-10-05 06:50:03 -0500

    updates

---
+++ online.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+while true; do
+  if ssh matthew uptime >/dev/null 2>&1; then
+    notify-send "Machine" "The machine is online!"
+    echo "the machine is online"
+  fi
+  sleep 15
+done
+
+exit 0
