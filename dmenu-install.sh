d50c0739a87033b85891a7f72ab4b964d8a42778
Brian Henning <henninb@msn.com>
2020-10-06 19:07:46 -0500

    updates

---
+++ dmenu-install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd "$HOME/projects" || exit
+git clone git@gitlab.com:dwt1/dmenu-distrotube.git
+cd dmenu-distrotube || exit
+make
+sudo mv -v dmenu dmenu_run /usr/local/bin/
+cd "$HOME" || exit
+
+exit 0
