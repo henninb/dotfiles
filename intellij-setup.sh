d28a5ef9cdea9aa615f60bfdf17717d0bd582b86
Brian Henning <henninb@msn.com>
2020-10-14 07:19:01 -0500

    updates

--- intellij-setup.sh
+++ intellij-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-find ~/.IntelliJIdea* -type d -exec touch -t "$(date +'%Y%m%d%H%M')" {} \;
-rm -rf "$HOME/.IntelliJIdea*/config/eval"
-rm -rf "$HOME/.IntelliJIdea*/config/options/other.xml"
+find ~/.config/JetBrains/IntelliJIdea2020.2 -type d -exec touch -t "$(date +'%Y%m%d%H%M')" {} \;
+rm -rf "$HOME/.config/JetBrains/IntelliJIdea2020.2/eval"
+rm -rf "$HOME/.config/JetBrains/IntelliJIdea2020.2/options/other.xml"
 rm -rf ~/.java/.userPrefs/jetbrains
 
 exit 0
