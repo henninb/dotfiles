95d9edc6fbcc6f7bbcf4299c7b16e1764e1fb5bb
Brian Henning <henninb@msn.com>
2020-11-01 08:00:47 -0600

    gtk updates

--- ant-dracula-theme-install.sh
+++ ant-dracula-theme-install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir "$HOME/.themes"
 if [ ! -f "dracula-gtk-theme.zip" ]; then
   wget https://github.com/dracula/gtk/archive/master.zip -O dracula-gtk-theme.zip
 fi
@@ -8,11 +9,23 @@ if [ ! -f "ant-dracula-gtk-theme.zip" ]; then
   wget https://github.com/EliverLara/Ant-Dracula/archive/master.zip -O ant-dracula-gtk-theme.zip
 fi
 
-rm -rf ant-dracula-theme
-rm -rf 
-unzip ant-dracula-gtk-theme.zip
-unzip dracula-gtk-theme.zip
-sudo mv ant-dracula-theme /usr/share/themes/Ant-Dracula
+rm -rf ant-dracula-theme ant-dracula-gtk-theme
+rm -rf gtk-master dracula-gtk-theme
+
+unzip -o ant-dracula-gtk-theme.zip
+mv -v gtk-master ant-dracula-gtk-theme
+unzip -o dracula-gtk-theme.zip
+mv -v gtk-master dracula-gtk-theme
+
+if [ ! -d "$HOME/.themes/dracula-gtk-theme" ]; then
+mv -v dracula-gtk-theme "$HOME/.themes"
+fi
+
+if [ ! -d "$HOME/.themes/ant-dracula-gtk-theme" ]; then
+mv -v ant-dracula-gtk-theme "$HOME/.themes"
+fi
+
+# sudo mv ant-dracula-theme /usr/share/themes/Ant-Dracula
 # gsettings set org.gnome.desktop.interface gtk-theme "Ant-Dracula"
 # gsettings set org.gnome.desktop.wm.preferences theme "Ant-Dracula"
 
