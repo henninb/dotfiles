476362dc52e4febe5fba331bc4fcb708fdd6b134
Brian Henning <henninb@msn.com>
2020-11-12 14:13:49 -0600

    updates

--- lpass-update.sh
+++ lpass-update.sh
@@ -9,13 +9,13 @@ for i in $(cat f.txt); do
   dir=$(echo "$i" | awk -F '|' '{print $1}')
   if [ -z "$dir" ]; then
     #echo "$dir | $uid"2>&1 | tee -a lpass-update-$$.log
-    echo
+    echo > /dev/null
   else
     echo "$dir | $uid"2>&1 | tee -a lpass-update-$$.log
     lpass mv "$uid" "$dir"
   fi
-  echo "$new_name | $uid"2>&1 | tee -a lpass-update-$$.log
-  echo "$new_name" | lpass edit --sync=now --non-interactive --name "$uid" 2>&1 | tee -a lpass-update-$$.log
+  #echo "$new_name | $uid"2>&1 | tee -a lpass-update-$$.log
+  #echo "$new_name" | lpass edit --sync=now --non-interactive --name "$uid" 2>&1 | tee -a lpass-update-$$.log
 done
 
 exit 0
