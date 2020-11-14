ac7c125948d3fbdde622443aae8a1892d9bee1d6
Brian Henning <henninb@msn.com>
2020-11-14 08:22:33 -0600

    updates

--- intellij-setup.sh
+++ intellij-setup.sh
@@ -5,4 +5,7 @@ rm -rf "$HOME/.config/JetBrains/IntelliJIdea2020.2/eval"
 rm -rf "$HOME/.config/JetBrains/IntelliJIdea2020.2/options/other.xml"
 rm -rf ~/.java/.userPrefs/jetbrains
 
+ls -l ~/.java/.userPrefs/
+ls -l ~/.config/JetBrains/IntelliJIdea2020.2/
+
 exit 0
