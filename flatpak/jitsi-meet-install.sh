eceb19ad8d47d86e4637e610a8551793cb5eedc3
Brian Henning <henninb@msn.com>
2020-11-21 16:06:17 -0600

    updates

---
+++ flatpak/jitsi-meet-install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+cat > jitsi-flatpak <<EOF
+#!/bin/sh
+
+flatpak run org.jitsi.jitsi-meet
+
+exit 0
+EOF
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak --user -y install flathub org.jitsi.jitsi-meet
+
+chmod 755 jitsi-flatpak
+mv jitsi-flatpak "$HOME/.local/bin/"
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub org.jitsi.jitsi-meet
+echo flatpak run org.jitsi.jitsi-meet
+
+exit 0
