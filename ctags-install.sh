987d207c9dc1e5e94f6c0b07f2568bec4b3ff605
Brian Henning <henninb@msn.com>
2020-12-03 06:25:49 -0600

    updates

---
+++ ctags-install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+cd "$HOME/projects" || exit
+
+git clone git@github.com:enechaev/packcc.git
+cd packcc || exit
+gcc -o packcc packcc.c
+mv packcc "$HOME/.local/bin"
+
+cd "$HOME/projects" || exit
+git clone git@github.com:universal-ctags/ctags.git
+cd ctags || exit
+./autogen.sh
+./configure
+make
+
+exit 0
