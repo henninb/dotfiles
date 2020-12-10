efad12432bc790c94d33a045dab4c40444753cad
Brian Henning <henninb@msn.com>
2020-12-09 18:56:10 -0600

    updates

--- docker/oracle-server/run.sh
+++ docker/oracle-server/run.sh
@@ -4,7 +4,7 @@ mkdir -p ~/oracle-data
 docker image build -t oracle-database-server .
 docker stop oracle-database-server
 docker rm oracle-database-server -f
-docker run --name=oracle-database-server --shm-size=1g -p 1521:1521 -v ~/oracle-data:/u01/app/oracle/oradata -h oracle-database-server -d oracle-database-server
+docker run --name=oracle-database-server --rm --shm-size=1g -p 1521:1521 -v ~/oracle-data:/u01/app/oracle/oradata -h oracle-database-server -d oracle-database-server
 echo sqlplus SYSTEM/monday1@localhost
 echo sqlplus SYSTEM/monday1@localhost:1521
 echo sqlplus SYSTEM/monday1@192.168.100.217:1521/XE
