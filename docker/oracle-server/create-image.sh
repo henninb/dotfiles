06e7bca47491f50d08db6f14e7d5949e8ecdae9e
Brian Henning <henninb@msn.com>
2020-12-09 18:16:45 -0600

    updates

--- docker/oracle-server/create-image.sh
+++ docker/oracle-server/create-image.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-RASPI_IP=$(nmap -sP --host-timeout 10 192.168.100.0/24 | grep raspb | grep -o '[0-9.]\+[0-9]')
-echo $RASPI_IP
+# RASPI_IP=$(nmap -sP --host-timeout 10 192.168.100.0/24 | grep raspb | grep -o '[0-9.]\+[0-9]')
+# echo $RASPI_IP
 
-cd $HOME/projects
+cd "$HOME/projects" || exit
 git clone git@github.com:oracle/docker-images.git oracle-server-docker
-cd $HOME/projects/oracle-server-docker/OracleDatabase/SingleInstance/dockerfiles/
+cd oracle-server-docker || exit
+git pull
+cd "$HOME/projects/oracle-server-docker/OracleDatabase/SingleInstance/dockerfiles/"
 if [ ! -f "11.2.0.2/oracle-xe-11.2.0-1.0.x86_64.rpm.zip" ]; then
   scp pi@$RASPI_IP:/home/pi/downloads/oracle-xe-11.2.0-1.0.x86_64.rpm.zip 11.2.0.2/
 fi
--- docker/oracle-server/run.sh
+++ docker/oracle-server/run.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-mkdir -p ~/data/oradata
-sudo docker image build -t oracle-database-server .
-sudo docker stop oracle-database-server
-sudo docker rm oracle-database-server -f
-#sudo docker run --name=oracle-database-server -h oracle-database-server --restart unless-stopped -d oracle-database-server
-sudo docker run --name=oracle-database-server --shm-size=1g -p 1521:1521 -v ~/data/oradata:/u01/app/oracle/oradata -h oracle-database-server -d oracle-database-server
+mkdir -p ~/oracle-data
+docker image build -t oracle-database-server .
+docker stop oracle-database-server
+docker rm oracle-database-server -f
+docker run --name=oracle-database-server --shm-size=1g -p 1521:1521 -v ~/oracle-data:/u01/app/oracle/oradata -h oracle-database-server -d oracle-database-server
 echo sqlplus SYSTEM/monday1@localhost
 echo sqlplus SYSTEM/monday1@localhost:1521
 echo sqlplus SYSTEM/monday1@192.168.100.217:1521/XE
