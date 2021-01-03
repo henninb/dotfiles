5c608a9c6f4cae5fc6f76ad9b7a101ec9269079a
Brian Henning <henninb@msn.com>
2021-01-03 11:22:04 -0600

    fix to address node vim coc

--- node-fix.sh
+++ node-fix.sh
@@ -1,1 +1,12 @@
-ps -eo pid,etime,comm  | grep node grep -v grep
+ps -eo pid,etime,comm  | grep node |grep -v grep
+
+echo ':echo fnamemodify(tempname(), ':p:h')'
+echo lsof  | grep 'coc-nvim'
+
+nvm install 14.15.3
+
+ps -ef| grep vim
+
+ps -ef| grep git-diff-wrapper
+pgrep -f git-diff-wrapper
+
