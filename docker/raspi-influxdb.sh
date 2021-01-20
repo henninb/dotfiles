ccae6b0422b3da2f473b374e529cba5cf632ae11
Brian Henning <henninb@msn.com>
2021-01-20 17:44:59 -0600

    updates

---
+++ docker/raspi-influxdb.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cat > influxdb.env <<EOF
+INFLUXDB_ADMIN_USER=henninb
+INFLUXDB_ADMIN_PASSWORD=monday1
+INFLUXDB_DB=metrics
+INFLUXDB_HTTP_ENABLED=true
+INFLUXDB_HTTP_AUTH_ENABLED=true
+EOF
+
+mkdir -p "$HOME/influxdb-data"
+export CURRENT_UID="$(id -u)"
+export CURRENT_GID="$(id -g)"
+
+
+docker run --name influxdb-server -d --restart unless-stopped -p 8086:8086 --env-file influxdb.env --user "$CURRENT_UID:$CURRENT_GID" -v "$HOME/influxdb-data:/var/lib/influxdb" influxdb:1.8.3
+
+exit 0
