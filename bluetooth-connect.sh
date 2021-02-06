211a5a6ae554bc9e50fa90098c613fbeb71d3aa0
Brian Henning <henninb@msn.com>
2021-02-06 16:08:17 -0600

    updates

--- bluetooth-connect.sh
+++ bluetooth-connect.sh
@@ -4,7 +4,7 @@ address=""
 
 for address in $(cat /opt/bluetooth.txt);do
   status=$(bluetoothctl info "$address" | grep Connected | grep -v grep)
-  echo "status=$status"
+  echo "status=$status for $address"
   if echo "$status" | grep -q "no"; then
     bluetoothctl remove "$address" >/dev/null 2>&1
     bluetoothctl disconnect "$address" >/dev/null 2>&1
