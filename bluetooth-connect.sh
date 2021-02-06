49e20ff1ec8efc9a245ed6a2a4639e8927aa5448
Brian Henning <henninb@msn.com>
2021-02-06 14:29:03 -0600

    updates

--- bluetooth-connect.sh
+++ bluetooth-connect.sh
@@ -4,9 +4,14 @@ address=""
 
 touch bluetooth.txt
 for address in $(cat bluetooth.txt);do
-  bluetoothctl trust "$address"
-  bluetoothctl disconnect "$address"
-  bluetoothctl connect "$address"
+  status=$(bluetoothctl info "$address" | grep Connected | grep -v grep)
+  echo "status=$status"
+  if echo "$status" | grep -q "no"; then
+    echo
+    bluetoothctl disconnect "$address" >/dev/null 2>&1
+    bluetoothctl trust "$address" >/dev/null 2>&1
+    bluetoothctl connect "$address"
+  fi
 done
 
 exit 0
