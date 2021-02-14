bedddfca16c5ff39ee1061407c82051eada45b78
Brian Henning <henninb@msn.com>
2021-02-14 12:30:30 -0600

    updates

--- matthew-shutdown.sh
+++ matthew-shutdown.sh
@@ -3,6 +3,7 @@
              num="$(shuf -i1-1000 -n1)"
              echo "$num"
              sleep "$num"
+             sudo su - matthew -c ./notify 'are you there?'
              ssh matthew "sudo shutdown -h now"
              echo 'restarted'
            done
