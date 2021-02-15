a5c77e61bada1f09fd461eded274e76b1b09feeb
Brian Henning <henninb@msn.com>
2021-02-15 16:08:44 -0600

    newsboat updates

--- newsboat-install.sh
+++ newsboat-install.sh
@@ -1,27 +1,45 @@
 #!/bin/sh
 
-sudo apt install -y newsboat
-sudo apt install -y cmus
-sudo apt install -y mpv
-sudo apt install -y elinks
-sudo apt install -y task-spooler
 
-sudo eopkg install -y sqlite3-devel
-sudo eopkg install -y stfl-devel
-sudo eopkg install -y json-c-devel
+if [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLinux" ]; then
+  sudo pacman --noconfirm --needed -S stfl
+  sudo pacman --noconfirm --needed -S elinks
+  sudo pacman --noconfirm --needed -S cmus
+  sudo pacman --noconfirm --needed -S asciidoctor
+elif [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Raspbian GNU/Linux" ]; then
+  sudo apt install -y newsboat
+  sudo apt install -y cmus
+  sudo apt install -y mpv
+  sudo apt install -y elinks
+  sudo apt install -y task-spooler
+elif [ "$OS" = "Solus" ]; then
+  sudo eopkg install -y sqlite3-devel
+  sudo eopkg install -y stfl-devel
+  sudo eopkg install -y json-c-devel
 # sudo eopkg install -y asciidoc
+else
+  echo "OS=$OS not setup yet."
+  exit 1
+fi
+
 cd "$HOME/projects" || exit
 git clone git@github.com:xenogenesi/task-spooler.git
-cd task-spooler
+cd task-spooler || exit
 ./configure
 autoreconf -f -i
-make
-
+if ! make; then
+  echo "failed to build task-spooler"
+  exit 1
+fi
 
 cd "$HOME/projects" || exit
 git clone git@github.com:newsboat/newsboat.git
 cd newsboat || exit
 make
+if ! make; then
+  echo "failed to build newsboat"
+  exit 1
+fi
 sudo make install
 
 exit 0
