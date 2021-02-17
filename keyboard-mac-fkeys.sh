16c17d4522078afae7f7de8f11e3036154efa03c
Brian Henning <henninb@msn.com>
2021-02-17 17:42:36 -0600

    updates

--- keyboard-mac-fkeys.sh
+++ keyboard-mac-fkeys.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode
+echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode
+
+if ! grep -q "hid_apple" < /etc/modprobe.d/hid_apple.conf; then
+  echo "options hid_apple fnmode=2" | sudo tee -a /etc/modprobe.d/hid_apple.conf
+fi
 
 sudo sed -i 's/XKBOPTIONS=""/XKBOPTIONS="caps:escape"/' /etc/default/keyboard
 
