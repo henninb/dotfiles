4974de06673683cbc60c442dec9b353edb0103aa
Brian Henning <henninb@msn.com>
2021-02-21 06:52:48 -0600

    xmonad mouse changes

--- .xmonad/xmonad-default.hs
+++ .xmonad/xmonad-default.hs
@@ -282,6 +282,18 @@ myWorkspaces = [ws1, ws2, ws3, ws4, ws5, ws6, ws7, ws8, ws9]
 isTerminal :: Window -> X Bool
 isTerminal = fmap (== "Alacritty") . runQuery className
 
+myMouseBindings XConfig {XMonad.modMask = modm} = M.fromList
+    [
+--     -- mod-button1, Set the window to floating mode and move by dragging
+     ((modm, button1),
+   \ w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster),
+--     -- mod-button2, Raise the window to the top of the stack
+     ((modm, button2), \ w -> focus w >> windows W.shiftMaster),
+--     -- mod-button3, Set the window to floating mode and resize by dragging
+      ((modm, button3),
+   \ w -> focus w >> mouseResizeWindow w >> windows W.shiftMaster)
+    ]
+
 myKeys :: [(String, X ())]
 myKeys = [
     ("M-S-e"             , spawn "emacs")
