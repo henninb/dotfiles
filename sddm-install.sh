7fd771af31bc99ae54add8cb2da7b5cd2f1fa437
Brian Henning <henninb@msn.com>
2021-02-26 06:56:12 -0600

    updates

--- sddm-install.sh
+++ sddm-install.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+cat > sddm-theme.conf <<EOF
+[Theme]
+#Current=maldives
+Current=elarun
+EOF
+
 sudo pacman -S sddm
 sudo systemctl enable sddm.service -f
-
 sudo systemctl disable lightdm
 
 sudo mkdir -p /etc/sddm.conf.d/
+sudo mv -v sddm-theme.conf /etc/sddm.conf.d/
 
 ls -l /usr/share/sddm/themes/
 
