ab416a7aa71f0846d4150bd9e0b14235fecafb2e
Brian Henning <henninb@msn.com>
2021-03-06 08:26:44 -0600

    updates

--- Arduino/ws2812b-neopixel-stm32f103c/ws2812b-neopixel-stm32f103c.ino
+++ Arduino/ws2812b-neopixel-stm32f103c/ws2812b-neopixel-stm32f103c.ino
@@ -1,29 +1,21 @@
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
-
-//ws2812b
 
 #define PIN 6
-
 #define NUMPIXELS 16
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 int delayval = 1000;
 
 void setup() {
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-#if defined (__AVR_ATtiny85__)
-  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-#endif
-  // End of trinket special code
 
-  pixels.begin(); // This initializes the NeoPixel library.
+    
+  pixels.begin();
+
+     //  you can change the brightness to lower if its too bright!
+  pixels.setBrightness(75);                // Set LED brightness 0-255
+  colorWipe(pixels.Color(255, 255, 255),0); // fill the strip with all white
+  pixels.show();   
 }
 
 void loop() {
