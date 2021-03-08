ca94de2dee312fab84404ad517656eb171ab7b06
Brian Henning <henninb@msn.com>
2021-03-08 15:22:07 -0600

    updates

--- Arduino/nrf24l01-radio-receiver-arduino/build.sh
+++ Arduino/nrf24l01-radio-receiver-arduino/build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # arduino-cli compile --fqbn arduino:avr:uno ~/Arduino/nrf24l01-radio-transmitter-arduino
-arduino-cli compile --fqbn arduino:avr:uno .
+if arduino-cli compile --fqbn arduino:avr:uno .; then
+  echo "build failed."
+  exit 1
+fi
 arduino-cli upload --port /dev/ttyUSB1 --fqbn arduino:avr:uno .
 
 exit 0
