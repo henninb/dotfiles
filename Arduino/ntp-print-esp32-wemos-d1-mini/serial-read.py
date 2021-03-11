1d4ca5867cfac815865c3cd0b21a5b26ad0f2de7
Brian Henning <henninb@msn.com>
2021-03-11 05:23:10 -0600

    updates

--- Arduino/ntp-print-esp32-wemos-d1-mini/serial-read.py
+++ Arduino/ntp-print-esp32-wemos-d1-mini/serial-read.py
@@ -7,7 +7,7 @@ import serial
 
 ser = serial.Serial(
   port='/dev/ttyUSB0',
-  baudrate = 9600,
+  baudrate = 115200,
   parity=serial.PARITY_NONE,
   stopbits=serial.STOPBITS_ONE,
   bytesize=serial.EIGHTBITS,
