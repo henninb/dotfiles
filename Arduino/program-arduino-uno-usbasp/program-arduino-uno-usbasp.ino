bd05cc18a93fff31472aaba7e79af5ad5d9a6f96
Brian Henning <henninb@msn.com>
2021-03-12 16:07:58 -0600

    updates

--- Arduino/program-arduino-uno-usbasp/program-arduino-uno-usbasp.ino
+++ Arduino/program-arduino-uno-usbasp/program-arduino-uno-usbasp.ino
@@ -11,5 +11,5 @@ void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(5000);                       // wait for a second
+  delay(500);                       // wait for a second
 }
--- Arduino/program-arduino-uno-usbasp/readme.md
+++ Arduino/program-arduino-uno-usbasp/readme.md
@@ -12,8 +12,10 @@ sudo cp 99-USBasp.rules /etc/udev/rules.d/
 reboot
 
 ## flash firmware of another programmer
-avrdude -c usbasp -p m8 -U flash:w:usbasp.atmega48.2009-02-28.hex:i
+avrdude -c usbasp -p m8 -B 1 -U flash:w:usbasp.atmega8.2011-05-28.hex
 
 ## programmer connected
 avrdude -p m8 -c usbasp -v
+
+
 
