164c2bc4b184462ee9ecce9f99adbc069913472c
Brian Henning <henninb@msn.com>
2021-03-12 13:55:29 -0600

    updates

--- Arduino/blink-esp12/blink-esp12.ino
+++ Arduino/blink-esp12/blink-esp12.ino
@@ -1,3 +1,15 @@
+/*
+  ESP12    |  FTDI
+  VCC      | 3.3V
+  GND      | GND
+  TX       | RX
+  RX       | TX
+
+  CHPD  - 3.3V
+  GPIO15 - GND
+  GPIO0  - GND
+*/
+
 void setup() {
   Serial.begin(9600);
   while (!Serial);
