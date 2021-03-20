4fdac4f95844d7a526639ba3e25d868ce8ba4fea
Brian Henning <henninb@msn.com>
2021-03-20 10:53:27 -0500

    updates

--- Arduino/example-stm32f103-esp01/example-stm32f103-esp01.ino
+++ Arduino/example-stm32f103-esp01/example-stm32f103-esp01.ino
@@ -1,7 +1,21 @@
 //Interfacing ESP8266 Wi-Fi with STM32F103C8
 //NOTE: Serial2 (TX2, RX2)is connected with ESP8266(RX,TX)respectively with baud rate (9600)
+/*
+FTDI | stm32f103
+RX   | TX1 (PC9)
+TX   | RX1 (PC10)
+GND  | GND
+3.3V | 3.3V
 
-/* int incomingByte = 0; */
+stm32f103 | ESP01
+GND       | GND
+3.3V      | 3.3V
+3.3V      | CH-PD
+PC3 (RX2) | TX
+PC2 (TX2) | RX
+
+*/
+
 char incomingByte = 0;
 char output[100] = {0};
 bool flag = true;
@@ -30,8 +44,6 @@ void loop() {
       output[idx] = incomingByte;
       idx++;
     }
-    /* delay(10); */
   } else {
-    /* delay(1000); */
   }
 }
