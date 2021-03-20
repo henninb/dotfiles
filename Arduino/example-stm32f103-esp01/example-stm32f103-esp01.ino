3b9eac9d3498e61332abe24654802d2c62461350
Brian Henning <henninb@msn.com>
2021-03-20 10:18:23 -0500

    updates

--- Arduino/example-stm32f103-esp01/example-stm32f103-esp01.ino
+++ Arduino/example-stm32f103-esp01/example-stm32f103-esp01.ino
@@ -1,7 +1,11 @@
 //Interfacing ESP8266 Wi-Fi with STM32F103C8
 //NOTE: Serial2 (TX2, RX2)is connected with ESP8266(RX,TX)respectively with baud rate (9600)
 
-int incomingByte = 0;
+/* int incomingByte = 0; */
+char incomingByte = 0;
+char output[100] = {0};
+bool flag = true;
+int idx = 0;
 
 void setup()
 {
@@ -9,20 +13,23 @@ void setup()
    while (!Serial);
    Serial2.begin(9600);   //begins serial communication with esp8266 with baud rate 9600 (Change according to your esp8266 module)
    while (!Serial2);
-   Serial.println("System Ready..");
+   Serial.println("serial port setup completed...");
 }
 
 void loop() {
-   while(Serial2.available()) {
-     // send data only when you receive data:
-  if (Serial.available() > 0) {
-    // read the incoming byte:
-    incomingByte = Serial.read();
-
-    // say what you got:
-    Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
+  if (Serial2.available() > 0) {
+    incomingByte = (char) Serial2.read();
+    if( incomingByte == '\n' ) {
+      idx = 0;
+      Serial.print("message: ");
+      Serial.println(output);
+      memset(output, '\0', sizeof(output));
+    } else {
+      output[idx] = incomingByte;
+      idx++;
+    }
+    delay(100);
+  } else {
+    delay(1000);
   }
-  delay(100);
- }
 }
