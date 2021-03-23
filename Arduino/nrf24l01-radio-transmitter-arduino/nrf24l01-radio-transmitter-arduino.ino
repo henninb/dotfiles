d9bb03b6de847a68696b633bd600a5edd43fc6b0
Brian Henning <henninb@msn.com>
2021-03-23 08:13:17 -0500

    updates

--- Arduino/nrf24l01-radio-transmitter-arduino/nrf24l01-radio-transmitter-arduino.ino
+++ Arduino/nrf24l01-radio-transmitter-arduino/nrf24l01-radio-transmitter-arduino.ino
@@ -8,24 +8,31 @@
   SCK        |   Pin13         | Pin52         |     A5 SCK1   (PA5) 3.3v
   MISO       |   Pin11 (MOSI)  | Pin50         |     A7 MISI1  (PA7) 3.3v
   MOSI       |   Pin12 (MOS0)  | Pin51         |     A6 MOSO1  (PA6) 3.3v
+
+
+ DHT22 data  pin to arduino pin2
+ DHT22 VCC to 5V on Arduino
+ DHT22 GND to GND on Arduino
  */
 
 #include <SPI.h>
 #include <RF24.h>
+#include <DHT.h>
 
-struct tempDataType {
-    byte ID;                // 1 byte
-    signed int temperature; // 2 bytes, -32,768 to 32,767, same as short
-    unsigned maxTemp;       // 2 bytes, 0 to 65,535
-    double humidity;        // 4 bytes 32-bit floating point (Due=8 bytes, 64-bit)
-    float dewPoint;         // 4 bytes 32-bit floating point, same as double
-    // Total 13, you can have max 32 bytes here
+struct WeatherType {
+    short temperature;           // 2 bytes
+    short temperature_decimal;   // 2 bytes
+    short humidity;              // 2 bytes
+    short humidity_decimal;      // 2 bytes
+    byte id;                     // 1 byte
+    // Total 9, you can have max 32 bytes here
 };
 
 // instantiate an object for the nRF24L01 transceiver
 RF24 radio(7, 8); // using pin 7 for the CE pin, and pin 8 for the CSN pin
+DHT dht(2, DHT22);
 
-tempDataType myDataTx;
+WeatherType myDataTx;
 
 const uint64_t writePipe = 0xE6E6E6E6E6E6;
 const uint64_t readPipe = 0xB3B4B5B601;
@@ -33,10 +40,8 @@ const uint64_t readPipe = 0xB3B4B5B601;
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
-  while (!Serial) {
-    // some boards need to wait to ensure access to serial over USB
-  }
-  Serial.println("RF24 example transmitter.");
+  while (!Serial);
+  Serial.println("RF24 transmitter setup.");
 
   /* radio.setChannel(104); */
 
@@ -47,6 +52,7 @@ void setup() {
   // this feature for all nodes (TX & RX) to use ACK payloads.
   //radio.enableAckPayload();
   /* radio.printDetails(); */
+  dht.begin();
 
   // initialize the transceiver on the SPI bus
   if (!radio.begin()) {
@@ -61,52 +67,64 @@ void setup() {
   radio.setPALevel(RF24_PA_LOW);     // RF24_PA_MAX is default.
   radio.openReadingPipe(1, readPipe);
   radio.openWritingPipe(writePipe); // Get NRF24L01 ready to transmit
+  Serial.println("RF24 transmitter setup complete.");
 }
 
 void loop() {
-    radio.stopListening();
-    myDataTx.ID = 'B';
-    myDataTx.temperature = 72;
-    myDataTx.maxTemp = 93;
-    myDataTx.humidity = 50.37;
-    myDataTx.dewPoint = 69.4;
+    float temperature = dht.readTemperature(true);
+    float humidity = dht.readHumidity();
+
+    if (isnan(humidity) || isnan(temperature)) {
+      Serial.println("Failed to read from DHT sensor!");
+      /* delay(2000); */
+      temperature = 0.0;
+      humidity = 0.0;
+      /* return; */
+    }
+    Serial.print("Actual Temperature: ");
+    Serial.println(temperature);
+    short leftSideOfDecimalPoint = (short) temperature;
+    short rightSideOfDecimalPoint = (temperature - leftSideOfDecimalPoint) * 1000;
+    digitalWrite(LED_BUILTIN, LOW);
+    myDataTx.id = 'C';
+    myDataTx.temperature = (short) temperature;
+    myDataTx.temperature_decimal = (temperature - myDataTx.temperature) * 1000;
+    myDataTx.humidity = (short) humidity;
+    myDataTx.humidity_decimal = (humidity - myDataTx.humidity) * 1000;
    if( !radio.write(&myDataTx, sizeof(myDataTx)) ) {
+     /* radio.printDetails(); */
      Serial.println("radio write failed, the receiver is not online or responding.");
+     Serial.println("radio write failed, transmitter may not have enough power.");
+     Serial.print("  PALevel (1 == RF24_PA_LOW): ");
+     Serial.println(radio.getPALevel());
+     Serial.print("  DataRate: ");
+     Serial.println(radio.getDataRate());
+     Serial.print("  Channel: ");
+     Serial.println(radio.getChannel());
+     delay(500);
+     return;
+     /* radio.print_status(radio.get_status()); */
+     /* Serial.print("  Status: "); */
+     /* Serial.println(radio.get_status()); */
    } else {
      Serial.println("Transmitting data below......");
+     /* radio.printDetails(); */
+     /* radio.print_status(radio.get_status()); */
      Serial.print("  PALevel (1 == RF24_PA_LOW): ");
      Serial.println(radio.getPALevel());
      Serial.print("  Channel: ");
      Serial.println(radio.getChannel());
-     Serial.print("  ID         : ");
-     Serial.println(myDataTx.ID);
-     Serial.print("  Temperature: ");
+     Serial.print("  id: ");
+     Serial.println(myDataTx.id);
+     Serial.print("  Temperature Whole: ");
      Serial.println(myDataTx.temperature);
-     Serial.print("  Max Temp.  : ");
-     Serial.println(myDataTx.maxTemp);
-     Serial.print("  Humidity   : ");
+     Serial.print("  Temperature Decimal: ");
+     Serial.println(myDataTx.temperature_decimal);
+     Serial.print("  Humidity Whole: ");
      Serial.println(myDataTx.humidity);
-     Serial.print("  Dew Point  : ");
-     Serial.println(myDataTx.dewPoint);
+     Serial.print("  Humidity Decimal: ");
+     Serial.println(myDataTx.humidity_decimal);
    }
-  /* delay(1000); */
-  digitalWrite(LED_BUILTIN, (millis() / 1000) % 2);
-
-      // Now listen for a response
-    radio.startListening();
-
-    // But we won't listen for long
-    unsigned long started_waiting_at = millis();
-
-    // Loop here until we get indication that some data is ready for us to read (or we time out)
-    while (!radio.available()) {
-        // Oh dear, no response received within our timescale
-        if (millis() - started_waiting_at > 250) {
-            Serial.print("  TX: Got no reply ");
-            delay(2000);
-            return;
-        }
-    }
-    radio.read(&myDataTx, sizeof(myDataTx));
-    delay(250);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(10000);
 }
