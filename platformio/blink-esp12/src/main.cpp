268bcc0187dd2e4318f2b68bbacf2e978f9ecaf2
Brian Henning <henninb@msn.com>
2021-03-25 16:34:17 -0500

    updates

---
+++ platformio/blink-esp12/src/main.cpp
@@ -0,0 +1,44 @@
+#include <ESP8266WiFi.h>
+/*
+  Operational Mode
+  ESP12    | FTDI
+  VCC      | 3.3V
+  GND      | GND
+  TX       | RX
+  RX       | TX
+  GPIO0    | DTR
+
+  CHPD  - 3.3V
+  1000uF capacitor (between VCC and GND), shorter leg GND
+  Optional RST - to button for resetting
+*/
+
+/*
+  Programming Mode
+  ESP12    | FTDI
+  VCC      | 3.3V
+  GND      | GND
+  TX       | RX
+  RX       | TX
+  RST      |RTS
+  GPIO0    |DTR
+
+  CHPD  - 3.3V
+  1000uF capacitor (between VCC and GND), shorter leg GND
+*/
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  pinMode(LED_BUILTIN, OUTPUT);
+  WiFi.mode(WIFI_STA);
+}
+
+void loop() {
+  Serial.println("Hello from ESP12");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  Serial.println(millis() / 1000);
+}
---
+++ platformio/blink-esp32-wemos-d1-mini/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  pinMode(BUILTIN_LED, OUTPUT);
+}
+
+void loop() {
+  Serial.println("Hello from ESP32");
+  digitalWrite(BUILTIN_LED, HIGH);
+  delay(1000);
+  digitalWrite(BUILTIN_LED, LOW);
+  delay(1000);
+  Serial.println(millis() / 1000);
+}
