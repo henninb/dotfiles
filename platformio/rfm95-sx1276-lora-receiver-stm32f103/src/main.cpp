2c28186f09205ef6cbc3560cd4b31c97415dc11c
Brian Henning <henninb@msn.com>
2021-03-26 13:56:27 -0500

    updates

--- platformio/rfm95-sx1276-lora-receiver-stm32f103/src/main.cpp
+++ platformio/rfm95-sx1276-lora-receiver-stm32f103/src/main.cpp
@@ -1,116 +1,52 @@
-// Arduino9x_RX
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messaging client (receiver)
-// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
-// reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example Arduino9x_TX
-
 #include <SPI.h>
-#include <RH_RF95.h>
+#include <LoRa.h>
 
 /*
+ *
+ *   RF95    | Arduino Uno | stm32f103
+  ---------------------------------
+  GND     | ??          | ??
+  3.3V    | VCC         | 3.3
+  DIO0    | 2           | B0 digital (PB0)
+  NSS     | 10 (SS)     | A4 NSS1 (PA4) 3.3v
+  SCK     | 13 (SCK)    | A5 SCK1   (PA5) 3.3v
+  MISI    | 11 (MOSI)   | A7 MISI1  (PA7) 3.3v
+  MISO    | 12 (MOS0)   | A6 MOSO1  (PA6) 3.3v
+  */
 
- RF95    | Arduino Uno | stm32f103
- ---------------------------------
- GND     | ??          | ??
- 3.3V    | VCC         | 3.3
- DIO0    | 2           | B0 digital (PB0)
- NSS     | 10 (SS)     | A4 NSS1 (PA4) 3.3v
- SCK     | 13 (SCK)    | A5 SCK1   (PA5) 3.3v
- MISI    | 11 (MOSI)   | A7 MISI1  (PA7) 3.3v
- MISO    | 12 (MOS0)   | A6 MOSO1  (PA6) 3.3v
-
- */
-
-#define RFM95_CS PA4
-#define RFM95_RST 9
-#define RFM95_INT PB0
-
-// Change to 434.0 or other frequency, must match RX's freq!
-#define RF95_FREQ 915.0
-
-// Singleton instance of the radio driver
-RH_RF95 rf95(RFM95_CS, RFM95_INT);
-
-// Blinky on receipt
-#define LED PC13
-
-int consecutiveLoopCount = 0;
 
 void setup() {
-  pinMode(LED, OUTPUT);
-  pinMode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, HIGH);
-
-  while (!Serial);
   Serial.begin(9600);
-  delay(100);
+  while (!Serial);
 
-  Serial.println("LoRa RX");
+  Serial.println("LoRa Receiver");
+    // override the default CS, reset, and IRQ pins (optional)
+  LoRa.setPins(PA4, PB1, PB0);// set CS, reset, IRQ pin
+  /* LoRa.setPins(10, 3, 2); // set CS, reset, IRQ pin¬ */
+  /* LoRa.setPins(ss, reset, dio0); */
 
-  // manual reset
-  digitalWrite(RFM95_RST, LOW);
-  delay(10);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
-
-  while (!rf95.init()) {
-    Serial.println("LoRa radio init failed");
+  if (!LoRa.begin(915E6)) {
+    Serial.println("setup LoRa failed!");
     while (1);
   }
-  Serial.println("LoRa radio init OK!");
-
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
-    while (1);
-  }
-  Serial.print("Set Freq to: ");
-  Serial.println(RF95_FREQ);
-
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
-  // you can set transmitter powers from 5 to 23 dBm:
-  rf95.setTxPower(23, false);
-  Serial.println("setup complete");
+  Serial.println("LoRa Receiver setup complete.");
 }
 
 void loop() {
-  while (rf95.available()) {
-    consecutiveLoopCount = 0;
-    // Should be a message for us now
-    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-    uint8_t len = sizeof(buf);
+  /* Serial.println("LoRa Receiver will start receiving."); */
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
 
-    if (rf95.recv(buf, &len)) {
-      digitalWrite(LED, HIGH);
-      RH_RF95::printBuffer("Received: ", buf, len);
-      Serial.print("Received: ");
-      Serial.println((char*)buf);
-       Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);
-
-      // Send a reply
-      /* uint8_t data[] = "And hello back to you"; */
-      /* rf95.send(data, sizeof(data)); */
-      /* rf95.waitPacketSent(); */
-      /* Serial.println("Sent a reply"); */
-      /* digitalWrite(LED, LOW); */
-    } else {
-      Serial.println("Receive failed");
+    // read packet
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
     }
-  }
 
-  consecutiveLoopCount++;
-  if( consecutiveLoopCount > 100 ) {
-    Serial.println("INFO: Outer Loop count exceeded threshold of 1000.");
-    delay(500);
-    /* lcd.clear(); */
-    /* lcd.print("Transmitter not sending data."); */
-    /* scrollInFromRight(0, "No TX is sending data to this RX."); */
-    consecutiveLoopCount = 0;
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
   }
 }
