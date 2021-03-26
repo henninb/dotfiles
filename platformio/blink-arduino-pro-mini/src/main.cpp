bc93511d88f6c05c3fe5690947bc698d28ac8d4c
Brian Henning <henninb@msn.com>
2021-03-25 19:48:42 -0500

    updates

---
+++ platformio/blink-arduino-pro-mini/src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+#define DEBUG 1
+void setup() {
+  Serial.begin(9600);
+  while( !Serial);
+#ifdef DEBUG
+  Serial.println("setup started...");
+#endif
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  delay(1000);
+  #ifdef DEBUG
+  Serial.println("setup completed...");
+#endif
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  Serial.println("Hello from Arduino");
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(5000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(5000);                       // wait for a second
+}
