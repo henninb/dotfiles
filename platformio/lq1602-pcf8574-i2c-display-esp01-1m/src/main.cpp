f738daa4be4abacb1a627398f6620c9c5d84c927
Brian Henning <henninb@msn.com>
2021-03-28 16:15:09 -0500

    updates

---
+++ platformio/lq1602-pcf8574-i2c-display-esp01-1m/src/main.cpp
@@ -0,0 +1,68 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+/*
+ pcf8574 | esp01
+ ===============
+  SDA    | GPIO0
+  SCL    | GPIO2
+  5V     | 5V (external)
+  GND    | GND
+ */
+
+int isPrime( int );
+
+/* LiquidCrystal_I2C lcd(CF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE); //0x27 */
+LiquidCrystal_I2C lcd(PCF8574A_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE); //0x3f
+
+int idx = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("setup started...");
+  pinMode(LED_BUILTIN,OUTPUT);
+  lcd.begin(16,2);
+  delay(500);
+  /* lcd.noBacklight(); */
+  lcd.setCursor(0, 0);
+  lcd.clear();
+  delay(2000);
+  Serial.println("setup completed...");
+}
+
+void loop() {
+  idx++;
+  /* lcd.backlight(); */
+  String message = String(idx);
+  lcd.setCursor(0, 0);
+
+  if( isPrime(idx) == 1 ) {
+    message.concat(" is prime");
+    lcd.clear();
+    /* lcd.backlight(); */
+    lcd.print(message);
+    Serial.println(message);
+  } else {
+    message.concat(" NOT prime");
+    lcd.clear();
+    lcd.print(message);
+    Serial.println(message);
+  }
+  /* lcd.noBacklight(); */
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(2000);
+}
+
+int isPrime(int number) {
+    int i;
+    for (i=2; i<number; i++) {
+        if (number % i == 0 && i != number) {
+          return 0;
+        }
+    }
+    return 1;
+}
