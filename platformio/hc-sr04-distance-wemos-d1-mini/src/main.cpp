bf14e5c567bd5f4fb1382287d80bc2d5868be727
Brian Henning <henninb@msn.com>
2021-03-28 13:32:35 -0500

    updates

---
+++ platformio/hc-sr04-distance-wemos-d1-mini/src/main.cpp
@@ -0,0 +1,34 @@
+#include <NewPing.h>
+
+#define MAX_DISTANCE 400 // Maximum distance we want to ping for (in centimeters).
+                         //Maximum sensor distance is rated at 400-500cm.
+
+#define triggerPin D6
+#define echoPin D7
+
+NewPing sonar(triggerPin, echoPin, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+int distanceIn;
+int distanceCm;
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("UltraSonic Distance Measurement - setup completed.");
+}
+
+
+void loop() {
+  delay(1000);
+  distanceIn = sonar.ping_in();
+  Serial.print("Distance: ");
+  Serial.print(distanceIn);
+                            // (0 = outside set distance range, no ping echo)
+  Serial.println(" inches");
+
+  delay(100);
+  distanceCm = sonar.ping_cm();
+  Serial.print("Distance: ");
+  Serial.print(distanceCm);
+  Serial.println(" centimeters");
+}
