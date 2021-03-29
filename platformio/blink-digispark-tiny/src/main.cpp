0acebc4a5c292d5bcf006224fa1a3089b8b0206b
Brian Henning <henninb@msn.com>
2021-03-29 17:34:28 -0500

    updates

--- platformio/blink-digispark-tiny/src/main.cpp
+++ platformio/blink-digispark-tiny/src/main.cpp
@@ -1,26 +1,17 @@
 #include <Arduino.h>
 /* #include <SoftwareSerial.h> */
-#include <TinyWireM.h>
-#include <ATtinySerialOut.h>
+/* #include <TinyWireM.h> */
+/* #include <ATtinySerialOut.h> */
 // ***
 // *** Define the RX and TX pins. Choose any two
 // *** pins that are unused. Try to avoid D0 (pin 5)
 // *** and D2 (pin 7) if you plan to use I2C.
 // ***
-#define RX    3   // *** D3, Pin 2
-#define TX    4   // *** D4, Pin 3
+#define RX    3
+#define TX    4
 
 
-/* SoftwareSerial Serial(RX, TX); */
-
-/*
- FTDI    | attiny85
- 5V      | 5V
- GND     | GND
- RX      | TX (PB3)
- TX      | RX (PB4)
- DTR     | Reset (PB5)
- */
+/* SoftwareSerial mySerial(RX, TX); */
 
 #define DEBUG 1
 void setup() {
@@ -37,11 +28,10 @@ void setup() {
 #endif
 }
 
-// the loop function runs over and over again forever
 void loop() {
   Serial.println("Hello from attiny85");
-  digitalWrite(1, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(1, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(1, HIGH);
+  delay(1000);
+  digitalWrite(1, LOW);
+  delay(1000);
 }
