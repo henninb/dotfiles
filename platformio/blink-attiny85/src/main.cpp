a47006edae635493cdaca56c6afbebf8c651a474
Brian Henning <henninb@msn.com>
2021-03-29 11:19:32 -0500

    updates

--- platformio/blink-attiny85/src/main.cpp
+++ platformio/blink-attiny85/src/main.cpp
@@ -17,7 +17,7 @@ void setup() {
   Serial.println("setup started...");
 #endif
 
-  /* pinMode(LED_BUILTIN, OUTPUT); */
+  pinMode(0, OUTPUT);
   delay(1000);
   #ifdef DEBUG
   Serial.println("setup completed...");
@@ -27,8 +27,8 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   Serial.println("Hello from attiny85");
-  /* digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level) */
+  digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  /* digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW */
+  digitalWrite(0, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
