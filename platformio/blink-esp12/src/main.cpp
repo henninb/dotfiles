53f8b4332ca37472beab4a5e13990d182bf57f8d
Brian Henning <henninb@msn.com>
2021-04-02 09:07:24 -0500

    updates

--- platformio/blink-esp12/src/main.cpp
+++ platformio/blink-esp12/src/main.cpp
@@ -8,22 +8,21 @@
   RX       | TX
   GPIO0    | DTR
 
-  CHPD  - 3.3V
+  CHPD     | 3.3V
   1000uF capacitor (between VCC and GND), shorter leg GND
   Optional RST - to button for resetting
 */
 
 /*
-  Programming Mode
+  Programming Mode (7 wires)
   ESP12    | FTDI
   VCC      | 3.3V
   GND      | GND
   TX       | RX
   RX       | TX
-  RST      |RTS
-  GPIO0    |DTR
-
-  CHPD  - 3.3V
+  RST      | RTS
+  GPIO0    | DTR
+  CHPD     | 3.3V
   1000uF capacitor (between VCC and GND), shorter leg GND
 */
 
