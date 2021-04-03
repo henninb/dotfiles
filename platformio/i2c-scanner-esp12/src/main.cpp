ed087551f271adcd812666a064e778a38719677c
Brian Henning <henninb@msn.com>
2021-04-02 19:35:18 -0500

    updates

---
+++ platformio/i2c-scanner-esp12/src/main.cpp
@@ -0,0 +1,71 @@
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+/*
+  Operational Mode
+  ESP12    | FTDI
+  VCC      | 3.3V
+  GND      | GND
+  TX       | RX
+  RX       | TX
+  GPIO0    | DTR
+
+  CHPD     | 3.3V
+  1000uF capacitor (between VCC and GND), shorter leg GND
+  Optional RST - to button for resetting
+*/
+
+/*
+  Programming Mode (7 wires)
+  ESP12    | FTDI
+  VCC      | 3.3V
+  GND      | GND
+  TX       | RX
+  RX       | TX
+  RST      | RTS
+  GPIO0    | DTR
+  CHPD     | 3.3V
+  1000uF capacitor (between VCC and GND), shorter leg GND
+*/
+
+void setup() {
+  Wire.begin();
+
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("I2C Scanner setup completed.");
+}
+
+void loop() {
+  int nDevices = 0;
+
+  Serial.println("Scanning...");
+
+  for (byte address = 1; address < 127; ++address) {
+    Wire.beginTransmission(address);
+    byte error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16) {
+        Serial.print("0");
+      }
+      Serial.print(address, HEX);
+      Serial.println("");
+
+      ++nDevices;
+    } else if (error == 4) {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16) {
+        Serial.print("0");
+      }
+      Serial.println(address, HEX);
+    }
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found");
+  } else {
+    Serial.println("I2C scan completed.");
+  }
+  Serial.println("");
+  delay(5000); // Wait 5 seconds for next scan
+}
