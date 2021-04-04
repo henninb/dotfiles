bb453838f061437111496d988590a0b8032b5e12
Brian Henning <henninb@msn.com>
2021-04-04 07:10:30 -0500

    updates

--- platformio/blink-attiny85/src/main.cpp
+++ platformio/blink-attiny85/src/main.cpp
@@ -1,10 +1,5 @@
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 
-const int Rx = 3;
-const int Tx = 4;
-
-SoftwareSerial mySerial(Rx, Tx);
 /*
  Programming
 
@@ -23,32 +18,35 @@ note: 10uF cap, negative to ground and positive to reset on the arduino
 
 
 0.1uF ceramic cap between vcc and gnd of the tiny 85 It is required for proper operation.
+
+
+for serial monitor
+ FTDI | attiny85
+ ================
+ 5V   | VCC
+ GND  | GND
+ RX   | PB2
  */
 
 #define DEBUG 1
 void setup() {
   Serial.begin(9600);
   while( !Serial);
-  mySerial.begin(9600);
-  while( !mySerial);
 #ifdef DEBUG
-  mySerial.println("setup started...");
   Serial.println("setup started...");
 #endif
 
-  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
   delay(1000);
   #ifdef DEBUG
-  mySerial.println("setup completed...");
+  Serial.println("setup completed...");
 #endif
 }
 
-// the loop function runs over and over again forever
 void loop() {
   Serial.println("Hello from attiny85");
-  mySerial.println("Hello from attiny85");
-  digitalWrite(0, HIGH);
-  delay(250);
-  digitalWrite(0, LOW);
-  delay(250);
+  digitalWrite(1, HIGH);
+  delay(1000);
+  digitalWrite(1, LOW);
+  delay(1000);
 }
