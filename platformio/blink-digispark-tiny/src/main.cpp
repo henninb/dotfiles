63e95e7cc968e60070b5ffedf709be098463d5fa
Brian Henning <henninb@msn.com>
2021-04-04 07:10:46 -0500

    updates

--- platformio/blink-digispark-tiny/src/main.cpp
+++ platformio/blink-digispark-tiny/src/main.cpp
@@ -1,35 +1,31 @@
 #include <Arduino.h>
-#include <SoftSerial.h>
 
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
+/*
+for serial monitor an FTDI is required
 
-/* serial monitor not built in by default
- * requires an external monitor */
-/* #define DEBUG_TX_RX_PIN 2 */
-#define RX    3
-#define TX    4
-/* SoftSerial mySerial(DEBUG_TX_RX_PIN, DEBUG_TX_RX_PIN, true); */
-SoftSerial mySerial(RX, TX);
+FTDI | digispark
+================
+5V   | VIN
+GND  | GND
+RX   | PB2
+ */
 
 #define DEBUG 1
 void setup() {
-  mySerial.begin(9600);
-  /* while( !mySerial); */
+  Serial.begin(9600);
 #ifdef DEBUG
-  mySerial.println("setup started...");
+  Serial.println("setup started...");
 #endif
 
   pinMode(1, OUTPUT);
   delay(1000);
   #ifdef DEBUG
-  mySerial.println("setup completed...");
+  Serial.println("setup completed...");
 #endif
 }
 
 void loop() {
-  mySerial.println("Hello from attiny85 digispark");
+  Serial.println("Hello from attiny85 digispark");
   digitalWrite(1, HIGH);
   delay(1000);
   digitalWrite(1, LOW);
