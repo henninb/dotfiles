16beed3caf9627b816b7769ef0d58283705504a8
Brian Henning <henninb@msn.com>
2021-04-05 05:46:24 -0500

    updates

---
+++ platformio/serial-monitor-digispark-tiny/src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+
+/*
+for serial monitor an FTDI is required
+
+FTDI | digispark
+================
+5V   | VIN
+GND  | GND
+RX   | PB2
+ */
+
+#define DEBUG 1
+void setup() {
+  Serial.begin(9600);
+#ifdef DEBUG
+  Serial.println("setup started...");
+#endif
+
+  pinMode(1, OUTPUT);
+  delay(1000);
+  #ifdef DEBUG
+  Serial.println("setup completed...");
+#endif
+}
+
+void loop() {
+  Serial.println("Hello from attiny85 digispark");
+  digitalWrite(1, HIGH);
+  delay(1000);
+  digitalWrite(1, LOW);
+  delay(1000);
+}
