4111fe24d1f1dd0629f17af7cab0c453260bf48b
Brian Henning <henninb@msn.com>
2021-04-06 05:50:41 -0500

    updates

---
+++ platformio/deep-sleep-arduino-nano/src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include <LowPower.h>
+
+#define DEBUG 1
+void setup() {
+  Serial.begin(9600);
+  while( !Serial);
+#ifdef DEBUG
+  Serial.println("setup started...");
+#endif
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  delay(1000);
+  #ifdef DEBUG
+  Serial.println("setup completed...");
+#endif
+}
+
+void loop() {
+  Serial.println("Hello from Arduino Nano");
+  Serial.println("Arduino:- I am going for a Nap");
+  delay(1000);
+  digitalWrite(LED_BUILTIN,LOW);
+  for( unsigned int idx = 0; idx < 8; idx++ ) {
+    LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
+  }
+
+  Serial.println("Arduino:- Hey I just Woke up");
+  Serial.println("");
+  delay(2000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
