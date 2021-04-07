0e9ab0662e76aa840ef7511bddd976150948d516
Brian Henning <henninb@msn.com>
2021-04-07 17:04:38 -0500

    updates

--- platformio/ssd1306-oled-display-arduino-uno/src/main.cpp
+++ platformio/ssd1306-oled-display-arduino-uno/src/main.cpp
@@ -3,6 +3,14 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+/*
+oled | arduino uno
+SDA  | SDA
+SDL  | SDL
+VCC  | 5V
+GND  | GND
+ * */
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
