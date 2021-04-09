bd8ee8c7f4d5fbc7deaa3156d009432d4f125b97
Brian Henning <henninb@msn.com>
2021-04-09 07:04:26 -0500

    updates

---
+++ platformio/blink-serial-stm32f103/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+/* #define ledPin PC13 */
+
+void setup() {
+  Serial.begin(9600);
+  while( !Serial);
+  Serial.println("setup started...");
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("setup completed...");
+}
+
+void loop() {
+  Serial.println("Hello from stm32f103");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+}
--- platformio/blink-stm32f103/readme.md
+++ platformio/blink-stm32f103/readme.md
@@ -1,1 +1,3 @@
 https://www.youtube.com/watch?v=rfBeq-Fu0hc
+
+dfu method of upload - fails to work for me as of now.
