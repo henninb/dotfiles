7bd6b3f051f56a3e7029edf8b77e8671234c2d0b
Brian Henning <henninb@msn.com>
2021-04-09 06:17:56 -0500

    updates

--- platformio/blink-esp01/src/main.cpp
+++ platformio/blink-esp01/src/main.cpp
@@ -10,6 +10,7 @@
 
 const short int ledBuiltin = 2; //GPIO2, the LED_BUILTIN is incorrectly set to 1
 void connectToWifi();
+void flashSize();
 
 void setup() {
   Serial.begin(9600);
@@ -17,13 +18,14 @@ void setup() {
   Serial.println("setup");
   pinMode(ledBuiltin, OUTPUT);
 
-  connectToWifi();
+  /* connectToWifi(); */
   WiFi.mode(WIFI_STA); //We don't want the ESP to act as an AP
   Serial.println("setup complete");
 }
 
 void loop() {
-  Serial.println("Hello from ESP01");
+  Serial.println("Hello from ESP01-4m");
+  flashSize();
   digitalWrite(ledBuiltin, HIGH);
   delay(1000);
   digitalWrite(ledBuiltin, LOW);
@@ -40,4 +42,25 @@ void connectToWifi() {
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
+}
+
+void flashSize() {
+  uint32_t realSize = ESP.getFlashChipRealSize();
+  uint32_t ideSize = ESP.getFlashChipSize();
+  FlashMode_t ideMode = ESP.getFlashChipMode();
+
+  Serial.printf("Flash real id:   %08X\n", ESP.getFlashChipId());
+  Serial.printf("Flash real size: %u bytes\n", realSize);
+  Serial.printf("Flash ide  size: %u bytes\n", ideSize);
+  Serial.printf("Flash ide speed: %u Hz\n", ESP.getFlashChipSpeed());
+  Serial.printf("Flash ide mode:  %s\n", (ideMode == FM_QIO ? "QIO" : ideMode == FM_QOUT ? "QOUT" : ideMode == FM_DIO ? "DIO" : ideMode == FM_DOUT ? "DOUT" : "UNKNOWN"));
+
+  if (ideSize != realSize) {
+    Serial.println("Flash Chip configuration wrong.");
+  } else {
+    Serial.println("Flash Chip configuration ok.");
+  }
+  Serial.println("");
+
+  delay(5000);
 }
