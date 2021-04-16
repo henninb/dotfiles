6cae5ee5f5575a07bf8ec55b83415d1029091e98
Brian Henning <henninb@msn.com>
2021-04-16 09:41:08 -0500

    updates

--- platformio/sdcard-stm32f103/src/main.cpp
+++ platformio/sdcard-stm32f103/src/main.cpp
@@ -3,13 +3,16 @@
 #include "config.h"
 
 /*
+
 FTDI | stm32f103
+================
 RX   | TX1 (PC9)
 TX   | RX1 (PC10)
 GND  | GND
 3.3V | 3.3V
 
 sd card | stm32f103
+===================
 GND     | GND
 5V      | 5V
 CS      | PA4
@@ -19,15 +22,15 @@ MOSI    | PA7
 
 */
 
-/* const int chipSelect = PA4; */
-const int pinCS = PA4;
+const int pinChipSelect = PA4;
+String fileName = "file.json";
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
   Serial.println("setup...");
 
-  if (SD.begin(pinCS)) {
+  if (SD.begin(pinChipSelect)) {
     Serial.println("SD card is ready to use.");
   } else {
     Serial.println("SD card initialization failed");
@@ -36,42 +39,45 @@ void setup() {
     return;
   }
 
-  File myFile = SD.open("data.txt", FILE_WRITE);
-
-  if (myFile) {
-    Serial.println("Writing to file...");
-
-    StaticJsonDocument<100> jsonStructure;
-    jsonStructure["humidity"] = 1.1;
-    jsonStructure["temperature"] = 2.2;
-    String payload;
-    serializeJson(jsonStructure, payload);
-    Serial.print("Payload: ");
-    Serial.println(payload);
-    myFile.println(payload);
-
-    /* myFile.println("Testing text 1, 2 ,3..."); */
-    myFile.close();
-    Serial.println("Done.");
-  } else {
-    Serial.println("error opening file");
-  }
-  myFile = SD.open("gps-data.txt");
-  if (myFile) {
-    Serial.println("Read:");
-    // Reading the whole file
-    while (myFile.available()) {
-      Serial.write(myFile.read());
-    }
-    myFile.close();
-  } else {
-    Serial.println("error opening file.");
-  }
+  delay(2000);
   Serial.print("upload timestamp: ");
   Serial.println(uploadTimestamp);
   Serial.println("setup completed.");
 }
 
 void loop() {
+  String milli = String(millis()/1000);
+  Serial.printlin(milli);
+  delay(1000);
+
+  //write file
+  File fileHandler = SD.open(fileName, FILE_WRITE);
+  if (fileHandler) {
+    Serial.println("Writing to file...");
+
+    StaticJsonDocument<100> jsonStructure;
+    jsonStructure["millis"] = 1.1;
+    String payload;
+    serializeJson(jsonStructure, payload);
+    Serial.print("Payload: ");
+    Serial.println(payload);
+    fileHandler.println(payload);
+    fileHandler.close();
+    Serial.println("Done.");
+  } else {
+    Serial.println("error opening file");
+  }
+
+  // read file
+  fileHandler = SD.open(fileName);
+  if (fileHandler) {
+    Serial.println("File Read:");
+    while (fileHandler.available()) {
+      Serial.write(fileHandler.read());
+    }
+    fileHandler.close();
+  } else {
+    Serial.println("error opening file.");
+  }
   return;
 }
