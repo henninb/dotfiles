b6fcf151f3fb4e75d042bcff43094b62b7bcd026
Brian Henning <henninb@msn.com>
2021-04-22 16:15:57 -0500

    updates

--- xmonad-stack-install.sh
+++ xmonad-stack-install.sh
@@ -5,120 +5,135 @@ if [ ! -x "$(command -v go)" ]; then
   exit 1
 fi
 
-curl -sSL 'https://get.haskellstack.org' | sh
-stack update
+if [ "${OS}" = "FreeBSD" ]; then
+  sudo pkg install -y hs-stack
+  cd /lib && sudo ln -s libncurses.so.9 libncursesw.so.8 && sudo ldconfig -R
+  sudo pkg install -y misc/compat12x
+else
+  curl -sSL 'https://get.haskellstack.org' | sh
+  stack update
+fi
 
 if [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Raspbian GNU/Linux" ]; then
-  sudo apt install -y libxss-dev
-  sudo apt install -y xscreensaver
-  sudo apt install -y feh
-  sudo apt install -y w3m
-  sudo apt install -y lxpolkit
-  sudo apt install -y cmake
-  sudo apt install -y libxpm-dev
-  sudo apt install -y xdotool
-  sudo apt install -y xclip
-  sudo apt install -y redshift
-  sudo apt install -y xdo
-  sudo apt install -y sxhkd
-  sudo apt install -y volumeicon-alsa
+  sudo apt install -y blueberry
   sudo apt install -y clipmenu
-  sudo apt install -y lxappearance
-  sudo apt install -y dunst
-  sudo apt install -y playerctl
+  sudo apt install -y cmake
+  sudo apt install -y conky
   sudo apt install -y copyq
-  sudo apt install -y numlockx
+  sudo apt install -y dunst
+  sudo apt install -y feh
+  sudo apt install -y flameshot
+  sudo apt install -y font-awesome-4
+  sudo apt install -y libbsd-dev
+  sudo apt install -y libxpm-dev
+  sudo apt install -y libxss-dev
+  sudo apt install -y lxappearance
+  sudo apt install -y lxpolkit
   sudo apt install -y mpc
   sudo apt install -y mpd
-  sudo apt install -y vimb
-  sudo apt install -y vifm
-  sudo apt install -y flameshot
-  sudo apt install -y conky
   sudo apt install -y mpdris2
+  sudo apt install -y numlockx
+  sudo apt install -y playerctl
+  sudo apt install -y redshift
+  sudo apt install -y sxhkd
+  sudo apt install -y vifm
+  sudo apt install -y vimb
+  sudo apt install -y volumeicon-alsa
+  sudo apt install -y w3m
   sudo apt install -y wmname
-  sudo apt install -y blueberry
-  sudo apt install -y libbsd-dev
-  sudo apt install -y font-awesome-4
+  sudo apt install -y xclip
+  sudo apt install -y xdo
+  sudo apt install -y xdotool
+  sudo apt install -y xscreensaver
   #sudo apt install -y icu-devtools libicu-dev
 elif [ "$OS" = "openSUSE Tumbleweed" ]; then
-  sudo zypper install -y libxss-devel
-  sudo zypper install -y xscreensaver
-  sudo zypper install -y feh
-  sudo zypper install -y w3m
   # sudo zypper install -y dzen2
+  sudo zypper install -y alsa-lib-devel
+  sudo zypper install -y blueberry
+  sudo zypper install -y clipmenu
   sudo zypper install -y cmake
+  sudo zypper install -y conky
+  sudo zypper install -y copyq
+  sudo zypper install -y dunst
+  sudo zypper install -y feh
+  sudo zypper install -y flameshot
+  sudo zypper install -y gmp-devel
+  sudo zypper install -y libXss-devel
+  sudo zypper install -y libxft-devel
   sudo zypper install -y libxpm-devel
-  sudo zypper install -y xdotool
+  sudo zypper install -y libxrandr-devel
+  sudo zypper install -y libxss-devel
+  sudo zypper install -y lxappearance
+  sudo zypper install -y mpc
+  sudo zypper install -y mpd
+  sudo zypper install -y mpdris2
+  sudo zypper install -y playerctl
+  sudo zypper install -y redshift
+  sudo zypper install -y sxhkd
+  sudo zypper install -y vifm
+  sudo zypper install -y volumeicon
+  sudo zypper install -y w3m
+  sudo zypper install -y wmname
   sudo zypper install -y xclip
   sudo zypper install -y xdo
-  sudo zypper install -y sxhkd
-  sudo zypper install -y dunst
-  sudo zypper install -y playerctl
-  sudo zypper install -y mpc
-  sudo zypper install -y redshift
-  sudo zypper install -y mpd
-  sudo zypper install -y volumeicon
-  sudo zypper install -y clipmenu
-  sudo zypper install -y copyq
-  sudo zypper install -y conky
-  sudo zypper install -y vifm
-  sudo zypper install -y mpdris2
-  sudo zypper install -y flameshot
-  sudo zypper install -y wmname
-  sudo zypper install -y lxappearance
-  sudo zypper install -y blueberry
-  sudo zypper install -y gmp-devel
-  sudo zypper install -y alsa-lib-devel
-  sudo zypper install -y libxrandr-devel
-  sudo zypper install -y libxft-devel
-  sudo zypper install -y libXss-devel
+  sudo zypper install -y xdotool
+  sudo zypper install -y xscreensaver
 elif [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLinux" ]; then
-  sudo pacman --noconfirm --needed -S xscreensaver
-  sudo pacman --noconfirm --needed -S feh
-  sudo pacman --noconfirm --needed -S xdotool
-  sudo pacman --noconfirm --needed -S cmake
-  sudo pacman --noconfirm --needed -S w3m
-  sudo pacman --noconfirm --needed -S playerctl
-  sudo pacman --noconfirm --needed -S lxappearance
-  sudo pacman --noconfirm --needed -S xdo
-  sudo pacman --noconfirm --needed -S xclip
-  sudo pacman --noconfirm --needed -S sxhkd
-  sudo pacman --noconfirm --needed -S dunst
-  sudo pacman --noconfirm --needed -S jq
-  sudo pacman --noconfirm --needed -S volumeicon
+  sudo pacman --noconfirm --needed -S alsa-utils
   sudo pacman --noconfirm --needed -S clipmenu
-  sudo pacman --noconfirm --needed -S lxsession
-  sudo pacman --noconfirm --needed -S copyq
-  sudo pacman --noconfirm --needed -S mpdris2
+  sudo pacman --noconfirm --needed -S cmake
   sudo pacman --noconfirm --needed -S conky
+  sudo pacman --noconfirm --needed -S copyq
+  sudo pacman --noconfirm --needed -S dmenu
+  sudo pacman --noconfirm --needed -S dunst
+  sudo pacman --noconfirm --needed -S feh
+  sudo pacman --noconfirm --needed -S flameshot
+  sudo pacman --noconfirm --needed -S font-awesome-4
+  sudo pacman --noconfirm --needed -S gtk-update-icon-cache
+  sudo pacman --noconfirm --needed -S jq
+  sudo pacman --noconfirm --needed -S lxappearance
+  sudo pacman --noconfirm --needed -S lxsession
   sudo pacman --noconfirm --needed -S mpc
   sudo pacman --noconfirm --needed -S mpd
-  sudo pacman --noconfirm --needed -S vimb
-  sudo pacman --noconfirm --needed -S font-awesome-4
-  sudo pacman --noconfirm --needed -S dmenu
-  sudo pacman --noconfirm --needed -S flameshot
-  sudo pacman --noconfirm --needed -S redshift
-  sudo pacman --noconfirm --needed -S wmname
-  sudo pacman --noconfirm --needed -S vifm
+  sudo pacman --noconfirm --needed -S mpdris2
+  sudo pacman --noconfirm --needed -S playerctl
   sudo pacman --noconfirm --needed -S pulseaudio
-  sudo pacman --noconfirm --needed -S alsa-utils
-  sudo pacman --noconfirm --needed -S gtk-update-icon-cache
+  sudo pacman --noconfirm --needed -S redshift
+  sudo pacman --noconfirm --needed -S sxhkd
+  sudo pacman --noconfirm --needed -S vifm
+  sudo pacman --noconfirm --needed -S vimb
+  sudo pacman --noconfirm --needed -S volumeicon
+  sudo pacman --noconfirm --needed -S w3m
+  sudo pacman --noconfirm --needed -S wmname
+  sudo pacman --noconfirm --needed -S xclip
+  sudo pacman --noconfirm --needed -S xdo
+  sudo pacman --noconfirm --needed -S xdotool
+  sudo pacman --noconfirm --needed -S xscreensaver
   yay -S mpdris2
   sudo systemctl disable mpd.socket
   sudo systemctl stop mpd.socket
 elif [ "$OS" = "FreeBSD" ]; then
   ln -sfn "$(find /usr/local/bin/ -type f -name "perl5*" | tail -1)" "$HOME/.local/bin/perl"
-  sudo pkg install -y neofetch
-  sudo pkg install -y w3m
-  sudo pkg install -y xz
-  sudo pkg install -y xscreensaver
-  sudo pkg install -y flameshot
+  sudo pkg install -y alsa-lib
+  sudo pkg install -y conky
+  sudo pkg install -y copyq
+  sudo pkg install -y dmenu
+  sudo pkg install -y dunst
   sudo pkg install -y feh
-  sudo pkg install -y xdotool
-  sudo pkg install -y xdo
+  sudo pkg install -y flameshot
   sudo pkg install -y jq
+  sudo pkg install -y neofetch
   sudo pkg install -y perl5
+  sudo pkg install -y redshift
+  sudo pkg install -y volumeicon
+  sudo pkg install -y w3m
   sudo pkg install -y wmname
+  sudo pkg install -y xdo
+  sudo pkg install -y xdotool
+  sudo pkg install -y xscreensaver
+  sudo pkg install -y xorg
+  # sudo pkg install -y libXft
+  # sudo pkg install -y xz
 elif [ "$OS" = "void" ]; then
   sudo ln -s /usr/lib/libncursesw.so.6 /usr/lib/libtinfo.so.6
   VOID_PKGS="xscreensaver feh xdotool w3m neofetch lxappearance volumeicon clipmenu xz make gcc gmp-devel dunst wmname libXScrnSaver-devel alsa-lib-devel emacs-gtk2 alsa-utils pulseaudio flameshot volumeicon blueman mpc mpd jq redshift vifm conky playerctl"
@@ -176,27 +191,27 @@ elif [ "$OS" = "Fedora" ]; then
   # sudo dnf remove -y lightdm
   # sudo dnf remove -y gdm
   # sudo dnf remove -y lxdm
+  # sudo dnf install -y clipmenu
   sudo dnf install -y alsa-lib-devel
-  sudo dnf install -y libXScrnSaver-devel
-  sudo dnf install -y feh
-  sudo dnf install -y neofetch
-  sudo dnf install -y w3m
-  sudo dnf install -y xscreensaver
+  sudo dnf install -y blueman
+  sudo dnf install -y conky
+  sudo dnf install -y copyq
+  sudo dnf install -y dbus-x11
   sudo dnf install -y dunst
-  sudo dnf install -y voluemeicon
-  sudo dnf install -y wmname
+  sudo dnf install -y feh
+  sudo dnf install -y flameshot
+  sudo dnf install -y flameshot
+  sudo dnf install -y libXScrnSaver-devel
+  sudo dnf install -y lxsession
   sudo dnf install -y mpc
   sudo dnf install -y mpd
-  sudo dnf install -y lxsession
-  sudo dnf install -y conky
+  sudo dnf install -y neofetch
   sudo dnf install -y playerctl
   sudo dnf install -y sxhkd
-  sudo dnf install -y copyq
-  # sudo dnf install -y clipmenu
-  sudo dnf install -y flameshot
-  sudo dnf install -y blueman
-  sudo dnf install -y flameshot
-  sudo dnf install -y dbus-x11
+  sudo dnf install -y voluemeicon
+  sudo dnf install -y w3m
+  sudo dnf install -y wmname
+  sudo dnf install -y xscreensaver
   echo iwlib missing
   cd "$HOME/projects" || exit
   git clone git@github.com:baskerville/xdo.git
