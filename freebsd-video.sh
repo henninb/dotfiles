5664391ab1d4cc622f254c8a16bb27edce279399
Brian Henning <henninb@msn.com>
2021-04-24 07:44:06 -0500

    updates

--- freebsd-video.sh
+++ freebsd-video.sh
@@ -1,3 +1,9 @@
-echo i915 is the driver used for nvidia
+echo i915 is the driver used for intel graphics cards
 sudo pkg install -y x11-drivers/xf86-video-intel
 echo i915kms_load="YES" > /boot/loader.conf
+
+echo nvidia-driver-460.67
+echo sudo pkg install nvidia-driver-460.67 for nvidia card
+sudo pkg install nvidia-driver-460.67
+echo add to /etc/rc.conf
+kldload_nvidia="nvidia-modeset nvidia"
