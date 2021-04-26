867aad33fbe2148a04e5f75b1659fcac1491fcca
Brian Henning <henninb@msn.com>
2021-04-26 07:45:41 -0500

    updates

--- sddm-install.sh
+++ sddm-install.sh
@@ -14,6 +14,18 @@ polkit.addRule(function(action, subject) {
 });
 EOF
 
+cat > xmonad.desktop << EOF
+[Desktop Entry]
+Name=xmonad
+Comment=xmonad dynamic tiling window manager
+Exec=xmonad-start
+TryExec=xmonad-start
+Type=Application
+X-LightDM-DesktopName=xmonad
+DesktopNames=xmonad
+Keywords=tiling;wm;windowmanager;window;manager;
+EOF
+
 if [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLinux" ]; then
   sudo pacman -S sddm
   sudo systemctl enable sddm.service -f
@@ -21,11 +33,12 @@ if [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLin
 
   sudo mkdir -p /etc/sddm.conf.d/
   sudo mv -v sddm-theme.conf /etc/sddm.conf.d/
+  ls -l /usr/share/sddm/themes/
 elif [ "${OS}" = "FreeBSD" ]; then
   sudo pkg install -y sddm
   sudo pkg install -y sysrc
-  sysrc sddm_enable=yes
-  sudo cp -v 40-wheel-group.rules /usr/local/etc/polkit-1/rules-d/40-wheel-group.rules
+  sysrc sddm_enable="YES"
+  sudo mv -v 40-wheel-group.rules /usr/local/etc/polkit-1/rules.d/40-wheel-group.rules
   sudo sddm --example-config /usr/local/etc/sddm.conf
   sudo service sddm start
   echo https://community.kde.org/FreeBSD/Setup#SDDM
@@ -33,7 +46,5 @@ else
   echo "${OS} is not setup"
   exit 1
 fi
-
-ls -l /usr/share/sddm/themes/
 
 exit 0
