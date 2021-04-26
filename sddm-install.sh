c40e9ea1989efc26a695d3c06499cc7741de6997
Brian Henning <henninb@msn.com>
2021-04-26 05:32:12 -0500

    updates

--- sddm-install.sh
+++ sddm-install.sh
@@ -6,12 +6,33 @@ cat > sddm-theme.conf <<EOF
 Current=elarun
 EOF
 
-sudo pacman -S sddm
-sudo systemctl enable sddm.service -f
-sudo systemctl disable lightdm
+cat > 40-wheel-group.rules <<EOF
+polkit.addRule(function(action, subject) {
+    if (subject.isInGroup("wheel")) {
+    	return polkit.Result.YES;
+    }
+});
+EOF
 
-sudo mkdir -p /etc/sddm.conf.d/
-sudo mv -v sddm-theme.conf /etc/sddm.conf.d/
+if [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLinux" ]; then
+  sudo pacman -S sddm
+  sudo systemctl enable sddm.service -f
+  sudo systemctl disable lightdm
+
+  sudo mkdir -p /etc/sddm.conf.d/
+  sudo mv -v sddm-theme.conf /etc/sddm.conf.d/
+elif [ "${OS}" = "FreeBSD" ]; then
+  sudo pkg install -y sddm
+  sudo pkg install -y sysrc
+  sysrc sddm_enable=yes
+  sudo cp -v 40-wheel-group.rules /usr/local/etc/polkit-1/rules-d/40-wheel-group.rules
+  sudo sddm --example-config /usr/local/etc/sddm.conf
+  sudo service sddm start
+  echo https://community.kde.org/FreeBSD/Setup#SDDM
+else
+  echo "${OS} is not setup"
+  exit 1
+fi
 
 ls -l /usr/share/sddm/themes/
 
