1ad50eac1e90002982dd21a698f475c9d4b8bd27
Brian Henning <henninb@msn.com>
2021-04-26 06:51:23 -0500

    polybar updates

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,21 +6,24 @@ killall -q polybar
 
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-echo $desktop > /tmp/wm
+echo "$desktop" > /tmp/wm
 
 case $desktop in
 
     spectrwm|/usr/share/xsessions/spectrwm)
-      polybar -c "$HOME/.config/polybar/config-master.ini" spectrwm | tee -a "$HOME/polybar.log" &
+      polybar -c "$HOME/.config/polybar/config.ini" spectrwm | tee -a "$HOME/polybar.log" &
       ;;
     xmonad|/usr/share/xsessions/xmonad)
-      polybar -c "$HOME/.config/polybar/config-master.ini" xmonad | tee -a "$HOME/polybar.log" &
+      if [ "${OS}" = "FreeBSD" ]; then
+        echo
+      fi
+      polybar -c "$HOME/.config/polybar/config.ini" xmonad | tee -a "$HOME/polybar.log" &
       ;;
     i3|/usr/share/xsessions/i3)
-      polybar -c "$HOME/.config/polybar/config-master.ini" i3 | tee -a "$HOME/polybar.log" &
+      polybar -c "$HOME/.config/polybar/config.ini" i3 | tee -a "$HOME/polybar.log" &
       ;;
     bspwm|/usr/share/xsessions/bspwm)
-      polybar -c "$HOME/.config/polybar/config-master.ini" bspwm | tee -a "$HOME/polybar.log" &
+      polybar -c "$HOME/.config/polybar/config.ini" bspwm | tee -a "$HOME/polybar.log" &
       ;;
 esac
 
