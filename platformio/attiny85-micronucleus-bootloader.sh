7c4ca3b3ca29e772f0d0f5b785201ddf15c366c7
Brian Henning <henninb@msn.com>
2021-05-01 07:20:20 -0500

    updates

---
+++ platformio/attiny85-micronucleus-bootloader.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# attiny85 hooked up to an arduino isp programmer
+avrdude -v -P /dev/ttyUSB0 -b 19200 -c avrisp -p attiny85 -Uflash:w:micronucleus-attiny85-default.hex:i -U lfuse:w:0xe1:m -U hfuse:w:0xdd:m -U efuse:w:0xfe:m
+
+exit 0
