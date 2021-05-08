6654e143c6c9dd782e2d04c3df0d9f4e4525f622
Brian Henning <henninb@msn.com>
2021-05-07 20:37:57 -0500

    updates

--- freebsd-archlinux.sh
+++ freebsd-archlinux.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-wget https://dl-cdn.archlinuxlinux.org/archlinux/v3.13/releases/x86_64/archlinux-minirootfs-3.13.5-x86_64.tar.gz
-wget https://mirror.rackspace.com/archlinux/iso/latest/archlinux-bootstrap-2020.04.01-x86_64.tar.gz
+wget https://mirror.rackspace.com/archlinux/iso/latest/archlinux-bootstrap-2021.05.01-x86_64.tar.gz
 
 sudo mkdir -p /compat/archlinux/dev
 sudo mkdir -p /compat/archlinux/proc
 sudo mkdir -p /compat/archlinux/sys
 sudo mkdir -p /compat/archlinux/shm
 
-sudo mv archlinux-minirootfs-3.13.5-x86_64.tar.gz /compat/archlinux
-cd /compat/archlinux
-sudo tar xzvf archlinux-minirootfs-3.13.5-x86_64.tar.gz
-sudo rm archlinux-minirootfs-3.13.5-x86_64.tar.gz
-cd -
+sudo mv archlinux-bootstrap-2021.05.01-x86_64.tar.gz /compat/archlinux
+cd /compat/archlinux || exit
+sudo tar xzvf archlinux-bootstrap-2021.05.01-x86_64.tar.gz
+sudo rm archlinux-bootstrap-2021.05.01-x86_64.tar.gz
+cd - || exit
 
 echo sudo chroot /compat/archlinux /bin/sh
 kldstat | grep linux
