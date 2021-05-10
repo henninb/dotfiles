281fe6609bef9ec7491a3f093d3f0d52f5b7f04e
Brian Henning <henninb@msn.com>
2021-05-09 19:08:13 -0500

    updates

--- .xmonad/lib/Local/Workspaces.hs
+++ .xmonad/lib/Local/Workspaces.hs
@@ -1,46 +1,8 @@
 module Local.Workspaces (myWorkspaces, scratchPads) where
 
 import XMonad
--- import qualified Data.Map as M
--- import Graphics.X11.Xlib
--- -- import System.Directory
--- -- import System.FilePath ((</>))
--- import XMonad hiding (keys)
--- import XMonad.Actions.CopyWindow (kill1)
--- import XMonad.Actions.DynamicProjects (switchProjectPrompt)
--- import XMonad.Actions.GroupNavigation (Direction (..), nextMatch)
--- import XMonad.Actions.Minimize
--- import XMonad.Actions.Navigation2D
--- import XMonad.Actions.PhysicalScreens (onNextNeighbour, onPrevNeighbour)
--- import XMonad.Actions.Promote (promote)
--- import XMonad.Actions.RotSlaves (rotSlavesDown, rotSlavesUp)
--- import XMonad.Actions.SwapPromote (swapHybrid)
--- import XMonad.Actions.TagWindows (addTag, delTag, withTagged)
--- import XMonad.Hooks.ManageDocks (ToggleStruts (..))
--- import XMonad.Hooks.UrgencyHook (focusUrgent)
--- import XMonad.Layout.LayoutBuilder (IncLayoutN (..))
--- import XMonad.Layout.Maximize (maximizeRestore)
--- import XMonad.Layout.ResizableTile
--- import XMonad.Layout.Spacing
--- import XMonad.Layout.ZoomRow (zoomIn, zoomOut, zoomReset)
--- import XMonad.Layout.WindowArranger -- for DecreaseRight, IncreaseUp
--- import Graphics.X11.ExtraTypes -- for xF86XK_Paste
--- import XMonad.Util.Paste (sendKey) -- for sendKey
--- -- import XMonad.Local.Layout (selectLayoutByName, toggleLayout)
--- -- import XMonad.Local.Layout.Columns (IncMasterCol (..))
--- -- import XMonad.Local.Music (radioPrompt)
--- -- import qualified XMonad.Local.Prompt as Local
--- -- import XMonad.Local.Tagging
--- -- import XMonad.Local.Workspaces (asKey, scratchPads, viewPrevWS)
--- import XMonad.Prompt
--- import XMonad.Prompt.Window (WindowPrompt (..), allWindows, windowMultiPrompt, wsWindows)
--- import qualified XMonad.StackSet as W
--- import XMonad.Util.EZConfig (mkKeymap)
--- import qualified XMonad.Util.ExtensibleState as XState
--- import XMonad.Util.NamedScratchpad (namedScratchpadAction)
 import XMonad.Util.NamedScratchpad
 import qualified XMonad.StackSet as W
-
 
 ws1 = "1"
 ws2 = "2"
