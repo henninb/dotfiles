e5ce0b9abe5cb504726b022ef3b168c00a5a35af
Brian Henning <henninb@msn.com>
2021-05-12 12:28:15 -0500

    updates

--- .xmonad/assets/bin/main.sh
+++ .xmonad/assets/bin/main.sh
@@ -2,14 +2,14 @@
 
 SLEEP=2
 
-cblue="^fg(#92bbd0)"
+#cblue="^fg(#92bbd0)"
 cgray="^fg(#999999)"
 cwhite="^fg(#ffffff)"
 cnormal="^fg(#dddddd)"
 cred="^fg(#bc4547)"
 cyellow="^fg(#a88c29)"
 cgreen="^fg(#00aa6c)"
-cpurple="^fg(#9542f4)"
+#cpurple="^fg(#9542f4)"
 
 fs_icon="^i($HOME/.xmonad/assets/icons/diskette.xbm)"
 cpu_icon="^i($HOME/.xmonad/assets/icons/cpu.xbm)"
@@ -20,54 +20,38 @@ rainny="^i($HOME/.xmonad/assets/icons/rainny.xbm)"
 snowy="^i($HOME/.xmonad/assets/icons/snowy.xbm)"
 stormydaniels="^i($HOME/.xmonad/assets/icons/stormydaniels.xbm)"
 cloudy="^i($HOME/.xmonad/assets/icons/cloudy.xbm)"
-partially="^i($HOME/.xmonad/assets/icons/partially.xbm)"
+#partially="^i($HOME/.xmonad/assets/icons/partially.xbm)"
 foggy="^i($HOME/.xmonad/assets/icons/fog.xbm)"
 sunny="^i($HOME/.xmonad/assets/icons/sunny.xbm)"
 
-blinking=0
-blinkcolor=cred
-blink=101
-
-# -------------------
-# FUNCTIONS
-# -------------------
 function wrapper {
-if [ "$1" -eq 0 ]; then
-  echo "000${cwhite}"
-elif [ ${#1} -ge 3 ]; then
-  echo "${cwhite}$1"
-else
-  echo $(printf "%02d" $1 | sed "s/\(^0\+\)/\1${cwhite}/")
-fi
+  if [ "$1" -eq 0 ]; then
+    echo "000${cwhite}"
+  elif [ ${#1} -ge 3 ]; then
+    echo "${cwhite}$1"
+  else
+    echo "$(printf "%02d" "$1" | sed "s/\(^0\+\)/\1${cwhite}/")"
+  fi
 }
 
 function colorcho {
-if [ $1 -ge 0 ] && [ $1 -lt 50 ]
-then
- echo "${cgreen}"
-fi
-if [ $1 -ge 50 ] && [ $1 -lt 75 ]
-then
- echo "${cyellow}"
-fi
-if [ $1 -ge 75 ] && [ $1 -le 100 ]
-then
- echo "${cred}"
-fi
-if [ $1 -eq 101 ]
-then
- echo "${cwhite}"
-fi
+  if [ "$1" -ge 0 ] && [ "$1" -lt 50 ]; then
+    echo "${cgreen}"
+  elif [ "$1" -ge 50 ] && [ "$1" -lt 75 ]; then
+    echo "${cyellow}"
+  elif [ "$1" -ge 75 ] && [ "$1" -le 100 ]; then
+    echo "${cred}"
+  elif [ "$1" -eq 101 ]; then
+    echo "${cwhite}"
+  fi
 }
 
 # cpu
 PREV_TOTAL=0
 PREV_IDLE=0
 
-
 temper=$(curl -s wttr.in/Minneapolis?lang=es | sed -n 4p | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | perl -nE 'say/([0-9]+)+|([0-9]+)+-+([0-9]+)/' )
 forecast=$(curl -s wttr.in/Minneapolis?lang=es | sed -n 3p | sed 's/\x1B\[[0-9;]\+[A-Za-z]//g'| sed -e 's/[\"\\\/_..()\*-]//g' | awk '{print $1 " " $2}' | sed -e 's/-//')
-
 
 case $(echo $forecast | awk '{print $1}') in
  Soleado)
@@ -101,14 +85,12 @@ esac
 
 while :; do
 
-# disk
 root_used=$(df / | grep -Eo '[0-9]+%' | sed s/%//)
 data_total=$(free | sed -n 3p | awk '{print $2}')
 data_used=$(free | sed -n 3p | awk '{print $3}')
 data_percent=$(($data_used * 100))
-if [ "$data_used" == "0" ]
-then
-data_used=1
+if [ "$data_used" == "0" ]; then
+  data_used=1
 fi
 data_percent=$(($data_percent / $data_total))
 storage_used=$(df /dev/sda2 | grep -Eo '[0-9]+%' | sed s/%//)
@@ -120,20 +102,20 @@ root="$(colorcho $root_used)${fs_icon} ${cgray}ROOT $(wrapper $root_used)%"
 home="${color_sec1}${fs_icon} /home ${color_sec2}$(wrapper $homesize)G"
 
 # cpu
-CPU=(`cat /proc/stat | grep '^cpu '`) # Get the total CPU statistics.
+CPU=$(cat /proc/stat | grep '^cpu ')
 unset CPU[0]                          # Discard the "cpu" prefix.
 IDLE=${CPU[4]}                        # Get the idle CPU time.
 
 # Calculate the total CPU time.
 TOTAL=0
 for VALUE in "${CPU[@]}"; do
-  let "TOTAL=$TOTAL+$VALUE"
+  TOTAL="$TOTAL+$VALUE"
 done
 
 # Calculate the CPU usage since we last checked.
-let "DIFF_IDLE=$IDLE-$PREV_IDLE"
-let "DIFF_TOTAL=$TOTAL-$PREV_TOTAL"
-let "DIFF_USAGE=(1000*($DIFF_TOTAL-$DIFF_IDLE)/$DIFF_TOTAL+5)/10"
+DIFF_IDLE="$IDLE-$PREV_IDLE"
+DIFF_TOTAL="$TOTAL-$PREV_TOTAL"
+DIFF_USAGE="(1000*($DIFF_TOTAL-$DIFF_IDLE)/$DIFF_TOTAL+5)/10"
 
 # Remember the total and idle CPU times for the next check.
 PREV_TOTAL="$TOTAL"
@@ -141,7 +123,6 @@ PREV_IDLE="$IDLE"
 
 cpu="$(colorcho $DIFF_USAGE)${cpu_icon}${cgray} $(wrapper ${DIFF_USAGE})%"
 
-# memory
 memory_total=$(free -m | awk 'FNR == 2 {print $2}')
 memory_used=$(free -m | awk 'FNR == 2 {print $3}')
 memory_free_percent=$[$memory_used * 100 / $memory_total]
