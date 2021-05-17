74bd48ac5083535d24c9c504670f34bdcd29468b
Brian Henning <henninb@msn.com>
2021-05-16 19:00:16 -0500

    updates

--- haskell/next-oncall/app/Main.hs
+++ haskell/next-oncall/app/Main.hs
@@ -10,15 +10,15 @@ startDate = fromGregorian 2021 4 24
 engineerNumber :: Integer
 engineerNumber = 6
 
-nextOncallDayNew :: Day -> Integer -> IO Day
-nextOncallDayNew prevDay numOfPeople = do
+nextOncallDay :: Day -> Integer -> IO Day
+nextOncallDay prevDay numOfPeople = do
   now <- currDay
   let diff = diffDays now startDate
   let dayCountOne = numOfPeople * 3
   let interval = div diff dayCountOne
   let dayCount = (numOfPeople * 3) + (interval * dayCountOne)
-  let next = addDays dayCount prevDay
-  return next
+  let futureDate = addDays dayCount prevDay
+  return futureDate
 
 currDay :: IO Day
 currDay = do localDay . zonedTimeToLocalTime <$> getZonedTime
@@ -32,6 +32,5 @@ daysTo (year, month, day) = do
 
 main = do
   today <- currDay
-  next <- nextOncallDayNew startDate engineerNumber
-  print (diffDays next today)
-  print next
+  futureDate <- nextOncallDay startDate engineerNumber
+  print (diffDays futureDate today)
