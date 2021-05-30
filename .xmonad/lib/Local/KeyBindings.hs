ed3a5fcd00e3cd464184d792f785009d04049b49
Brian Henning <henninb@msn.com>
2021-05-30 16:18:06 -0500

    updates

--- .xmonad/lib/Local/KeyBindings.hs
+++ .xmonad/lib/Local/KeyBindings.hs
@@ -91,6 +91,7 @@ myRemoveKeys = [
                  , (superKeyMask, xK_p)
                  , (superKeyMask, xK_x)
                  , (controlMask, xK_p)
+                 , (controlMask, xK_n)
                  , (superKeyMask .|. shiftMask, xK_q)
                  , (superKeyMask .|. shiftMask, xK_c)
                  -- , (superKeyMask, xK_space)
@@ -207,8 +208,9 @@ windowKeys =
 workspaceKeys :: [(String, X ())]
 workspaceKeys =
       [
-          ("M-;", viewPrevWS)
-        , ("M-<Tab>", toggleWS)
+          ("M-;"     , viewPrevWS)
+        , ("M-<Tab>" , toggleWS)
+        , ("M-?"     , helpCommand)
       ]
         -- change active workspace
       ++ [("M-" ++ workSpace, windows $ W.greedyView workSpace) | workSpace <- myWorkspaces ]
@@ -218,6 +220,9 @@ workspaceKeys =
       ++ [("M1-S-" ++ workSpace, windows $ W.shift workSpace) | workSpace <- myWorkspaces ]
       --  copy window
       ++ [("M-C-" ++ workSpace, windows $ copy workSpace) | workSpace <- myWorkspaces ]
+         where
+          helpCommand :: X ()
+          helpCommand = spawn ("echo " ++ show help ++ " | xmessage -file -")
 -- | Keys for manipulating workspaces.
 -- workspaceKeys :: XConfig Layout -> [(String, X ())]
 -- workspaceKeys conf =
@@ -405,3 +410,53 @@ spawnToWorkspace :: String -> String -> X ()
 spawnToWorkspace program workspace = do
               spawn program
               windows $ W.greedyView workspace . W.shift workspace
+
+help :: String
+help = unlines ["The default modifier key is 'super'. Default keybindings:",
+    "",
+    "-- launching and killing programs",
+    "mod-Shift-Enter  Launch xterminal",
+    "mod-p            Launch dmenu",
+    "mod-Shift-p      Launch gmrun",
+    "mod-Shift-c      Close/kill the focused window",
+    "mod-Space        Rotate through the available layout algorithms",
+    "mod-Shift-Space  Reset the layouts on the current workSpace to default",
+    "mod-n            Resize/refresh viewed windows to the correct size",
+    "",
+    "-- move focus up or down the window stack",
+    "mod-Tab        Move focus to the next window",
+    "mod-Shift-Tab  Move focus to the previous window",
+    "mod-j          Move focus to the next window",
+    "mod-k          Move focus to the previous window",
+    "mod-m          Move focus to the master window",
+    "",
+    "-- modifying the window order",
+    "mod-Return   Swap the focused window and the master window",
+    "mod-Shift-j  Swap the focused window with the next window",
+    "mod-Shift-k  Swap the focused window with the previous window",
+    "",
+    "-- resizing the master/slave ratio",
+    "mod-h  Shrink the master area",
+    "mod-l  Expand the master area",
+    "",
+    "-- floating layer support",
+    "mod-t  Push window back into tiling; unfloat and re-tile it",
+    "",
+    "-- increase or decrease number of windows in the master area",
+    "mod-comma  (mod-,)   Increment the number of windows in the master area",
+    "mod-period (mod-.)   Deincrement the number of windows in the master area",
+    "",
+    "-- quit, or restart",
+    "mod-Shift-q  Quit xmonad",
+    "mod-q        Restart xmonad",
+    "",
+    "-- Workspaces & screens",
+    "mod-[1..9]         Switch to workSpace N",
+    "mod-Shift-[1..9]   Move client to workspace N",
+    "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3",
+    "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3",
+    "",
+    "-- Mouse bindings: default actions bound to mouse events",
+    "mod-button1  Set the window to floating mode and move by dragging",
+    "mod-button2  Raise the window to the top of the stack",
+    "mod-button3  Set the window to floating mode and resize by dragging"]
