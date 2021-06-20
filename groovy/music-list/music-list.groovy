9c54d9633bf7cc872ef02232a1311e3229eca53d
Brian Henning <henninb@msn.com>
2021-06-20 10:43:27 -0500

    updates

--- groovy/music-list/music-list.groovy
+++ groovy/music-list/music-list.groovy
@@ -1,20 +1,42 @@
 @Grab('com.fasterxml.jackson.core:jackson-core:2.12.3')
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.databind.DeserializationFeature
+import groovy.transform.ToString
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
 
-@JsonIgnoreProperties(ignoreUnknown = true)
+@ToString
 class SongEntry {
-  String artist = "empty"
+  String artist
+
   String track
+
   String album
+
   Integer year
-  String fileName
 }
 
-json = """
-{ artist:"Tom Petty", track:"It's Good To Be King", album: "It's Good To Be King", year:1994, fileName:"tom_petty_its_good_to_be_king.mp3"}
-"""
+void process() {
+  String json = """
+  { "artist": "Tom Petty", "track": "It's Good To Be King", "album": "It's Good To Be King", "year": 1994 }
+  """
+  // ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+  ObjectMapper mapper = new ObjectMapper()
+  // mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  // mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  SongEntry songEntry = mapper.readValue(json, SongEntry.class)
+  println songEntry
+}
 
-ObjectMapper mapper = new ObjectMapper()
-SongEntry songEntry = mapper.readValue(json, SongEntry.class)
-print songEntry
+
+process()
+
+// def main() {
+//   // File file = new File('tom_petty_its_good_to_be_king.json')
+//   String json = """
+//   { "artist": "Tom Petty", "track": "It's Good To Be King", "album": "It's Good To Be King", "year": 1994 }
+//   """
+//   process()
+// }
+
+// main()
