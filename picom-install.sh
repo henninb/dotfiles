098484af56303aadfb7478687e7421d53354f4e9
Brian Henning <henninb@msn.com>
2021-06-21 16:32:45 -0500

    updates

--- picom-install.sh
+++ picom-install.sh
@@ -5,7 +5,7 @@ if [ "$OS" = "Arch Linux" ] || [ "$OS" = "ArcoLinux" ]; then
 elif [ "$OS" = "FreeBSD" ]; then
   sudo pkg install -y ninja meson uthash
 elif [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Raspbian GNU/Linux" ]; then
-  sudo apt install -y uthash-dev libxcb-sync-dev libxcb-present-dev
+  sudo apt install -y uthash-dev libxcb-sync-dev libxcb-present-dev libxcb-damage0-dev libconfig-dev libdbus-1-dev
   sudo pip install ninja
   sudo pip install meson
 elif [ "$OS" = "Gentoo" ]; then
