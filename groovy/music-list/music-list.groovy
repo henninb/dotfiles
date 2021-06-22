6e1b4149c39cd2770df943e8226746c284cc789c
Brian Henning <henninb@msn.com>
2021-06-22 05:09:32 -0500

    updates

--- groovy/music-list/music-list.groovy
+++ groovy/music-list/music-list.groovy
@@ -1,3 +1,8 @@
+// @Grapes([
+//    @Grab(group='org.xerial', module='sqlite-jdbc', version='3.7.2'),
+//    @GrabConfig(systemClassLoader=true)
+//  ])
+@Grab('org.xerial:sqlite-jdbc:3.34.0')
 @Grab('com.fasterxml.jackson.core:jackson-core:2.12.3')
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.DeserializationFeature
@@ -17,6 +22,8 @@ class SongEntry {
 }
 
 void process() {
+  File jsonListFile = new File("list.json")
+  File jsonFile = new File("tom_petty_its_good_to_be_king.json")
   String json = """
   { "artist": "Tom Petty", "track": "It's Good To Be King", "album": "It's Good To Be King", "year": 1994 }
   """
@@ -24,19 +31,11 @@ void process() {
   ObjectMapper mapper = new ObjectMapper()
   // mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
   // mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-  SongEntry songEntry = mapper.readValue(json, SongEntry.class)
+  SongEntry songEntry = mapper.readValue(json, SongEntry)
   println songEntry
+  SongEntry songEntry1 = mapper.readValue(jsonFile, SongEntry)
+  println songEntry1
+  List<SongEntry> list = mapper.readValue(jsonListFile, SongEntry[])
+  print list
 }
-
-
 process()
-
-// def main() {
-//   // File file = new File('tom_petty_its_good_to_be_king.json')
-//   String json = """
-//   { "artist": "Tom Petty", "track": "It's Good To Be King", "album": "It's Good To Be King", "year": 1994 }
-//   """
-//   process()
-// }
-
-// main()
