cf618587de5d2cb81cc0a0b8269ef53057c8d17f
Brian Henning <henninb@msn.com>
2021-08-07 09:54:46 -0500

    updates

--- .xmonad/lib/Local/KeyBindings.hs
+++ .xmonad/lib/Local/KeyBindings.hs
@@ -63,6 +63,47 @@ myEmacs = "emacsclient -c -a 'emacs' "
 lockScreen :: X ()
 lockScreen = spawn "xscreensaver-command -lock"
 
+emacs :: X ()
+emacs = do
+  name <- gets (W.tag . W.workspace . W.current . windowset)
+  spawn ("e -cs " ++ name)
+
+data MessageMenu = MessageMenu
+
+instance XPrompt MessageMenu where
+  showXPrompt MessageMenu = "XMonad Action: "
+
+-- | Remember certain actions taken so they can be repeated.
+newtype LastXMessage = LastXMessage
+  {getLastMessage :: X ()}
+
+instance ExtensionClass LastXMessage where
+  initialValue = LastXMessage (return ())
+
+-- | Record the given message as the last used message, then execute it.
+recordXMessage :: X () -> X ()
+recordXMessage message = do
+  XState.put (LastXMessage message)
+  message
+
+-- | Execute the last recorded message.
+repeatLastXMessage :: X ()
+repeatLastXMessage = getLastMessage =<< XState.get
+
+spawnToWorkspace :: String -> String -> X ()
+spawnToWorkspace program workspace = do
+              spawn program
+              windows $ W.greedyView workspace . W.shift workspace
+
+showKeyBindings :: [((XMonad.KeyMask, XMonad.KeySym), NamedActions.NamedAction)] -> NamedActions.NamedAction
+showKeyBindings x =
+  NamedActions.addName "Show Keybindings" $
+  XMonad.io $ do
+    h <- Run.spawnPipe "yad --text-info"
+    Run.hPutStr h (unlines $ NamedActions.showKm x)
+    IO.hClose h
+    return ()
+
 keyMaps :: [(String, X ())]
 keyMaps =
         baseKeys ++
@@ -118,7 +159,6 @@ windowKeys =
   , ("M-C-<Left>", sendMessage (DecreaseLeft 10))
   , ("M-S-h", sendMessage Shrink)
   , ("M-S-l", sendMessage Expand)
-
   ]
 
 workspaceKeys :: [(String, X ())]
@@ -126,7 +166,7 @@ workspaceKeys =
       [
           ("M-;"     , viewPrevWS)
         , ("M-<Tab>" , toggleWS)
-        , ("M-?"     , helpCommand)
+        -- , ("M-?"     , helpCommand)
       ]
         -- change active workspace
       ++ [("M-" ++ workSpace, windows $ W.greedyView workSpace) | workSpace <- myWorkspaces ]
@@ -136,9 +176,9 @@ workspaceKeys =
       ++ [("M1-S-" ++ workSpace, windows $ W.shift workSpace) | workSpace <- myWorkspaces ]
       --  copy window
       ++ [("M-C-" ++ workSpace, windows $ copy workSpace) | workSpace <- myWorkspaces ]
-         where
-          helpCommand :: X ()
-          helpCommand = spawn ("echo " ++ show help ++ " | xmessage -file -")
+         -- where
+         --  helpCommand :: X ()
+         --  helpCommand = spawn ("echo " ++ show help ++ " | xmessage -file -")
 
 screenKeys :: [(String, X ())]
 screenKeys =
@@ -212,93 +252,3 @@ musicKeys =
   , ("<XF86AudioNext>", spawn "mpc next")
   ]
 
-emacs :: X ()
-emacs = do
-  name <- gets (W.tag . W.workspace . W.current . windowset)
-  spawn ("e -cs " ++ name)
-
-data MessageMenu = MessageMenu
-
-instance XPrompt MessageMenu where
-  showXPrompt MessageMenu = "XMonad Action: "
-
--- | Remember certain actions taken so they can be repeated.
-newtype LastXMessage = LastXMessage
-  {getLastMessage :: X ()}
-
-instance ExtensionClass LastXMessage where
-  initialValue = LastXMessage (return ())
-
--- | Record the given message as the last used message, then execute it.
-recordXMessage :: X () -> X ()
-recordXMessage message = do
-  XState.put (LastXMessage message)
-  message
-
--- | Execute the last recorded message.
-repeatLastXMessage :: X ()
-repeatLastXMessage = getLastMessage =<< XState.get
-
-spawnToWorkspace :: String -> String -> X ()
-spawnToWorkspace program workspace = do
-              spawn program
-              windows $ W.greedyView workspace . W.shift workspace
-
-showKeyBindings :: [((XMonad.KeyMask, XMonad.KeySym), NamedActions.NamedAction)] -> NamedActions.NamedAction
-showKeyBindings x =
-  NamedActions.addName "Show Keybindings" $
-  XMonad.io $ do
-    h <- Run.spawnPipe "yad --text-info"
-    Run.hPutStr h (unlines $ NamedActions.showKm x)
-    IO.hClose h
-    return ()
-
-help :: String
-help = unlines ["The default modifier key is 'super'. Default keybindings:",
-    "",
-    "-- launching and killing programs",
-    "mod-Shift-Enter  Launch xterminal",
-    "mod-p            Launch dmenu",
-    "mod-Shift-p      Launch gmrun",
-    "mod-Shift-c      Close/kill the focused window",
-    "mod-Space        Rotate through the available layout algorithms",
-    "mod-Shift-Space  Reset the layouts on the current workSpace to default",
-    "mod-n            Resize/refresh viewed windows to the correct size",
-    "",
-    "-- move focus up or down the window stack",
-    "mod-Tab        Move focus to the next window",
-    "mod-Shift-Tab  Move focus to the previous window",
-    "mod-j          Move focus to the next window",
-    "mod-k          Move focus to the previous window",
-    "mod-m          Move focus to the master window",
-    "",
-    "-- modifying the window order",
-    "mod-Return   Swap the focused window and the master window",
-    "mod-Shift-j  Swap the focused window with the next window",
-    "mod-Shift-k  Swap the focused window with the previous window",
-    "",
-    "-- resizing the master/slave ratio",
-    "mod-h  Shrink the master area",
-    "mod-l  Expand the master area",
-    "",
-    "-- floating layer support",
-    "mod-t  Push window back into tiling; unfloat and re-tile it",
-    "",
-    "-- increase or decrease number of windows in the master area",
-    "mod-comma  (mod-,)   Increment the number of windows in the master area",
-    "mod-period (mod-.)   Deincrement the number of windows in the master area",
-    "",
-    "-- quit, or restart",
-    "mod-Shift-q  Quit xmonad",
-    "mod-q        Restart xmonad",
-    "",
-    "-- Workspaces & screens",
-    "mod-[1..9]         Switch to workSpace N",
-    "mod-Shift-[1..9]   Move client to workspace N",
-    "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3",
-    "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3",
-    "",
-    "-- Mouse bindings: default actions bound to mouse events",
-    "mod-button1  Set the window to floating mode and move by dragging",
-    "mod-button2  Raise the window to the top of the stack",
-    "mod-button3  Set the window to floating mode and resize by dragging"]
