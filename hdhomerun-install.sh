110891626861a27c04301c5ce96e3e121289ebb8
Brian Henning <henninb@msn.com>
2021-08-11 06:12:30 -0500

    updates

--- hdhomerun-install.sh
+++ hdhomerun-install.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
 
-wget http://download.silicondust.com/hdhomerun/libhdhomerun_20190621.tgz
-wget http://download.silicondust.com/hdhomerun/hdhomerun_config_gui_20190621.tgz
+# hdhomerun_config_gui_20210624.tgz
+date=20210624
 
-tar xvf libhdhomerun_20190621.tgz
-tar xvf hdhomerun_config_gui_20190621.tgz
+cd "$HOME/projects" || exit
+wget "http://download.silicondust.com/hdhomerun/libhdhomerun_${date}.tgz" -O libhdhomerun.tar.gz
+wget "http://download.silicondust.com/hdhomerun/hdhomerun_config_gui_${date}.tgz" -O hdhomerun_config_gui.tar.gz
 
-sudo dnf install -y hdhomerun
+tar xvf libhdhomerun.tar.gz
+tar xvf hdhomerun_config_gui.tar.gz
+
+cd libhdhomerun || exit
+make
+mv -v hdhomerun_config "$HOME/.local/bin/hdhomerun_config"
+mv -v libhdhomerun.so "$HOME/.local/bin/libhdhomerun.so"
+cd - || exit
+
+
+cd hdhomerun_config_gui || exit
+./configure
+make
+mv -v src/hdhomerun_config_gui "$HOME/.local/bin/hdhomerun_config_gui"
+
+# sudo dnf install -y hdhomerun
+echo hdhomerun_config 101AE8FC save /tuner0 file.ts
 
 exit 0
---
+++ todo-tech-bucket-list.md
@@ -0,0 +1,27 @@
+ddwrt web frontend certificate setup
+virtualize pfsense on proxmox
+port rapi-finance-endpoint to ktor
+port rapi-finance-endpoint to rust
+code submenu names in xmonad
+find a project to work on - fiverr.com
+use letsencrypt on pfsense
+setup webserver for brianhenning.xyz
+code aes encrypt a file using haskell
+fix shell script to use new git pattern
+virtualize pihole on proxmox
+learn how to code android app
+record live tv on linux with silicondust hdhomerun
+learn to use obs for content creation
+practice squashing with git
+code and app to stream audio to a phone
+update main windows 10 machine to the latest version
+linux - swap init program with bash (testing)
+virtualize webserver on proxmox
+backup 2fa codes
+backup photos
+learn more about nextcloud
+learn more about digital ocean
+learn more about aws
+learn more about azure
+learn more about goodle cloud
+lora - setup a sender/receiver
