1961b95c241439493034fd667b79118d1bcec72a
Brian Henning <henninb@msn.com>
2021-09-07 07:59:54 -0500

    wip

--- docker-install.sh
+++ docker-install.sh
@@ -38,7 +38,11 @@ elif [ "$OS" = "openSUSE Tumbleweed" ]; then
   sudo systemctl start docker
   sudo systemctl status docker
 elif [ "$OS" = "Ubuntu" ]; then
-  sudo apt install -y docker
+  sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt update
+  # sudo apt install -y docker
   sudo systemctl enable docker
   sudo systemctl start docker
   sudo usermod -a -G docker "$(id -un)"
