54bd50618ebdb4b444efae2b541a7be67e178ed9
Brian Henning <henninb@msn.com>
2021-09-08 05:04:31 -0500

    wip

---
+++ plex-install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
+echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+sudo apt update
+sudo apt install plexmediaserver
+sudo systemctl status plexmediaserver
+sudo mkdir -p /opt/plexmedia/{movies,series,music}
+sudo chown -R plex: /opt/plexmedia
+
+echo http://YOUR_SERVER_IP:32400/web
+echo http://192.168.100.54:32400/web
+
+exit 0
