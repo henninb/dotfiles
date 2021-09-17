90e5961658eb2423978d99c7dd0947d266a9d503
Brian Henning <henninb@msn.com>
2021-09-17 05:51:30 -0500

    wip

--- postgresql-install.sh
+++ postgresql-install.sh
@@ -44,6 +44,9 @@ if [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLin
   rm -v /tmp/install_psql_settings.sql
   sudo netstat -lntp | grep postgres
   sudo fuser 5432/tcp
+elif [ "$OS" = "void" ]; then
+  echo
+  sudo xbps-install -y postgresql
 elif [ "$OS" = "Solus" ]; then
   sudo eopkg install postgresql
   sudo -u postgres sh -c 'initdb -D /var/lib/postgres/data'
