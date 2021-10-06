c44089183188d036c24690f083b9edc4c6b694f5
Brian Henning <henninb@msn.com>
2021-10-06 08:08:40 -0500

    wip

--- nix-install.sh
+++ nix-install.sh
@@ -1,5 +1,10 @@
+#!/bin/sh
+
+sudo eopkg install -y rsync
+
 sh <(curl -L https://nixos.org/nix/install) --daemon
 
+exit 0
 
 example
 https://github.com/endofunky/nix-config/tree/4b0332656f835f6b2fcbcfa27b4ea60576f0b7e3
