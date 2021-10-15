2467b06302f034b296d2d7db252a2adca9bb95be
Brian Henning <henninb@msn.com>
2021-10-15 05:21:20 -0500

    wip

--- iso-to-flashdrive.sh
+++ iso-to-flashdrive.sh
@@ -11,11 +11,15 @@ if [ ! -f "$FILE" ]; then
   exit 1
 fi
 
+lsusb | grep Toshiba
 sudo dmesg | grep -A 5 'Direct-Access' | grep sd | grep -i log
 #drive=$(dmesg | grep -A 5 'Direct-Access' | grep sd | grep -i log)
 
 echo sudo dd "if=${FILE}" of=/dev/sdc bs=4M status=progress && sync
 echo sudo dd "if=${FILE}" of=/dev/sdd bs=4M status=progress && sync
 echo sudo dd "if=${FILE}" of=/dev/sdg bs=4M status=progress && sync
+echo sudo dd "if=${FILE}" of=/dev/sdg bs=16k status=progress && sync
+
+#hdparm -i /dev/sdg
 
 exit 0
