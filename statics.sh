1d99206f3653876d05bcd90d7c0b401f263e69a7
Brian Henning <henninb@msn.com>
2021-11-14 06:57:10 -0600

    wip

--- statics.sh
+++ statics.sh
@@ -2,16 +2,14 @@
 
 
 cat > statics <<EOF
-192.168.100.4 proxmox
-192.168.100.3 esxi
-192.168.100.2 ddwrt-switch
-192.168.100.1 ddwrt
-192.168.10.1 opnsense
 192.168.10.1 pfsense
+192.168.10.1 opnsense
+192.168.10.2 ddwrt-switch
+192.168.10.4 proxmox
+192.168.10.6 esxi
 192.168.10.10 pihole
-192.168.100.11 ddwrt-bridge1
+192.168.10.11 ddwrt-bridge1
+192.168.10.25 raspi
 EOF
-
-
 
 exit 0
