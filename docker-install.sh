5938ea6fda965d40f77b6e3d111271e6f890a991
Brian Henning <henninb@msn.com>
2021-11-23 06:24:02 -0600

    wip

--- docker-install.sh
+++ docker-install.sh
@@ -49,6 +49,10 @@ elif [ "$OS" = "Debian GNU/Linux" ]; then
   echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
   sudo apt update
   sudo apt install -y docker-ce docker-ce-cli containerd.io
+  sudo usermod -G docker -a "$USER"
+  sudo systemctl enable docker
+  sudo systemctl start docker
+  sudo systemctl status docker
 elif [ "$OS" = "Ubuntu" ]; then
   sudo snap install docker
   sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
