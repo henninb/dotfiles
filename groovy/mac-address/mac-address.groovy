40ad4fa85355abc6d6eed6ad00f6f3a65f96afaa
Brian Henning <henninb@msn.com>
2021-11-25 08:50:52 -0600

    wip

--- groovy/mac-address/mac-address.groovy
+++ groovy/mac-address/mac-address.groovy
@@ -1,10 +1,10 @@
-@Grab('org.xerial:sqlite-jdbc:3.34.0')
 @Grab('com.fasterxml.jackson.core:jackson-core:2.12.3')
+
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.DeserializationFeature
+// import com.fasterxml.jackson.databind.DeserializationFeature
 import groovy.transform.ToString
-import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
+// import groovy.json.JsonOutput
+// import groovy.json.JsonSlurper
 
 @ToString
 class StaticNode
@@ -14,17 +14,17 @@ class StaticNode
 }
 
 ObjectMapper mapper = new ObjectMapper()
-println('got em')
+List<StaticNode> staticNodes = []
 
-  File files = new File("intput.txt")
-  files.eachLine {line ->
-    StaticNode staticNode = new StaticNode()
-    def list = line.split('\t')
-    staticNode.mac = list[0]
-    staticNode.ip = list[1]
-    staticNode.hostname = list[2]
-    String jsonString = mapper.writeValueAsString(staticNode)
-    println jsonString
-  }
+File files = new File("intput.txt")
+files.eachLine { line ->
+  StaticNode staticNode = new StaticNode()
+  List<String> list = line.split('\t')
+  staticNode.mac = list[0]
+  staticNode.ip = list[1]
+  staticNode.hostname = list[2]
+  staticNodes.add(staticNode)
+  // String jsonString = mapper.writeValueAsString(staticNode)
+}
 
-readthem()
+staticNodes
