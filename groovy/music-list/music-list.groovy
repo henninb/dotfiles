18348f63f8c5df5535e4ff71c985bac3430c9136
Brian Henning <henninb@msn.com>
2021-11-26 06:11:59 -0600

    wip

--- groovy/music-list/music-list.groovy
+++ groovy/music-list/music-list.groovy
@@ -1,39 +1,43 @@
-// @Grapes([
-//    @Grab(group='org.xerial', module='sqlite-jdbc', version='3.7.2'),
-//    @GrabConfig(systemClassLoader=true)
-//  ])
 @Grab('org.xerial:sqlite-jdbc:3.34.0')
 @Grab('com.fasterxml.jackson.core:jackson-core:2.12.3')
+
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.DeserializationFeature
 import groovy.transform.ToString
-import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
+import java.nio.file.Paths
 
 @ToString
 class SongEntry {
   String artist
-
   String track
-
   String album
-
   Integer year
-
-  // String genre
 }
 
+void readthem() {
+  List<SongEntry> songEntries = []
+  ObjectMapper mapper = new ObjectMapper()
+
+  File files = new File("songs.txt")
+  files.eachLine {line ->
+    SongEntry songEntry = new SongEntry()
+    List<String> list = line.split(' - ')
+    println list[0]
+    println list[1].replace('.mp3', '')
+    // songentries.add(songEntry)
+  }
+  // if( songentries ) {
+  //   mapper.writeValue(Paths.get("output.json").toFile(), songentries)
+  // }
+}
+
+readthem()
 
 void process() {
+  String json = ''' { "artist": "Tom Petty", "track": "It's Good To Be King", "album": "It's Good To Be King", "year": 1994 } '''
   File jsonListFile = new File("list.json")
   File jsonFile = new File("tom_petty_its_good_to_be_king.json")
-  String json = """
-  { "artist": "Tom Petty", "track": "It's Good To Be King", "album": "It's Good To Be King", "year": 1994 }
-  """
-  // ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
   ObjectMapper mapper = new ObjectMapper()
-  // mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-  // mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
   SongEntry songEntry = mapper.readValue(json, SongEntry)
   println songEntry
   SongEntry songEntry1 = mapper.readValue(jsonFile, SongEntry)
@@ -41,19 +45,5 @@ void process() {
   List<SongEntry> list = mapper.readValue(jsonListFile, SongEntry[])
   print list
 }
+
 process()
-
-void readthem() {
-  File files = new File("songs.txt")
-  // files.withReader {reader ->
-  //  println reader.readLine()
-  // }
-  files.eachLine {line ->
-    // println line
-    def list = line.split(' - ')
-    println list[0]
-    println list[1].replace('.mp3', '')
-  }
-}
-
-readthem()
