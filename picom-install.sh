d86cf91836ffc7cf56da822a30804eaa97411067
Brian Henning <henninb@msn.com>
2021-11-27 05:40:00 -0600

    wip

--- picom-install.sh
+++ picom-install.sh
@@ -6,13 +6,15 @@ elif [ "$OS" = "FreeBSD" ]; then
   sudo pkg install -y ninja meson uthash
 elif [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Raspbian GNU/Linux" ]; then
   sudo apt install -y uthash-dev libxcb-sync-dev libxcb-present-dev libxcb-damage0-dev libconfig-dev libdbus-1-dev
-  #sudo pip install ninja
-  #sudo pip install meson
-  sudo apt install ninja-build
-  sudo apt install meson
+  sudo apt install -y libx11-xcb-dev
+  sudo apt install -y libev-dev
+  sudo apt install -y libxcb-render-util0-dev
+  sudo apt install -y libxcb-image0-dev
+  sudo apt install -y libxcb-composite0-dev
+  sudo apt install -y libxcb-xinerama0-dev
+  sudo python3 -m pip install ninja
+  sudo python3 -m pip install meson
 elif [ "$OS" = "Gentoo" ]; then
-  # sudo pip install ninja
-  # sudo pip install meson
   sudo emerge --update --newuse ninja
   sudo emerge --update --newuse uthash
   sudo emerge --update --newuse libconfig
