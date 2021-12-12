c9b797bb98eae27ee3e2cd723edfb626cd381b88
Brian Henning <henninb@msn.com>
2021-12-11 18:18:45 -0600

    wip

--- mp3info.sh
+++ mp3info.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-for f in $(find . -type f -name "*.mp3"); do
-  # mp3info -a artist -t song file.mp3
-  echo  $f
-done
+# for f in $(find . -type f -name "*.mp3" -print); do
+#   # mp3info -a artist -t song file.mp3
+#   echo  $f
+# done
+
+find . -type f -name '*.mp3' -exec sh -c '
+  for file do
+    echo "$file"
+  done
+' exec-sh {} +
 
 exit 0
