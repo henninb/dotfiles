c365aa6875b7691ac4d8138434f2f496e99635b8
Brian Henning <henninb@msn.com>
2021-12-13 17:14:08 -0600

    wip

--- mp3info.sh
+++ mp3info.sh
@@ -7,17 +7,8 @@ for f in $(find . -type f -name "*.mp3" -print); do
   song=$(echo $f | awk '{split($0,a, "-"); print a[2];}' | sed 's/\.mp3//g' | sed 's/_/ /g' | awk '{$1=$1};1')
   echo $artist
   echo $song
-  mp3info -a "$artist" -t "$song" "$f"
+  # mp3info -a "$artist" -t "$song" "$f"
+  mp3info "$f"
 done
-
-# find . -type f -name '*.mp3' -exec sh -c '
-#   for file do
-#     echo "$file"
-#   done
-# ' exec-sh {} +
-
-# find . -name "*.mp3" -print0 | while IFS= read -r -d '' file; do
-#     echo "file = $file"
-# done
 
 exit 0
