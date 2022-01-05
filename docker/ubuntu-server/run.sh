0469f32d8f9d5deb778166d2085f8fa28e01d757
Brian Henning <henninb@msn.com>
2022-01-05 07:02:49 -0600

    wip

--- docker/ubuntu-server/run.sh
+++ docker/ubuntu-server/run.sh
@@ -3,14 +3,19 @@
 cp $HOME/.ssh/id_rsa .
 cp $HOME/.ssh/known_hosts .
 
+docker stop ubuntu-server
+docker rm ubuntu-server -f
+
 if ! docker build -t ubuntu-server .; then
-  echo  failed docker build.
+  echo  "failed docker build"
 fi
 
+# if ! docker-compose up -d; then
+#   echo "failed docker-compose"
+# fi
+
 rm -rf id_rsa
-sudo docker stop ubuntu-server
-sudo docker rm ubuntu-server -f
-sudo docker run -dit --name ubuntu-server -h ubuntu-server ubuntu-server
-sudo docker exec -it --user henninb ubuntu-server /bin/bash
+docker run -dit --name ubuntu-server -h ubuntu-server ubuntu-server
+docker exec -it --user henninb ubuntu-server /bin/bash
 
 exit 0
