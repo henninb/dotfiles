e160c9fab472f5a186f2da919cc180c0cec32b4c
Brian Henning <henninb@msn.com>
2022-01-05 18:25:56 -0600

    wip

--- docker/apache-server/run.sh
+++ docker/apache-server/run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-sudo docker build -t apache-server .
-sudo docker stop apache-server
-sudo docker rm apache-server -f
+docker build -t apache-server .
+docker stop apache-server
+docker rm apache-server -f
 #sudo sudo docker images
 #sudo docker run -dit --name apache-server -h apache-server -p 8080:80 apache-server
-sudo docker run --name=apache-server -h apache-server -h apache-server --restart unless-stopped -p 8080:80 -d apache-server
+docker run --name=apache-server -h apache-server -h apache-server --restart unless-stopped -p 8080:80 -d apache-server
 echo running server on port 8080
-sudo docker exec -it --user root apache-server /bin/bash
+docker exec -it --user root apache-server /bin/bash
 
 exit 0
