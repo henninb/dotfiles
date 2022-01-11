1a300eb2512f11792dfad881265297010c81d9df
Brian Henning <henninb@msn.com>
2022-01-11 16:01:18 -0600

    wip

--- kvm-status.sh
+++ kvm-status.sh
@@ -1,39 +1,39 @@
 #!/bin/sh
 
 sudo echo
-echo sudo virsh net-list --all
-sudo virsh net-list --all
+echo virsh net-list --all
+virsh net-list --all
 echo
 
-echo sudo virsh list --all
-sudo virsh list --all
+echo virsh list --all
+virsh list --all
 echo
 
 #sudo virsh net-autostart default
 
 # start and stop
-echo "sudo virsh start <domain>"
-echo "sudo virsh edit <domain>"
-echo "sudo virsh shutdown <domain>"
-echo "sudo virsh destroy <domain>"
-echo "sudo virsh undefine <domain>"
-echo "sudo virsh net-autostart <network>"
-echo "sudo virsh pool-autostart default"
-sudo virsh pool-autostart default
+echo "virsh start <domain>"
+echo "virsh edit <domain>"
+echo "virsh shutdown <domain>"
+echo "virsh destroy <domain>"
+echo "virsh undefine <domain>"
+echo "virsh net-autostart <network>"
+echo "virsh pool-autostart default"
+virsh pool-autostart default
 
-echo "sudo virsh net-autostart default"
-sudo virsh net-autostart default
+echo "virsh net-autostart default"
+virsh net-autostart default
 #sudo systemctl enable serial-getty@ttyS0.service
 #sudo systemctl start serial-getty@ttyS0.service
 
-echo sudo virsh net-dhcp-leases default
-sudo virsh net-dhcp-leases default
+echo virsh net-dhcp-leases default
+virsh net-dhcp-leases default
 brctl show
 
-echo sudo virsh pool-list --all
-sudo virsh pool-list --all
+echo virsh pool-list --all
+virsh pool-list --all
 
-echo sudo virsh -c qemu+ssh://vagrant@ho/system
+echo virsh -c qemu+ssh://vagrant@ho/system
 
 echo ls -l /var/lib/libvirt/dnsmasq
 
