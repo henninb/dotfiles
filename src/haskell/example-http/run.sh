81c402977e3c8c8b167516f40443ff0d8ab35cf0
Brian Henning <henninb@msn.com>
2022-01-11 17:55:30 -0600

    wip

--- src/haskell/example-http/run.sh
+++ src/haskell/example-http/run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+stack install aeson
+stack install ekg
+stack install QuickCheck
+stack install wai
+stack install warp
 stack ghc -- --make example_http.hs -fforce-recomp
 ./example_http
 
