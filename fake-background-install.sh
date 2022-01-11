9f15b260cf020b26cc5b5a16151d6d69545cf667
Brian Henning <henninb@msn.com>
2022-01-11 16:12:58 -0600

    wip

--- fake-background-install.sh
+++ fake-background-install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+sudo pacman -S v4l2loopback-dkms
 git clone https://github.com/fangfufu/Linux-Fake-Background-Webcam.git fake-background
+cd fake-background
+./install.sh
+echo install pip3
 echo ./fake.py --no-foreground --width 640 --height 480 --background-blur 0
 
 exit 0
