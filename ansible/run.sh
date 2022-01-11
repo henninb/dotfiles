89182fcd7323bdda8b16aae7272107f323d6f1b2
Brian Henning <henninb@msn.com>
2022-01-10 18:04:19 -0600

    wip

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ansible-playbook -i 192.168.10.25, -u pi  -e "shell=ls" run_shell.yml
-ansible-playbook -i 192.168.10.218, -u henninb git_pull_master.yml
-#ansible-playbook -i 192.168.100.25, -u pi  -e "shell=git pull origin master" run_shell.yml
+ansible-playbook -i 192.168.10.25, -u pi  -e "shell=pwd" run-shell.yml
+ansible-playbook -i 192.168.10.25, -u henninb git-pull-main.yml
+#ansible-playbook -i 192.168.10.25, -u pi  -e "shell=git pull origin master" run_shell.yml
 ansible-playbook -u henninb -K ubuntu_install.yml
 
 exit 0
