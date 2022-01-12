af61044682c4da35bd3654e348b11e8baf5bdae1
Brian Henning <henninb@msn.com>
2022-01-12 07:23:53 -0600

    wip

--- fastly-install.sh
+++ fastly-install.sh
@@ -2,16 +2,22 @@
 
 # wget https://github.com/fastly/cli/releases/download/v1.3.0/fastly_1.3.0_linux_amd64.rpm
 
-wget 'https://github.com/fastly/cli/archive/refs/tags/v1.3.0.tar.gz' -O ~/projects/fastly-v1.3.0.tar.gz
-cd ~/projects/ || exit
-tar xvf fastly-v1.3.0.tar.gz
-cd cli-1.3.0 || exit
-make
-make install
-rm -rf "$HOME/projects/fastly-v1.3.0.tar.gz"
+sudo pacman --noconfirm --needed -S fakeroot
+sudo pacman --noconfirm --needed -S rpm
 
-#go get github.com/fastly/go-fastly
-# go get github.com/fastly/cli
+wget 'https://github.com/fastly/cli/releases/download/v1.4.0/fastly_1.4.0_linux_amd64.rpm' -O "$HOME/projects/fastly_1.4.0_linux_amd64.rpm"
+
+exit 1
+
+# wget 'https://github.com/fastly/cli/archive/refs/tags/v1.4.0.tar.gz' -O ~/projects/fastly-v1.4.0.tar.gz
+# cd ~/projects/ || exit
+# tar xvf fastly-v1.4.0.tar.gz
+# cd cli-1.3.0 || exit
+# make
+# make install
+# rm -rf "$HOME/projects/fastly-v1.4.0.tar.gz"
+
+sudo rpm -i --nodeps ~/projects/fastly_1.4.0_linux_amd64.rpm
 
 mkdir ~/projects/edge
 cd ~/projects/edge || exit
