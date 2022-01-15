724d51b662c3005d7c17a82d9ccb3eb1e641a4bb
Brian Henning <henninb@msn.com>
2022-01-15 14:49:50 -0600

    wip

--- nvidia-driver-install.sh
+++ nvidia-driver-install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sudo emerge x11-drivers/nvidia-drivers
+
 wget https://us.download.nvidia.com/XFree86/Linux-x86_64/470.94/NVIDIA-Linux-x86_64-470.94.run
+sudo sh ./NVIDIA-Linux-x86_64-470.94.run
 
 exit 0
