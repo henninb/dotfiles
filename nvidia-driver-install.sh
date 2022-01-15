508962eb155c2c3a55df49d375e63bfbe9a1fe2a
Brian Henning <henninb@msn.com>
2022-01-15 14:55:18 -0600

    wip

--- nvidia-driver-install.sh
+++ nvidia-driver-install.sh
@@ -3,6 +3,8 @@
 sudo emerge x11-drivers/nvidia-drivers
 
 wget https://us.download.nvidia.com/XFree86/Linux-x86_64/470.94/NVIDIA-Linux-x86_64-470.94.run
+
+echo tty with the shortcut - Ctl-Alt-F1-F7
 sudo sh ./NVIDIA-Linux-x86_64-470.94.run
 
 lsmod | grep nvidia
