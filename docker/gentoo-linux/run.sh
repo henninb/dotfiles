16201100630aa49b4daafd82d068410b795e2e67
Brian Henning <henninb@msn.com>
2022-01-15 07:22:44 -0600

    wip

--- docker/gentoo-linux/run.sh
+++ docker/gentoo-linux/run.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+rm -rf gentoo
+mkdir -p gentoo
+cp -R ~/gentoo .
 cp ~/.ssh/id_rsa .
+
 docker build -t gentoobox -f Dockerfile .
+rm -rf gentoo
 
 rm -rf id_rsa
 docker stop gentoobox
 docker rm gentoobox -f
 docker run -dit --name gentoobox -h gentoobox gentoobox
+echo docker exec -it --user henninb gentoobox /bin/bash
 docker exec -it --user henninb gentoobox /bin/bash
 
 exit 0
