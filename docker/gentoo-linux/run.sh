a7a0f0e2486cb93940b1562b5a75a7d145233f57
Brian Henning <henninb@msn.com>
2022-01-15 06:38:38 -0600

    wip

--- docker/gentoo-linux/run.sh
+++ docker/gentoo-linux/run.sh
@@ -6,9 +6,6 @@ docker build -t gentoobox -f Dockerfile .
 rm -rf id_rsa
 docker stop gentoobox
 docker rm gentoobox -f
-#sudo sudo docker images
-#sudo docker run -dit --name gentoobox -h gentoobox gentoo/stage3-amd64
-#sudo docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -dit --name gentoobox -h gentoobox gentoobox
 docker run -dit --name gentoobox -h gentoobox gentoobox
 docker exec -it --user henninb gentoobox /bin/bash
 
