a8b1768d2718f4045c4415736e6cbd5804372cbf
Brian Henning <henninb@msn.com>
2022-01-16 07:01:02 -0600

    wip

--- nvidia-driver-install.sh
+++ nvidia-driver-install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+sudo xbps-install -y xtools
+git clone git@github.com:void-linux/void-packages.git
+cd void-packages || exit
+./xbps-src binary-bootstrap
+./xbps-src pkg nvidia
+xi nvidia
 echo sudo emerge --update --newuse x11-drivers/nvidia-drivers
 
 sudo pacman -S nvidia lib32-nvidia-utils
