611bd7dd09ba995b58bab7c8b45481b560bab66f
Brian Henning <henninb@msn.com>
2022-01-16 05:44:09 -0600

    wip

--- nvidia-driver-install.sh
+++ nvidia-driver-install.sh
@@ -3,10 +3,11 @@
 echo sudo emerge x11-drivers/nvidia-drivers
 
 sudo pacman -S nvidia lib32-nvidia-utils
+sudo emerge --update --newuse media-libs/vulkan-loader
 
 vulkaninfo | less
 
-wget https://us.download.nvidia.com/XFree86/Linux-x86_64/470.94/NVIDIA-Linux-x86_64-470.94.run
+wget 'https://us.download.nvidia.com/XFree86/Linux-x86_64/470.94/NVIDIA-Linux-x86_64-470.94.run'
 
 echo sudo chvt 3
 echo tty with the shortcut - Ctl-Alt-F1-F7
