4363be25eda0b307fce3299e14f81aa5b8bc9567
Brian Henning <henninb@msn.com>
2022-01-18 17:07:56 -0600

    wip

--- keyboard-magic.sh
+++ keyboard-magic.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
 cat > 99-magic-kbd.rules <<EOF
-ACTION=="add", ATTRS{idVendor}=="05ac", ATTRS{idProduct}=="0267", RUN+="/usr/local/bin/magic"
+ACTION=="add", ATTRS{idVendor}=="05ac", ATTRS{idProduct}=="0267", RUN+="/bin/sh /usr/local/bin/magic"
 EOF
+#SUBSYSTEM=="usb"
+# sudo udevadm trigger --action=add --subsystem-match=usb
 
 cat > magic << 'EOF'
 export DISPLAY=:0
-xmodmap -e "keycode 169 = Insert"
-xmodmap -e "keycode 66 = Escape"
-echo "$(date) magic-keyboard" > /tmp/magic.txt
+export XAUTHORITY=/home/henninb/.Xauthority
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+
+sleep 1
+# xhost +
+xmodmap -e "keycode 169 = Insert" 2>> /tmp/magic.txt
+xmodmap -e "keycode 66 = Escape" 2>> /tmp/magic.txt
 EOF
 
 sudo mkdir -p /etc/udev/rules.d/
@@ -24,7 +30,5 @@ sudo udevadm control --reload
 # xinput | grep -i 'Mechanical Keyboard'
 echo udevadm monitor --udev
 echo "lsusb | grep 'Magic Keyboard'"
-
-echo "$(date) magic-keyboard" > /tmp/magic.txt
 
 exit 0
