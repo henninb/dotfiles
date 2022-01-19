8580fc6c7a1dc37d762858b12e8eada0c80fde78
Brian Henning <henninb@msn.com>
2022-01-19 07:17:45 -0600

    wip

--- nvidia-driver-install.sh
+++ nvidia-driver-install.sh
@@ -4,7 +4,7 @@ if [ "$OS" = "Gentoo" ]; then
   echo sudo emerge --update --newuse x11-drivers/nvidia-drivers
   echo sudo emerge --update --newuse media-libs/vulkan-loader
 elif [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLinux" ]; then
-  sudo pacman -S nvidia lib32-nvidia-utils
+  sudo pacman -S nvidia lib32-nvidia-utils  --overwrite '*'
 elif [ "$OS" = "void" ]; then
   sudo xbps-install -y xtools
   git clone git@github.com:void-linux/void-packages.git
@@ -22,16 +22,18 @@ fi
 
 vulkaninfo | less
 
-wget 'https://us.download.nvidia.com/XFree86/Linux-x86_64/470.94/NVIDIA-Linux-x86_64-470.94.run'
+if [ ! -f NVIDIA-Linux-x86_64-470.94.run ]; then
+  wget 'https://us.download.nvidia.com/XFree86/Linux-x86_64/470.94/NVIDIA-Linux-x86_64-470.94.run'
+fi
 
 echo sudo chvt 3
 echo tty with the shortcut - Ctl-Alt-F1-F7
-sudo sh ./NVIDIA-Linux-x86_64-470.94.run
+echo sudo sh ./NVIDIA-Linux-x86_64-470.94.run
 
 lsmod | grep nvidia
 
 glxinfo | grep direct
 
-nvidia-settings
+echo nvidia-settings
 
 exit 0
