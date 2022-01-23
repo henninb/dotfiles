f971ffcb1f71aa3e3e3e00b030ceabd9c9f39ba3
Brian Henning <henninb@msn.com>
2022-01-23 09:18:02 -0600

    wip

--- gentoo/run.sh
+++ gentoo/run.sh
@@ -16,8 +16,8 @@ sudo cp -v package.license /etc/portage/package.license
 sudo cp -v /etc/portage/package.use/zz-autounmask /tmp/zz-autounmask.bak.$$
 sudo cp -v zz-autounmask /etc/portage/package.use/zz-autounmask
 
-sudo cp -v /etc/portage/package.use/layman /tmp/layman.bak.$$
-sudo cp -v layman /etc/portage/package.use/layman
+#sudo cp -v /etc/portage/package.use/layman /tmp/layman.bak.$$
+#sudo cp -v layman /etc/portage/package.use/layman
 
 sudo cp -v /etc/portage/make.conf /etc/portage/make.conf.bak.$$
 sudo cp -v make.conf /etc/portage/make.conf
@@ -25,8 +25,8 @@ sudo cp -v make.conf /etc/portage/make.conf
 sudo cp -v /etc/portage/package.unmask /etc/portage/package.unmask.bak.$$
 sudo cp -v package.unmask /etc/portage/package.unmask
 
-sudo cp -v /etc/portage/package.use/steam /tmp/steam.bak.$$
-sudo cp -v steam /etc/portage/package.use/steam
+#sudo cp -v /etc/portage/package.use/steam /tmp/steam.bak.$$
+#sudo cp -v steam /etc/portage/package.use/steam
 
 # echo "ruby_targets_ruby27" | sudo tee -a /etc/portage/use.mask
 
