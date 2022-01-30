8e91716babb189a22397e3f84193da6c920bb841
Brian Henning <henninb@msn.com>
2022-01-30 13:19:52 -0600

    wip

--- src/javascript/example-rest-server/index.js
+++ src/javascript/example-rest-server/index.js
@@ -1,8 +1,35 @@
 var restify = require('restify');
 
 function respond(req, res, next) {
-  res.send('<h1>hello '+ req.params.name + '</h1>');
+  // res.setHeader("content-type", "text/html");
+  res.contentType = "text/plain";
+  // res.setHeader('content-type', 'application/foo');
+  // res.send('hello '+ req.params.name);
+  res.end('<html><head><title>test</title></head><body><h1>hello</h1></body></html>');
+
+// var body = '<html><body>hello</body></html>';
+// res.writeHead(200, {
+//   'Content-Length': Buffer.byteLength(body),
+//   'Content-Type': 'text/html'
+// });
+// res.write(body);
+// res.end();
+
+  console.log("response");
 }
+
+// server.get("/", function (req, res, next) {
+//     fs.readFile(__dirname + "/index.html", "utf-8", function (err, file) {
+//         if (err) {
+//             return next(err);
+//         }
+
+//         res.setHeader("content-type", "text/html");
+//         res.send(file);
+//         return next();
+//     });
+// });
+
 
 var server = restify.createServer();
 server.get('/hello/:name', respond);
--- src/javascript/example-rest-server/run.sh
+++ src/javascript/example-rest-server/run.sh
@@ -2,10 +2,11 @@
 
 # echo nvm use 16.13.0
 echo npm init
-npm install --save restify bunyan node-config-manager
-npm install gulp gulp-jshint
+echo npm install --save restify bunyan node-config-manager
+echo npm install gulp gulp-jshint
 
 echo 'http://localhost:8080/hello/brian'
+echo "curl -is http://localhost:8080/hello/mark -H 'accept: text/plain'"
 npm start
 
 exit 0
