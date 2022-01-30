7bdd1786b6e8a276d5eb121b386c114d5a670c29
Brian Henning <henninb@msn.com>
2022-01-30 09:07:49 -0600

    wip

--- src/java/example-servlet/run.sh
+++ src/java/example-servlet/run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-gradle init --type basic
+javac -cp /opt/tomcat/lib/servlet-api.jar HelloServlet.java
+
+mkdir -p /opt/tomcat/webapps/myservlet/WEB-INF/lib
+mkdir -p /opt/tomcat/webapps/myservlet/WEB-INF/classes
+mv HelloServlet.class /opt/tomcat/webapps/myservlet/WEB-INF/classes
+
+# echo gradle init --type basic
 
 exit 0
