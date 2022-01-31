a1c2a24712baf0003109fb8411a97789352339bb
Brian Henning <henninb@msn.com>
2022-01-31 06:12:37 -0600

    wip

---
+++ src/javascript/example-cloudflare/index.js
@@ -0,0 +1,38 @@
+const redirectMap = new Map([
+  ['/test1', 'https://mysite.com/newlocation1'],
+  ['/test2', 'https://mysite.com/newlocation2'],
+  ['/test3', 'https://mysite.com/newlocation3'],
+  ['/test4', 'https://mysite.com/newlocation4'],
+])
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  return new Response('hello world', {
+    headers: { 'content-type': 'text/plain' },
+  })
+}
+
+
+
+
+// addEventListener('fetch', async event => {
+//   event.respondWith(handleRequest(event.request))
+// })
+
+// async function handleRequest(request) {
+//   let requestURL = new URL(request.url)
+//   let path       = requestURL.pathname.split('/redirect')[1]
+//   let location   = redirectMap.get(path)
+
+//   if (location) {
+//     return Response.redirect(location, 301)
+//   }
+
+//   return fetch(request)
+// }
