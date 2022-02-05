7cbb40eb8fe73ad3b59225e569eb0e675ac6a690
Brian Henning <henninb@msn.com>
2022-02-05 06:00:25 -0600

    wip

---
+++ pulseaudio-start.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo pulseaudio -k && sudo alsa force-reload
+pulseaudio --check && (pulseaudio -k || sudo killall pulseaudio)
+pactl list short sinks
+pgrep pulseaudio
+echo pacmd set-default-sink 1
+
+# pulseaudio --check
+# It normally prints no output, just exit code. 0 means running. Mine were not running, so I just advanced to step 3.
+
+# If any instance is running:
+
+# pulseaudio -k
+# Finally, start pulseaudio again as a daemon:
+
+# pulseaudio -D
+
+exit 0
