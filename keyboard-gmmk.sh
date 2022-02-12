08af499272174677744bed2e465e3a7daafbe2c5
Brian Henning <henninb@msn.com>
2022-02-12 07:03:50 -0600

    wip

---
+++ keyboard-gmmk.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+cat > 99-gmmk-kbd.rules <<EOF
+ACTION=="add", ATTRS{idVendor}=="0c45", ATTRS{idProduct}=="652f", ENV{XKBLAYOUT}="us", ENV{XKBOPTIONS}="altwin:swap_alt_win"
+EOF
+
+cat > 98-gmmk-kbd.rules <<EOF
+ACTION=="add", ATTRS{idVendor}=="0c45", ATTRS{idProduct}=="652f", RUN+="/usr/local/bin/gmmk"
+EOF
+
+cat > gmmk << 'EOF'
+export DISPLAY=:0
+kbd_ids=$(xinput -list | grep "SONiX USB DEVICE" | grep -v pointer | awk -F'=' '{print $2}' | cut -c 1-2)
+for kbd_id in $kbd_ids; do
+  setxkbmap -device "${kbd_id}" -option altwin:swap_alt_win
+done
+
+# vim: set ft=sh:
+EOF
+
+sudo mkdir -p /etc/udev/rules.d/
+
+chmod 755 gmmk
+# xinput | grep -i 'Mechanical Keyboard'
+sudo mv -v 99-gmmk-kbd.rules  /etc/udev/rules.d/
+sudo mv -v 98-gmmk-kbd.rules  /etc/udev/rules.d/
+sudo mv -v gmmk /usr/local/bin/
+
+sudo udevadm control --reload-rules
+
+export XAUTHORITY=/home/henninb/.Xauthority
+export DISPLAY=:0
+
+gmmk
+
+echo udevadm monitor --udev
+echo "lsusb | grep 'SONiX USB DEVICE Keyboard'"
+
+exit 0
