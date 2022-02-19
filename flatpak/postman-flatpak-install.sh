e64e6e6b376a8c4384bd35a1b6fe303564861a7d
Brian Henning <henninb@msn.com>
2022-02-19 16:47:24 -0600

    wip

---
+++ flatpak/postman-flatpak-install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cat > postman-flatpak <<EOF
+#!/bin/sh
+
+flatpak run --filesystem=home com.getpostman.Postman
+
+exit 0
+EOF
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+if ! flatpak --user -y install flathub com.getpostman.Postman; then
+  flatpak -y install flathub com.getpostman.Postman
+fi
+
+chmod 755 postman-flatpak
+mv postman-flatpak "$HOME/.local/bin/"
+
+exit 0
