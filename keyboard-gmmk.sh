5a7c34b903a2547c2677afa663aa0052991c3319
Brian Henning <henninb@msn.com>
2022-02-27 16:46:48 -0600

    wip

--- keyboard-gmmk.sh
+++ keyboard-gmmk.sh
@@ -2,20 +2,13 @@
 
 cat > 99-gmmk-kbd.rules <<EOF
 ACTION=="add", ATTRS{idVendor}=="0c45", ATTRS{idProduct}=="652f", ENV{XKBLAYOUT}="us", ENV{XKBOPTIONS}="altwin:swap_alt_win"
-ACTION=="add", ATTRS{idVendor}=="0C45", ATTRS{idProduct}=="652F", ENV{XKBLAYOUT}="us", ENV{XKBOPTIONS}="altwin:swap_alt_win"
+#ACTION=="add", ATTRS{idVendor}=="0C45", ATTRS{idProduct}=="652F", ENV{XKBLAYOUT}="us", ENV{XKBOPTIONS}="altwin:swap_alt_win"
 EOF
 
 cat > 98-gmmk-kbd.rules <<EOF
 ACTION=="add", ATTRS{idVendor}=="0c45", ATTRS{idProduct}=="652f", ENV{DISPLAY}=":0", ENV{XAUTHORITY}="/home/henninb/.Xauthority", RUN+="/bin/sh /usr/local/bin/gmmk"
-ACTION=="add", ATTRS{idVendor}=="0C45", ATTRS{idProduct}=="652F", ENV{DISPLAY}=":0", ENV{XAUTHORITY}="/home/henninb/.Xauthority", RUN+="/bin/sh /usr/local/bin/gmmk"
+#ACTION=="add", ATTRS{idVendor}=="0C45", ATTRS{idProduct}=="652F", ENV{DISPLAY}=":0", ENV{XAUTHORITY}="/home/henninb/.Xauthority", RUN+="/bin/sh /usr/local/bin/gmmk"
 EOF
-
-# , ENV{DISPLAY}=":0" \
-# , ENV{XAUTHORITY}="/run/user/1000/gdm/Xauthority" \
-# , RUN+="/usr/bin/xinput --disable 16"
-# ENV{XAUTHORITY}="/var/run/gdm/auth-for-vazquez-OlbTje/database"
-# ENV{XAUTHORITY}="/home/henninb/.Xauthority"
-# b85mds3h /etc/udev/rules.d  💡 ls -l /var/run/sddm/\{c168d8f6-fd8a-4b72-9c7f-6fb3aff4b5a7\}
 
 cat > gmmk << 'EOF'
 export DISPLAY=:0
