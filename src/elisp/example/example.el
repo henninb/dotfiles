1967cde1c7a8df49cb49abc159579c8bb0edde12
Brian Henning <henninb@msn.com>
2022-03-17 19:33:01 -0500

    wip

--- src/elisp/example/example.el
+++ src/elisp/example/example.el
@@ -1,3 +1,20 @@
 ;emacs -Q --script example.el
 (print "hello world")
      (newline)
+
+(defun insert-p-tag ()
+  "Insert <p></p> at cursor point."
+  (interactive)
+  (insert "<p></p>")
+  (backward-char 4))
+
+(insert-p-tag)
+
+(defun main ()
+  (print (format "Called with %s" command-line-args))
+  (print (format "I did it. you passed in %s" command-line-args-left)))
+
+(when (member "-scriptload" command-line-args)
+  (main))
+
+; print( (+ 2 3));
