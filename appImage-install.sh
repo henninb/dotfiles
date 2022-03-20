3a36fcbb1fd3d677538485db3d009d069c1e6c73
Brian Henning <henninb@msn.com>
2022-03-19 19:22:45 -0500

    wip

---
+++ appImage-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+wget https://github.com/AppImageCrafters/appimage-cli-tool/releases/download/v0.1.4/appimage-cli-tool-0.1.4-x86_64.AppImage -O appimage-cli
+# wget https://github.com/AppImageCrafters/appimage-cli-tool/releases/latest/download/appimage-cli-tool -O appimage-cli
+chmod 755 appimage-cli
+sudo cp appimage-cli /usr/local/bin/
+# sudo chmod +x /usr/local/bin/appimage-cli-tool
+
+exit 0
