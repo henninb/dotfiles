80a6a1bdb64be56c463062610ee312e25ad10fce
Brian Henning <henninb@msn.com>
2022-03-23 07:42:45 -0500

    wip

--- src/javascript/example-react/src/App.jsx
+++ src/javascript/example-react/src/App.jsx
@@ -7,20 +7,25 @@ import AllRoutes from "./components/AllRoutes";
 export default class App extends Component {
   render() {
     return <>
-      <nav className="navbar navbar-expand-sm bg-secondary navbar-dark">
-  <div className="container-fluid">
-    <ul className="navbar-nav">
-      <li className="nav-item">
-        <a className="nav-link active" href="#">Active</a>
+      <nav class="navbar navbar-expand-lg navbar-dark primary-color">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
       </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
       </li>
-      <li className="nav-item">
-        <a className="nav-link disabled" href="#">Disabled</a>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
   </div>
