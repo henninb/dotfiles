3c6f8ea1b06f2b1d0c928544ab200099331a25a7
Brian Henning <henninb@msn.com>
2022-03-28 05:28:01 -0500

    wip

---
+++ cribbage-cards.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+wget http://cribbageclassic.com/images/Card_Heart_2.jpg
+wget http://cribbageclassic.com/images/Card_Heart_3.jpg
+wget http://cribbageclassic.com/images/Card_Heart_4.jpg
+wget http://cribbageclassic.com/images/Card_Heart_5.jpg
+wget http://cribbageclassic.com/images/Card_Heart_6.jpg
+wget http://cribbageclassic.com/images/Card_Heart_7.jpg
+wget http://cribbageclassic.com/images/Card_Heart_8.jpg
+wget http://cribbageclassic.com/images/Card_Heart_9.jpg
+wget http://cribbageclassic.com/images/Card_Heart_10.jpg
+wget http://cribbageclassic.com/images/Card_Heart_Jack.jpg
+wget http://cribbageclassic.com/images/Card_Heart_Ace.jpg
+wget http://cribbageclassic.com/images/Card_Heart_Queen.jpg
+wget http://cribbageclassic.com/images/Card_Heart_King.jpg
+
+wget http://cribbageclassic.com/images/Card_Diamond_2.jpg
+wget http://cribbageclassic.com/images/Card_Diamond_3.jpg
+wget http://cribbageclassic.com/images/Card_Diamond_4.jpg
+wget http://cribbageclassic.com/images/Card_Diamond_5.jpg
+wget http://cribbageclassic.com/images/Card_Diamond_6.jpg
+wget http://cribbageclassic.com/images/Card_Diamond_7.jpg
+wget http://cribbageclassic.com/images/Card_Diamond_8.jpg
+wget http://cribbageclassic.com/images/Card_Diamond_9.jpg
+wget http://cribbageclassic.com/images/Card_Diamond_10.jpg
+wget http://cribbageclassic.com/images/Card_Diamond_Jack.jpg
+wget http://cribbageclassic.com/images/Card_Diamond_Ace.jpg
+wget http://cribbageclassic.com/images/Card_Diamond_Queen.jpg
+wget http://cribbageclassic.com/images/Card_Diamond_King.jpg
+
+wget http://cribbageclassic.com/images/Card_Club_2.jpg
+wget http://cribbageclassic.com/images/Card_Club_3.jpg
+wget http://cribbageclassic.com/images/Card_Club_4.jpg
+wget http://cribbageclassic.com/images/Card_Club_5.jpg
+wget http://cribbageclassic.com/images/Card_Club_6.jpg
+wget http://cribbageclassic.com/images/Card_Club_7.jpg
+wget http://cribbageclassic.com/images/Card_Club_8.jpg
+wget http://cribbageclassic.com/images/Card_Club_9.jpg
+wget http://cribbageclassic.com/images/Card_Club_10.jpg
+wget http://cribbageclassic.com/images/Card_Club_Jack.jpg
+wget http://cribbageclassic.com/images/Card_Club_Ace.jpg
+wget http://cribbageclassic.com/images/Card_Club_Queen.jpg
+wget http://cribbageclassic.com/images/Card_Club_King.jpg
+
+wget http://cribbageclassic.com/images/Card_Spade_2.jpg
+wget http://cribbageclassic.com/images/Card_Spade_3.jpg
+wget http://cribbageclassic.com/images/Card_Spade_4.jpg
+wget http://cribbageclassic.com/images/Card_Spade_5.jpg
+wget http://cribbageclassic.com/images/Card_Spade_6.jpg
+wget http://cribbageclassic.com/images/Card_Spade_7.jpg
+wget http://cribbageclassic.com/images/Card_Spade_8.jpg
+wget http://cribbageclassic.com/images/Card_Spade_9.jpg
+wget http://cribbageclassic.com/images/Card_Spade_10.jpg
+wget http://cribbageclassic.com/images/Card_Spade_Jack.jpg
+wget http://cribbageclassic.com/images/Card_Spade_Ace.jpg
+wget http://cribbageclassic.com/images/Card_Spade_Queen.jpg
+wget http://cribbageclassic.com/images/Card_Spade_King.jpg
+
+
+wget http://cribbageclassic.com/images/card_back_blue.jpg
+wget http://cribbageclassic.com/images/card_back_red.jpg
+wget http://cribbageclassic.com/images/card_back_green.jpg
+
+exit 0
