d267aeac699e2fd03168c431a00e229d9638fee7
Brian Henning <henninb@msn.com>
2022-04-18 10:46:37 -0500

    wip

--- src/cloudflare/api-wild-hockey/src/index.ts
+++ src/cloudflare/api-wild-hockey/src/index.ts
@@ -1,15 +1,30 @@
-async function handleRequest(_request: Request) {
+// import jwt from '@tsndr/cloudflare-worker-jwt'
+
+async function handleRequest(request: Request) {
   console.log('calling method');
-    const response = await fetch('https://fixturedownload.com/feed/json/nhl-2021/minnesota-wild', {
+
+  const token = request.headers.get('authorization')?.split(" ")[1] || '';
+  console.log(token);
+  // let response = await fetch('/api/auth', {
+  //       method: 'GET',
+  //       redirect: 'follow',
+  //       headers: {
+  //         "Content-Type": "application/text",
+  //         "Authorization": "Bearer " + token
+  //       },
+  // });
+  // const verified = await response.text();
+  // console.log("verified: " + verified);
+  const response = await fetch('https://fixturedownload.com/feed/json/nhl-2021/minnesota-wild', {
         method: 'GET',
         redirect: 'follow',
         headers: {
           "Content-Type": "application/json",
         },
-    });
-    console.log('apiCall was made.');
-    const json = await response.json();
-    console.log(json);
+  });
+  console.log('apiCall was made.');
+  const json = await response.json();
+  console.log(json);
   return new Response(JSON.stringify(json), {
     headers: { 'content-type': 'application/json' },
   })
