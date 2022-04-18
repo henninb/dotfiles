bd06e23d441510da176061dd66d69b3a2e10b677
Brian Henning <henninb@msn.com>
2022-04-18 17:58:06 -0500

    wip

--- src/javascript/example-react/src/components/ProtectedRoutes.jsx
+++ src/javascript/example-react/src/components/ProtectedRoutes.jsx
@@ -1,64 +1,73 @@
+import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-// delete a cookie
-// document.cookie = 'access-token=; Max-Age=0'
+export default function ProtectedRoutes() {
+  const [auth, setAuth] = useState(null);
 
-function getCookies() {
-    console.log('getCookies');
-    let cookies = document.cookie.split(';');
-    let authTokens = {
-        'access-token': null
-    };
+  async function getCookies() {
+      console.log('getCookies');
+      let cookies = document.cookie.split(';');
+      let authTokens = {
+          'access-token': null
+      };
 
-    for (const cookie of cookies) {
-        let cookiePair = cookie.split('=');
+      for (const cookie of cookies) {
+          let cookiePair = cookie.split('=');
 
-        if (authTokens.hasOwnProperty(cookiePair[0].trim().toLowerCase()))
-            authTokens[cookiePair[0].trim()] = decodeURIComponent(cookiePair[1]);
+          if (authTokens.hasOwnProperty(cookiePair[0].trim().toLowerCase())) {
+              authTokens[cookiePair[0].trim()] = decodeURIComponent(cookiePair[1]);
+          }
+      }
+
+      return authTokens;
+  }
+
+  const authApiCall = async (payload) => {
+    console.log('authApiCall');
+
+    try {
+    const response = await axios.get("/api/auth", {
+      timeout: 0,
+      headers: {
+        "Content-Type": "application/text",
+        "Authorization": "Bearer " + payload
+      },
+    });
+
+    return response.data;
+    } catch ( error ) {
+      console.log(error.data);
+      window.location.href = '/login'
     }
+    return false
+  };
 
-    return authTokens;
-}
+  const fetchUserAuth = useCallback(async () => {
 
-const authApiCall = async (payload) => {
-  console.log('authApiCall');
-
-  try {
-  const response = await axios.get("/api/auth", {
-    timeout: 0,
-    headers: {
-      "Content-Type": "application/text",
-      "Authorization": "Bearer " + payload
-    },
-  });
-
-  return response.data;
-  } catch (error ) {
-    console.log(error.data);
+    console.log('fetchUserAuth');
+    const token = await getCookies();
+    if( token && token["access-token"] ) {
+      const authBoolean = await authApiCall(token["access-token"]);
+      if( authBoolean === true ) {
+        setAuth(true)
+        return true
+      }
+    }
+    setAuth(false)
     window.location.href = '/login'
-  }
-  return false
-  //return response.data.toLowerCase() === 'true'
-};
+    return false
+  }, []);
 
-const useAuth = async () => {
-  console.log('useAuth');
-  const token = getCookies();
-  if( token && token["access-token"] ) {
-    const authBoolean = await authApiCall(token["access-token"]);
-    if( authBoolean === true ) {
-      //console.log("true: " + token["access-token"]);
-      return true
+  useEffect(() => {
+    if( !auth ) {
+      fetchUserAuth();
     }
-  }
-  return false
-};
+  }, [fetchUserAuth, auth])
 
-export default async function ProtectedRoutes() {
-  const isAuth = await useAuth();
-  //console.log('is auth: ' + isAuth);
-  console.log('isAuth: ' + JSON.stringify(isAuth));
   //pass the auth token to the Outlet
-  return isAuth ? <Outlet /> : <Navigate to="/login" />;
+    //auth ? <Outlet /> : <Navigate to="/login" />
+  return (
+    auth ? <Outlet /> : null
+  )
 }
