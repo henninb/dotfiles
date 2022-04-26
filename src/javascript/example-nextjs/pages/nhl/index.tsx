bff134fb2163fc3125e84c34bee7afb60ff81d3f
Brian Henning <henninb@msn.com>
2022-04-26 05:06:24 -0500

    wip

--- src/javascript/example-nextjs/pages/nhl/index.tsx
+++ src/javascript/example-nextjs/pages/nhl/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
+import { useState, useEffect } from 'react'
 
 export default function Hockey() {
+  const [data, setData] = useState(null)
 
   const handleClick = async () => {
     console.log('handle click');
@@ -22,7 +24,17 @@ export default function Hockey() {
     console.log('apiCall was made.');
     const json = await apiResponse.json();
     console.log(json);
+    setData(json);
+
+    json.forEach(function(item, index){
+      console.log(index);
+  // div.innerHTML = div.innerHTML + ("Name:" + item.name + " URL:" + item.url + "<br>");
+    });
   }
+
+  useEffect(() => {
+    handleClick();
+  }, [])
 
   return (
     <div>
@@ -34,6 +46,16 @@ export default function Hockey() {
       <main>
         <h1>Hockey Schedule</h1>
          <button onClick={handleClick}>click</button>
+
+    <ul>
+      { data ?
+      data.map((item) => (
+        <li key={item.id}>{item.Location}</li>
+      )) : <p>nothing</p>
+      }
+    </ul>
+
+
       </main>
 
       <footer>
