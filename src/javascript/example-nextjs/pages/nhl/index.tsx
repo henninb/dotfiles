87189690e56d408f350e5d9aee8c3d543f0b7631
Brian Henning <henninb@msn.com>
2022-04-27 07:16:47 -0500

    wip

--- src/javascript/example-nextjs/pages/nhl/index.tsx
+++ src/javascript/example-nextjs/pages/nhl/index.tsx
@@ -4,11 +4,27 @@ import { useState, useEffect } from 'react'
 export default function Hockey() {
   const [data, setData] = useState(null)
 
-  const handleClick = async () => {
-    console.log('handle click');
+  function generateTable(games) {
+   let table = '<table border="1">';
+   table += `<tr><th>ID</th><th>HomeTeam</th><th>AwayTeam</th></tr>`;
 
+   if( games ) {
+     games.map((game, index) => {
+         table = table + `<tr>`;
+         table = table + `<td>${index}</td>`;
+         table = table + `<td>${game.HomeTeam}</td>`;
+         table = table + `<td>${game.AwayTeam}</td>`;
+         table += `</tr>`;
+      });
+    }
+    table += "</table>";
+    document.getElementById("games-div").innerHTML = table;
+  }
 
-        // const url = new URL('/api/nhl')
+  const loadSchedule = async () => {
+    console.log('loadSchedule called');
+
+    // const url = new URL('/api/nhl')
 
     // const params = {
     // };
@@ -25,15 +41,21 @@ export default function Hockey() {
     const json = await apiResponse.json();
     console.log(json);
     setData(json);
+    generateTable(json);
 
-    json.forEach(function(item, index){
-      console.log(index);
-  // div.innerHTML = div.innerHTML + ("Name:" + item.name + " URL:" + item.url + "<br>");
-    });
+    // json.forEach(function(item, index){
+    //   console.log(index);
+    // });
   }
 
+
   useEffect(() => {
-    handleClick();
+    if( !data) {
+      loadSchedule();
+    }
+    // if (data) {
+    //   generateTable();
+    // }
   }, [])
 
   return (
@@ -45,22 +67,13 @@ export default function Hockey() {
 
       <main>
         <h1>Hockey Schedule</h1>
-         <button onClick={handleClick}>click</button>
 
-    <ul>
-      { data ?
-      data.map((item) => (
-        <li key={item.id}>{item.Location}</li>
-      )) : <p>nothing</p>
-      }
-    </ul>
+
+   <div id="games-div" />
 
 
       </main>
 
-      <footer>
-       footer
-      </footer>
     </div>
   )
 }
