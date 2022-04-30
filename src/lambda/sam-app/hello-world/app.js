124db9b66d362f0506cbe0023ebf47984af478af
Brian Henning <henninb@msn.com>
2022-04-30 06:43:39 -0500

    wip

---
+++ src/lambda/sam-app/hello-world/app.js
@@ -0,0 +1,19 @@
+let response;
+
+exports.lambdaHandler = async (event, context) => {
+    try {
+        // const ret = await axios(url);
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'hello world',
+                // location: ret.data.trim()
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
