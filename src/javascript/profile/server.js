45efb69f7f97dd5313fd43f398916ba65c33b956
Brian Henning <henninb@msn.com>
2022-05-10 07:26:55 -0500

    wip

--- src/javascript/profile/server.js
+++ src/javascript/profile/server.js
@@ -1,15 +1,14 @@
 const express = require('express')
-const axios = require('axios')
-const cors = require('cors');
-const bodyParser = require('body-parser');
+//const cors = require('cors');
+//const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000
 const app = express()
 
 app.listen(port, () => { console.log(`listening on port ${port}`) });
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(cors());
+// app.use(cors());
 
 app.post('/api/login', (_req, res) => {
    res.send('api login POST called.');
