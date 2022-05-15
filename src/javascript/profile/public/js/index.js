88ce21752782b225121d9e025d9d25d12006a62a
Brian Henning <henninb@msn.com>
2022-05-15 05:52:03 -0500

    wip

--- src/javascript/profile/public/js/index.js
+++ src/javascript/profile/public/js/index.js
@@ -1,12 +1,26 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+// const navToggle = document.querySelector('.nav-toggle');
+// const navLinks = document.querySelectorAll('.nav__link')
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
+// navToggle.addEventListener('click', () => {
+//     document.body.classList.toggle('nav-open');
+// });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
+// navLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//         document.body.classList.remove('nav-open');
+//     })
+// })
+
+
+let menuToggle = document.querySelector(".menuToggle");
+let navigation = document.querySelector(".navigation");
+menuToggle.onclick = function () {
+  navigation.classList.toggle("active");
+};
+
+let navWrapper = document.querySelector('.nav-wrapper'),
+    navToogler =  document.querySelector('.nav-toogler')
+
+navToogler.addEventListener('click', function (event) {
+    navWrapper.classList.toggle('active')
 })
