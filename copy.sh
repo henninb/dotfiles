9b06322ea39c799a0b25f0fc171b6dc0880fb866
Brian Henning <henninb@msn.com>
2022-05-22 14:23:35 -0500

    wip

--- copy.sh
+++ copy.sh
@@ -8,5 +8,6 @@ scp -r postgresql-status.sh henninb@192.168.10.10:/home/henninb/
 scp -r monofur-fonts.zip henninb@192.168.10.10:/home/henninb/
 scp -r java-install.sh henninb@192.168.10.10:/home/henninb/
 scp -r cli-install.sh henninb@192.168.10.10:/home/henninb/
+scp -r docker-install.sh henninb@192.168.10.10:/home/henninb/
 
 exit 0
