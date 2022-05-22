42b8a344de620edee0c8d957688197f0eaf4a64e
Brian Henning <henninb@msn.com>
2022-05-22 07:24:54 -0500

    wip

---
+++ i3lock-install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd ~/projects
+git clone https://github.com/i3/i3lock.git
+# gh repo clone i3/i3lock
+cd i3lock
+rm -rf build/
+mkdir -p build && cd build/
+
+meson .. -Dprefix=/usr
+ninja
+
+echo xss-lock
+echo slock
+
+exit 0
