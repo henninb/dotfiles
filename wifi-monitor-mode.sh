36fbaf84926b4704b3b9f1a9df1adad9db1b7927
Brian Henning <henninb@msn.com>
2022-05-27 14:46:03 -0500

    wip

--- wifi-monitor-mode.sh
+++ wifi-monitor-mode.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
+sudo systemctl stop NetworkManager
+
 sudo ip link set wlp0s20u7 down
-sudo iw dev wlp0s20u7 set type monitor
+sudo airmon-ng check kill
+
+sudo airmon-ng start wlp0s20u7
+
+echo sudo airmon-ng start wlp0s20u7 1
+echo sudo airodump-ng -c 1 wlp0s20u7mon
+# sudo iw dev wlp0s20u7 set type monitor
 sudo ip link set wlp0s20u7 up
+
+echo sudo airodump-ng wlp0s20u7
+
+# besside-ng -R 'Target Network' wlan0mon
 
 exit 0
