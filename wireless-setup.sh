a9710cb755758ee92a480cdff79b8348525e58f0
Brian Henning <henninb@msn.com>
2022-05-27 09:24:11 -0500

    wip

--- wireless-setup.sh
+++ wireless-setup.sh
@@ -12,13 +12,25 @@ network={
 }
 EOF
 
+echo sudo modprobe -w
+sudo modprobe | grep idProduct=c811
+echo New USB device found, idVendor=0bda, idProduct=c811, bcdDevice= 2.00
+echo yay -S rtl88xxau-aircrack-dkms-git
+
+echo sudo usb_modeswitch -KW -v 0bda -p 1a2b
+# rtl88xxau-aircrack-dkms-git and rtl8821au-dkms-git
+echo sudo dkms install rtl8821CU/5.4.1 -k $(uname -r)
+
 cd "$HOME/projects" || exit
 git clone https://github.com/brektrou/rtl8821CU.git
-git clone https://github.com/lwfinger/rtl8723bu.git
+# git clone https://github.com/lwfinger/rtl8723bu.git
 cd rtl8723bu || exit
 make
-# sudo make install
+sudo make install
+sudo modprobe 8812au
 # sudo modprobe -v 8723bu
+
+exit 0
 
 #nmcli
 nmcli dev status
