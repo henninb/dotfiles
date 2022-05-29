8e2f03e2303eb4309a5fbe28fd854b2093e72ce0
Brian Henning <henninb@msn.com>
2022-05-29 07:21:23 -0500

    wip

--- copy-debian.sh
+++ copy-debian.sh
@@ -18,7 +18,7 @@ scp -r .gitconfig henninb@192.168.10.10:/home/henninb/
 scp -r ssl/ henninb@192.168.10.10:/home/henninb/
 scp -r python-pip-install.sh henninb@192.168.10.10:/home/henninb/
 scp -r update.sh henninb@192.168.10.10:/home/henninb/
-# scp -r .config/nvim/ henninb@192.168.10.10:/home/henninb/
+# scp -r .config/nvim/ henninb@192.168.10.10:/home/henninb/.config
 
 
 exit 0
