7fd8f4d6be88d4914db34e1edb0d5de785c82f69
Brian Henning <henninb@msn.com>
2022-06-22 07:23:49 -0500

    wip

--- src/api/youtube/run.sh
+++ src/api/youtube/run.sh
@@ -46,22 +46,22 @@ for channel in $(cat channels.txt); do
   # echo "https://www.googleapis.com/youtube/v3/channels?id=${channelId}&key=${apikey}&part=contentDetails"
 
   if [ "${channelName}" = "techhut" ]; then
-    count=1
+    count=10
   elif [ "${channelName}" = "mrturvy" ]; then
-    count=1
+    count=15
   elif [ "${channelName}" = "coffeehouse" ]; then
-    count=1
+    count=15
   else
-    count=1
+    count=7
   fi
 
   payload=$(curl -s "https://www.googleapis.com/youtube/v3/playlistItems?playlistId=${upload}&key=${apikey}&part=snippet&maxResults=${count}&order=date")
   echo "https://www.googleapis.com/youtube/v3/playlistItems?playlistId=${upload}&key=${apikey}&part=snippet&maxResults=${count}&order=date"
   rm -rf "*.csv"
-  echo "${payload}" | jq -r '.items[].snippet.title' | tr " " "-" | sed 's/[^a-zA-Z0-9-]//g' | tr '[:upper:]' '[:lower:]' > title.csv
-  # echo "${payload}" | jq -r '.items[].snippet.title' | sed 's/[[:punct:]]//g' | tr 'ᴴᴰ' ' ' | tr " " "-" | tr '[:upper:]' '[:lower:]' > title.csv
-  echo "${payload}" | jq -r '.items[].snippet.publishedAt' > published.csv
-  echo "${payload}" | jq -r '.items[].snippet.resourceId.videoId' > videoid.csv
+
+  echo "${payload}" | tr -d '\n' | jq -r '.items[].snippet.title' | tr " " "-" | sed 's/[^a-zA-Z0-9-]//g' | tr '[:upper:]' '[:lower:]' > title.csv
+  echo "${payload}" | tr -d '\n'| jq -r '.items[].snippet.publishedAt' > published.csv
+  echo "${payload}" | tr -d '\n' | jq -r '.items[].snippet.resourceId.videoId' > videoid.csv
 
   paste -d ',' published.csv title.csv videoid.csv > output.csv
 
