39f5baf7b30857123fd55a2e291d7d87bcc91a24
Brian Henning <henninb@msn.com>
2022-06-23 18:13:25 -0500

    wip

--- .xmonad/lib/Local/MouseBinding.hs
+++ .xmonad/lib/Local/MouseBinding.hs
@@ -1,17 +1,20 @@
 module Local.MouseBinding (myMouseBindings) where
 
 import XMonad
-import qualified Data.Map as M
-import qualified XMonad.StackSet as W
+-- import qualified Data.Map as M
+import Data.Map (Map(..), fromList)
+-- import qualified XMonad.StackSet as W
+import XMonad.StackSet (shiftMaster)
 
-myMouseBindings XConfig {XMonad.modMask = modm} = M.fromList
+myMouseBindings :: XConfig l -> Map (KeyMask, Button) (Window -> X ())
+myMouseBindings XConfig {XMonad.modMask = modm} = fromList
     [
 --     -- mod-button1, Set the window to floating mode and move by dragging
      ((modm, button1),
-   \ w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster),
+   \ w -> focus w >> mouseMoveWindow w >> windows shiftMaster),
 --     -- mod-button2, Raise the window to the top of the stack
-     ((modm, button2), \ w -> focus w >> windows W.shiftMaster),
+     ((modm, button2), \ w -> focus w >> windows shiftMaster),
 --     -- mod-button3, Set the window to floating mode and resize by dragging
       ((modm, button3),
-   \ w -> focus w >> mouseResizeWindow w >> windows W.shiftMaster)
+   \ w -> focus w >> mouseResizeWindow w >> windows shiftMaster)
     ]
