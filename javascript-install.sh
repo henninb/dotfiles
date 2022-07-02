e70d6ef129cbe45f86428f715cd4398f5f994b97
Brian Henning <henninb@msn.com>
2022-07-01 19:29:56 -0500

    wip

--- javascript-install.sh
+++ javascript-install.sh
@@ -9,56 +9,43 @@
 
 if [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Debian GNU/Linux" ]; then
   sudo apt install -y curl
-  curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-  sudo apt install -y nodejs
+  # curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+  echo sudo apt install -y nodejs
 elif [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLinux" ]; then
   echo sudo pacman --noconfirm --needed -S nodejs
   echo sudo pacman --noconfirm --needed -S npm
 elif [ "$OS" = "openSUSE Tumbleweed" ]; then
-  sudo zypper install -y nodejs
-  echo
+  echo sudo zypper install -y nodejs
 elif [ "$OS" = "void" ]; then
   echo
 elif [ "$OS" = "Gentoo" ]; then
-  sudo emerge  --update --newuse nodejs
+  echo sudo emerge  --update --newuse nodejs
 elif [ "$OS" = "Solus" ]; then
-  sudo eopkg install nodejs
+  echo sudo eopkg install nodejs
 elif [ "$OS" = "CentOS Linux" ]; then
   echo centos
 elif [ "$OS" = "FreeBSD" ]; then
-  sudo pkg install -y gcc python27 gmake
-  sudo pkg install -y npm
-  exit 0
+  echo sudo pkg install -y gcc python27 gmake
+  echo sudo pkg install -y npm
 else
   echo "$OS is not yet implemented."
   exit 1
 fi
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | zsh
 
-export NVM_DIR="$HOME/.nvm"
-
-chmod 755 "$HOME/.nvm/nvm.sh"
-zsh -c 'source ./.nvm/nvm.sh'
-. ./.nvm/nvm.sh
-source ./.nvm/nvm.sh
-#. "$HOME/.nvm/nvm.sh"
-#if [ $? -ne 0 ]; then
-#  source "$HOME/.nvm/nvm.sh"
-#fi
-
-# node version 16.14.0 as of 4/3/2022
-if ! nvm install 16.14.0; then
-  sudo nvm install 16.14.0
+if [ -x "$(command -v nvm)" ]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | zsh
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+  if ! nvm install 16.15.1; then
+    echo nvm failed.
+    exit 1
+  fi
 fi
-nvm use 16.14.0
 
-[ ! -x "$(command -v npm)" ] && echo "npm is not installed." && exit 1
-if [ ! -x "$(command -v n)" ]; then
-  if ! npm install n; then
-    sudo npm install -g n
-    sudo n stable
-  else
-    n stable
+if [ -x "$(command -v node)" ]; then
+  if ! nvm install 16.15.1; then
+    echo nvm failed.
+    exit 1
   fi
 fi
 
