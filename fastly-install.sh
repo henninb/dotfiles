4ca08c598d565a7293cf188c18b97f71d1b51f1f
Brian Henning <henninb@msn.com>
2022-07-03 15:49:06 -0500

    wip

--- fastly-install.sh
+++ fastly-install.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 
+ver=3.1.0
 # wget https://github.com/fastly/cli/releases/download/v1.3.0/fastly_1.3.0_linux_amd64.rpm
 
-sudo pacman --noconfirm --needed -S fakeroot
-sudo pacman --noconfirm --needed -S rpm
-sudo xbps-install -y rpm
+if [ -x "$(command -v pacman)" ]; then
+  sudo pacman --noconfirm --needed -S fakeroot
+  sudo pacman --noconfirm --needed -S rpm
+elif [ -x "$(command -v emerge)" ]; then
+  echo
+elif [ -x "$(command -v apt)" ]; then
+  echo
+elif [ -x "$(command -v xbps-install)" ]; then
+  sudo xbps-install -y rpm
+elif [ -x "$(command -v eopkg)" ]; then
+  echo
+elif [ -x "$(command -v dnf)" ]; then
+  echo
+elif [ -x "$(command -v brew)" ]; then
+  echo
+else
+  echo "$OS is not yet implemented."
+  exit 1
+fi
 
-wget 'https://github.com/fastly/cli/releases/download/v1.4.0/fastly_1.4.0_linux_amd64.rpm' -O "$HOME/projects/fastly_1.4.0_linux_amd64.rpm"
+wget "https://github.com/fastly/cli/releases/download/v${ver}/fastly_${ver}_linux_amd64.rpm" -O "$HOME/projects/fastly_${ver}_linux_amd64.rpm"
 
 # wget 'https://github.com/fastly/cli/archive/refs/tags/v1.4.0.tar.gz' -O ~/projects/fastly-v1.4.0.tar.gz
 # cd ~/projects/ || exit
@@ -16,11 +33,11 @@ wget 'https://github.com/fastly/cli/releases/download/v1.4.0/fastly_1.4.0_linux_
 # make install
 # rm -rf "$HOME/projects/fastly-v1.4.0.tar.gz"
 
-sudo rpm -i --nodeps ~/projects/fastly_1.4.0_linux_amd64.rpm
+sudo rpm -i --nodeps "$HOME/projects/fastly_${ver}_linux_amd64.rpm"
 
-mkdir ~/projects/edge
-cd ~/projects/edge || exit
-fastly compute init
+# mkdir ~/projects/edge
+# cd ~/projects/edge || exit
+# fastly compute init
 
 exit 0
 
