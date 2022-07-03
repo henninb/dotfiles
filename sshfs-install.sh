ddce8fc39a0bb8cb54020d25fb838ac08840d469
Brian Henning <henninb@msn.com>
2022-07-03 15:25:27 -0500

    wip

--- sshfs-install.sh
+++ sshfs-install.sh
@@ -10,7 +10,9 @@ fi
 mkdir -p "$HOME/mnt/downloads"
 mkdir -p "$HOME/mnt/audio"
 echo sshfs pi@raspi:/home/pi/downloads ~/mnt/downloads -o reconnect
-echo sshfs henninb@hornsup:/home/henninb/src/api/youtube/audio ~/mnt/audio -o reconnect
+sshfs pi@raspi:/home/pi/downloads ~/mnt/downloads -o reconnect
 
+echo sshfs henninb@hornsup:/home/henninb/src/api/youtube/audio ~/mnt/audio -o reconnect
+sshfs henninb@hornsup:/home/henninb/src/api/youtube/audio ~/mnt/audio -o reconnect
 
 exit 0
