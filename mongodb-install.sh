d9e173b3b5fe81e37e0e4def1f5507eadd6bce84
Brian Henning <henninb@msn.com>
2022-07-08 17:20:35 -0500

    wip

--- mongodb-install.sh
+++ mongodb-install.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 
-yay -S mongodb-bin
-# yay -S mongodb-tools
-yay -S mongodb-tools-bin
-# wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+if [ -x "$(command -v pacman)" ]; then
+  yay -S mongodb-bin
+  # yay -S mongodb-tools
+  yay -S mongodb-tools-bin
+  # wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+  sudo systemctl enable mongodb
+  sudo systemctl start mongodb
+elif [ -x "$(command -v emerge)" ]; then
+  sudo emerge --update --newuse dev-db/mongodb:4.4.10
+elif [ -x "$(command -v apt)" ]; then
+  echo
+elif [ -x "$(command -v xbps-install)" ]; then
+  echo
+elif [ -x "$(command -v eopkg)" ]; then
+  echo
+elif [ -x "$(command -v dnf)" ]; then
+  echo
+elif [ -x "$(command -v brew)" ]; then
+  echo
+else
+  echo "$OS is not yet implemented."
+  exit 1
+fi
 
-sudo systemctl enable mongodb
-sudo systemctl start mongodb
 
 echo mongo
 
