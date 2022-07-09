9e5012e90bf51e2e8221c7038535de8b0c78c678
Brian Henning <henninb@msn.com>
2022-07-09 08:33:41 -0500

    wip

--- obs-studio-install.sh
+++ obs-studio-install.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
 
-sudo pacman --noconfirm --needed -S obs-studio
-yay -S v4l2loopback-dkms
-sudo modprobe v4l2loopback devices=1 video_nr=10 card_label="OBS Cam" exclusive_caps=1
+if [ -x "$(command -v pacman)" ]; then
+  sudo pacman --noconfirm --needed -S obs-studio
+  yay -S v4l2loopback-dkms
+  sudo modprobe v4l2loopback devices=1 video_nr=10 card_label="OBS Cam" exclusive_caps=1
+elif [ -x "$(command -v emerge)" ]; then
+  emerge --update --newuse media-video/v4l2loopback
+  emerge --update --newuse media-video/obs-studio
+  echo
+elif [ -x "$(command -v apt)" ]; then
+  echo
+elif [ -x "$(command -v xbps-install)" ]; then
+  echo
+elif [ -x "$(command -v eopkg)" ]; then
+  echo
+elif [ -x "$(command -v dnf)" ]; then
+  echo
+elif [ -x "$(command -v brew)" ]; then
+  echo
+else
+  echo "$OS is not yet implemented."
+  exit 1
+fi
 
 exit 0
 
