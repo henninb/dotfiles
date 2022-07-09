b4b189bfd6ea1fc49776c8b2e0cca03ef068cd84
Brian Henning <henninb@msn.com>
2022-07-09 14:37:51 -0500

    wip

--- aws-cli.sh
+++ aws-cli.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -o awscliv2.zip
 sudo ./aws/install
 
 exit 0
--- podman-install.sh
+++ podman-install.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
 
-sudo xbps-install -y podman
-
-sudo pacman --noconfirm --needed -S podman
-# sudo usermod --add-subuids 10000-75535 henninb
-# sudo usermod --add-subgids 10000-75535 henninb
-
-echo henninb:10000:65536 | sudo tee -a /etc/subuid
-echo henninb:10000:65536 | sudo tee -a /etc/subgid
+if [ -x "$(command -v pacman)" ]; then
+  sudo pacman --noconfirm --needed -S podman
+  # sudo usermod --add-subuids 10000-75535 henninb
+  # sudo usermod --add-subgids 10000-75535 henninb
+  echo henninb:10000:65536 | sudo tee -a /etc/subuid
+  echo henninb:10000:65536 | sudo tee -a /etc/subgid
+elif [ -x "$(command -v emerge)" ]; then
+  sudo emerge --update --newuse podman
+  sudo usermod --add-subuids 1065536-1131071 "$(whoami)"
+  sudo usermod --add-subgids 1065536-1131071 "$(whoami)"
+elif [ -x "$(command -v apt)" ]; then
+  echo
+elif [ -x "$(command -v xbps-install)" ]; then
+  sudo xbps-install -y podman
+elif [ -x "$(command -v eopkg)" ]; then
+  echo
+elif [ -x "$(command -v dnf)" ]; then
+  echo
+elif [ -x "$(command -v brew)" ]; then
+  echo
+else
+  echo "$OS is not yet implemented."
+  exit 1
+fi
 
 sudo touch /etc/subuid /etc/subgid
 # cat /etc/sysctl.d/userns.conf
