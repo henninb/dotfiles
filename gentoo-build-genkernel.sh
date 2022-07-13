adb8049249d850ecc1bbad967f407c9c63c85481
Brian Henning <henninb@msn.com>
2022-07-13 16:56:12 -0500

    wip

--- gentoo-build-genkernel.sh
+++ gentoo-build-genkernel.sh
@@ -3,7 +3,7 @@
 ls -1 /usr/portage/sys-kernel/gentoo-sources/*.ebuild
 echo sudo emerge sys-kernel/gentoo-sources:5.18.10
 eselect kernel list
-echo eselect kernel set 4
+echo sudo eselect kernel set 4
 echo sudo genkernel all
 
 exit 0
