9d2daf7c84f5508db3cd1402c4e9096141f53f2c
Brian Henning <henninb@msn.com>
2022-07-17 06:38:12 -0500

    wip

--- scripts/alacritty-install.sh
+++ scripts/alacritty-install.sh
@@ -1,15 +1,32 @@
 #!/bin/sh
 
-sudo xbps-install -y mesa-dri
-sudo apt install -y libxcb-shape0-dev
-sudo apt install -y libxcb-xfixes0-dev
-sudo apt install -y libxkbcommon-dev
+if [ -x "$(command -v pacman)" ]; then
+  echo
+elif [ -x "$(command -v emerge)" ]; then
+  echo
+elif [ -x "$(command -v apt)" ]; then
+  sudo apt install -y libxcb-shape0-dev
+  sudo apt install -y libxcb-xfixes0-dev
+  sudo apt install -y libxkbcommon-dev
+elif [ -x "$(command -v xbps-install)" ]; then
+  sudo xbps-install -y mesa-dri
+elif [ -x "$(command -v eopkg)" ]; then
+  echo
+elif [ -x "$(command -v swupd)" ]; then
+  sudo swupd bundle-add devpkg-freetype
+  sudo swupd bundle-add devpkg-libxml2
+  sudo swupd bundle-add devpkg-expat
+  sudo swupd bundle-add performance-tools
+  sudo swupd bundle-add devpkg-libxcb
+elif [ -x "$(command -v dnf)" ]; then
+  echo
+elif [ -x "$(command -v brew)" ]; then
+  echo
+else
+  echo "$OS is not yet implemented."
+  exit 1
+fi
 
-sudo swupd bundle-add devpkg-freetype
-sudo swupd bundle-add devpkg-libxml2
-sudo swupd bundle-add devpkg-expat
-sudo swupd bundle-add performance-tools
-sudo swupd bundle-add devpkg-libxcb
 
 mkdir -p "$HOME/projects/github.com/alacritty"
 cd "$HOME/projects/github.com/alacritty" || exit
@@ -18,10 +35,6 @@ cd ./alacritty || exit
 git pull origin master
 cargo build --release
 sudo mv -v target/release/alacritty /usr/local/bin
-
-# cd "$HOME/projects" || exit
-# git clone https://github.com/sei40kr/tmux-airline-dracula.git
-# cd - || exit
 
 exit 0
 
