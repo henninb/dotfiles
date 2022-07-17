2b640be8554edb8186fa5cf5d50b9b2a39106f2f
Brian Henning <henninb@msn.com>
2022-07-17 09:07:22 -0500

    wip

--- scripts/sddm-install.sh
+++ scripts/sddm-install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-cat > sddm-theme.conf <<EOF
+cat > "$HOME/tmp/sddm-theme.conf" <<EOF
 [Theme]
 #Current=maldives
 Current=elarun
 EOF
 
-cat > 40-wheel-group.rules <<EOF
+cat > "$HOME/tmp/40-wheel-group.rules" <<EOF
 polkit.addRule(function(action, subject) {
     if (subject.isInGroup("wheel")) {
     	return polkit.Result.YES;
@@ -14,7 +14,7 @@ polkit.addRule(function(action, subject) {
 });
 EOF
 
-cat > xmonad.desktop << EOF
+cat > "$HOME/tmp/xmonad.desktop" << EOF
 [Desktop Entry]
 Name=xmonad
 Comment=xmonad dynamic tiling window manager
@@ -26,12 +26,12 @@ DesktopNames=xmonad
 Keywords=tiling;wm;windowmanager;window;manager;
 EOF
 
-cat > Xsetup << EOF
+cat > "$HOME/tmp/Xsetup" << EOF
 setxkbmap us
 EOF
 chmod 755 Xsetup
 
-cat > sddm.conf << EOF
+cat > "$HOME/tmp/sddm.conf" << EOF
 [Users]
 DefaultPath=/usr/local/sbin:/usr/local/bin:/usr/bin
 HideShells=
@@ -51,7 +51,7 @@ Session=xmonad
 EOF
 
 sudo mkdir -p /usr/share/xsessions/
-sudo cp -v xmonad.desktop /usr/share/xsessions/
+sudo mv -v "$HOME/tmp/xmonad.desktop" /usr/share/xsessions/
 
 if [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLinux" ]; then
   sudo pacman --noconfirm --needed -S sddm
@@ -59,7 +59,7 @@ if [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLin
   sudo systemctl disable lightdm
 
   sudo mkdir -p /etc/sddm.conf.d/
-  sudo mv -v sddm-theme.conf /etc/sddm.conf.d/
+  sudo mv -v "$HOME/tmp/sddm-theme.conf" /etc/sddm.conf.d/
   ls -l /usr/share/sddm/themes/
 
   systemctl --user stop xdg-desktop-portal{,gtk}
@@ -71,26 +71,25 @@ elif [ "${OS}" = "void" ]; then
   sudo mkdir -p /etc/sddm.conf.d/
   sudo ln -s /etc/sv/sddm /var/service/sddm
   sudo ln -s /etc/sv/dbus /var/service/dbus
-  sudo mv -v sddm.conf /etc/sddm.conf
-#  sudo sudo mv -v Xsetup /etc/sddm/scripts/Xsetup
+  sudo mv -v "$HOME/tmp/sddm.conf" /etc/sddm.conf
 elif [ "${OS}" = "Ubuntu" ]; then
   sudo apt install -y sddm
   sudo mkdir -p /etc/sddm.conf.d/
-  sudo mv -v sddm-theme.conf /etc/sddm.conf.d/
+  sudo mv -v "$HOME/tmp/sddm-theme.conf" /etc/sddm.conf.d/
 elif [ "${OS}" = "FreeBSD" ]; then
   sudo pkg install -y sddm
   sudo pkg install -y sysrc
   sysrc sddm_enable="YES"
-  sudo mv -v 40-wheel-group.rules /usr/local/etc/polkit-1/rules.d/40-wheel-group.rules
+  sudo mv -v "$HOME/tmp/40-wheel-group.rules" /usr/local/etc/polkit-1/rules.d/40-wheel-group.rules
   sudo sddm --example-config /usr/local/etc/sddm.conf
   sudo service sddm start
-  echo https://community.kde.org/FreeBSD/Setup#SDDM
+  echo "https://community.kde.org/FreeBSD/Setup#SDDM"
 elif [ "$OS" = "Gentoo" ]; then
   sudo emerge --update --newuse sddm
   sudo usermod -a -G video sddm
-  sudo mv -v sddm.conf /etc/sddm.conf
+  sudo mv -v "$HOME/tmp/sddm.conf" /etc/sddm.conf
   sudo mkdir -p /etc/sddm/scripts
-  sudo sudo mv -v Xsetup /etc/sddm/scripts/Xsetup
+  sudo sudo mv -v "$HOME/tmp/Xsetup" /etc/sddm/scripts/Xsetup
   sudo chmod 755 /etc/sddm/scripts/Xsetup
   sudo emerge --update --newuse gui-libs/display-manager-init
   # echo sudo vi /etc/conf.d/display-manager
