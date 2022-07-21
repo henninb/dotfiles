6851c0e6e6d06671a4650ae3cc5ff5cc48311149
Brian Henning <henninb@msn.com>
2022-07-20 19:00:00 -0500

    wip

--- vagrant/guest-alpine-vagrant/run.sh
+++ vagrant/guest-alpine-vagrant/run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-unset GEM_ROOT
-unset GEM_HOME
-unset GEM_PATH
+# unset GEM_ROOT
+# unset GEM_HOME
+# unset GEM_PATH
 
-sudo iptables -I FORWARD -m physdev --physdev-is-bridged -j ACCEPT
+vagrant validate
+#sudo iptables -I FORWARD -m physdev --physdev-is-bridged -j ACCEPT
 vagrant destroy
 vagrant up --provider libvirt
 echo vagrant ssh
