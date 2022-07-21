57e361af3992dfa2e990c28815bf41c27f0e9275
Brian Henning <henninb@msn.com>
2022-07-21 15:29:52 -0500

    wip

---
+++ scripts/snapd-install.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if command -v pacman; then
+  echo "archlinux"
+elif command -v emerge; then
+  echo "gentoo"
+  sudo emerge --update --newus sys-apps/systemd
+  sudo emerge --update --newus sys-apps/apparmor
+  echo GRUB_CMDLINE_LINUX_DEFAULT="apparmor=1 security=apparmor"
+  echo grub-mkconfig -o /boot/grub/grub.cfg
+  sudo emerge --update --newus app-containers/snapd
+  sudo gpasswd --add username adm
+  sudo systemctl enable --now snapd
+  sudo systemctl enable --now snapd.socket
+  sudo systemctl enable --now snapd.apparmor
+elif command -v apt; then
+  echo "debian"
+elif command -v xbps-install; then
+  echo "void"
+elif command -v eopkg; then
+  echo "solus"
+elif command -v dnf; then
+  echo "fedora"
+elif command -v brew; then
+  echo "macos"
+else
+  echo "$OS is not yet implemented."
+  exit 1
+fi
+
+exit 0
---
+++ snap/gimp/snapd/gimp-install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+snap install gimp
+
+exit 0
