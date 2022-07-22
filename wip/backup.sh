becb2408737d5822b3629dc29dcd6b74782eb502
Brian Henning <henninb@msn.com>
2022-07-22 14:51:20 -0500

    wip

--- wip/backup.sh
+++ wip/backup.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
-sudo dnf install timeshift
+if command -v pacman; then
+  echo "archlinux"
+elif command -v emerge; then
+  echo "gentoo"
+elif command -v apt; then
+  echo "debian"
+elif command -v xbps-install; then
+  echo "void"
+elif command -v eopkg; then
+  echo "solus"
+elif command -v dnf; then
+  echo "fedora"
+  sudo dnf install timeshift
+elif command -v brew; then
+  echo "macos"
+else
+  echo "$OS is not yet implemented."
+  exit 1
+fi
+
 
 exit 0
 
