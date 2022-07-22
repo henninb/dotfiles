9d2ceb4a0ea79b6611eabce29e3109a6bfb76812
Brian Henning <henninb@msn.com>
2022-07-22 09:22:51 -0500

    wip

--- scripts/mosquitto-install.sh
+++ scripts/mosquitto-install.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
 
-# mqtt server
-sudo pacman -noconfirm --needed -S mosquitto
-sudo apt install -y mosquitto mosquitto-clients
+if command -v pacman; then
+  echo "archlinux"
+  sudo pacman -noconfirm --needed -S mosquitto
+elif command -v emerge; then
+  echo "gentoo"
+  sudo emerge --update --newuse mosquitto
+elif command -v apt; then
+  echo "debian"
+  sudo apt install -y mosquitto mosquitto-clients
+elif command -v xbps-install; then
+  echo "void"
+elif command -v eopkg; then
+  echo "solus"
+elif command -v dnf; then
+  echo "fedora"
+elif command -v brew; then
+  echo "macos"
+else
+  echo "$OS is not yet implemented."
+  exit 1
+fi
+
 sudo systemctl enable mosquitto
 sudo systemctl start mosquitto
 
@@ -11,4 +30,4 @@ echo mosquitto_pub -h localhost -t test -m "hello world"
 
 exit 0
 
-# vim: set ft=sh
+# vim: set ft=sh:
