ab771ab749cfc304f75f8a56d8882d1474c67b4a
Brian Henning <henninb@msn.com>
2022-07-23 09:58:04 -0500

    wip

--- scripts/copy-debian.sh
+++ scripts/copy-debian.sh
@@ -1,32 +1,18 @@
 #!/bin/sh
 
-# ssh henninb@192.168.10.10 "mkdir -p /home/henninb/src/api/youtube"
-scp -r .ssh/ henninb@192.168.10.10:/home/henninb/
-scp -r .irssi/ henninb@192.168.10.10:/home/henninb/
-scp -r .zsh* henninb@192.168.10.10:/home/henninb/
-scp -r .alias* henninb@192.168.10.10:/home/henninb/
-scp -r .profile henninb@192.168.10.10:/home/henninb/
-scp -r zsh-install.sh henninb@192.168.10.10:/home/henninb/
-scp -r postgresql-install.sh henninb@192.168.10.10:/home/henninb/
-scp -r postgresql-status.sh henninb@192.168.10.10:/home/henninb/
-scp -r monofur-fonts.zip henninb@192.168.10.10:/home/henninb/
-scp -r java-install.sh henninb@192.168.10.10:/home/henninb/
-scp -r javascript-install.sh henninb@192.168.10.10:/home/henninb/
-scp -r cli-install.sh henninb@192.168.10.10:/home/henninb/
-scp -r docker-install.sh henninb@192.168.10.10:/home/henninb/
-scp -r git-clone-projects.sh henninb@192.168.10.10:/home/henninb/
-scp -r .gitignore henninb@192.168.10.10:/home/henninb/
-scp -r .gitconfig henninb@192.168.10.10:/home/henninb/
-scp -r ssl/ henninb@192.168.10.10:/home/henninb/
-scp -r python-pip-install.sh henninb@192.168.10.10:/home/henninb/
-scp -r update.sh henninb@192.168.10.10:/home/henninb/
-scp -r src/api/youtube/run.sh henninb@192.168.10.10:/home/henninb/src/api/youtube/
-scp -r src/api/youtube/run-info.sh henninb@192.168.10.10:/home/henninb/src/api/youtube/
-scp -r src/api/youtube/channels.txt henninb@192.168.10.10:/home/henninb/src/api/youtube/
-scp -r src/api/youtube/apikey henninb@192.168.10.10:/home/henninb/src/api/youtube/
-# scp -r .config/nvim/ henninb@192.168.10.10:/home/henninb/.config
+rsync -arvz "$HOME/.ssh/" "hornsup:/home/henninb/.ssh/"
+rsync -arvz "$HOME/scripts/" "hornsup:/home/henninb/scripts/"
+rsync -arvz "$HOME/.profile" "hornsup:/home/henninb/.portfolio"
+rsync -arvz --include=".zsh*" --exclude '*' "$HOME/" "hornsup:/home/henninb/"
+rsync -arvz --include=".alias*" --exclude '*' "$HOME/" "hornsup:/home/henninb/"
+rsync -arvz "$HOME/.gitignore" "hornsup:/home/henninb/"
+rsync -arvz "$HOME/.gitconfig" "hornsup:/home/henninb/"
+rsync -arvz "$HOME/ssl/" "hornsup:/home/henninb/ssl/"
+rsync -arvz "$HOME/.local/fonts/" "hornsup:/home/henninb/.local/fonts/"
 
+rsync -arvz "$HOME/.irssi/" "hornsup:/home/henninb/.irssi/"
+rsync -arvz --exclude='audio' "$HOME/src/api/youtube/" "hornsup:/home/henninb/src/api/youtube/"
 
 exit 0
 
-# vim: set ft=sh
+# vim: set ft=sh:
--- scripts/copy-openbsd.sh
+++ scripts/copy-openbsd.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 
-scp -r .ssh/ henninb@192.168.10.10:/home/henninb/
-scp -r .zsh* henninb@192.168.10.10:/home/henninb/
-scp -r zsh-install.sh henninb@192.168.10.10:/home/henninb/
-scp -r postgresql-install.sh henninb@192.168.10.10:/home/henninb/
-scp -r postgresql-status.sh henninb@192.168.10.10:/home/henninb/
-scp -r monofur-fonts.zip henninb@192.168.10.10:/home/henninb/
-scp -r java-install.sh henninb@192.168.10.10:/home/henninb/
-scp -r javascript-install.sh henninb@192.168.10.10:/home/henninb/
-scp -r cli-install.sh henninb@192.168.10.10:/home/henninb/
-scp -r docker-install.sh henninb@192.168.10.10:/home/henninb/
-scp -r git-clone-projects.sh henninb@192.168.10.10:/home/henninb/
-scp -r .gitignore henninb@192.168.10.10:/home/henninb/
-scp -r .gitconfig henninb@192.168.10.10:/home/henninb/
-scp -r ssl/ henninb@192.168.10.10:/home/henninb/
-scp -r python-pip-install.sh henninb@192.168.10.10:/home/henninb/
-scp -r update.sh henninb@192.168.10.10:/home/henninb/
+rsync -arvz "$HOME/.ssh/" "openbsd:/home/henninb/.ssh/"
+rsync -arvz "$HOME/scripts/" "openbsd:/home/henninb/scripts/"
+rsync -arvz "$HOME/.profile" "openbsd:/home/henninb/.portfolio"
+rsync -arvz --include=".zsh*" --exclude '*' "$HOME/" "openbsd:/home/henninb/"
+rsync -arvz --include=".alias*" --exclude '*' "$HOME/" "openbsd:/home/henninb/"
+rsync -arvz "$HOME/.gitignore" "openbsd:/home/henninb/"
+rsync -arvz "$HOME/.gitconfig" "openbsd:/home/henninb/"
+rsync -arvz "$HOME/ssl/" "openbsd:/home/henninb/ssl/"
+rsync -arvz "$HOME/.local/fonts/" "openbsd:/home/henninb/.local/fonts/"
 
 exit 0
 
-# vim: set ft=sh
+# vim: set ft=sh:
