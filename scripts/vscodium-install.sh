b6cab353d2bec7f67e602f0e5266137e1876f4f8
Brian Henning <henninb@msn.com>
2022-07-24 18:14:15 -0500

    wip

---
+++ scripts/vscodium-install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+if command -v pacman; then
+  echo "archlinux"
+elif command -v emerge; then
+  sudo emerge --update --newuse app-editors/vscodium
+elif command -v apt; then
+  echo "debian"
+elif command -v xbps-install; then
+  echo "void"
+elif command -v eopkg; then
+  echo "solus"
+elif command -v dnf; then
+  echo "fedora"
+elif command -v brew; then
+  echo "macos"
+else
+  echo "$OS is not yet implemented."
+  exit 1
+fi
+
+exit 0
+
+# vim: set ft=sh:
