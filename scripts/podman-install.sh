afa33cfa54734e5de4f0f7562df30f94c03a5e60
Brian Henning <henninb@msn.com>
2022-07-27 07:51:00 -0500

    wip

--- scripts/podman-install.sh
+++ scripts/podman-install.sh
@@ -1,25 +1,31 @@
 #!/bin/sh
 
-if [ -x "$(command -v pacman)" ]; then
+cat << EOF > "$HOME/tmp/registries.conf"
+[registries.search]
+registries = ['docker.io', 'quay.io', 'registry.access.redhat.com']
+registries = ['docker.io']
+EOF
+
+if command -v pacman; then
   sudo pacman --noconfirm --needed -S podman
   # sudo usermod --add-subuids 10000-75535 henninb
   # sudo usermod --add-subgids 10000-75535 henninb
   echo henninb:10000:65536 | sudo tee -a /etc/subuid
   echo henninb:10000:65536 | sudo tee -a /etc/subgid
-elif [ -x "$(command -v emerge)" ]; then
+elif command -v emerge; then
   sudo emerge --update --newuse podman
   sudo usermod --add-subuids 1065536-1131071 "$(whoami)"
   sudo usermod --add-subgids 1065536-1131071 "$(whoami)"
 elif [ -x "$(command -v apt)" ]; then
-  echo
+  echo "debian"
 elif [ -x "$(command -v xbps-install)" ]; then
   sudo xbps-install -y podman
 elif [ -x "$(command -v eopkg)" ]; then
-  echo
+  echo "solus"
 elif [ -x "$(command -v dnf)" ]; then
-  echo
+  echo "fedora"
 elif [ -x "$(command -v brew)" ]; then
-  echo
+  echo "macoos"
 else
   echo "$OS is not yet implemented."
   exit 1
@@ -32,12 +38,8 @@ sysctl kernel.unprivileged_userns_clone
 
 usermod --add-subuids 100000-165535 --add-subgids 100000-165535 "$(id -un)"
 
-echo sudo vim /etc/containers/registries.conf.
-
-echo [registries.search]
-echo registries = ['docker.io', 'quay.io', 'registry.access.redhat.com']
-echo registries = ['docker.io']
+sudo mv -v "$HOME/tmp/registries.conf" /etc/containers/registries.conf
 
 exit 0
 
-# vim: set ft=sh
+# vim: set ft=sh:
