a53b4ddf90fa5d9b27098add44c39b08faaf2f96
Brian Henning <henninb@msn.com>
2022-07-28 18:38:19 -0500

    wip

--- docker/nginx-server/run.sh
+++ docker/nginx-server/run.sh
@@ -3,10 +3,9 @@
 docker build -t nginx-server .
 docker stop nginx-server
 docker rm nginx-server -f
-#sudo sudo docker images
-#sudo docker run -dit --name nginx-server -h nginx-server -p 8080:80 nginx-server
-docker run --name=nginx-server -h nginx-server -h nginx-server --restart unless-stopped -p 8080:80 -d nginx-server
-echo running server on port 8080
+
+docker run --name=nginx-server -h nginx-server -h nginx-server --restart unless-stopped -p 443:443 -d nginx-server
+echo running server on port 443
 echo docker exec -it --user root nginx-server /bin/bash
 echo docker logs nginx-server
 
