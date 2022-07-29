ecb9ca326e6e3c0ef7fa6efb31a2bbea8860d327
Brian Henning <henninb@msn.com>
2022-07-28 19:14:17 -0500

    wip

--- docker/nginx-server/run.sh
+++ docker/nginx-server/run.sh
@@ -2,11 +2,15 @@
 
 docker build -t nginx-server .
 docker stop nginx-server
-docker rm nginx-server -f
+docker rm -f nginx-server
 
-docker run --name=nginx-server -h nginx-server -h nginx-server --restart unless-stopped -p 443:443 -d nginx-server
 echo running server on port 443
 echo docker exec -it --user root nginx-server /bin/bash
 echo docker logs nginx-server
+if command -v docker-compose; then
+  docker-compose up
+else
+  docker run --name=nginx-server -h nginx-server -h nginx-server --restart unless-stopped -p 443:443 -d nginx-server
+fi
 
 exit 0
