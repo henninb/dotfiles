baeb6b41d2620f2564bf7e6275ba1516b344eb53
Brian Henning <henninb@msn.com>
2022-07-28 19:11:48 -0500

    wip

--- docker/nginx-reverse-proxy/run.sh
+++ docker/nginx-reverse-proxy/run.sh
@@ -7,7 +7,11 @@ docker rm nginx-reverse-proxy -f
 echo nginx-reverse-proxy running server on port 3001, 3002, 3003
 echo docker exec -it --user root nginx-reverse-proxy /bin/bash
 echo docker logs nginx-reverse-proxy
-docker run --name=nginx-reverse-proxy -h nginx-reverse-proxy -h nginx-reverse-proxy --restart unless-stopped -p 3003:3003 -p 3001:3001 -p 3002:3002 -p 3004:3004 -d nginx-reverse-proxy
-# docker-compose up
+
+if command -v docker-compose; then
+  docker-compose up
+else
+  docker run --name=nginx-reverse-proxy -h nginx-reverse-proxy -h nginx-reverse-proxy --restart unless-stopped -p 3003:3003 -p 3001:3001 -p 3002:3002 -p 3004:3004 -d nginx-reverse-proxy
+fi
 
 exit 0
