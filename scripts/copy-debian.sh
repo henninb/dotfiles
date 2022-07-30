bd262d36660a493bf1a36527e042524c3a973705
Brian Henning <henninb@msn.com>
2022-07-30 17:05:30 -0500

    wip

--- scripts/copy-debian.sh
+++ scripts/copy-debian.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ssh hornsup mkdir -p /home/henninb/docker/
 rsync -arvz "$HOME/.ssh/" "hornsup:/home/henninb/.ssh/"
 rsync -arvz "$HOME/scripts/" "hornsup:/home/henninb/scripts/"
 rsync -arvz "$HOME/.profile" "hornsup:/home/henninb/.portfolio"
@@ -9,6 +10,7 @@ rsync -arvz "$HOME/.gitignore" "hornsup:/home/henninb/"
 rsync -arvz "$HOME/.gitconfig" "hornsup:/home/henninb/"
 rsync -arvz "$HOME/ssl/" "hornsup:/home/henninb/ssl/"
 rsync -arvz "$HOME/.local/fonts/" "hornsup:/home/henninb/.local/fonts/"
+rsync -arvz --exclude='.git' --exclude='*.crt' --exclude='*.key' "$HOME/docker/nginx-reverse-proxy/" "hornsup:/home/henninb/docker/nginx-reverse-proxy/"
 
 rsync -arvz "$HOME/.irssi/" "hornsup:/home/henninb/.irssi/"
 rsync -arvz --exclude='audio' --exclude='apikey' "$HOME/src/api/youtube/" "hornsup:/home/henninb/src/api/youtube/"
