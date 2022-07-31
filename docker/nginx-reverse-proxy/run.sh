16f1265a8691261ea6330a51bb59132f99f04057
Brian Henning <henninb@msn.com>
2022-07-31 14:00:38 -0500

    wip

--- docker/nginx-reverse-proxy/run.sh
+++ docker/nginx-reverse-proxy/run.sh
@@ -8,7 +8,7 @@ $(cat nginx.conf)
 }
 EOF
 
-if command -v nginxnative; then
+if command -v nginx; then
   sudo cp -v "$HOME/tmp/nginx-full.conf" /etc/nginx/nginx.conf
   sudo cp -v ./proxy.crt /etc/ssl/certs/
   sudo cp -v ./proxy.key /etc/ssl/private/
