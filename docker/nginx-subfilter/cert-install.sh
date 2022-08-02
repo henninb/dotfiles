0054408da61e47cc1d7dbdcb257e99e40baa6023
Brian Henning <henninb@msn.com>
2022-08-02 07:50:38 -0500

    wip

--- docker/nginx-subfilter/cert-install.sh
+++ docker/nginx-subfilter/cert-install.sh
@@ -1,33 +1,43 @@
 #!/usr/bin/env sh
 
+# if [ $# -ne 1 ]; then
+#   echo "Usage: $0 <server_name>"
+#   exit 1
+# fi
+
 server_name="proxy"
+server_subject="/C=US/ST=Texas/L=Denton/O=Brian LLC/OU=None/CN=${server_name}"
+rootca_subject="/C=US/ST=Texas/L=Denton/O=Brian LLC/OU=None/CN=Brian LLC rootCA"
 
 mkdir -p "$HOME/ssl"
+mkdir -p "$HOME/tmp"
 
-stty -echo
-printf "Cert Password: "
-read -r password
-stty echo
+# stty -echo
+# printf "Cert Password: "
+# read -r password
+# stty echo
 
 if [ ! -f "$HOME/ssl/rootCA.pem" ]; then
   echo "generate new rootCA"
   read -r pause
-  openssl genrsa -out "$HOME/ssl/rootCA.key" 2048
-  openssl req -x509 -new -nodes -key "$HOME/ssl/rootCA.key" -sha256 -days 1024 -out "$HOME/ssl/rootCA.pem" -subj "/C=US/ST=Texas/L=Denton/O=Brian LLC/OU=prod/CN=Brian LLC rootCA"
+  echo generate key
+  openssl genrsa -aes256 -out "$HOME/ssl/rootCA.key" 4096
+  echo generae a public CA certificate  file
+  openssl req -x509 -new -nodes -key "$HOME/ssl/rootCA.key" -sha256 -days 1024 -out "$HOME/ssl/rootCA.pem" -subj "$rootca_subject"
+  echo view cert data
   openssl x509 -in rootCA.pem -inform PEM -out rootCA.crt
 
   # archlinux
-  sudo trust anchor --store rootCA.pem
+  # sudo trust anchor --store rootCA.pem
 
   # gentoo
-  echo sudo cp -v rootCA.crt /usr/local/share/ca-certificates
-  echo sudo update-ca-certificates
+  # echo sudo cp -v rootCA.crt /usr/local/share/ca-certificates
+  # echo sudo update-ca-certificates
 fi
 
-cat > v3.ext << EOF
-authorityKeyIdentifier=keyid,issuer
-basicConstraints=CA:FALSE
-keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+# echo "subjectAltName=DNS:pfsense,IP:192.168.10.1" > v3.ext
+
+cat << EOF > "$HOME/tmp/$servername.ext"
 subjectAltName = @alt_names
 
 [alt_names]
@@ -35,10 +45,18 @@ DNS.1 = ${server_name}
 DNS.2 = localhost
 EOF
 
-SUBJECT="/C=US/ST=Texas/L=Denton/O=Brian LLC/OU=None/CN=${server_name}"
-openssl req -new -newkey rsa:2048 -sha256 -nodes -keyout ${server_name}.key -subj "$SUBJECT" -out ${server_name}.csr
-openssl x509 -req -in ${server_name}.csr -CA "$HOME/ssl/rootCA.pem" -CAkey "$HOME/ssl/rootCA.key" -CAcreateserial -out ${server_name}.crt -days 365 -sha256 -extfile v3.ext
+echo Generate an rsa key
+openssl genrsa -out "./$server_name.key" 4096
 
-rm -rf v3.ext *.csr
+echo Generate a certificate signing request
+openssl req -new -sha256 -key "./$server_name.key" -subj "$server_subject" -out ${server_name}.csr
+
+echo Generate the certificate using the intermediate.key
+openssl x509 -req -sha256 -days 365 -in ${server_name}.csr -CA "$HOME/ssl/rootCA.pem" -CAkey "$HOME/ssl/rootCA.key" -CAcreateserial -out "./${server_name}.crt" -extfile "$HOME/tmp/$servername.ext"
+
+echo Verify the certificate
+openssl verify -CAfile "$HOME/ssl/rootCA.pem" -verbose "./${server_name}.crt"
+
+rm -rf *.csr
 
 exit 0
