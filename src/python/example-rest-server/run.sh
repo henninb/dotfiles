bc9de7b92d7913f65a5441dc3984bdafe3e5e472
Brian Henning <henninb@msn.com>
2022-08-07 08:07:31 -0500

    wip

--- src/python/example-rest-server/run.sh
+++ src/python/example-rest-server/run.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-curl -O https://bootstrap.pypa.io/get-pip.py
-pip install Flask
-#py -m pip install Flask
+# curl -O https://bootstrap.pypa.io/get-pip.py
+# pip install Flask
+
+pip install -r requirements.txt
+
 export FLASK_ENV=development
 export FLASK_APP=example.py
-# python example.py
 flask run
 
 exit 0
