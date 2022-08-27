6883ce911db47a5e028b0b5a651e8ff48114a8ec
Brian Henning <henninb@msn.com>
2022-08-27 14:08:23 -0500

    wip

--- docker/portainer/run.sh
+++ docker/portainer/run.sh
@@ -3,7 +3,11 @@
 mkdir -p data
 docker volume create portainer-data
 
-docker run -d -p 8000:8000 -p 6443:6443 --name portainer \
+docker stop portainer
+docker rm portainer -f
+docker rmi portainer
+
+docker run -d -p 8000:8000 -p 6443:9443 --name portainer \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer-data:/data \
