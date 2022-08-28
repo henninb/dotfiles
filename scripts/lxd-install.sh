1bde033a1b6fe975f67db33ff6fc8df670a54f40
Brian Henning <henninb@msn.com>
2022-08-28 07:29:15 -0500

    wip

--- scripts/lxd-install.sh
+++ scripts/lxd-install.sh
@@ -3,7 +3,9 @@
 if command -v pacman; then
   echo "archlinux"
 elif command -v emerge; then
-  sudo emerge --update --newuse lxd
+  if ! command -v lxd; then
+    sudo emerge --update --newuse lxd
+  fi
 elif command -v apt; then
   echo "debian"
 elif command -v xbps-install; then
@@ -23,6 +25,12 @@ sudo usermod -aG lxd "$(id -un)"
 sudo systemctl enable lxd
 sudo systemctl start lxd
 sudo lxd init
+echo 'lxc image list images:'
+echo 'lxc image list images: debian'
+echo 'lxc launch ubuntu:22.04'
+echo 'lxc launch images:centos/7 centos-7'
+
+lxc list
 
 exit 0
 
