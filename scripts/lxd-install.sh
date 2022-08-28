e995463643573286d93525f94d206aee89edc7c9
Brian Henning <henninb@msn.com>
2022-08-28 07:44:27 -0500

    wip

--- scripts/lxd-install.sh
+++ scripts/lxd-install.sh
@@ -24,13 +24,24 @@ fi
 sudo usermod -aG lxd "$(id -un)"
 sudo systemctl enable lxd
 sudo systemctl start lxd
+sudo touch /etc/subuid
+sudo touch /etc/subgid
+sudo usermod --add-subuids 100000-165535 root
+sudo usermod --add-subgids 100000-165535 root
 sudo lxd init
+
 echo 'lxc image list images:'
 echo 'lxc image list images: debian'
 echo 'lxc launch ubuntu:22.04'
 echo 'lxc launch images:centos/7 centos-7'
 
-lxc list
+echo 'lxc exec archlinux -- pacman -S nginx'
+echo 'lxc exec archlinux -- systemctl enable nginx'
+echo 'lxc exec archlinux -- systemctl start nginx'
+echo 'lxc list'
+echo 'lxc start'
+echo 'lxc stop'
+echo 'lxc delete <name>'
 
 exit 0
 
