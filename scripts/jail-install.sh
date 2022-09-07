8f14010b493c5b00926f661d3c6fb745f5bebe93
Brian Henning <henninb@msn.com>
2022-09-07 06:12:38 -0500

    updates

--- scripts/jail-install.sh
+++ scripts/jail-install.sh
@@ -1,10 +1,33 @@
 #!/bin/sh
 
-if [ "$OS" = "FreeBSD" ]; then
-  sudo pkg install -y ezjail
-  sudo sysrc ezjail_enable="YES"
-  sudo sysrc cloned_interfaces="bridge0 tap0 bridge1 tap1 bridge2 tap2 lo1"
-fi
+mkdir -p "/opt/jails/"
+sudo bsdinstall jail "/opt/jails/www"
+
+
+cat << EOF > "$HOME/tmp/jail.conf"
+www {
+   host.hostname = www.lan;
+   ip4.addr = 192.168.10.27;
+   path = /home/henninb/jails/www;
+   mount.devfs;
+   exec.start = "/bin/sh /etc/rc";
+   exec.stop = "/bin/sh /etc/rc.shutdown";
+}
+EOF
+
+sudo cp -v "$HOME/tmp/jail.conf" /etc/jail.conf
+sudo sysrc hald_enable=YES
+echo 'ifconfig_re0_alias0="inet 192.168.10.27"'
+sudo service jail start www
+jls
+sudo jexec 1 sh
+sudo jexec 1 sh
+
+# if [ "$OS" = "FreeBSD" ]; then
+#   sudo pkg install -y ezjail
+#   sudo sysrc ezjail_enable="YES"
+#   sudo sysrc cloned_interfaces="bridge0 tap0 bridge1 tap1 bridge2 tap2 lo1"
+# fi
 
 exit 0
 
