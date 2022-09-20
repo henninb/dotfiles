139c024f26d3dfb457d0dcd5748ae040a56e5555
Brian Henning <henninb@msn.com>
2022-09-20 07:49:22 -0500

    wip

--- scripts/kvm-install.sh
+++ scripts/kvm-install.sh
@@ -84,7 +84,7 @@ elif [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoL
   sudo pacman --noconfirm --needed -S dnsmasq
   sudo pacman --noconfirm --needed -S ebtables iptables dnsmasq
   sudo pacman --noconfirm --needed -S ebtables
-
+  sudo pacman --noconfirm --needed -S edk2-ovmf
   #sudo pacman --noconfirm --needed -S firewalld
   sudo pacman --noconfirm --needed -S cdrtools
   sudo pacman --noconfirm --needed -S iptables
