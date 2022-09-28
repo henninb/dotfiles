b4379f098ecb35779c4717a50c18694f6cee9249
Brian Henning <henninb@msn.com>
2022-09-28 05:45:52 -0500

    wip

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -47,44 +47,44 @@ set --universal fish_greeting
 # set SPACESHIP_KUBECTL_PREFIX ""
 
 # install fisher add matchai/spacefish
-set SPACEFISH_PROMPT_ORDER exit_code host dir git jobs char
-set SPACEFISH_PROMPT_ADD_NEWLINE false
-set SPACEFISH_PROMPT_SEPARATE_LINE false
-set SPACEFISH_CHAR_SYMBOL ❯
-set SPACEFISH_CHAR_SYMBOL ♜
-set SPACEFISH_CHAR_SUFFIX ' '
-set SPACEFISH_HG_SHOW false
-set SPACEFISH_PACKAGE_SHOW false
-set SPACEFISH_NODE_SHOW false
-set SPACEFISH_RUBY_SHOW false
-set SPACEFISH_ELM_SHOW false
-set SPACEFISH_ELIXIR_SHOW false
-set SPACEFISH_XCODE_SHOW_LOCAL false
-set SPACEFISH_SWIFT_SHOW_LOCAL false
-set SPACEFISH_GOLANG_SHOW false
-set SPACEFISH_PHP_SHOW false
-set SPACEFISH_RUST_SHOW false
-set SPACEFISH_JULIA_SHOW false
-set SPACEFISH_DOCKER_SHOW false
-set SPACEFISH_DOCKER_CONTEXT_SHOW false
-set SPACEFISH_AWS_SHOW false
-set SPACEFISH_CONDA_SHOW false
-set SPACEFISH_VENV_SHOW false
-set SPACEFISH_PYENV_SHOW false
-set SPACEFISH_DOTNET_SHOW false
-set SPACEFISH_EMBER_SHOW false
-set SPACEFISH_KUBECONTEXT_SHOW false
-set SPACEFISH_TERRAFORM_SHOW false
-set SPACEFISH_TERRAFORM_SHOW false
-set SPACEFISH_VI_MODE_SHOW false
-set SPACEFISH_JOBS_SHOW false
-set SPACEFISH_DIR_PREFIX ''
-set SPACEFISH_DIR_TRUNC_REPO false
-set SPACEFISH_HOST_PREFIX '@'
-set SPACEFISH_HOST_SHOW always
-set SPACEFISH_USER_SHOW false
-set SPACEFISH_GIT_PREFIX ''
-#
+# set SPACEFISH_PROMPT_ORDER exit_code host dir git jobs char
+# set SPACEFISH_PROMPT_ADD_NEWLINE false
+# set SPACEFISH_PROMPT_SEPARATE_LINE false
+# set SPACEFISH_CHAR_SYMBOL ❯
+# set SPACEFISH_CHAR_SYMBOL ♜
+# set SPACEFISH_CHAR_SUFFIX ' '
+# set SPACEFISH_HG_SHOW false
+# set SPACEFISH_PACKAGE_SHOW false
+# set SPACEFISH_NODE_SHOW false
+# set SPACEFISH_RUBY_SHOW false
+# set SPACEFISH_ELM_SHOW false
+# set SPACEFISH_ELIXIR_SHOW false
+# set SPACEFISH_XCODE_SHOW_LOCAL false
+# set SPACEFISH_SWIFT_SHOW_LOCAL false
+# set SPACEFISH_GOLANG_SHOW false
+# set SPACEFISH_PHP_SHOW false
+# set SPACEFISH_RUST_SHOW false
+# set SPACEFISH_JULIA_SHOW false
+# set SPACEFISH_DOCKER_SHOW false
+# set SPACEFISH_DOCKER_CONTEXT_SHOW false
+# set SPACEFISH_AWS_SHOW false
+# set SPACEFISH_CONDA_SHOW false
+# set SPACEFISH_VENV_SHOW false
+# set SPACEFISH_PYENV_SHOW false
+# set SPACEFISH_DOTNET_SHOW false
+# set SPACEFISH_EMBER_SHOW false
+# set SPACEFISH_KUBECONTEXT_SHOW false
+# set SPACEFISH_TERRAFORM_SHOW false
+# set SPACEFISH_TERRAFORM_SHOW false
+# set SPACEFISH_VI_MODE_SHOW false
+# set SPACEFISH_JOBS_SHOW false
+# set SPACEFISH_DIR_PREFIX ''
+# set SPACEFISH_DIR_TRUNC_REPO false
+# set SPACEFISH_HOST_PREFIX '@'
+# set SPACEFISH_HOST_SHOW always
+# set SPACEFISH_USER_SHOW false
+# set SPACEFISH_GIT_PREFIX ''
+# #
 if [ -f /etc/os-release ];
   set OS (cat /etc/os-release | grep '^NAME=' | tr -d '"' | cut -d = -f2)
   set OS_VER (cat /etc/os-release | grep '^VERSION_ID=' | tr -d '"' | cut -d = -f2)
@@ -125,5 +125,7 @@ function gp
   git commit -m "$arg1"
   git push
 end
+
+starship init fish | source
 
 # vim: set ft=sh:
--- README.md
+++ README.md
@@ -79,7 +79,7 @@
 | terminal               | alacritty, st, urxvt                                                                 |
 | editor                 | neovim, emacs, vscodium, intellij, neovide, notepadqq, vscode                        |
 | shell                  | zsh, bash, dash, fish, ksh, emacs (eshell)                                           |
-| shell prompt           | spaceship                                                                            |
+| shell prompt           | starship                                                                             |
 | zsh plugins            | zsh-syntax-highlighting, zsh-autosuggestions, autojump, zsh-history-substring-search |
 | file manager           | lf, pcmanfm, spacefm                                                                 |
 | bar                    | dzen2, conky, polybar                                                                |
