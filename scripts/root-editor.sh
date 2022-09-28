f0f3fff244b8fefadc3a86e697f0c3117798d818
Brian Henning <henninb@msn.com>
2022-09-28 07:54:29 -0500

    wip

--- scripts/root-editor.sh
+++ scripts/root-editor.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-echo 'export EDITOR=vim' | sudo tee -a /root/.bashrc
+echo 'export EDITOR=vim' | sudo tee -a /etc/bash.bashrc
 
 sudo ln -sfn /usr/bin/vim /bin/vim
 sudo ln -sfn /usr/bin/nvim /bin/nvim
 sudo sed -i "s/nano/vim/g" /etc/profile
+
+# echo archlinux add a file to /etc/profile.d/
+# echo archlinux add /etc/bash.bashrc
 
 exit 0
