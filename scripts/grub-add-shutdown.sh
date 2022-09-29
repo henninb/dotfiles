423ffe53f2a9f295319783c8543eb0e1e51de9fa
Brian Henning <henninb@msn.com>
2022-09-29 05:29:55 -0500

    wip

--- scripts/grub-add-shutdown.sh
+++ scripts/grub-add-shutdown.sh
@@ -9,11 +9,11 @@ exec tail -n +3 "$0"
 # sudo update-grub
 # validate /boot/grub/grub.cfg
 
-menuentry "Arch Linux" {
-  set root=(hd1,gpt1)
-  linux /boot/vmlinuz-linux root=/dev/nvme1n1p3
-  initrd /boot/initramfs-linux.img
-}
+# menuentry "Arch Linux" {
+#   set root=(hd1,gpt1)
+#   linux /boot/vmlinuz-linux root=/dev/nvme1n1p3
+#   initrd /boot/initramfs-linux.img
+# }
 
 menuentry "System Reboot" {
   echo "System Reboot"
