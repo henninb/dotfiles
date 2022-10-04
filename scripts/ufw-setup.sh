7378f9b1936fea7279a4404b1f0de8b7a8276034
Brian Henning <henninb@msn.com>
2022-10-04 16:23:36 -0500

    wip

--- scripts/ufw-setup.sh
+++ scripts/ufw-setup.sh
@@ -1,9 +1,36 @@
 #!/bin/sh
 
-sudo emerge
+if ! command -v ufw; then
+  sudo emerge --update --newuse ufw
+  sudo systemctl enable ufw --now
+fi
 
-sudo systemctl enable ufw --now
-# sudo systemctl start ufw
+sudo ufw disable
+sudo ufw reset
+sudo ufw default deny outgoing
+sudo ufw default deny incoming
+# sudo ufw allow dns
+# sudo ufw status --verbose
+# sudo ufw status
+sudo ufw allow out https
+sudo ufw allow out http
+sudo ufw allow in https
+# sudo ufw allow from 192.168.10.1
+# sudo ufw allow from 192.168.10.10
+sudo ufw allow out to 192.168.10.10 port 53
+# sudo ufw allow out to 192.168.10.1 port 53
+sudo ufw allow out to 2a00:1828:a00d:ffff::6 port 873
+sudo ufw allow out to 89.238.71.6 port 873
+sudo ufw allow out to 81.91.253.252 port 873
+sudo ufw limit out to 192.168.10.25 port 22
+sudo ufw limit out to 140.82.112.4 port 22 #github
+sudo ufw limit out to 140.82.112.3 port 22 #github
+sudo ufw limit out to 140.82.114.4 port 22 #github
+# sudo ufw limit out to any port 22
+sudo ufw enable
+
+exit 1
+
 sudo ufw enable
 sudo ufw status
 sudo ufw app list
@@ -16,5 +43,14 @@ sudo ufw status numbered
 sudo ufw allow dns
 sudo ufw allow 80/tcp comment 'accept http connections'
 sudo ufw allow 443/tcp comment 'accept https connections'
+
+sudo ufw disable
+
+sudo ufw reset
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+sudo ufw limit 22/tcp
+# sudo ufw allow 80/tcp
 
 exit 0
