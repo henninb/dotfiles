9eed6611fa0a1b89e91780e017c59909ea35cb3d
Brian Henning <henninb@msn.com>
2022-10-03 19:07:00 -0500

    wip

--- scripts/ufw-setup.sh
+++ scripts/ufw-setup.sh
@@ -7,8 +7,14 @@ sudo systemctl enable ufw --now
 sudo ufw enable
 sudo ufw status
 sudo ufw app list
+sudo ufw default allow outgoing
+sudo ufw default deny incoming
 sudo ufw limit SSH
+sudo ufw delete 1
 sudo ufw status
+sudo ufw status numbered
 sudo ufw allow dns
+sudo ufw allow 80/tcp comment 'accept http connections'
+sudo ufw allow 443/tcp comment 'accept https connections'
 
 exit 0
