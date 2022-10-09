6f2d3c7f769d38d8d7b0a6100d332a083ff93734
Brian Henning <henninb@msn.com>
2022-10-09 10:36:17 -0500

    wip

--- scripts/github-repos-list.sh
+++ scripts/github-repos-list.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-#curl https://api.github.com/users/BitExplorer/repos
-curl -s https://api.github.com/users/BitExplorer/repos?per_page=100 | jq -r '.[]|.html_url'
-# Link: <https://api.github.com/user/repos?page=3&per_page=100>; rel="next",
-#   <https://api.github.com/user/repos?page=50&per_page=100>; rel="last"
+curl -s 'https://api.github.com/users/henninb/repos?per_page=1000' | jq -r '.[]|.html_url' | sed "s/https:\/\/github.com\/henninb\///g" | sort
 
 exit 0
 
