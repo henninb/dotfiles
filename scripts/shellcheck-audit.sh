672ec31af715633ae28bca3193bce6d0c3827daa
Brian Henning <henninb@msn.com>
2022-10-09 10:23:07 -0500

    wip

--- scripts/shellcheck-audit.sh
+++ scripts/shellcheck-audit.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-if [ ! -x "$(command -v shellcheck)" ]; then
-  echo "Please install shellcheck."
-  sudo apt install shellcheck
-  sudo pacman --noconfirm --needed -S shellcheck
+
+if ! command -v shellcheck; then
+  curl -sSL https://get.haskellstack.org/ | sh
+  stack update
+  stack install ShellCheck
 fi
+
+# if [ ! -x "$(command -v shellcheck)" ]; then
+#   echo "Please install shellcheck."
+#   sudo apt install shellcheck
+#   sudo pacman --noconfirm --needed -S shellcheck
+#   exit 1
+# fi
+
 #find . -maxdepth 1 -type f -exec ls -ld "{}" \;
 files=$(find . -maxdepth 1 -type f -name "*.sh")
 
--- scripts/shellcheck-install.sh
+++ scripts/shellcheck-install.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
+
 curl -sSL https://get.haskellstack.org/ | sh
 stack update
 
-cd "$HOME/projects/github.com" || exit
-mkdir -p koalaman
-cd koalaman || exit
-#git clone https://github.com/koalaman/shellcheck.git
-git clone git@github.com:koalaman/shellcheck.git
-cd shellcheck || exit
-stack build
-stack install
+stack install ShellCheck
+
+if ! command -v shellcheck; then
+  cd "$HOME/projects/github.com" || exit
+  mkdir -p koalaman
+  cd koalaman || exit
+  #git clone https://github.com/koalaman/shellcheck.git
+  git clone git@github.com:koalaman/shellcheck.git
+  cd shellcheck || exit
+  stack build
+  stack install
+fi
 
 exit 0
 
