edcb2fc876d4cdda795f552b0f5a0d4271ccabad
Brian Henning <henninb@msn.com>
2022-10-10 16:00:33 -0500

    wip

---
+++ kvm/fedora-guest.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+if [ $# -gt 1 ]; then
+    echo "Usage: $0 [remove y/n]"
+    exit 1
+fi
+
+remove=$1
+iso_file="Fedora-Workstation-Live-x86_64-37_Beta-1.5.iso"
+guest_name="fedora"
+
+virsh shutdown "guest-$guest_name"
+virsh destroy "guest-$guest_name"
+virsh undefine "guest-$guest_name"
+
+sudo mkdir -p /var/lib/libvirt/boot
+sudo rm "/var/lib/libvirt/images/guest-${guest_name}.qcow2"
+
+if [ "$remove" = "y" ]; then
+  echo "remove only"
+  exit 1
+fi
+
+if [ ! -f "/var/lib/libvirt/boot/${iso_file}" ]; then
+  scp -p "pi:/home/pi/shared/template/iso/${iso_file}" .
+  sudo mv "${iso_file}" /var/lib/libvirt/boot/
+fi
+
+echo "osinfo-query os"
+echo "disk bus can be virtio i.e. vda, or scsi i.e. sda"
+
+exec sudo virt-install \
+--virt-type=kvm \
+--name "guest-$guest_name" \
+--memory=4096,maxmemory=4096 \
+--vcpus=1,maxvcpus=2 \
+--virt-type=kvm \
+--hvm \
+--boot uefi \
+--cdrom=/var/lib/libvirt/boot/${iso_file} \
+--network=bridge=virbr0,model=virtio \
+--graphics vnc \
+--disk path=/var/lib/libvirt/images/guest-$guest_name.qcow2,size=40,bus=scsi,format=qcow2
+
+exit 0
