e3a470031d5b5a9ec8333ff615fb9ba605532794
Brian Henning <henninb@msn.com>
2022-10-10 07:09:41 -0500

    wip

--- scripts/sdr-install.sh
+++ scripts/sdr-install.sh
@@ -2,7 +2,8 @@
 
 lsusb | grep 2838
 
-echo gqrx
+sudo emerge --update --newuse gqrx
+sudo emerge --update --newuse net-wireless/rtl-sdr
 
 cat << EOF > "$HOME/tmp/20.rtlsdr.rules"
 SUBSYSTEM=="usb", ATTRS{idVendor}=="0bda", ATTRS{idProduct}=="2838", GROUP="adm", MODE="0666", SYMLINK+="rtl_sdr"
@@ -12,5 +13,9 @@ sudo mv -v "$HOME/tmp/20.rtlsdr.rules" /etc/udev/rules.d/20.rtlsdr.rules
 if command -v udevadm; then
   sudo udevadm control --reload-rules
 fi
+
+sudo groupadd sdr
+sudo usermod -aG sdr henninb
+rtl_test -t
 
 exit 0
