3876041faa5b10f46e955fb1d7f5370acfc97cb7
Brian Henning <henninb@msn.com>
2022-10-10 13:41:18 -0500

    wip

--- scripts/ufw-setup.sh
+++ scripts/ufw-setup.sh
@@ -41,6 +41,7 @@ sudo ufw allow out to 168.235.89.132 port 123 proto udp comment 'ntp'
 sudo ufw allow out to 162.159.200.1 port 123 proto udp comment 'ntp'
 sudo ufw allow out to 34.223.228.170 port 123 proto udp comment 'ntp'
 sudo ufw allow out to 142.250.191.163 port 123 proto udp comment 'ntp'
+sudo ufw allow out to 174.20.27.91 port 8006 proto tcp comment 'https for pfsense'
 # sudo ufw limit out to any port 22
 sudo sed -i '/ufw-before-input.*icmp/s/ACCEPT/DROP/g' /etc/ufw/before.rules
 sudo ufw enable
