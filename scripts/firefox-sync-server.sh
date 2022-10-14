1496f7c3de3fc05738a88d0627542d5c7e6bde80
Brian Henning <henninb@msn.com>
2022-10-14 06:06:23 -0500

    wip

---
+++ scripts/firefox-sync-server.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p "$HOME/projects/github.com/crazy-max"
+cd "$HOME/projects/github.com/crazy-max" || exit
+git clone https://github.com/crazy-max/docker-firefox-syncserver
+cd docker-firefox-syncserver || exit
+docker buildx bake
+
+exit 0
