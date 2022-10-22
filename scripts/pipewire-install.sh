06df7abb3214eaa250e8d4353eaea68ab51c9697
Brian Henning <henninb@msn.com>
2022-10-22 08:57:46 -0500

    wip

---
+++ scripts/pipewire-install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if command -v pacman; then
+  echo "archlinux"
+elif command -v emerge; then
+  echo "gentoo"
+  sudo emerge --update --newuse pipewire
+elif command -v apt; then
+  sudo apt install -y pipewire
+elif command -v xbps-install; then
+  echo "void"
+elif command -v pkg; then
+  echo "freebsd"
+elif command -v eopkg; then
+  echo "solus"
+elif command -v dnf; then
+  echo "fedora"
+elif command -v brew; then
+  echo "macos"
+else
+  echo "$OS is not yet implemented."
+  exit 1
+fi
+
+systemctl --user enable pipewire.socket pipewire-pulse.socket
+systemctl --user disable pipewire-media-session.service
+systemctl --user --force enable wireplumber.service
+
+exit 0
