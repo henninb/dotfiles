b348e1c7c45880639698ade3067b459ca0d0adc3
Brian Henning <henninb@msn.com>
2022-11-26 09:47:27 -0600

    wip

--- containers/nginx-inject-js/run.sh
+++ containers/nginx-inject-js/run.sh
@@ -1,21 +1,37 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <platform>"
+  echo "docker or podman"
+  exit 1
+fi
 
-docker stop nginx-inject-js
-docker rm nginx-inject-js -f
-#sudo sudo docker images
+platform=$1
 
-echo nginx-inject-js running server on port 443
-echo docker exec -it --user root nginx-inject-js /bin/bash
-echo docker exec -it --user root nginx-inject-js ss --listen
-echo docker logs nginx-inject-js
+if [ "$platform" = "podman" ]; then
+  podman stop nginx-inject-js
+  podman rm -f nginx-inject-js
+  echo "running server on port 443"
 
-if command -v docker-compose; then
-  docker-compose build
-  docker-compose up
-else
-  docker build -t nginx-inject-js .
-  docker run --name=nginx-inject-js -h nginx-inject-js -h nginx-inject-js --restart unless-stopped -p 443:443 -d nginx-inject-js
+  echo 0 | sudo tee /proc/sys/net/ipv4/ip_unprivileged_port_start
+  podman-compose build
+  podman-compose up
+elif [ "$platform" = "docker" ]; then
+  docker stop nginx-inject-js
+  docker rm -f nginx-inject-js
+  echo "running server on port 443"
+
+  echo docker exec -it --user root nginx-inject-js /bin/bash
+  echo docker exec -it --user root nginx-inject-js ss --listen
+  echo docker logs nginx-inject-js
+
+  if command -v docker-compose; then
+    docker-compose build
+    docker-compose up
+  else
+    docker build -t nginx-inject-js .
+    docker run --name=nginx-inject-js -h nginx-inject-js -h nginx-inject-js --restart unless-stopped -p 443:443 -d nginx-inject-js
+  fi
 fi
 
 exit 0
