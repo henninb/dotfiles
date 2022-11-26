ddc2366fb6043c4affbb457f1eda04d013b507dc
Brian Henning <henninb@msn.com>
2022-11-26 09:52:27 -0600

    wip

--- containers/nginx-inject-lua/run.sh
+++ containers/nginx-inject-lua/run.sh
@@ -1,23 +1,37 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <platform>"
+  echo "docker or podman"
+  exit 1
+fi
 
-docker stop nginx-inject-lua
-docker rm nginx-inject-lua -f
-#sudo sudo docker images
-#sudo docker run -dit --name nginx-inject-lua -h nginx-inject-lua -p 8080:80 nginx-inject-lua
-# docker build -t nginx-inject-lua .
-# docker run --name=nginx-inject-lua -h nginx-inject-lua -h nginx-inject-lua --restart unless-stopped -p 80:80 -p 443:443 -d nginx-inject-lua
-echo nginx-inject-lua running server on port 443
-echo docker exec -it --user root nginx-inject-lua /bin/bash
-echo docker exec -it --user root nginx-inject-lua ss --listen
-echo docker logs nginx-inject-lua
+platform=$1
 
-if command -v docker-compose; then
-  docker-compose build
-  docker-compose up
-else
-  docker build -t nginx-inject-lua .
-  docker run --name=nginx-inject-lua -h nginx-inject-lua -h nginx-inject-lua --restart unless-stopped -p 80:80 -p 443:443 -d nginx-inject-lua
+if [ "$platform" = "podman" ]; then
+  podman stop nginx-inject-lua
+  podman rm -f nginx-inject-lua
+  echo "running server on port 443"
+
+  echo 0 | sudo tee /proc/sys/net/ipv4/ip_unprivileged_port_start
+  podman-compose build
+  podman-compose up
+elif [ "$platform" = "docker" ]; then
+  docker stop nginx-inject-lua
+  docker rm -f nginx-inject-lua
+  echo "running server on port 443"
+
+  echo docker exec -it --user root nginx-inject-lua /bin/bash
+  echo docker exec -it --user root nginx-inject-lua ss --listen
+  echo docker logs nginx-inject-lua
+
+  if command -v docker-compose; then
+    docker-compose build
+    docker-compose up
+  else
+    docker build -t nginx-inject-lua .
+    docker run --name=nginx-inject-lua -h nginx-inject-lua -h nginx-inject-lua --restart unless-stopped -p 443:443 -d nginx-inject-lua
+  fi
 fi
 
 exit 0
