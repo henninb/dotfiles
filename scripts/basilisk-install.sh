58f4e5807d4e5f4161f6a8055db6a38c97187119
Brian Henning <henninb@msn.com>
2022-12-15 18:34:16 -0600

    wip

--- scripts/basilisk-install.sh
+++ scripts/basilisk-install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-cd projects || exit
+mkdir -p "$HOME/projects/github.com/cebix"
+cd "$HOME/projects/github.com/cebix" || exit
 git clone https://github.com/cebix/macemu.git
-cd macemu || exit
+cd macemu/BasiliskII/src/Unix || exit
+./autogen.sh
+./configure
+make
+sudo make install
 
 exit 0
 
