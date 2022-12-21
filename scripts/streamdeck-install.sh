9d81855ae61bb08918df62d1f31597b11b93a1ec
Brian Henning <henninb@msn.com>
2022-12-21 06:38:37 -0600

    wip

--- scripts/streamdeck-install.sh
+++ scripts/streamdeck-install.sh
@@ -8,20 +8,22 @@ fi
 if command -v dnf; then
   sudo dnf install -y libusb1-devel
   sudo dnf install -y libgudev-devel
+  sudo dnf install -y hidapi-devel
+  sudo dnf install -y libudev-devel
 fi
 
-cat << EOF > "$HOME/tmp/70-streamdeck.rules"
+cat << EOF > "$HOME/tmp/99-streamdeck.rules"
 SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0060", TAG+="uaccess"
 SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0063", TAG+="uaccess"
 SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="006c", TAG+="uaccess"
 SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="006d", TAG+="uaccess"
 EOF
 
-sudo mv -v "$HOME/tmp/70-streamdeck.rules" /etc/udev/rules.d/70-streamdeck.rules
+sudo mv -v "$HOME/tmp/99-streamdeck.rules" /etc/udev/rules.d/99-streamdeck.rules
 if command -v udevadm; then
   sudo udevadm control --reload-rules
 fi
-sudo chown root:root /etc/udev/rules.d/70-streamdeck.rules
+sudo chown root:root /etc/udev/rules.d/99-streamdeck.rules
 
 if ! command -v pip3; then
    pip install --user streamdeck_ui
