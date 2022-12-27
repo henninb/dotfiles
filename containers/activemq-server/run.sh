fdf4eb52f4eca744f822af156f8f2aeb4dca554e
Brian Henning <henninb@msn.com>
2022-12-27 17:47:13 -0600

    wip

--- containers/activemq-server/run.sh
+++ containers/activemq-server/run.sh
@@ -5,7 +5,7 @@ sudo docker image build -t activemq-server .
 sudo docker stop activemq-server
 sudo docker rm activemq-server -f
 #sudo sudo docker images
-sudo docker run --name=activemq-server -h activemq-server -h activemq-server --restart unless-stopped -p 8161:8161 -p 61616:61616 -p 61613:61613 -v $HOME/activemq_data:/opt/activemq/data -d activemq-server
-sudo docker exec -it --user root activemq-server /bin/sh
+docker run --name=activemq-server -h activemq-server -h activemq-server --restart unless-stopped -p 8161:8161 -p 61616:61616 -p 61613:61613 -v $HOME/activemq_data:/opt/activemq/data activemq-server
+docker exec -it --user root activemq-server /bin/sh
 
 exit 0
