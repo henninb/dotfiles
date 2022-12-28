497f08355cc2c868a6463b3746f4fb264f0c9a72
Brian Henning <henninb@msn.com>
2022-12-28 08:20:28 -0600

    wip

--- scripts/kafka-client-install.sh
+++ scripts/kafka-client-install.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
+scala_version=2.13
+kafka_version=3.3.1
+
 export PATH=/opt/kafka-client/bin:$PATH
 
-if [ ! -f "kafka_2.12-1.1.1.tgz" ]; then
-  wget https://archive.apache.org/dist/kafka/1.1.1/kafka_2.12-1.1.1.tgz
-  tar xvf kafka_2.12-1.1.1.tgz
-  sudo mv kafka_2.12-1.1.1 /opt/kafka-client
+if [ ! -f "$HOME/tmp/kafka_${scala_version}-${kafka_version}.tgz" ]; then
+  wget https://archive.apache.org/dist/kafka/${kafka_version}/kafka_${scala_version}-${kafka_version}.tgz -O "$HOME/tmp/kafka_${scala_version}-${kafka_version}.tgz"
+  cd /opt/kafka-client
+  sudo rm -rf /opt/kafka-client
+  tar xvf "$HOME/tmp/kafka_${scala_version}-${kafka_version}.tgz"
+  sudo mv kafka_${scala_version}-${kafka_version} /opt/kafka-client
 fi
 
-echo 'kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic ynot'
-echo kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic ynot
+echo kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ynot
+echo kafka-topics.sh --bootstrap-server localhost:9092 --list
 
 which kafka-topics.sh
 
