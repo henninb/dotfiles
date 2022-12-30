f00ec26813fd5ba574bf2526c3ccd13d6b1355c4
Brian Henning <henninb@msn.com>
2022-12-30 13:46:47 -0600

    wip

--- json/finance-db-data.sh
+++ json/finance-db-data.sh
@@ -4,11 +4,12 @@ if [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Raspbian GNU/L
   sudo apt install jq
 fi
 
-scp pi@raspi:/home/pi/json_in.zip .
+# scp pi@raspi:/home/pi/json_in.zip .
 
 mkdir -p json_in
 cd json_in || exit
-unzip -o ../json_in.zip
+curl -s http://hornsup:8443/transaction/account/select/chase_kari > chase_kari.json
+# unzip -o ../json_in.zip
 cd - || exit
 
 cat json_in/chase_kari.json | jq '. | map([.guid, .transactionDate, .description, .category, .amount, .reoccurring, .cleared, .notes, .dateUpdated, .dateAdded, .accountType, .accountNameOwner]|join("||"))'
