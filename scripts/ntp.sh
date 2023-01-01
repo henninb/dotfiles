ee26dc7d55756562a8bfad68c6a7f6fe2924482e
Brian Henning <henninb@msn.com>
2023-01-01 09:24:45 -0600

    wip

--- scripts/ntp.sh
+++ scripts/ntp.sh
@@ -2,26 +2,26 @@
 
 if command -v pacman; then
   date
-  sudo ntpdate -s time.nist.gov
+  sudo ntpdate -u time.nist.gov
   date
   # sudo hwclock
   # sudo hwclock -w
 elif command -v zypper; then
-  # sudo zypper install -y ntp
   date
-  sudo ntpdate -s time.nist.gov
+  sudo ntpdate -u time.nist.gov
   date
 elif command -v dnf; then
   date
-  sudo ntpdate -s time.nist.gov
+  sudo ntpdate -u time.nist.gov
   date
 elif command -v emerge; then
   date
-  sudo ntpdate -s time.nist.gov
+  # sudo ntpdate -s time.nist.gov
+  sudo ntpdate -u pool.ntp.org
   date
 elif command -v pkg; then
   date
-  sudo ntpdate -s time.nist.gov
+  sudo ntpdate -u time.nist.gov
   date
 else
   echo "$OS is not yet implemented."
