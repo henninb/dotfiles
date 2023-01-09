d417304b08ca3a16d6916eb434b5c8b29b6ff8a7
Brian Henning <henninb@msn.com>
2023-01-09 05:56:39 -0600

    wip

---
+++ chroot/archlinux.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+echo arch-install-scripts
+
+# pacstrap arch-chroot base linux linux-firmware neovim
+pacstrap arch-chroot base linux linux-firmware neovim
+
+exit 0
--- chroot/ubuntu-jammy.sh
+++ chroot/ubuntu-jammy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 sudo debootstrap --variant=buildd --arch amd64 jammy "$HOME/chroot/jammy" http://archive.ubuntu.com/ubuntu/
+sudo chroot jammy
 
 exit 0
