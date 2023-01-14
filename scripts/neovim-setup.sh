89e1f4c89192f7d31d97600a3a5e6f50cfbe00f7
Brian Henning <henninb@msn.com>
2023-01-13 19:55:24 -0600

    wip

--- scripts/neovim-setup.sh
+++ scripts/neovim-setup.sh
@@ -13,4 +13,10 @@ echo sudo dnf install -y llvm-libs
 echo sudo dnf install cmake
 echo sudo dnf groupinstall "Development Tools" "Development Libraries"
 
+sudo xbps-install -y cmake
+sudo xbps-install -y llvm
+sudo xbps-install -y clang
+sudo xbps-install -y libxml2-devel
+
+
 exit 0
