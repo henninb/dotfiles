336d4d7709d0002a4190f9cd19509c2345f2296b
Brian Henning <henninb@msn.com>
2023-01-30 07:07:10 -0600

    wip

--- chroot/ubuntu-jammy.sh
+++ chroot/ubuntu-jammy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# sudo debootstrap --variant=buildd --arch amd64 jammy "$HOME/chroot/jammy" http://archive.ubuntu.com/ubuntu/
+sudo debootstrap --variant=buildd --arch amd64 jammy "$HOME/chroot/jammy" http://archive.ubuntu.com/ubuntu/
 xhost +local:
 sudo mount -t proc none jammy/proc
 sudo mount --rbind /dev jammy/dev
