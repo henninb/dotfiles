ec04cba9991be362360fd3e4211dd6dcbf8fe234
Brian Henning <henninb@msn.com>
2023-02-13 18:50:05 -0600

    wip

--- scripts/librewolf-install.sh
+++ scripts/librewolf-install.sh
@@ -23,6 +23,10 @@ else
   exit 1
 fi
 
+wget 'https://ocs-dl.fra1.cdn.digitaloceanspaces.com/data/files/1645459027/LibreWolf.x86_64.AppImage?response-content-disposition=attachment%3B%2520LibreWolf.x86_64.AppImage&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=RWJAQUNCHT7V2NCLZ2AL%2F20230214%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230214T004555Z&X-Amz-SignedHeaders=host&X-Amz-Expires=60&X-Amz-Signature=1a597334caeae548d0b51e8a2771c158cc0b7eaa1e9d1ee6b00a3f0c8c2050a4' -O "$HOME/Applications/LibreWolf.x86_64.AppImage"
+
+chmod 755 "$HOME/Applications/LibreWolf.x86_64.AppImage"
+
 exit 0
 
 # vim: set ft=sh:
