04135e983362412aa9f5b6613355396b6f41513b
Brian Henning <henninb@msn.com>
2023-02-16 07:06:43 -0600

    wip

--- src/rust/parser/src/main.rs
+++ src/rust/parser/src/main.rs
@@ -4,15 +4,6 @@ use std::fs::File;
 use std::error::Error;
 use csv::ReaderBuilder;
 
-#[allow(dead_code)]
-struct Book {
-    title: String,
-    author: String,
-    subject: String,
-    book_id: u32,
-}
-
-#[allow(dead_code)]
 type ParsedLine = ((String, String), usize);
 
 fn file_read_csv(fname: String) -> Result<(), Box<dyn Error>> {
@@ -21,9 +12,8 @@ fn file_read_csv(fname: String) -> Result<(), Box<dyn Error>> {
         .has_headers(false)
         .delimiter(b'|')
         .from_reader(file);
-    //let mut records = csv::Reader::from_reader(file); //.delimiter(b'|').has_headers(false);
+
     for result in records.records() {
-        // The iterator yields Result<StringRecord, Error>, so we check the error here.
         let record = result?;
         println!("{:?}", record);
         for name in record.iter() {
@@ -33,22 +23,16 @@ fn file_read_csv(fname: String) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-#[allow(dead_code)]
-fn example(fname : String) -> Result<(), Box<dyn Error>> {
-    let file = File::open(fname).expect("Couldn't open input");
-    let mut rdr = ReaderBuilder::new()
-        .has_headers(false)
-        .delimiter(b'|')
-        .from_reader(file);
-        //.from_reader(data.as_bytes());
-
-    if let Some(result) = rdr.records().next() {
-        let record = result?;
-        //assert_eq!(record, vec!["Boston", "United States", "4628910"]);
-        println!("{:?}", record);
-        Ok(())
+fn parse_line(line: String) -> Option<ParsedLine> {
+    let mut fields = line
+        .split('|')
+        .map(str::trim);
+    if let (Some(fa), Some(fb), Some(fc)) = (fields.next(), fields.next(), fields.next()) {
+        return fb.parse()
+            .ok()
+            .map(|v| ((fa.to_string(), fc.to_string()), v));
     } else {
-        Err(From::from("expected at least one record but got none"))
+        return None;
     }
 }
 
@@ -57,24 +41,11 @@ fn file_read(input_filename: String) {
     let contents = fs::read_to_string(input_filename)
         .expect("Something went wrong reading the file");
     println!("{}", contents);
-    let x = parse_line("one|two|three".to_string());
-}
-//https://stackoverflow.com/questions/43887908/how-to-read-and-process-a-pipe-delimited-file-in-rust
-fn parse_line(line: String) -> Option<ParsedLine> {
-    let mut fields = line
-        .split('|')
-        .map(str::trim);
-    if let (Some(fa), Some(fb), Some(fc)) = (fields.next(), fields.next(), fields.next()) {
-        fb.parse()
-            .ok()
-            .map(|v| ((fa.to_string(), fc.to_string()), v))
-    } else {
-        None
-    }
+    let x = parse_line("one|2|three".to_string());
+    println!("{:?}", x);
 }
 
 fn main() {
- //   file_read("books.txt".to_string());
     file_read_csv("books.txt".to_string());
-//    example("books.txt".to_string());
+    file_read("books.txt".to_string());
 }
