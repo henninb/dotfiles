55f367c7acca944d422121eac59cd4d08b0dd258
Brian Henning <henninb@msn.com>
2023-02-19 08:48:08 -0600

    wip

---
+++ flatpak/neovim-flatpak-install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cat > neovim-flatpak <<EOF
+#!/bin/sh
+
+flatpak run io.neovim.nvim
+
+exit 0
+# vim: set ft=sh:
+EOF
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak --user -y install flathub io.neovim.nvim
+
+chmod 755 neovim-flatpak
+mv neovim-flatpak "$HOME/.local/bin/"
+
+exit 0
+
+# vim: set ft=sh:
