3336ad88ecdccc10ca853cbff3b6fa8658b163f6
Brian Henning <henninb@msn.com>
2023-02-21 14:02:31 -0600

    wip

--- scripts/aws-cli.sh
+++ scripts/aws-cli.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "$HOME/tmp/awscliv2.zip"
+cd "$HOME/tmp/"
 unzip -o awscliv2.zip
 sudo ./aws/install
-
+sudo ./aws/install --update
 exit 0
 
 # vim: set ft=sh
