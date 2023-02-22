3e347c24adba4aad5bc21f45da8700fdeece947d
Brian Henning <henninb@msn.com>
2023-02-22 17:24:12 -0600

    wip

--- scripts/dragon-install.sh
+++ scripts/dragon-install.sh
@@ -6,6 +6,8 @@ if [ "$OS" = "Solus" ]; then
 elif [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Raspbian GNU/Linux" ]; then
   sudo apt install -y libgtk2.0-dev
   sudo apt install -y libgtk-3-dev
+elif [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLinux" ]; then
+  echo
 elif [ "$OS" = "Void" ]; then
   sudo xbps-install  gtk+3-devel
 elif [ "$OS" = "openSUSE Tumbleweed" ]; then
