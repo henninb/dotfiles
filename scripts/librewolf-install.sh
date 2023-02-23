b8e864db8891ec51ddeec3301bf15acb2346a852
Brian Henning <henninb@msn.com>
2023-02-23 16:35:53 -0600

    wip

--- scripts/librewolf-install.sh
+++ scripts/librewolf-install.sh
@@ -2,7 +2,7 @@
 
 
 if command -v pacman; then
-  yay -S librewolf-bin
+  yay --noconfirm --needed -S librewolf-bin
 elif command -v emerge; then
   sudo emerge --update --newuse app-eselect/eselect-repository
   sudo eselect repository add librewolf git https://gitlab.com/librewolf-community/browser/gentoo.git
