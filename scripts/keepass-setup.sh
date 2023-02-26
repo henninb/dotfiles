0ce9eb9fe3c78d16c6d64819b1f32fb1be08962b
Brian Henning <henninb@msn.com>
2023-02-26 09:19:00 -0600

    wip

---
+++ scripts/keepass-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir -p "$HOME/files/keepass-git"
+cd "$HOME/files/keepass-git" || exit
+git init .
+git remote add origin pi:/home/pi/downloads/keepass-git
+git fetch
+git merge origin main
+
+exit 0
