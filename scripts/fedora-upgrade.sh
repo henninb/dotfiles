cbaadde3767dfd1a1a8612666bbd4de1e9fccc36
Brian Henning <henninb@msn.com>
2023-02-28 17:58:22 -0600

    wip

--- scripts/fedora-upgrade.sh
+++ scripts/fedora-upgrade.sh
@@ -3,7 +3,7 @@
 sudo dnf upgrade --refresh
 sudo dnf install dnf-plugin-system-upgrade
 
-sudo dnf system-upgrade download --refresh --releasever=33
+sudo dnf system-upgrade download --refresh --releasever=38
 sudo dnf system-upgrade reboot
 
 exit 0
