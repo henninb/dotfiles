888aaff60d71ebac92556bbf33a802fd00e284f6
Brian Henning <henninb@msn.com>
2023-02-28 12:50:31 -0600

    wip

--- scripts/chroot-mount.sh
+++ scripts/chroot-mount.sh
@@ -20,14 +20,10 @@ fi
 lsblk -o UUID,MOUNTPOINT > $HOME/tmp/lsblk.txt
 
 if [ "$os" = "voidlinux" ]; then
-  # disk=nvme0n1
   sudo mkdir -p /mnt/voidlinux
-  # sudo mount /dev/${disk}p2 /mnt/voidlinux
   sudo mount UUID=b09663a8-7ff5-42f5-b2ad-8ea0df1f18ff /mnt/voidlinux
   sudo mkdir -p /mnt/voidlinux/boot/efi
-  # sudo mount /dev/${disk}p1 /mnt/voidlinux/boot/efi
   sudo mount UUID=F9D5-CA2F /mnt/voidlinux/boot/efi
-  # cd /mnt/voidlinux || exit
   sudo mount -t proc none /mnt/voidlinux/proc
   sudo mount --rbind /dev /mnt/voidlinux/dev
   sudo mount --rbind /sys /mnt/voidlinux/sys
@@ -35,19 +31,19 @@ if [ "$os" = "voidlinux" ]; then
   echo 'export PS1="(voidlinux-chroot) $PS1"'
   sudo chroot /mnt/voidlinux /bin/su - "$(id -un)"
 elif [ "$os" = "gentoo" ]; then
-  #disk=sdd
-  sudo mkdir -p /mnt/gentoo
-  #sudo mount /dev/${disk}2 /mnt/gentoo
-  sudo mount UUID=6d8ab46d-3e64-4ee5-b6e2-940433d01d56 /mnt/gentoo
-  sudo mkdir -p /mnt/gentoo/boot/efi
-  #sudo mount /dev/${disk}1 /mnt/gentoo/boot/efi
-  sudo mount UUID=CCF7-57C5 /mnt/gentoo/boot/efi
-  cd /mnt/gentoo || exit
-  sudo mount -t proc none /mnt/gentoo/proc
-  sudo mount --rbind /dev /mnt/gentoo/dev
-  sudo mount --rbind /sys /mnt/gentoo/sys
+  if [ "$(grep -c "6d8ab46d-3e64-4ee5-b6e2-940433d01d56 /mnt/gentoo" $HOME/tmp/lsblk.txt)" -ne 1 ]; then
+    sudo mkdir -p /mnt/gentoo
+    sudo mount UUID=6d8ab46d-3e64-4ee5-b6e2-940433d01d56 /mnt/gentoo
+    sudo mkdir -p /mnt/gentoo/boot/efi
+    cd /mnt/gentoo || exit
+    sudo mount -t proc none /mnt/gentoo/proc
+    sudo mount --rbind /dev /mnt/gentoo/dev
+    sudo mount --rbind /sys /mnt/gentoo/sys
+    sudo mount UUID=CCF7-57C5 /mnt/gentoo/boot/efi
+  else
+    echo already mounted
+  fi
   echo 'export PS1="(gentoo-chroot) $PS1"'
-
   sudo chroot /mnt/gentoo /bin/su - "$(id -un)"
 elif [ "$os" = "fedora" ]; then
   if [ "$(grep -c "722c5ee8-b300-4b51-86de-9221ebabc617 /mnt/fedora" $HOME/tmp/lsblk.txt)" -ne 1 ]; then
