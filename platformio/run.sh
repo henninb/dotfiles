f159a9acca710082670412bc49f4217f65a45827
Brian Henning <henninb@msn.com>
2023-03-01 07:35:01 -0600

    wip

--- platformio/run.sh
+++ platformio/run.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+sudo pacman --noconfirm --needed -S avr-gcc
+sudo pacman --noconfirm --needed -S avrdude
+sudo pacman --noconfirm --needed -S arduino-cli
 pip install platformio
-# pip install esptool.py
+pip install esptool.py
+arduino-cli core update-index
+arduino-cli core install arduino:avr
+arduino-cli core install stm32duino:STM32F1
+arduino-cli core install esp32:esp32
+arduino-cli core install esp8266:esp8266
+
 exit 0
