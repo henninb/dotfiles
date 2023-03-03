c9e9e0e66ae422acf28a8ae40490c766561c7bdf
Brian Henning <henninb@msn.com>
2023-03-03 16:17:41 -0600

    wip

--- scripts/dzen-install.sh
+++ scripts/dzen-install.sh
@@ -1,41 +1,79 @@
 #!/bin/sh
 
+cat > "$HOME/tmp/config.mk" <<EOF
+VERSION = 0.9.5-svn
+
+# DPKG build flags:
+CPPFLAGS:=$(shell dpkg-buildflags --get CPPFLAGS)
+CFLAGS:=$(shell dpkg-buildflags --get CFLAGS)
+CXXFLAGS:=$(shell dpkg-buildflags --get CXXFLAGS)
+LDFLAGS:=$(shell dpkg-buildflags --get LDFLAGS)
+CFLAGS+=$(CPPFLAGS)
+
+# paths
+PREFIX = /usr
+MANPREFIX = ${PREFIX}/share/man
+
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+INCS = -I. -I/usr/include -I${X11INC}
+
+## Option 7: With Xinerama and XPM and XFT
+LIBS += -L/usr/lib -lc -L${X11LIB} -lX11 -lXinerama -lXpm `pkg-config --libs xft`
+CFLAGS += -Wall -Os ${INCS} -DVERSION=\"${VERSION}\" -DDZEN_XINERAMA -DDZEN_XPM -DDZEN_XFT `pkg-config --cflags xft`
+
+LDFLAGS += ${LIBS}
+
+# Solaris, uncomment for Solaris
+#CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
+#LDFLAGS = ${LIBS}
+#CFLAGS += -xtarget=ultra
+
+# Debugging
+#CFLAGS = ${INCS} -DVERSION=\"${VERSION}\" -std=gnu89 -pedantic -Wall -W -Wundef -Wendif-labels -Wshadow -Wpointer-arith -Wbad-function-cast -Wcast-align -Wwrite-strings -Wstrict-prototypes -Wmissing-prototypes -Wnested-externs -Winline -Wdisabled-optimization -O2 -pipe -DDZEN_XFT `pkg-config --cflags xft`
+#LDFLAGS = ${LIBS}
+
+CC = gcc
+LD = ${CC}
+EOF
+
 # export DZEN_XINERAMA
 # export DZEN_XPM
 # export DZEN_XFT
 #export CFLAGS="-DDZEN_XINERAMA -DDZEN_XPM -DDZEN_XFT"
 #
-if command -v zypper; then
-  sudo zypper install -y libXinerama-devel
-  sudo zypper install -y libXpm-devel
-  sudo zypper install -y libXft-devel
-  sudo zypper install -y freetype-devel
-fi
+pkg-config --cflags freetype2
+pkg-config --cflags xft
+
+# if command -v zypper; then
+#   sudo zypper install -y libXinerama-devel
+#   sudo zypper install -y libXpm-devel
+#   sudo zypper install -y libXft-devel
+#   sudo zypper install -y freetype-devel
+# fi
 
 mkdir -p "$HOME/projects/github.com/minos-org"
 cd "$HOME/projects/github.com/minos-org" || exit
 git clone https://github.com/minos-org/dzen2.git
-#git clone https://github.com/robm/dzen.git
 cd "$HOME/projects/github.com/minos-org/dzen2" || exit
-# sudo make clean install
-# sudo make -DDZEN_XINERAMA -DDZEN_XPM -DDZEN_XFT clean install
-#make CFLAGS='-DDZEN_XINERAMA -DDZEN_XPM -DDZEN_XFT'
-# make LDFLAGS='-lXinerama -lXpm'
 make clean
+# make CFLAGS='-Wall -Os -DVERSION=\"${VERSION}\" -DDZEN_XINERAMA -DDZEN_XPM -DDZEN_XFT -I/usr/include/freetype2'
 make
-#sudo make
 ./dzen2 -v
 ls -l dzen2
+
+echo
+echo
+echo
 
 mkdir -p "$HOME/projects/github.com/robm"
 cd "$HOME/projects/github.com/robm" || exit
 git clone https://github.com/robm/dzen.git
 cd dzen || exit
-# sudo make clean install
 # sudo make -DDZEN_XINERAMA -DDZEN_XPM -DDZEN_XFT clean install
 make clean
 # make CFLAGS='-DDZEN_XINERAMA -DDZEN_XPM -DDZEN_XFT'
-make CFLAGS='-Wall -Os -DVERSION=\"${VERSION}\" -DDZEN_XINERAMA -DDZEN_XPM -DDZEN_XFT'
+make
 ./dzen2 -v
 ls -l dzen2
 
