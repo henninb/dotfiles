3d34c2de18fb7d75a8a365fe05896b781baf79cd
Brian Henning <henninb@msn.com>
2023-03-14 18:44:56 -0500

    wip

--- scripts/mongodb-install.sh
+++ scripts/mongodb-install.sh
@@ -1,27 +1,33 @@
 #!/bin/sh
 
-if [ -x "$(command -v pacman)" ]; then
+if [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLinux" ]; then
   yay -S mongodb-bin
   # yay -S mongodb-tools
   yay -S mongodb-tools-bin
   # wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
   sudo systemctl enable mongodb
   sudo systemctl start mongodb
-elif [ -x "$(command -v emerge)" ]; then
+elif [ "$OS" = "Gentoo" ]; then
   # sudo emerge --update --newuse app-admin/mongo-tools
   # sudo emerge --update --newuse dev-db/mongodb
   sudo emerge -update --newuse '=dev-db/mongodb-4.4.10::gentoo'
   # sudo emerge --update --newuse app-admin/mongosh-bin
-elif [ -x "$(command -v apt)" ]; then
-  echo
-elif [ -x "$(command -v xbps-install)" ]; then
-  echo
-elif [ -x "$(command -v eopkg)" ]; then
-  echo
-elif [ -x "$(command -v dnf)" ]; then
-  echo
-elif [ -x "$(command -v brew)" ]; then
-  echo
+elif [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Raspbian GNU/Linux" ]; then
+  echo "debian"
+elif [ "$OS" = "Void" ]; then
+  echo "void"
+elif [ "$OS" = "FreeBSD" ]; then
+  echo "freebsd"
+elif [ "$OS" = "Solus" ]; then
+  echo "solus"
+elif [ "$OS" = "openSUSE Tumbleweed" ]; then
+  echo opensuse
+elif [ "$OS" = "Fedora Linux" ]; then
+  echo "fedora"
+elif [ "$OS" = "Clear Linux OS" ]; then
+  echo clearlinux
+elif [ "$OS" = "Darwin" ]; then
+  echo macos
 else
   echo "$OS is not yet implemented."
   exit 1
