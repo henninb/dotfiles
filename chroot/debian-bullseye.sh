896fd48a73017fdad7e1741d5342ed4cbce28156
Brian Henning <henninb@msn.com>
2023-03-14 07:10:56 -0500

    wip

--- chroot/debian-bullseye.sh
+++ chroot/debian-bullseye.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-sudo debootstrap --arch amd64 bullseye "$HOME/chroot/bullseye" http://deb.debian.org/debian/
-xhost +local:
-sudo mount -t proc none bullseye/proc
-sudo mount --rbind /dev bullseye/dev
-sudo mount --rbind /sys bullseye/sys
-sudo chroot bullseye
+if command -v debootstrap; then
+  sudo mkdir -p bullseye
+  sudo debootstrap --arch amd64 bullseye "$HOME/chroot/bullseye" http://deb.debian.org/debian/
+  xhost +local:
+  sudo mount -t proc none bullseye/proc
+  sudo mount --rbind /dev bullseye/dev
+  sudo mount --rbind /sys bullseye/sys
+  sudo chroot bullseye
+fi
 
 exit 0
