1eafb46ae9f6237b66812b2e7b082e7f96dad3c1
Brian Henning <henninb@msn.com>
2023-03-14 05:41:38 -0500

    wip

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
-javac --version
-node --version
-nvim --version | head -1
-xmonad --version
-stack --version
-alacritty --version
-keepassxc --version
-dzen2 -v
-netlify --version
-dunst --version
-kitty --version
-nix-env --version
-starship --version | head -1
-arduino-cli version
-llvm-config --version
-pip --version
-stack exec -- ghc --version
-conky -version | head -1
-emacs --version | head -1
+date=$(date '+%Y-%m-%d')
+
+javac --version | tee -a "$HOME/tmp/version-$date-$$.dat"
+node --version | tee -a "$HOME/tmp/version-$date-$$.dat"
+nvim --version | head -1 | tee -a "$HOME/tmp/version-$date-$$.dat"
+xmonad --version | tee -a "$HOME/tmp/version-$date-$$.dat"
+stack --version | tee -a "$HOME/tmp/version-$date-$$.dat"
+alacritty --version | tee -a "$HOME/tmp/version-$date-$$.dat"
+keepassxc --version | tee -a "$HOME/tmp/version-$date-$$.dat"
+dzen2 -v | head -1 | tee -a "$HOME/tmp/version-$date-$$.dat"
+netlify --version | tee -a "$HOME/tmp/version-$date-$$.dat"
+dunst --version | tee -a "$HOME/tmp/version-$date-$$.dat"
+kitty --version | tee -a "$HOME/tmp/version-$date-$$.dat"
+nix-env --version | tee -a "$HOME/tmp/version-$date-$$.dat"
+starship --version | head -1 | tee -a "$HOME/tmp/version-$date-$$.dat"
+arduino-cli version | tee -a "$HOME/tmp/version-$date-$$.dat"
+llvm-config --version | tee -a "$HOME/tmp/version-$date-$$.dat"
+pip --version | tee -a "$HOME/tmp/version-$date-$$.dat"
+stack exec -- ghc --version | tee -a "$HOME/tmp/version-$date-$$.dat"
+conky -version | head -1 | tee -a "$HOME/tmp/version-$date-$$.dat"
+emacs --version | head -1 | tee -a "$HOME/tmp/version-$date-$$.dat"
+brave-browser --version | tee -a "$HOME/tmp/version-$date-$$.dat"
 
 exit 0
