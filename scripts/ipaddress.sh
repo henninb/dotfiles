9b0a67dc1035dee2259fb93207336f26e33c12ad
Brian Henning <henninb@msn.com>
2023-03-19 08:16:34 -0500

    wip

---
+++ scripts/ipaddress.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Get the local IP address
+#ip_addr="$(ip -o addr show up primary scope global | awk '{print $4}')"
+#ip_addr="$(ip -o addr show up primary scope global | awk '{print $4}' | cut -d'/' -f1)"
+#ip_addr="$(ip -o addr show up primary scope global | awk '/inet /{print $4}' | cut -d'/' -f1 | grep -v '^[[:space:]]*127\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')"
+#ip_addr="$(ip -o addr show up primary scope global | awk '/inet /{print $4}' | cut -d'/' -f1 | grep -vE '^(127\.|[[:space:]]*172\.18\.0\.1)')"
+#ip_addr="$(ip -o addr show up primary scope global | awk '/inet /{print $4}' | cut -d'/' -f1 | grep -vE '^(127\.|172\.(1[6-9]|2[0-9]|3[01])\.)')"
+#ip_addr="$(ip -o addr show up primary scope global | awk '/inet /{print $4}' | cut -d'/' -f1 | grep -vE '^(127\.|172\.(1[6-9]|2[0-9]|3[01])\.|192\.168\.122\.1)')"
+ip_addr="$(ip -o addr show up primary scope global | awk '/inet /{print $4}' | cut -d'/' -f1 | grep -vE '^(127\.|172\.(1[6-9]|2[0-9]|3[01])\.|192\.168\.122\.)')"
+
+
+
+
+
+# Print the local IP address
+echo "Local IP address: ${ip_addr}"
+
+exit 0
