3446b60374a2eab965b73a6b93a355f9777b1172
Brian Henning <henninb@msn.com>
2023-03-20 07:20:32 -0500

    wip

--- gentoo/run.sh
+++ gentoo/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sudo mkdir -p /etc/portage/package.mask
+sudo mkdir -p /etc/portage/package.unmask
 sudo mkdir -p /etc/portage/package.accept_keywords
 sudo mkdir -p /etc/portage/package.use
 sudo mkdir -p /etc/portage/backup
@@ -14,14 +15,17 @@ sudo cp -v package.license /etc/portage/package.license
 sudo cp -v /etc/portage/package.use/zz-autounmask /etc/portage/backup/zz-autounmask.bak.$$
 sudo cp -v zz-autounmask /etc/portage/package.use/zz-autounmask
 
-sudo cp -v /etc/portage/package.mask/aa-autounmask /etc/portage/backup/aa-autounmask.bak.$$
-sudo cp -v aa-autounmask /etc/portage/package.mask/aa-autounmask
+sudo cp -v /etc/portage/package.mask/package.mask /etc/portage/backup/package.mask.bak.$$
+sudo cp -v package.mask /etc/portage/package.mask/package.mask
 
 sudo cp -v /etc/portage/make.conf /etc/portage/backup/make.conf.bak.$$
 sudo cp -v make.conf /etc/portage/make.conf
 
-sudo cp -v /etc/portage/package.unmask /etc/portage/backup/package.unmask.bak.$$
-sudo cp -v package.unmask /etc/portage/package.unmask
+sudo cp -v /etc/portage/package.unmask/package.unmask /etc/portage/backup/package.unmask.bak.$$
+sudo cp -v package.unmask /etc/portage/package.unmask/package.unmask
+
+#sudo cp -v /etc/portage/package.unmask /etc/portage/backup/package.unmask.bak.$$
+#sudo cp -v package.unmask /etc/portage/package.unmask
 
 sudo cp -v /etc/portage/package.env /etc/portage/backup/package.env.bak.$$
 sudo cp -v package.env /etc/portage/package.env
