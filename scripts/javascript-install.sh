d7fbdcb1ad00a6d7342b435a33e72b69bad73675
Brian Henning <henninb@msn.com>
2023-04-01 11:55:35 -0500

    wip

--- scripts/javascript-install.sh
+++ scripts/javascript-install.sh
@@ -13,7 +13,7 @@ node_ver=$(curl -s 'https://nodejs.org/download/release/latest-v18.x/' | grep no
 
 if [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Debian GNU/Linux" ]; then
   sudo apt install -y curl
-  # curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+  # curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
   echo sudo apt install -y nodejs
 elif [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLinux" ]; then
   sudo pacman --noconfirm --needed -S curl
