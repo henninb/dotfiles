e740a8b03994b530325642341719965ce9fe806b
Brian Henning <henninb@msn.com>
2023-04-02 06:13:06 -0500

    wip

--- scripts/ntp-install.sh
+++ scripts/ntp-install.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+cat <<  EOF > "$HOME/tmp/ntpd.service"
+[Unit]
+Description=Network Time Service
+After=network.target
+
+[Service]
+Type=forking
+ExecStart=/usr/bin/ntpd -g -u ntp:ntp -p /var/run/ntpd.pid
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
 if [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLinux" ]; then
   date
   sudo ntpdate -u time.nist.gov
@@ -7,6 +21,7 @@ if [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLin
   # sudo hwclock
   # sudo hwclock -w
 elif [ "$OS" = "Gentoo" ]; then
+  #sudo cp -v $HOME/tmp/ntpd.service /etc/systemd/system/ntpd.service
   date
   # sudo ntpdate -s time.nist.gov
   sudo ntpdate -u pool.ntp.org
@@ -42,9 +57,22 @@ else
   exit 1
 fi
 
-echo timedatectl set-ntp true
-echo timedatectl status
-echo timedatectl show-timesync --all
+sudo hwclock --systohc --localtime
+sudo hwclock --show
+
+if command -v systemctl; then
+  sudo systemctl enable systemd-timesyncd.service --now
+fi
+sudo touch /etc/conf.d/hwclock
+# echo clock_systohc="YES" | sudo tee -a /etc/conf.d/hwclock
+if command -v timedatectl; then
+  sudo timedatectl set-ntp true
+  sudo timedatectl set-local-rtc 0
+  timedatectl status
+  timedatectl show-timesync --all
+else
+  echo time sync is it running?
+fi
 
 echo example:
 echo "sudo date -s '27 DEC 2021 12:24:00'"
