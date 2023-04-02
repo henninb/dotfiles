92c9a0956e52f9aaa977596fa52786caa28e135a
Brian Henning <henninb@msn.com>
2023-04-02 10:06:09 -0500

    wip

--- scripts/xmonad-stack-install.sh
+++ scripts/xmonad-stack-install.sh
@@ -676,6 +676,7 @@ echo "install failures = $failures"
 
 mkdir -p "$HOME/keepass-git"
 cd "$HOME/keepass-git" || exit
+git init .
 git remote add origin pi:/home/pi/downloads/keepass-git
 git fetch
 git merge origin/main
