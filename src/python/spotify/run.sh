de4e4ccdbc6fa5c7e72f83b2312a08a9f20a0bd9
Brian Henning <henninb@msn.com>
2023-04-06 19:54:01 -0500

    wip

--- src/python/spotify/run.sh
+++ src/python/spotify/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+export SPOTIPY_CLIENT_ID='your-spotify-client-id'
+export SPOTIPY_CLIENT_SECRET='your-spotify-client-secret'
+export SPOTIPY_REDIRECT_URI='your-app-redirect-url'
+
 pip install -r requirements.txt
 python spotify.py
 
