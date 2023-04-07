5895ffdd1f867fa822d44014c3073350bcaffb21
Brian Henning <henninb@msn.com>
2023-04-07 06:46:23 -0500

    wip

--- scripts/git-setup.sh
+++ scripts/git-setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 git config --global --list
+git config --global core.pager less
+# git config --list
 # git config --global core.pager 'nvim -'
 # git config --global diff.tool meld
 git difftool --tool-help
