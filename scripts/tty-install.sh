6cd8293b1bd6fb75876dee1654d9f675aff5826a
Brian Henning <henninb@msn.com>
2023-04-08 11:23:35 -0500

    wip

--- scripts/tty-install.sh
+++ scripts/tty-install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 sudo systemctl enable getty@tty{2,3,4,5,6}.service
+sudo sed -i 's/^#NAutoVTs=/NAutoVTs=/' /etc/systemd/logind.conf
 echo /etc/systemd/logind.conf | grep NAutoVTs
-echo /etc/systemd/logind.conf
-echo NAutoVTs=6
+# echo /etc/systemd/logind.conf
+# echo NAutoVTs=6
 exit 0
+
+# vim: set ft=sh
