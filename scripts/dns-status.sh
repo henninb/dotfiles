8d6eab73d39c3b8caa7f98cd5903b6a11d69b52a
Brian Henning <henninb@msn.com>
2023-04-08 10:51:58 -0500

    wip

--- scripts/dns-status.sh
+++ scripts/dns-status.sh
@@ -2,8 +2,12 @@
 
 systemd-resolve --status
 
+# Global
+#        Protocols: LLMNR=resolve -mDNS -DNSOverTLS DNSSEC=no/unsupported
+# resolv.conf mode: stub
+
 
 echo fedora
-echo /etc/resolv.conf -> ../run/systemd/resolve/stub-resolv.conf
+echo /etc/resolv.conf /run/systemd/resolve/stub-resolv.conf
 
 exit 0
