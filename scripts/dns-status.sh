d3ebabd443d8ae9a1a4b4e8eeacd3755c7acc4fb
Brian Henning <henninb@msn.com>
2023-04-09 09:47:27 -0500

    wip

--- scripts/dns-status.sh
+++ scripts/dns-status.sh
@@ -7,6 +7,7 @@ nameserver 192.168.10.1
 EOF
 
 systemd-resolve --status
+resolvectl status
 
 echo stub vs uplink
 # Global
@@ -16,17 +17,27 @@ echo stub vs uplink
 
 
 if [ "$OS" = "Fedora Linux" ]; then
-  sudo systemctl disable systemd-resolved.service
-  sudo systemctl stop systemd-resolved.service
+  # echo dnf -y install dnsmasq
+  # Uncomment the DNSStubListener line in resolved.conf
+  sudo sed -i 's/#DNSStubListener=/DNSStubListener=/' /etc/systemd/resolved.conf
 
-  sudo rm /etc/resolv.conf
-  sudo cp "$HOME/tmp/resolv.conf" /etc/resolv.conf
-  sudo chattr +i /etc/resolv.conf
-  echo /etc/resolv.conf /run/systemd/resolve/stub-resolv.conf
-  echo sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
-  $ sudo apt-get install resolvconf
-  $ sudo systemctl start resolvconf.service
-  $ sudo systemctl enable resolveconf.service
+  # Add the DNSStubListener=yes line after the uncommented line
+  sudo sed -i '/DNSStubListener=/a DNSStubListener=yes' /etc/systemd/resolved.conf
+
+  # Restart the systemd-resolved service
+  sudo systemctl restart systemd-resolved.service
+  cat /etc/systemd/resolved.conf
+  # sudo systemctl disable systemd-resolved.service
+  # sudo systemctl stop systemd-resolved.service
+  #
+  # sudo rm /etc/resolv.conf
+  # sudo cp "$HOME/tmp/resolv.conf" /etc/resolv.conf
+  # sudo chattr +i /etc/resolv.conf
+  # echo /etc/resolv.conf /run/systemd/resolve/stub-resolv.conf
+  # echo sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+  # $ sudo apt-get install resolvconf
+  # $ sudo systemctl start resolvconf.service
+  # $ sudo systemctl enable resolveconf.service
 fi
 
 exit 0
