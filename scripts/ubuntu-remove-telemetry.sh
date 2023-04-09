1b7799b1d898db583c8ce2b2c2b6276574efbb67
Brian Henning <henninb@msn.com>
2023-04-09 09:08:35 -0500

    wip

---
+++ scripts/ubuntu-remove-telemetry.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Disable telemetry
+
+# Disable the services
+sudo systemctl stop apport.service
+sudo systemctl disable apport.service
+sudo systemctl stop whoopsie.service
+sudo systemctl disable whoopsie.service
+
+echo "Disabling telemetry"
+sudo apt remove ubuntu-report whoopsie apport -y
+
+# Prevent telemetry from being reinstalled 
+printf "Package: ubuntu-report\nPin: release a=*\nPin-Priority: -10" >> no-ubuntu-report.pref 
+sudo mv no-ubuntu-report.pref /etc/apt/preferences.d/
+sudo chown root:root /etc/apt/preferences.d/no-ubuntu-report.pref
+
+printf "Package: whoopsie\nPin: release a=*\nPin-Priority: -10" >> no-whoopsie.pref 
+sudo mv no-whoopsie.pref /etc/apt/preferences.d/
+sudo chown root:root /etc/apt/preferences.d/no-whoopsie.pref
+
+printf "Package: apport\nPin: release a=*\nPin-Priority: -10" >> no-apport.pref 
+sudo mv no-apport.pref /etc/apt/preferences.d/
+sudo chown root:root /etc/apt/preferences.d/no-apport.pref
+echo "Completed"
+
+exit 0
