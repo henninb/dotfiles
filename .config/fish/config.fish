70417673505c4d4b9758528203db9291d36edada
Brian Henning <henninb@msn.com>
2023-04-11 17:05:57 -0500

    wip

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -54,11 +54,15 @@ if [ \( "$OS" = "FreeBSD" \) -o \(  "$OS" = "Alpine Linux" \) -o \(  "$OS" = "Op
   source $HOME/.alias-bsd
 end
 
+ls -l /sys/module/hid_apple/parameters/fnmode
+cat /sys/module/hid_apple/parameters/fnmode
 if [ (uname) = "Linux" ]
   if test -f /sys/module/hid_apple/parameters/fnmode
     if not cat /sys/module/hid_apple/parameters/fnmode | grep -q 2
       echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode
-      echo "options hid_apple fnmode=2" | sudo tee -a /etc/modprobe.d/hid_apple.conf
+      # sudo chattr +i /sys/module/hid_apple/parameters/fnmode
+      # sudo chattr -i /sys/module/hid_apple/parameters/fnmode
+      echo "options hid_apple fnmode=2" | sudo tee /etc/modprobe.d/hid_apple.conf
       if command -v update-initramfs
         sudo update-initramfs -u -k all
       else if command -v mkinitcpio
