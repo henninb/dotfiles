75b30b426d52da9035b2cb6b4189f18f47786814
Brian Henning <henninb@msn.com>
2023-04-13 15:52:52 -0500

    wip

---
+++ scripts/sudoers-config-install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+if [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLinux" ]; then
+  echo "sudo pacman --noconfirm --needed -S"
+elif [ "$OS" = "Gentoo" ]; then
+  if ! sudo grep -q "^henninb ALL=(ALL:ALL) NOPASSWD: ALL$" /etc/sudoers; then
+    echo "henninb ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
+    echo "added henninb to /etc/sudoers"
+  fi
+elif [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Raspbian GNU/Linux" ]; then
+  echo "sudo apt install -y"
+elif [ "$OS" = "Void" ]; then
+  echo "sudo xbps-install -y"
+elif [ "$OS" = "FreeBSD" ]; then
+  echo "sudo pkg install -y"
+elif [ "$OS" = "OpenBSD" ]; then
+  echo "OpenBSD"
+elif [ "$OS" = "Solus" ]; then
+  "sudo eopkg install -y"
+elif [ "$OS" = "openSUSE Tumbleweed" ]; then
+  echo "sudo zypper install -y"
+elif [ "$OS" = "Fedora Linux" ]; then
+  echo "sudo dnf install -y"
+elif [ "$OS" = "Clear Linux OS" ]; then
+  "sudo swupd bundle-add"
+elif [ "$OS" = "Darwin" ]; then
+  echo "brew install"
+else
+  echo "$OS is not yet implemented."
+  exit 1
+fi
+
+exit 0
+
+# vim: set ft=sh:
