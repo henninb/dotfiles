09bdf27672d65006aabca27fa97efb7d6a80ea4e
Brian Henning <henninb@msn.com>
2023-04-14 19:41:06 -0500

    wip

--- scripts/doas-install.sh
+++ scripts/doas-install.sh
@@ -15,8 +15,6 @@ elif [ "$OS" = "Void" ]; then
   sudo xbps-install -y opendoas
 elif [ "$OS" = "FreeBSD" ]; then
   sudo pkg install -y doas
-  sudo mv -v "$HOME/tmp/doas.conf" /usr/local/etc/doas.conf
-  sudo chown root:wheel /usr/local/etc/doas.conf
 elif [ "$OS" = "Solus" ]; then
   echo "solus"
 elif [ "$OS" = "openSUSE Tumbleweed" ]; then
@@ -34,8 +32,14 @@ else
   exit 1
 fi
 
-sudo mv -v "$HOME/tmp/doas.conf" /etc/doas.conf
-sudo chown root:root /etc/doas.conf
-sudo chmod 600 /etc/doas.conf
+if [ "$OS" = "FreeBSD" ]; then
+  sudo mv -v "$HOME/tmp/doas.conf" /usr/local/etc/doas.conf
+  sudo chown root:wheel /usr/local/etc/doas.conf
+  sudo chmod 600 /usr/local/etc/doas.conf
+else
+  sudo mv -v "$HOME/tmp/doas.conf" /etc/doas.conf
+  sudo chown root:root /etc/doas.conf
+  sudo chmod 600 /etc/doas.conf
+fi
 
 exit 0
