33f7212c80842622897c4609a1944ad618406750
Brian Henning <henninb@msn.com>
2023-04-15 09:19:03 -0500

    wip

---
+++ scripts/openresty-install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+wget https://openresty.org/download/openresty-1.21.4.1.tar.gz
+tar -xvf openresty-1.21.4.1.tar.gz
+cd openresty-1.21.4.1
+
+./configure --prefix=/usr/local/openresty \
+            --with-luajit \
+            --without-http_redis2_module \
+            --with-http_iconv_module \
+            --with-pcre-jit \
+            --with-openssl
+make
