0338443b2f85b9ff2504f5452b5fa51598aca104
Brian Henning <henninb@msn.com>
2023-04-20 17:20:21 -0500

    wip

--- scripts/update.sh
+++ scripts/update.sh
@@ -81,7 +81,6 @@ elif [ "$OS" = "Gentoo" ]; then
       sudo genkernel all
       sudo grub-mkconfig -o /boot/grub/grub.cfg
       sudo grub-mkconfig -o /boot/efi/EFI/gentoo/grub.cfg
-      sudo grub-mkconfig -o /boot/efi/EFI/gentoo-new/grub.cfg
     fi
   fi
   blender=$(find /usr/bin -name "blender-*[0-9]")
