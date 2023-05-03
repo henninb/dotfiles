9ceac35564c60a97b21a1a42d636031e911f414b
Brian Henning <henninb@msn.com>
2023-05-03 17:51:53 -0500

    wip

---
+++ scripts/envoy-install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+sudo emerge --ask --verbose dev-libs/protobuf net-libs/grpc dev-libs/openssl
+
+cd "$HOME/projects" || exit
+git clone https://github.com/envoyproxy/envoy.git
+cd envoy
+git submodule update --init --recursive
+bazel build //source/exe:envoy-static
+
+exit 0
+
+# vim: set ft=sh:
