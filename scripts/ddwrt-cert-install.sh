bd292448cd58b8052fc1813b7c633cd9b7094f55
Brian Henning <henninb@msn.com>
2023-05-10 08:27:46 -0500

    wip

--- scripts/ddwrt-cert-install.sh
+++ scripts/ddwrt-cert-install.sh
@@ -1,5 +1,46 @@
 #!/bin/sh
 
+cat > "$HOME/tmp/ddwrt-cert-mount.sh" << 'EOF'
+#!/bin/sh
+# enable jffs support
+# scp this script to /jffs/startup
+
+if [ `nvram get https_enable` -gt 0 ] ; then
+    # get the absolute directory of the executable
+    SELF_PATH=$(cd -P "$(dirname "$0")" && pwd -P)
+    echo SELF_PATH: ${SELF_PATH}
+
+    # extract the mount path
+    MOUNT_PATH=`echo ${SELF_PATH//startup/}`
+    echo MOUNT_PATH: ${MOUNT_PATH}
+
+    # do binds
+    for BIND_PATH in ${MOUNT_PATH} ; do
+         echo Binding ${BIND_PATH}
+         if [ "${MOUNT_PATH}" != "${BIND_PATH}" ]; then
+                    grep -q -e "${BIND_PATH}" /proc/mounts || mount -o bind ${MOUNT_PATH}${BIND_PATH} ${BIND_PATH}
+         fi
+    done
+
+    HTTPS_RESET=0
+
+    if [ `pidof httpd` -gt 0 ]; then
+        echo Stopping httpd
+        stopservice httpd
+        HTTPS_RESET=1
+    fi
+
+    echo Binding HTTPS certificate
+    grep -q -e "/etc/cert.pem" /proc/mounts || mount -o bind ${MOUNT_PATH}/etc/cert.pem /etc/cert.pem
+    grep -q -e "/etc/key.pem" /proc/mounts || mount -o bind ${MOUNT_PATH}/etc/key.pem /etc/key.pem
+
+    if [ "$HTTPS_RESET" = "1" ]; then
+        echo Starting httpd
+        startservice httpd
+        unset HTTPS_RESET
+    fi
+fi
+EOF
 # if [ $# -ne 1 ]; then
 #   echo "Usage: $0 <server_name>"
 #   exit 1
@@ -48,8 +89,11 @@ cat "${server_name}.crt" | openssl x509 -noout -enddate
 # scp ddwrt.key root@192.168.10.2:/tmp/root/key.pem
 
 ssh root@192.168.10.2 mkdir -p /jffs/etc
+ssh root@192.168.10.2 mkdir -p /jffs/startup
 scp ddwrt.crt root@192.168.10.2:/jffs/etc/cert.pem
 scp ddwrt.key root@192.168.10.2:/jffs/etc/key.pem
+scp "$HOME/tmp/ddwrt-cert-mount.sh" root@192.168.10.2:/jffs/startup/ddwrt-cert-mount.sh
+ssh root@192.168.10.2 chmod 755 /jffs/startup/ddwrt-cert-mount.sh
 
 rm -rf *.csr
 rm ddwrt.crt ddwrt.key
