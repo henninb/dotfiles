dcd83bff7efb28e65f8e9b16404b10f55e720188
Brian Henning <henninb@msn.com>
2023-05-27 18:52:21 -0500

    wip

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -100,7 +100,7 @@ end
 #   set -gx JAVA_HOME (/usr/libexec/java_home)
 
 # set -gx PATH $JAVA_HOME/bin $PATH
-
+set -x PATH /opt/mullvad-browser/Browser $PATH
 set -x PATH $HOME/Applications $PATH
 set -x PATH /opt/yubico-authenticator $PATH
 set -x PATH $HOME/.ghcup/bin $PATH
---
+++ scripts/mullvad-browser.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+wget https://mullvad.net/en/download/browser/linux64/latest -O "$HOME/tmp/mullvad-browser.tar.xz"
+
+doas tar -xvf "$HOME/tmp/mullvad-browser.tar.xz" -C /opt
+
+sudo chown -R henninb:henninb /opt/mullvad-browser
+
+# wget 'https://cdn.mullvad.net/browser/12.0.6/mullvad-browser-linux64-12.0.6_ALL.tar.xz' -O "$HOME/tmp/mullvad-browser-linux64-12.0.6_ALL.tar.xz"
+
+
+exit 0
+
+# vim: set ft=sh:
