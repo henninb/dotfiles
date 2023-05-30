1473b894e8eb09e62fb3217d91527e061c7619a8
Brian Henning <henninb@msn.com>
2023-05-30 17:40:04 -0500

    wip

--- scripts/update.sh
+++ scripts/update.sh
@@ -88,8 +88,18 @@ elif [ "$OS" = "Gentoo" ]; then
       sudo grub-mkconfig -o /boot/efi/EFI/gentoo/grub.cfg
     fi
   fi
-  blender=$(find /usr/bin -name "blender-*[0-9]")
-  if [ -z ${blender+x} ]; then echo "var is unset"; else sudo ln -sfn "${blender}" /usr/bin/blender; fi
+
+  # blender=$(find /usr/bin -name "blender-*[0-9]")
+  # if [ -z ${blender+x} ]; then echo "var is unset"; else sudo ln -sfn "${blender}" /usr/bin/blender; fi
+
+  blender=$(find /usr/bin -name "blender-*[0-9]" -print -quit)
+  if [ -z "${blender}" ]; then
+    echo "Blender executable not found."
+  else
+    sudo ln -sfn "${blender}" /usr/bin/blender
+  fi
+
+
   librewolf=$(find /usr/bin -name "librewolf-bin")
   if [ -z ${librewolf+x} ]; then echo "var is unset"; else sudo ln -sfn "${librewolf}" /usr/bin/librewolf; fi
   echo eselect editor list
