1f516cc6028f2c76b2534a75bd78599df646f455
Brian Henning <henninb@msn.com>
2023-07-09 15:15:09 -0500

    wip

--- scripts/statics.sh
+++ scripts/statics.sh
@@ -2,14 +2,15 @@
 
 cat > statics <<EOF
 192.168.10.1 pfsense
-# 192.168.10.1 opnsense
-192.168.10.3 ddwrt-switch
+192.168.10.2 ddwrt-switch
+192.168.10.3 proxmox
 192.168.10.4 proxmox
-# 192.168.10.6 esxi
-192.168.10.10 pihole
-192.168.10.10 hornsup
-# 192.168.10.11 ddwrt-bridge1
+192.168.10.10 debian
 192.168.10.25 raspi
+192.168.10.30 TL-SG108E
+192.168.10.40 silverfox
+192.168.10.35 hp-printer
+192.168.10.36 keylight
 EOF
 
 exit 0
