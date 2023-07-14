1d9e2aae9fbc9cf947743a6b68b0b35fcd8276de
Brian Henning <henninb@msn.com>
2023-07-14 07:17:56 -0500

    wip

--- scripts/xmonad-stack-install.sh
+++ scripts/xmonad-stack-install.sh
@@ -736,6 +736,7 @@ mkdir -p "$HOME/keepass-git"
 cd "$HOME/keepass-git" || exit
 git init .
 git remote add origin pi:/home/pi/downloads/keepass-git
+# git remote add origin pi@192.168.10.25:/home/pi/downloads/keepass-git
 git branch --set-upstream-to=origin/main main
 git fetch
 git merge origin/main
