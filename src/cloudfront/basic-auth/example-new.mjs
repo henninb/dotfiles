c4aa44b78fa7ab43a127b04f15d3998f1ad5d7c6
Brian Henning <henninb@msn.com>
2023-09-01 05:35:00 -0500

    wip

---
+++ src/cloudfront/basic-auth/example-new.mjs
@@ -0,0 +1,28 @@
+// const { Buffer } = require('buffer');
+import { Buffer } from 'buffer';
+
+exports.handler = async (event) => {
+    let isAllowedAccess = false;
+
+    console.log('lambda function');
+    const request = event.Records[0].cf.request;
+
+    if (request && request.headers && request.headers.authorization) {
+        const basicAuthHeader = request.headers.authorization[0].value;
+        const authString = 'Basic ' + Buffer.from('admin' + ':' + 'monday1').toString('base64');
+        isAllowedAccess = (basicAuthHeader === authString);
+    }
+
+    if (!isAllowedAccess) {
+        const response = {
+            status: '401',
+            body: JSON.stringify('no access for you'),
+            headers: {
+                'www-authenticate': [{ key: 'WWW-Authenticate', value: 'Basic' }],
+            },
+        };
+        return response;
+    } else {
+        return request;
+    }
+};
