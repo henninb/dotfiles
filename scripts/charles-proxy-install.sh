1d031c5f82dd4d4ebff85ba1a0f7e525f738bae1
Brian Henning <henninb@msn.com>
2023-09-02 05:19:04 -0500

    wip

---
+++ scripts/charles-proxy-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+curl -sO "https://www.charlesproxy.com/assets/release/4.6.4/charles-proxy-4.6.4_amd64.tar.gz?k=b18cbdaed0"
+cd "$HOME/tmp/" || exit
+doas tar -xvf charles-proxy-4.6.4_amd64.tar.gz -C /opt
+
+exit 0
+
+# vim: set ft=sh:
