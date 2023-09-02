575204b7bbd56395bcb03a0c63d201551449be9e
Brian Henning <henninb@msn.com>
2023-09-02 11:33:53 -0500

    wip

--- scripts/flutter-install.sh
+++ scripts/flutter-install.sh
@@ -4,6 +4,8 @@ curl -s "https://storage.googleapis.com/flutter_infra_release/releases/stable/li
 cd "$HOME/tmp/" || exit
 doas tar -xvf flutter_linux_3.13.2-stable.tar.xz -C /opt
 
+#git clone -b stable http://github.com/flutter/flutter.git
+
 exit 0
 
 # vim: set ft=sh:
