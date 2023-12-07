84ec3722419a4dc3f9c0b93f3b78a84067d9b9cd
Brian Henning <henninb@msn.com>
2023-12-06 18:08:24 -0600

    wip

--- scripts/android-studio-install.sh
+++ scripts/android-studio-install.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-curl -s "https://r2---sn-jxou0gtapo3-hn2e.gvt1.com/edgedl/android/studio/ide-zips/2022.3.1.19/android-studio-2022.3.1.19-linux.tar.gz?cms_redirect=yes&mh=69&mip=97.116.132.108&mm=28&mn=sn-jxou0gtapo3-hn2e&ms=nvh&mt=1692833788&mv=m&mvi=2&pl=18&rmhost=r1---sn-jxou0gtapo3-hn2e.gvt1.com&shardbypass=sd" -o "$HOME/tmp/android-studio-2022.3.1.19-linux.tar.gz"
+ver=2003.1.1.26
+
+curl "https://r2---sn-jxou0gtapo3-hn2e.gvt1.com/edgedl/android/studio/ide-zips/2023.1.1.26/android-studio-2023.1.1.26-linux.tar.gz" -o "$HOME/tmp/android-studio-${ver}-linux.tar.gz"
+# curl "https://r2---sn-jxou0gtapo3-hn2e.gvt1.com/edgedl/android/studio/ide-zips/${ver}/android-studio-${ver}-linux.tar.gz" -o "$HOME/tmp/android-studio-${ver}-linux.tar.gz"
+#curl "https://dl.google.com/edgedl/android/studio/ide-zips/2003.1.1.26/android-studio-2003.1.1.26-linux.tar.gz" -o "$HOME/tmp/android-studio-${ver}-linux.tar.gz"
+
+#curl "https://r2---sn-jxou0gtapo3-hn2e.gvt1.com/edgedl/android/studio/ide-zips/${ver}/android-studio-2023.1.1.26-linux.tar.gz" -o "$HOME/tmp/android-studio-${ver}-linux.tar.gz"
 cd "$HOME/tmp/" || exit
-doas tar -xvf android-studio-2022.3.1.19-linux.tar.gz -C /opt
+doas tar -xvf "android-studio-${ver}-linux.tar.gz" -C /opt
 
 exit 0
 
