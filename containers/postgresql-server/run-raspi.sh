bea06be58afdbe4ca36e10fcd2a64a6ffc37a38e
Brian Henning <henninb@msn.com>
2023-12-30 12:44:27 -0600

    wip

--- containers/postgresql-server/run-raspi.sh
+++ containers/postgresql-server/run-raspi.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+date=$(date '+%Y-%m-%d')
+
 cat > /tmp/sql-setup <<EOF
 CREATE ROLE vagrant WITH LOGIN PASSWORD 'monday1';
 CREATE ROLE henninb WITH LOGIN PASSWORD 'monday1';
@@ -17,11 +19,13 @@ GRANT ALL PRIVILEGES ON DATABASE postgres TO henninb;
 ALTER USER postgres WITH PASSWORD 'monday1';
 EOF
 
+docker stop postgresql-server
+mv "$HOME/postgresql-data" "$HOME/postgresql-data-${date}"
+docker save -o postgresql-server-${date}.tar postgresql-server
+
 mkdir -p "$HOME/postgresql-data"
 export CURRENT_UID="$(id -u)"
 export CURRENT_GID="$(id -g)"
-
-#docker run --name postgresql-server -d --restart unless-stopped -p 5432:5432 -e POSTGRES_PASSWORD=monday1 -v ${PWD}/database-data:/var/lib/postgresql/data postgres:12.5
 
 # docker run --name postgresql-server -d --restart unless-stopped -p 5432:5432 -e POSTGRES_PASSWORD=monday1 --user "$CURRENT_UID:$CURRENT_GID" -v "$HOME/postgresql-data:/var/lib/postgresql/data" postgres:15.1
 docker run --name postgresql-server -d --restart unless-stopped -p 5432:5432 -e POSTGRES_PASSWORD=monday1 --user "$CURRENT_UID:$CURRENT_GID" -v "$HOME/postgresql-data:/var/lib/postgresql/data" postgres:16.1
