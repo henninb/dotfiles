3374048fa3fe529eab42c6cbf722151a8fd40b27
Brian Henning <henninb@msn.com>
2024-01-04 07:29:49 -0600

    wip

--- scripts/hyprland-install.sh
+++ scripts/hyprland-install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-doas emerge --update --newuse xdg-desktop-portal-hyprland
+doas emerge --update --newuse app-eselect/eselect-repository
+doas eselect repository enable guru
+doas emaint sync -r guru
 doas emerge --update --newuse hyprland
 doas emerge --update --newuse gui-apps/waybar
 doas emerge --update --newuse swappy
@@ -13,11 +15,9 @@ doas emerge --update --newuse wl-clipboard
 
 # doas emerge --update --newuse looking-glass
 echo 'wlprop | jq -r '.name''
-doas emerge --update --newuse app-eselect/eselect-repository
-doas eselect repository enable guru
-doas emaint sync -r guru
 # eselect repository enable guru
 # emaint sync -r guru
+doas emerge --update --newuse xdg-desktop-portal-hyprland
 doas emerge --update --newuse gui-libs/wlroots
 doas emerge --update --newuse gui-apps/hyprland-plugins
 doas emerge --update --newuse gui-apps/hyprpaper
