46a489c2782e6876bd15d1b9a87d4a60dc6b98ac
Brian Henning <henninb@msn.com>
2024-01-23 06:01:10 -0600

    wip

--- scripts/android-studio-install.sh
+++ scripts/android-studio-install.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-ver=2003.1.1.26
+VER=2003.1.1.27
 
-echo "https://r2---sn-jxou0gtapo3-hn2e.gvt1.com/edgedl/android/studio/ide-zips/${ver}/android-studio-${ver}-linux.tar.gz"
+echo "https://r2---sn-jxou0gtapo3-hn2e.gvt1.com/edgedl/android/studio/ide-zips/${VER}/android-studio-${VER}-linux.tar.gz"
 
-wget "https://r2---sn-jxou0gtapo3-hn2e.gvt1.com/edgedl/android/studio/ide-zips/2023.1.1.26/android-studio-2023.1.1.26-linux.tar.gz" -O "$HOME/tmp/android-studio-linux.tar.gz"
+if ! curl -sL --output "$HOME/tmp/android-studio-linux.tar.gz" "https://r2---sn-jxou0gtapo3-hn2e.gvt1.com/edgedl/android/studio/ide-zips/${VER}/android-studio-${VER}-linux.tar.gz"; then
+  echo fail
+else
+  echo success
+fi
 
-#wget "https://r2---sn-jxou0gtapo3-hn2e.gvt1.com/edgedl/android/studio/ide-zips/${ver}/android-studio-${ver}-linux.tar.gz" -O "$HOME/tmp/android-studio-linux.tar.gz"
+wget "https://r2---sn-jxou0gtapo3-hn2e.gvt1.com/edgedl/android/studio/ide-zips/2023.1.1.27/android-studio-2023.1.1.27-linux.tar.gz" -O "$HOME/tmp/android-studio-linux.tar.gz"
+echo "https://r2---sn-jxou0gtapo3-hn2e.gvt1.com/edgedl/android/studio/ide-zips/2003.1.1.27/android-studio-2003.1.1.27-linux.tar.gz"
+
+#wget --max-redirect=50 "https://r2---sn-jxou0gtapo3-hn2e.gvt1.com/edgedl/android/studio/ide-zips/${VER}/android-studio-${VER}-linux.tar.gz" -O "$HOME/tmp/android-studio-linux.tar.gz"
 # curl "https://r2---sn-jxou0gtapo3-hn2e.gvt1.com/edgedl/android/studio/ide-zips/2023.1.1.26/android-studio-2023.1.1.26-linux.tar.gz" -o "$HOME/tmp/android-studio-${ver}-linux.tar.gz"
 # curl "https://r2---sn-jxou0gtapo3-hn2e.gvt1.com/edgedl/android/studio/ide-zips/${ver}/android-studio-${ver}-linux.tar.gz" -o "$HOME/tmp/android-studio-${ver}-linux.tar.gz"
 #curl "https://dl.google.com/edgedl/android/studio/ide-zips/2003.1.1.26/android-studio-2003.1.1.26-linux.tar.gz" -o "$HOME/tmp/android-studio-${ver}-linux.tar.gz"
