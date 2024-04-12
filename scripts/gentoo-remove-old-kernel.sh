0a50ce5aa17ee8c1b3c17dba21cd28e0d6f40a53
Brian Henning <henninb@msn.com>
2024-04-12 05:36:53 -0500

    wip

--- scripts/gentoo-remove-old-kernel.sh
+++ scripts/gentoo-remove-old-kernel.sh
@@ -4,10 +4,29 @@ eselect kernel list
 
 cd /boot
 # Get a list of all kernel files except the newest two
-latest_kernels=$(ls vmlinuz-* | sort -V | head -n -2)
+OLD_KERNELS=$(ls vmlinuz-* | sort -V | head -n -2)
+NEW_KERNELS=$(ls vmlinuz-* | sort -V | tail -n 2)
+
+echo "keeping these kernels"
+echo "${NEW_KERNELS}"
+echo
+echo "delete these kernels"
+echo "${OLD_KERNELS}"
+
+while true; do
+    echo "Press 'y' to continue..."
+    read -r input
+    if [ "$input" = "y" ]; then
+        break
+    else
+        echo "Invalid input. Please press 'y' to continue."
+    fi
+done
+
+echo "Continuing with removing the old kernels"
 
 # Include System.map, config, and initramfs files in the deletion process
-for kernel in $latest_kernels; do
+for kernel in $OLD_KERNELS; do
   # echo $kernel
   # echo System.map-${kernel#vmlinuz-}*
   # echo config-${kernel#vmlinuz-}*
