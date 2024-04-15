318561876a0dc63ff8e357ed9d003b62974eeb8b
Brian Henning <henninb@msn.com>
2024-04-15 05:18:23 -0500

    wip

--- containers/grafana-server/run-raspi.sh
+++ containers/grafana-server/run-raspi.sh
@@ -18,6 +18,5 @@ export DOCKER_HOST=ssh://pi@192.168.10.25
 
 docker rm -f grafana-server
 docker run --name grafana-server -d --restart unless-stopped --privileged -p 3000:3000 --env-file grafana.env --user "$CURRENT_UID:$CURRENT_GID" -v "/home/pi/grafana-data:/var/lib/grafana" grafana/grafana:10.4.2
-# docker run --name grafana-server -d --restart unless-stopped --privileged -p 3000:3000 --env-file grafana.env grafana/grafana:10.4.2
 
 exit 0
