e979b71f542859137babda8585074d56002eba2a
Brian Henning <henninb@msn.com>
2024-12-22 17:09:01 -0600

    wip

---
+++ scripts/yubikey-auth.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+doas emerge --update --newuse sys-auth/yubikey-personalization-gui
+doas emerge --update --newuse sys-auth/pam_u2f
+
+mkdir ~/.config/Yubico/
+
+pamu2fcfg > ~/.config/Yubico/u2f_keys
+chmod 600 ~/.config/Yubico/u2f_keys
+
+echo sudo vi /etc/pam.d/system-auth
+echo top of the file
+echo auth        sufficient                  pam_u2f.so      cue
+# echo sudo systemctl edit getty@tty1
+# echo /etc/systemd/system/getty@tty1.service.d/override.conf
+
+exit 0
