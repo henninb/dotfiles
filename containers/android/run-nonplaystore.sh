fc0f368badd3e5de353190be2b0ebf00ff315564
Brian Henning <henninb@msn.com>
2024-12-24 07:43:38 -0600

    wip

---
+++ containers/android/run-nonplaystore.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Define the VM name
+VM_NAME="Pixel-8a-API-35-x86"
+
+# Check if the VM already exists
+if avdmanager list avd | grep -q "^Name: $VM_NAME$"; then
+  echo "The VM '$VM_NAME' already exists. Skipping creation."
+else
+  echo "The VM '$VM_NAME' does not exist. Creating it now..."
+  # Create the VM
+  avdmanager create avd -n "$VM_NAME" -k "system-images;android-35;google_apis;x86_64" -d "pixel_8a"
+  if [ $? -eq 0 ]; then
+    echo "The VM '$VM_NAME' was successfully created."
+  else
+    echo "Failed to create the VM '$VM_NAME'. Exiting."
+    exit 1
+  fi
+fi
+
+echo "Press Enter to continue..."
+read
+
+# Start the VM
+echo "Starting the VM '$VM_NAME'..."
+emulator -avd "$VM_NAME" -writable-system -http-proxy 192.168.10.40:8081
+
+
+exit 0
+
+#!/bin/sh
+
+VM_NAME=Pixel-8a-API-35-x86
+
+avdmanager list avd
+
+avdmanager create avd -n Pixel-8a-API-35-x86 -k "system-images;android-35;google_apis;x86_64" -d "pixel_8a"
+
+emulator -avd Pixel-8a-API-35-x86 -writable-system -http-proxy 192.168.10.40:8081
+
+echo adb root
+echo emulator -list-avds
+
+exit 0
