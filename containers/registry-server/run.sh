1fb37d7311ee637d132192e8572843027afc3dfc
Brian Henning <henninb@msn.com>
2025-02-03 19:57:00 -0600

    wip

---
+++ containers/registry-server/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+docker run -d -p 5000:5000 --name registry registry:2
+docker tag nextjs-website 192.168.10.40:5000/nextjs-website:latest
+
+kubectl create secret docker-registry my-registry-secret \
+  --docker-server=192.168.10.40:5000 \
+  --docker-username=henninb \
+  --docker-password=monday1 \
+  --docker-email=henninb@gmail.com
