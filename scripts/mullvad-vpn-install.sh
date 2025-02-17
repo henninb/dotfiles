817c717cfb9eacc39cbdc8301afab584c4960e2a
Brian Henning <henninb@msn.com>
2025-02-16 18:07:40 -0600

    wip

---
+++ scripts/mullvad-vpn-install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+doas emerge --update --newuse  net-vpn/mullvadvpn-app
+sudo systemctl enable mullvad-daemon
+sudo systemctl start mullvad-daemon
+systemctl status mullvad-daemon
+
+echo added to hyprland
+echo mullvad-vpn
+
+mullvad account list-devices
+mullvad account -h
+mullvad account login
+echo mullvad disconnect
+echo mullvad connect
+mullvad status
+mullvad --help
+
+exit 0
