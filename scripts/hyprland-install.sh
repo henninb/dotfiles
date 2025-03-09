e2030a184c2adc4879ea2af8b8667eb19355f98d
Brian Henning <henninb@msn.com>
2025-03-09 16:38:51 -0500

    wip

--- scripts/hyprland-install.sh
+++ scripts/hyprland-install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ARCHLINUX_PKGS="setxkbmap i3lock qalculate-gtk hddtemp feh xdotool dunst wmname w3m flameshot volumeicon neofetch blueman qtwaylandscanner copyq clipmenu media-sound/mpc mpd blueman redshift playerctl network-manager numlockx nm-applet trayer-srg sxiv spacefm lxappearance hardinfo gentoolkit jq pavucontrol neovim lsof clipman gammastep ghostty"
+ARCHLINUX_PKGS="setxkbmap i3lock qalculate-gtk hddtemp feh xdotool dunst wmname w3m flameshot volumeicon neofetch blueman qtwaylandscanner copyq clipmenu media-sound/mpc mpd blueman redshift playerctl network-manager numlockx nm-applet trayer-srg sxiv spacefm lxappearance hardinfo gentoolkit jq pavucontrol neovim lsof clipman gammastep ghostty obsidian"
 
-GENTOO_PKGS="rust-bin setxkbmap i3lock qalculate-gtk hddtemp feh xdotool dunst wmname w3m sys-apps/dbus flameshot volumeicon neofetch blueman dev-qt/qtwaylandscanner copyq clipmenu media-sound/mpc mpd net-wireless/blueman redshift playerctl net-misc/networkmanager numlockx nm-applet trayer-srg sxiv spacefm lxappearance hardinfo gentoolkit app-misc/jq pavucontrol neovim lsof clipman gammastep dmidecode ghostty wlr-randr"
+GENTOO_PKGS="rust-bin setxkbmap i3lock qalculate-gtk hddtemp feh xdotool dunst wmname w3m sys-apps/dbus flameshot volumeicon neofetch blueman dev-qt/qtwaylandscanner copyq clipmenu media-sound/mpc mpd net-wireless/blueman redshift playerctl net-misc/networkmanager numlockx nm-applet trayer-srg sxiv spacefm lxappearance hardinfo gentoolkit app-misc/jq pavucontrol neovim lsof clipman gammastep dmidecode ghostty wlr-randr obsidian"
 
 if [ "$OS" = "Arch Linux" ] || [ "$OS" = "Manjaro Linux" ] || [ "$OS" = "ArcoLinux" ]; then
   FAILURE=""
