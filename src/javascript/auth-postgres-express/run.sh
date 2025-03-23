5b9845beee328565dddba14270415434ff5eea4e
Brian Henning <henninb@msn.com>
2025-03-22 19:17:02 -0500

    updates

--- src/javascript/auth-postgres-express/run.sh
+++ src/javascript/auth-postgres-express/run.sh
@@ -58,31 +58,11 @@ else
   usage
 fi
 
-exit 0
-
-
-
-
-#!/bin/sh
-
-echo npm init
-
-echo 'http://localhost:3000'
-
-echo npm install
-echo npm start
-
-echo ssh-add ~/.ssh/id_rsa_gcp
-
-docker context create remote-webserver --docker "host=ssh://brianhenning@34.132.189.202"
-export DOCKER_HOST=ssh://brianhenning@34.132.189.202
-export DOCKER_HOST=ssh://gcp-api
-
-docker build -t express-app .
-docker rm -f express-app
-docker run --name=express-app -h express-app --network my-network --restart unless-stopped -p 80:3000 -d express-app
-
-echo connect my-network  postgresql-server
-echo connect my-network  express-app
+gcloud compute firewall-rules create allow-http-port80 \
+  --network=default \
+  --allow tcp:80 \
+  --source-ranges=0.0.0.0/0 \
+  --target-tags=bhenning-api \
+  --description="Allow incoming HTTP traffic on port 80"
 
 exit 0
