72a79be8ceb09fa643b9ac4c6ff87053fa3eca0c
Brian Henning <henninb@msn.com>
2025-04-28 08:08:08 -0500

    wip

---
+++ scripts/vpn-wiregard-setup.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+echo raspberrypi
+
+
+sudo apt install wireguard
+
+umask 077
+wg genkey | tee /etc/wireguard/server_private.key | wg pubkey > /etc/wireguard/server_public.key
+
+Edit /etc/sysctl.conf, ensure:
+net.ipv4.ip_forward=1
+
+raspberrypi:/etc/wireguard# cat wg0.conf
+cat > "$HOME/tmp/wg0.conf" << EOF
+[Interface]
+Address    = 10.200.200.1/24
+ListenPort = 51820
+PrivateKey     = sBj5CNLLGiNfYN0T8yuE9a4iZe1Q8LcLGE+ybgxBBW0=
+
+PostUp   = sysctl -w net.ipv4.ip_forward=1 net.ipv4.conf.all.rp_filter=0 net.ipv4.conf.wlan0.rp_filter=0 && iptables -A FORWARD -i wg0  -o wlan0 -j ACCEPT && iptables -A FORWARD -i wlan0 -o wg0  -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT && iptables -t nat -A POSTROUTING -s 10.200.200.0/24 -o wlan0 -j MASQUERADE
+
+PostDown = sysctl -w net.ipv4.ip_forward=0 && iptables -D FORWARD -i wg0  -o wlan0 -j ACCEPT && iptables -D FORWARD -i wlan0 -o wg0  -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT && iptables -t nat -D POSTROUTING -s 10.200.200.0/24 -o wlan0 -j MASQUERADE
+
+
+[Peer]
+PublicKey  = N9MHtjlFZX0WH649aKpwci8Um8QI3+VqctI5Ti6dmgA=
+AllowedIPs = 10.200.200.2/32, 10.200.200.3/32
+EOF
+
+root@raspberrypi:/etc/wireguard# ls
+server_private.key  server_public.key  wg0.conf
+
+
+
+sudo wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
+sudo dhclient wlan0
+
+sudo wg-quick up wg0
+sudo wg show wg0
+
+
+sudo wg up gentoo
+sudo wg show gentoo
+
+
+# gentoo
+umask 077
+wg genkey | tee /etc/wireguard/client_private.key | wg pubkey > /etc/wireguard/client_public.key
+
+emerge net-vpn/wireguard-tools
+
+
+sudo systemctl enable wg-quick@gentoo
+
+gentoo /etc/wireguard > ls
+client_private.key  client_public.key  gentoo.conf
+
+silverfox /etc/wireguard > cat gentoo.conf
+cat > "$HOME/tmp/gentoo.conf" << EOF
+[Interface]
+PrivateKey = eEh358hMmhIFJl9fKxR0qgw9FoszhipTHporPQNxXVI=
+Address = 10.200.200.2/24
+DNS = 1.1.1.1
+
+[Peer]
+PublicKey = kI+DNJ9qPsCgILWOmDbfuTWDb4zAYHDAJ+DY5UuAnDo=
+Endpoint = 10.0.0.175:51820
+AllowedIPs = 10.200.200.1/32, 192.168.4.0/22
+PersistentKeepalive = 25
+EOF
+
+exit 0
