a1df7f4bc1ef3ab4d58ac1f2944119ddea6a17ff
Brian Henning <henninb@msn.com>
2025-05-07 05:42:55 -0500

    wip

---
+++ containers/frigate-server/run.sh
@@ -0,0 +1,18 @@
+docker run -d \
+  --name frigate \
+  --restart unless-stopped \
+  --shm-size=512mb \
+  -p 5000:5000 \
+  -p 1935:1935 \
+  -v "$(pwd)/config/config.yml:/config/config.yml:ro" \
+  -v "$(pwd)/clips:/clips" \
+  -v "$(pwd)/recordings:/recordings" \
+  -v "$(pwd)/cache:/cache" \
+  ghcr.io/blakeblackshear/frigate:stable
+
+
+docker run -d \
+  --name mosquitto \
+  --restart unless-stopped \
+  -p 1883:1883 \
+  eclipse-mosquitto:2
